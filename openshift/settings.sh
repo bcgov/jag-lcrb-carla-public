193cd7000521c0f6c1f3894f1fa373369ab8ffdd
George Walker <george@walkersoftware.ca>
2018-03-13 10:33:34 -0700

    BC-31 - pipeline setup (#4)
    * BC-31 - pipeline setup
    
    * BC-31 adjust git URL

---
+++ openshift/settings.sh
@@ -0,0 +1,23 @@
+# bash script to be sourced to set environment variables for OpenShift scripts
+export PROJECT_NAMESPACE="lclb-cllc"
+export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshift}
+
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden
+# with the values of GIT_URI and GIT_REF
+export skip_git_overrides=""
+export GIT_URI="https://github.com/bcgov/ag-lclb-cllc-public.git"
+export GIT_REF="master"
+
+# The project components
+# - defaults to the support the Simple Project Structure
+export components=${components:-"lclb-public-app"}
+
+# The builds to be triggered after buildconfigs created (not auto-triggered)
+export builds=${builds:-""}
+
+# The images to be tagged after build
+export images=${images:-"lclb-public"}
+
+# The routes for the project
+export routes=${routes:-""}
