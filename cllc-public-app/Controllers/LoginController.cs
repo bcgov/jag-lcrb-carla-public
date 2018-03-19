a366db63318f3d06ef56c40dcf3b1467ba918397
George Walker <george@walkersoftware.ca>
2018-03-19 12:00:06 -0700

    BC-62 Setup claim based authentication (#11)

---
+++ cllc-public-app/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gov.Lclb.Cllb.Public.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Gov.Lclb.Cllb.Public.Controllers
+{
+    [Route("login")]
+    public class LoginController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        private readonly DataAccess db;
+        public LoginController(DataAccess db, IConfiguration configuration)
+        {
+            Configuration = configuration;
+            this.db = db;
+        }
+        [Authorize]
+        [HttpGet]
+        public ActionResult Login(string path)
+        {
+            // check to see if we have a local path.  (do not allow a redirect to another website)
+            if (!string.IsNullOrEmpty(path) && Url.IsLocalUrl(path))
+            {
+                return LocalRedirect(path);
+            }
+            else
+            {
+                return Redirect("/");
+            }            
+        }
+    }    
+}
