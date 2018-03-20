a5fe50a522d274ad198ed29b6c9573d88abedb1e
George Walker <george@walkersoftware.ca>
2018-03-20 16:40:02 -0700

    BC-80 seeder for jurisdiction

---
+++ cllc-public-app/Models/Jurisdiction.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+
+namespace Gov.Lclb.Cllb.Public.Models
+{
+    /// <summary>
+    /// Role Database Model
+    /// </summary>
+        public sealed partial class Jurisdiction :  IEquatable<Jurisdiction>
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Role" /> class.
+        /// </summary>
+        /// <param name="id">A system-generated unique identifier for a Role (required).</param>
+        /// <param name="name">The name of the Role, as established by the user creating the role. (required).</param>
+        /// <param name="description">A description of the role as set by the user creating&amp;#x2F;updating the role. (required).</param>
+        /// <param name="rolePermissions">RolePermissions.</param>
+        /// <param name="userRoles">UserRoles.</param>
+        public Jurisdiction(ObjectId id, string name, string selectMessage)
+        {   
+            Id = id;
+            Name = name;
+            SelectMessage = selectMessage;            
+        }
+
+        /// <summary>
+        /// A system-generated unique identifier for a Role
+        /// </summary>
+        /// <value>A system-generated unique identifier for a Role</value>
+        [Key]
+        public ObjectId Id { get; set; }
+
+        /// <summary>
+        /// The name of the Role, as established by the user creating the role.
+        /// </summary>
+        /// <value>The name of the Role, as established by the user creating the role.</value>
+        [MaxLength(255)]        
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// A description of the role as set by the user creating&#x2F;updating the role.
+        /// </summary>
+        /// <value>A description of the role as set by the user creating&#x2F;updating the role.</value>
+        [MaxLength(2048)]        
+        public string SelectMessage { get; set; }
+                
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("class Jurisdiction {\n");
+            sb.Append("  Id: ").Append(Id).Append("\n");
+            sb.Append("  Name: ").Append(Name).Append("\n");
+            sb.Append("  SelectMessage: ").Append(SelectMessage).Append("\n");
+            sb.Append("}\n");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is null) { return false; }
+            if (ReferenceEquals(this, obj)) { return true; }
+            return obj.GetType() == GetType() && Equals((Jurisdiction)obj);
+        }
+
+        /// <summary>
+        /// Returns true if Role instances are equal
+        /// </summary>
+        /// <param name="other">Instance of Role to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(Jurisdiction other)
+        {
+            if (other is null) { return false; }
+            if (ReferenceEquals(this, other)) { return true; }
+
+            return
+                (
+                    Id == other.Id ||
+                    Id.Equals(other.Id)
+                ) &&
+                (
+                    Name == other.Name ||
+                    Name != null &&
+                    Name.Equals(other.Name)
+                ) &&
+                (
+                    SelectMessage == other.SelectMessage ||
+                    SelectMessage != null &&
+                    SelectMessage.Equals(other.SelectMessage)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+
+                // Suitable nullity checks                                   
+                hash = hash * 59 + Id.GetHashCode();
+
+                if (Name != null)
+                {
+                    hash = hash * 59 + Name.GetHashCode();
+                }
+
+                if (SelectMessage != null)
+                {
+                    hash = hash * 59 + SelectMessage.GetHashCode();
+                }                
+                                   
+                return hash;
+            }
+        }
+
+        #region Operators
+        
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(Jurisdiction left, Jurisdiction right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        /// Not Equals
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(Jurisdiction left, Jurisdiction right)
+        {
+            return !Equals(left, right);
+        }
+
+        #endregion Operators
+    }
+}
