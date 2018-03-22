e75859e8efa41b8bfed6d36e6478246ba22d96f8
George Walker <george@walkersoftware.ca>
2018-03-22 11:43:46 -0700

    BC-79 Service for Jurisdictions

---
+++ cllc-public-app/Controllers/JurisdictionController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gov.Lclb.Cllb.Public.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Gov.Lclb.Cllb.Public.Controllers
+{
+    [Route("api/[controller]")]
+    public class JurisdictionController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        private readonly DataAccess db;
+        public JurisdictionController(DataAccess db, IConfiguration configuration)
+        {
+            Configuration = configuration;
+            this.db = db;
+        }
+
+        [HttpGet()]
+        public JsonResult GetJurisdictions()
+        {
+            return Json(db.GetJurisdictions());
+        }
+
+        [HttpGet("names")]
+        public JsonResult GetNames()
+        {
+            return Json(db.GetJurisdictionNames());
+        }
+    }
+}
