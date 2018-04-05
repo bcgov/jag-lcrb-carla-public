78378c641bf059263afa504b37346c9b61667055
Ian Costanzo <ian@middleware360.com>
2018-04-05 12:04:32 -0700

    Fixed unit test to use id attribute to identify control

--- functional-tests/src/test/groovy/pages/app/HomePage.groovy
+++ functional-tests/src/test/groovy/pages/app/HomePage.groovy
@@ -1,9 +1,14 @@
 package pages.app
 
 import geb.Page
+import pages.app.SurveyPage
 
 class HomePage extends Page {
 
     static at = { title=="LCLB" }
     static url = "/"
+
+    static content = {
+    	startSurvey(to: SurveyPage) { $("a", 0, id: "start_button") }
+    }
 }
