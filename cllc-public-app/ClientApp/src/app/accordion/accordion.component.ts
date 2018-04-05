7c6f578cadd1b2f4e940c6b11e0eab3da38a9c1f
George Walker <george@walkersoftware.ca>
2018-04-05 15:58:07 -0700

    BC-95 updated style (#37)

--- cllc-public-app/ClientApp/src/app/accordion/accordion.component.ts
+++ cllc-public-app/ClientApp/src/app/accordion/accordion.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-//import { MatExpansionModule } from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrls: ['./accordion.component.css']
+  styleUrls: ['./accordion.component.scss']
 })
 export class AccordionComponent implements OnInit {
 
