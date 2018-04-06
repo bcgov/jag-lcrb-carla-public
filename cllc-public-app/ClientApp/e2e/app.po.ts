51cfdc78f38e0e9cad2f7de382366bf7f67e7d3a
Ian Costanzo <ian@middleware360.com>
2018-04-06 14:20:51 -0700

    Build unit tests in Protractor

--- cllc-public-app/ClientApp/e2e/app.po.ts
+++ cllc-public-app/ClientApp/e2e/app.po.ts
@@ -1,11 +1,65 @@
 import { browser, by, element } from 'protractor';
 
-export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+export class AppHomePage {
+    navigateTo() {
+      return browser.get('/cannabislicensing/');
+    }
 
-  getMainHeading() {
-    return element(by.css('app-root h1')).getText();
-  }
+    getMainHeading() {
+      return element(by.css('app-root h1')).getText();
+    }
+
+    inputEmail() {
+        return element(by.id("inputEmail"));
+    }
+    inputEmailButton() {
+        return element(by.id("inputEmailButton"));
+    }
+
+    hasEmailRegistration() {
+        return this.inputEmail().isPresent() && this.inputEmailButton().isPresent();
+    }
+
+    linkToSurvey() {
+        return element(by.id("surveyStartButton"));
+    }
+
+    hasLinkToSurvey() {
+        return this.linkToSurvey().isPresent();
+    }
+
+    hasVotingWidgets() {
+        return element(by.id("showVoteResultsButton")).isPresent() || element(by.id("hideVoteResultsButton")).isPresent();
+    }
 }
+
+/*
+var AppHomePage = function() {
+    var inputEmail = element(by.id("inputEmail"));
+    var inputEmailButton = element(by.id("inputEmailButton"));
+    var surveyStartButton = element(by.id("surveyStartButton"));
+    var showVoteResultsButton = element(by.id("showVoteResultsButton"));
+    var hideVoteResultsButton = element(by.id("hideVoteResultsButton"));
+
+    this.navigateTo = function() {
+      return browser.get('/cannabislicensing/');
+    };
+
+    this.getMainHeading = function() {
+      return element(by.css('app-root h1')).getText();
+    };
+
+    this.hasEmailRegistration = function() {
+        return element(by.id("inputEmail")).isPresent() && element(by.id("inputEmailButton")).isPresent();
+    };
+
+    this.hasLinkToSurvey = function() {
+        return element(by.id("surveyStartButton")).isPresent();
+    };
+
+    this.hasVotingWidgets = function() {
+        return element(by.id("showVoteResultsButton")).isPresent() || element(by.id("hideVoteResultsButton")).isPresent();
+    };
+};
+module.exports = new AppHomePage();
+*/
--- cllc-public-app/go-cllc.sh
+++ cllc-public-app/go-cllc.sh
@@ -1,3 +1,4 @@
+# setup environment and then run dotnet with whatever passed params
 export DATABASE_SERVICE_NAME=127.0.0.1,1401
 export DB_ADMIN_PASSWORD=Test1234
 export DB_USER=test
@@ -5,5 +6,7 @@ export DB_PASSWORD=Test4321
 export DB_DATABASE=cllc
 export ASPNETCORE_ENVIRONMENT=Development
 export PathBase=/cannabislicensing
-dotnet run
+
+# e.g. dotnet run, dotnet test, etc.
+dotnet "$@"
 
