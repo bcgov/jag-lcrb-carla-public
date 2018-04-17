f0c89ce6e3323fca7d41a5e39f9fc5cc12f3d232
George Walker <george@walkersoftware.ca>
2018-04-17 16:35:49 -0700

    BC-119 Send email confirmation (#59)

---
+++ cllc-public-app/ClientApp/src/app/newsletter-confirmation/newsletter-confirmation.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input } from '@angular/core';
+import { NewsletterDataService } from "../newsletter-signup/newsletter-data.service"
+
+@Component({
+    selector: 'app-newsletter-confirmation',
+    templateUrl: './newsletter-confirmation.component.html',
+    styleUrls: ['./newsletter-confirmation.component.scss']
+})
+/** newsletter-confirmation component*/
+export class NewsletterConfirmationComponent {
+  @Input('slug') slug: string;
+  @Input('code') code: string;
+
+  public description: string;
+  public title: string;
+  public email: string;
+  public validEmail: any;
+
+    /** newsletter-confirmation ctor */
+  constructor(private newsletterDataService: NewsletterDataService) {
+
+  }
+
+  ngOnInit(): void {
+
+    if (this.slug != null) {
+      // validate the code.
+      this.newsletterDataService.verifyCode(this.slug, this.code)
+        .then((verificationResult) => {
+          alert(verificationResult); 
+        });
+
+      this.newsletterDataService.getNewsletter(this.slug)
+        .then((newsletter) => {
+          this.description = newsletter.description;
+          this.title = newsletter.title;
+        });
+    }
+
+
+  }
+}
--- cllc-public-app/ClientApp/src/app/newsletter-signup/newsletter-data.service.ts
+++ cllc-public-app/ClientApp/src/app/newsletter-signup/newsletter-data.service.ts
@@ -28,7 +28,6 @@ export class NewsletterDataService {
        .catch(this.handleError);
    }
 
-
    signup(slug: any, email: any) {
      let headers = new Headers();
      headers.append("Content-Type", "application/json");
@@ -43,8 +42,20 @@ export class NewsletterDataService {
        .catch(this.handleError);
    }
 
+  verifyCode(slug: any, code: any) {
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
 
-
+    return this.http.get("api/newsletter/" + slug + "/verifycode?code=" + code, {
+      headers: headers
+    })
+      .toPromise()
+      .then((res: Response) => {
+        let data = res.json();        
+        return data;
+      })
+      .catch(this.handleError);
+  }  
 
      private handleError(error: Response | any) {
      let errMsg: string;
