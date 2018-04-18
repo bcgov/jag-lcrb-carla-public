7927bf6b2ba9f4e3db61285cb114fbedc99abfc5
George Walker <george@walkersoftware.ca>
2018-04-18 12:36:24 -0700

    Policy document feature (#62)
    * BC-154 Seeder for policy data
    
    * BC-153 Policy Document feature

---
+++ cllc-public-app/ViewModels/PolicyDocumentSummary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gov.Lclb.Cllb.Public.ViewModels
+{
+    public class PolicyDocumentSummary
+    {
+        public string slug { get; set; }
+        public string title { get; set; }     
+    }
+}
