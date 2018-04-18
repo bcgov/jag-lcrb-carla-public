51835cac58152ee4c41f4e7a446601360b04b5a0
George Walker <george@walkersoftware.ca>
2018-04-18 09:57:35 -0700

    BC-154 Seeder for policy data (#61)

---
+++ cllc-public-app/Models.Extensions/PolicyDocument.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gov.Lclb.Cllb.Public.Models
+{
+    /// <summary>
+    /// ViewModel transforms.
+    /// </summary>
+    public static class PolicyDocumentExtensions
+    {
+        /// <summary>
+        /// Convert a given voteQuestion to a ViewModel
+        /// </summary>        
+        public static ViewModels.PolicyDocument ToViewModel(this Models.PolicyDocument policyDocument)
+        {
+            ViewModels.PolicyDocument result = null;
+            if (policyDocument != null)
+            {
+                result = new ViewModels.PolicyDocument();
+                result.id = policyDocument.Id.ToString();                
+                result.slug = policyDocument.Slug;
+                result.title = policyDocument.Title;
+                result.intro = policyDocument.Intro;
+                result.body = policyDocument.Body;
+                result.displayOrder = policyDocument.DisplayOrder;
+            }            
+            return result;
+        }        
+    }
+}
