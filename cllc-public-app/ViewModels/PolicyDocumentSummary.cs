e9880f7faee3407d0bf736ba9b8afe6b12995d2a
George Walker <george@walkersoftware.ca>
2018-04-20 16:48:52 -0700

    BC-136 Content changes, add new feature for subnav (#76)

--- cllc-public-app/ViewModels/PolicyDocumentSummary.cs
+++ cllc-public-app/ViewModels/PolicyDocumentSummary.cs
@@ -8,6 +8,6 @@ namespace Gov.Lclb.Cllb.Public.ViewModels
     public class PolicyDocumentSummary
     {
         public string slug { get; set; }
-        public string title { get; set; }     
+        public string menuText { get; set; }     
     }
 }
