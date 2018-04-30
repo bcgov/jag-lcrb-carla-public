2c23e31009fb6d685844dfa0cef07a53341b75ce
George Walker <george@walkersoftware.ca>
2018-04-30 15:17:13 -0700

    BC-27 Dynamics Integration Research (#95)
    * Interim commit for dynamics integration - forms service
    
    * Interim commit
    
    * Updated Dynamics integration code
    
    * BC-27 implementation for Dynamics integration

---
+++ cllc-public-app/ClientApp/src/app/models/dynamics-form-tab.model.ts
@@ -0,0 +1,7 @@
+import { DynamicsFormSection } from "./dynamics-form-section.model";
+export class DynamicsFormTab {
+  id: string;
+  name: string;
+  sections: DynamicsFormSection[];   
+  constructor() { }
+}
