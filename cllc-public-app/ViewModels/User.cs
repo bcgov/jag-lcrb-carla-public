df0b806fb135338a37248447672ed93e260a763e
George Walker <george@walkersoftware.ca>
2018-05-02 17:25:11 -0700

    BC-187 - New user registration supporting code (#103)

---
+++ cllc-public-app/ViewModels/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gov.Lclb.Cllb.Public.ViewModels
+{
+    public class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+
+        public string firstname { get; set; }
+
+        public string lastname { get; set; }
+
+        public string Emailaddress1 { get; set; }
+
+        public bool isNewUser { get; set; }
+
+    }
+}
