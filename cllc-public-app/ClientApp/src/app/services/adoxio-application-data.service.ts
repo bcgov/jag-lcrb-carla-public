89067d0e89978ba32f02dfb7a91b36dc01eb3163
Rafael Ponce de Leon <rafael.poncedeleon@quartech.com>
2018-05-22 14:57:26 -0700

    BC-255 interim

--- cllc-public-app/ClientApp/src/app/services/adoxio-application-data.service.ts
+++ cllc-public-app/ClientApp/src/app/services/adoxio-application-data.service.ts
@@ -26,6 +26,9 @@ export class AdoxioApplicationDataService {
            adoxioApplication.applyingPerson = entry.applyingPerson;
            adoxioApplication.jobNumber = entry.jobNumber;
            adoxioApplication.licenseType = entry.licenseType;
+           adoxioApplication.establishmentName = entry.establishmentName;
+           adoxioApplication.establishmentAddress = entry.establishmentAddress;
+           adoxioApplication.applicationStatus = entry.applicationStatus;
            allAdoxioApplications.push(adoxioApplication);
          });
          
