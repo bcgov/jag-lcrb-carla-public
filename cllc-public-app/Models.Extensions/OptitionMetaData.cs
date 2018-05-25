5ec10120a0155a4ab10621bf842afda88d5021d6
George Walker <george@walkersoftware.ca>
2018-05-25 16:23:54 -0700

    BC-258 Picklists, Applicant Information form

---
+++ cllc-public-app/Models.Extensions/OptitionMetaData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gov.Lclb.Cllb.Public.Contexts.Microsoft.Dynamics.CRM;
+
+namespace Gov.Lclb.Cllb.Public.Models
+{
+    /// <summary>
+    /// ViewModel transforms.
+    /// </summary>
+    public static class OptionSetExtensions
+    {
+        /// <summary>
+        /// Convert a given voteQuestion to a ViewModel
+        /// </summary>        
+        public static ViewModels.OptionMetadata ToViewModel(this OptionMetadata optionMetadata)
+        {
+            ViewModels.OptionMetadata result = null;
+            if (optionMetadata != null)
+            {
+                result = new ViewModels.OptionMetadata();
+                result.value = (int) optionMetadata.Value;
+                result.label = optionMetadata.Label.LocalizedLabels.FirstOrDefault().Label;
+                // Description is not set as there is no data
+            }            
+            return result;
+        }   
+        
+    }
+}
