ea78bfc3183a601866a63168eb8dccbf5f7436c6
George Walker <george@walkersoftware.ca>
2018-05-24 22:04:01 -0700

    BC-258 Applicant Information Form

--- cllc-public-app/ClientApp/src/app/business-profile/business-profile.component.ts
+++ cllc-public-app/ClientApp/src/app/business-profile/business-profile.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-business-profile',
@@ -7,8 +7,13 @@
 })
 /** BusinessProfile component*/
 export class BusinessProfileComponent {
+  public view_tab: string;
     /** BusinessProfile ctor */
     constructor() {
+      this.view_tab = "application-information-tab";
+  }
 
-    }
+  changeTab(tab) {
+    this.view_tab = tab;
+  }
 }
