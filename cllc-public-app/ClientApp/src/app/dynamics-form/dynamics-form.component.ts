f2970a8916fa28a72c12937fb0d92d97a6d6faad
George Walker <george@walkersoftware.ca>
2018-05-28 15:31:21 -0700

    BC-258 - Application Information implementation (#140)
    * Fix Build
    
    * BC-258 - Application Information implementation

--- cllc-public-app/ClientApp/src/app/dynamics-form/dynamics-form.component.ts
+++ cllc-public-app/ClientApp/src/app/dynamics-form/dynamics-form.component.ts
@@ -21,11 +21,15 @@ export class DynamicsFormComponent {
   @Input('hideSubmit') hideSubmit: boolean;
   @Input('enableUserOverride') enableUserOverride: boolean;
   @Input('currentUser') currentUser: User;
+  // record to fetch data (for an edit operation)
+  @Input('recordId') recordId: string; 
+
 
   public payload: string;
   public responseText: string;
   public dynamicsForm: DynamicsForm;
   public form: FormGroup;
+  public currentData: any;
 
     /** dynamics-form ctor */
   constructor(private dynamicsDataService: DynamicsDataService) {
@@ -43,6 +47,11 @@ export class DynamicsFormComponent {
             if (field.controltype == "CheckBoxControl") {
               group[field.datafieldname].patchValue(false);
             }
+
+            // bring over data from initial values.
+            group[field.datafieldname].patchValue(this.currentData[field.datafieldname]);
+
+
             if (this.enableUserOverride) {
               if (field.datafieldname == "firstname") {
                 group[field.datafieldname].patchValue(this.currentUser.firstname);
@@ -67,19 +76,30 @@ export class DynamicsFormComponent {
     ngOnInit(): void {
 
       if (this.formid != null) {
-        // get data.
+        // get the form definition.
         this.dynamicsDataService.getForm(this.formid)
           .then((dynamicsForm) => {
             this.dynamicsForm = dynamicsForm;
-            // update the form group.
-            this.form = this.toFormGroup(this.dynamicsForm);
+
+            if (this.recordId) {
+              this.dynamicsDataService.getRecord(this.dynamicsForm.entity, this.recordId)
+                .then((data) => {
+                  //update the form data.
+                  this.currentData = data;
+
+                  // update the form group.
+                  this.form = this.toFormGroup(this.dynamicsForm);
+                });
+            }
+            
           });
       }
     }
 
     onSubmit() {
       this.payload = JSON.stringify(this.form.value);
-      this.dynamicsDataService.createRecord(this.dynamicsForm.entity, this.payload)
+      
+      this.dynamicsDataService.updateRecord(this.dynamicsForm.entity, this.recordId, this.payload)
         .then((data) => {
           this.responseText = JSON.stringify(data);
         });
