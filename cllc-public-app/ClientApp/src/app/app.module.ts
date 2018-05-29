8d489939907997cd5693dabb888e38674b54a50c
temp <USER@250SPAREM1001.qsl.pri>
2018-05-28 17:08:05 -0700

    Dashboard - first iteration (no data)

--- cllc-public-app/ClientApp/src/app/app.module.ts
+++ cllc-public-app/ClientApp/src/app/app.module.ts
@@ -82,6 +82,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ApplicationsListComponent } from './applications-list/applications-list.component';
 import { BusinessProfileComponent } from './business-profile/business-profile.component';
 import { LicenseApplicationSummaryComponent } from './license-application-summary/license-application-summary.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -112,7 +113,8 @@ import { LicenseApplicationSummaryComponent } from './license-application-summar
     SurveySidebarComponent,
     SurveyTestComponent,
     VoteComponent,
-    LicenseApplicationSummaryComponent
+    LicenseApplicationSummaryComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
