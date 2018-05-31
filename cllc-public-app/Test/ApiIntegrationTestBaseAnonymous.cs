00737d364c8c6afcb0e9a3215382fa37f86f562a
Ian Costanzo <ian@middleware360.com>
2018-05-31 13:16:10 -0700

    Initial updates to login controller tests

---
+++ cllc-public-app/Test/ApiIntegrationTestBaseAnonymous.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.IO;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Gov.Lclb.Cllb.Public.Test
+{
+    public abstract class ApiIntegrationTestBaseAnonymous
+    {
+        protected readonly TestServer _server;
+        protected readonly HttpClient _client;
+
+        /// <summary>
+        /// Setup the test
+        /// </summary>        
+		protected ApiIntegrationTestBaseAnonymous()
+        {
+            var testConfig = new ConfigurationBuilder()
+                .AddUserSecrets<ApiIntegrationTestBase>()
+                .AddEnvironmentVariables()
+                .Build();
+
+            var builder = WebHost.CreateDefaultBuilder()               
+                .UseEnvironment("Staging")
+                .UseConfiguration(testConfig)
+                .UseStartup<Startup>();
+
+            _server = new TestServer(builder);
+			_client = _server.CreateClient();
+        }
+    }
+}
