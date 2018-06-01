2c96a2deca9072379a6d79eb642d7cfebeb559f7
Rafael Ponce de Leon <rafael.poncedeleon@quartech.com>
2018-06-01 10:58:37 -0700

    BC-206. Initial angular component setup, updated legal entity controller to handle different and multiple positions

---
+++ cllc-public-app/ClientApp/src/app/directors-and-officers/directors-and-officers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directors-and-officers',
+  templateUrl: './directors-and-officers.component.html',
+  styleUrls: ['./directors-and-officers.component.css']
+})
+export class DirectorsAndOfficersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- cllc-public-app/ClientApp/src/app/services/adoxio-legal-entity-data.service.ts
+++ cllc-public-app/ClientApp/src/app/services/adoxio-legal-entity-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from "@angular/http";
 import { Shareholder } from "../models/shareholder.model";
+import { AdoxioLegalEntity } from "../models/adoxio-legalentities.model";
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -63,6 +64,47 @@ export class AdoxioLegalEntityDataService {
             allShareholders.push(shareholder);
           });
           return allShareholders;
+      })
+      .catch(this.handleError);
+  }
+
+  getDirectorsAndOfficers() {
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    return this.http.get("api/adoxiolegalentity/?directorandofficer=true", {
+      headers: headers
+    })
+      .toPromise()
+      .then((res: Response) => {
+        //console.log(res);
+        let data = res.json();
+        let allDirectorsAndOfficers = [];
+
+        data.forEach((entry) => {
+          let directorofficer = new AdoxioLegalEntity();
+          directorofficer.account = entry.account;
+          directorofficer.commonnonvotingshares = entry.commonnonvotingshares;
+          directorofficer.commonvotingshares = entry.commonvotingshares;
+          directorofficer.dateofbirth = entry.dateofbirth;
+          directorofficer.firstname = entry.xxfirstname
+          directorofficer.id = entry.id;
+          directorofficer.interestpercentage = entry.interestpercentage;
+          directorofficer.isindividual = entry.isindividual;
+          directorofficer.lastname = entry.lastname;
+          directorofficer.legalentitytype = entry.legalentitytype;
+          directorofficer.middlename = entry.middlename;
+          directorofficer.name = entry.name;
+          directorofficer.otherlegalentitytype = entry.otherlegalentitytype;
+          directorofficer.position = entry.position;
+          directorofficer.preferrednonvotingshares = entry.preferrednonvotingshares;
+          directorofficer.preferredvotingshares = entry.preferredvotingshares;
+          directorofficer.relatedentities = entry.relatedentities;
+          directorofficer.sameasapplyingperson = entry.sameasapplyingperson;
+          directorofficer.shareholderType = entry.shareholderType;
+          allDirectorsAndOfficers.push(directorofficer);
+        });
+        return allDirectorsAndOfficers;
       })
       .catch(this.handleError);
   }
