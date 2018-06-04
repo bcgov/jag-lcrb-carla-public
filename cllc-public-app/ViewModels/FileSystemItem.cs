41d1ff9a44ce9d4204e4907517f2e0ff168b2cc9
George Walker <george@walkersoftware.ca>
2018-06-04 10:44:32 -0700

    BC-374 FileSystemItem view model

---
+++ cllc-public-app/ViewModels/FileSystemItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gov.Lclb.Cllb.Public.ViewModels
+{
+    public class FileSystemItem
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string documenttype { get; set; }
+        public int size { get; set; }
+        public DateTime timecreated { get; set; }
+        public DateTime timelastmodified { get; set; }
+    }
+}
