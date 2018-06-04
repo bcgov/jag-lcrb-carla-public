b1cf064f24add5957c2bbb8999e7c44f0b555d68
George Walker <george@walkersoftware.ca>
2018-06-04 10:44:32 -0700

    BC-374 FileSystemItem view model

---
+++ cllc-public-app/Models.Extensions/FileSystemItem.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gov.Lclb.Cllb.Public.Contexts.Microsoft.Dynamics.CRM;
+
+namespace Gov.Lclb.Cllb.Public.Models
+{
+    /// <summary>
+    /// ViewModel transforms.
+    /// </summary>
+    public static class FileSystemItemExtensions
+    {
+        const string NameDocumentTypeSeparator = "__";
+
+        private static string GetDocumentName (string value)
+        {
+            string result = "";
+            if (value != null)
+            {
+                int pos = value.IndexOf(NameDocumentTypeSeparator);
+                if (pos > -1)
+                {  
+                    result = value.Substring(pos + 1);
+                }                
+            }
+            return result;
+        }
+
+        private static string GetDocumentType (string value)
+        {
+            string result = "";
+            if (value != null)
+            {
+                int pos = value.IndexOf(NameDocumentTypeSeparator);
+                if (pos > -1)
+                {
+                    result = value.Substring(0, pos);
+                }                
+            }
+            return result;
+        }
+
+        private static string CombineNameDocumentType (string name, string documentType)
+        {
+            string result = name + NameDocumentTypeSeparator + documentType;
+            return result;
+        }
+
+        /// <summary>
+        /// Copy values from a Dynamics legal entity to a view model.
+        /// </summary>
+        /// <param name="to"></param>
+        /// <param name="from"></param>
+        public static void CopyValues(this MS.FileServices.FileSystemItem to, ViewModels.FileSystemItem from)
+        {
+            to.Name = CombineNameDocumentType (from.name, from.documenttype);
+            to.Size = from.size;
+            to.TimeCreated = from.timecreated;
+            to.TimeLastModified = from.timelastmodified;            
+        }
+
+        /// <summary>
+        /// Convert a given voteQuestion to a ViewModel
+        /// </summary>        
+        public static ViewModels.FileSystemItem ToViewModel(this MS.FileServices.FileSystemItem fileSystemItem)
+        {
+            ViewModels.FileSystemItem result = null;
+            if (fileSystemItem != null)
+            {
+                result = new ViewModels.FileSystemItem();
+                if (fileSystemItem.Id != null)
+                {
+                    result.id = fileSystemItem.Id;
+                }
+
+                result.name = GetDocumentName(fileSystemItem.Name);
+                result.documenttype = GetDocumentType(fileSystemItem.Name);
+                result.size = fileSystemItem.Size;
+                result.timecreated = fileSystemItem.TimeCreated;
+                result.timelastmodified = fileSystemItem.TimeLastModified;
+
+            }            
+            return result;
+        }                   
+
+        public static MS.FileServices.FileSystemItem ToModel(this ViewModels.FileSystemItem fileSystemItem)
+        {
+            MS.FileServices.File result = null;
+            if (fileSystemItem != null)
+            {
+                result = new MS.FileServices.File()
+                {
+                    Id = fileSystemItem.id,
+                    Name = CombineNameDocumentType(fileSystemItem.name,fileSystemItem.documenttype),
+                    Size = fileSystemItem.size,
+                    TimeCreated = fileSystemItem.timecreated,
+                    TimeLastModified = fileSystemItem.timelastmodified
+                };               
+            }
+            
+            return result;
+        }
+    }
+}
