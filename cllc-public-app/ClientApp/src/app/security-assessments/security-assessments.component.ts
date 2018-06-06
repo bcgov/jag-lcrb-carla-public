9c8382d5a3e7957c9c527b8f8b29d11c0769056e
Rafael Ponce de Leon <rafael.poncedeleon@quartech.com>
2018-06-05 17:05:11 -0700

    BC-191 - Added email field to legal entity models, controller, service

--- cllc-public-app/ClientApp/src/app/security-assessments/security-assessments.component.ts
+++ cllc-public-app/ClientApp/src/app/security-assessments/security-assessments.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AdoxioLegalEntity } from '../models/adoxio-legalentities.model';
+import { AdoxioLegalEntityDataService } from "../services/adoxio-legal-entity-data.service";
+import { MatPaginator, MatTableDataSource, MatSort, MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-security-assessments',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecurityAssessmentsComponent implements OnInit {
 
-  constructor() { }
+  @Input() accountId: string;
+
+  adoxioLegalEntityList: AdoxioLegalEntity[] = [];
+  dataSource = new MatTableDataSource<AdoxioLegalEntity>();
+  public dataLoaded;
+  displayedColumns = ['firstname', 'lastname', 'email', 'position', 'emailsent'];
+
+  constructor(private legalEntityDataservice: AdoxioLegalEntityDataService) { }
 
   ngOnInit() {
+    this.getDirectorsAndOfficersAndShareholders();
+  }
+
+  getDirectorsAndOfficersAndShareholders() {
+    this.legalEntityDataservice.getLegalEntitiesbyPosition("director-officer-shareholder")
+      .then((data) => {
+        //console.log("getLegalEntitiesbyPosition("director-officer-shareholder"): ", data);
+        this.dataSource.data = data;
+        this.dataLoaded = true;
+      });
   }
 
 }
