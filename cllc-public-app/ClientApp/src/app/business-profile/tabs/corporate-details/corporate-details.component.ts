674e3266c7389cb7130308e12c0072e64cec0fe6
Rafael Ponce de Leon <rafael.poncedeleon@quartech.com>
2018-06-12 18:52:43 -0700

    BC-413 - In progress

--- cllc-public-app/ClientApp/src/app/business-profile/tabs/corporate-details/corporate-details.component.ts
+++ cllc-public-app/ClientApp/src/app/business-profile/tabs/corporate-details/corporate-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDataService } from '../../../services/user-data.service';
+import { AccountDataService } from '../../../services/account-data.service';
 import { User } from '../../../models/user.model';
+import { DynamicsAccount } from '../../../models/dynamics-account.model';
+import { FormBuilder, FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-corporate-details',
@@ -9,8 +12,35 @@ import { User } from '../../../models/user.model';
 })
 export class CorporateDetailsComponent implements OnInit {
   user: User;
+  corporateDetailsForm: FormGroup;
 
-  constructor(private userDataService: UserDataService) { }
+  constructor(private userDataService: UserDataService, private accountDataService: AccountDataService, private frmbuilder: FormBuilder) {
+    this.corporateDetailsForm = frmbuilder.group({
+      bcIncorporationNumber: ['', Validators.required],
+      businessNumber: ['', Validators.required],
+      pstNumber: ['', Validators.required],
+      mailName: ['', Validators.required],
+      mailAddress: ['', Validators.required],
+      mailCity: ['', Validators.required],
+      mailCountry: ['', Validators.required],
+      mailProvince: ['', Validators.required],
+      mailPostalcode: ['', Validators.required]
+    });//, { validator: this.dateLessThanToday('dateIssued') });
+  }
+
+  dateLessThanToday(field1) {
+    return form => {
+      const d1 = form.controls[field1].value;
+      if (!d1) {
+        return {};
+      }
+      const d1Date = new Date(d1.year, d1.month, d1.day);
+      if (d1Date < new Date()) {
+        return { dateLessThanToday: true };
+      }
+      return {};
+    }
+  }
 
   ngOnInit() {
     this.userDataService.getCurrentUser().then(user =>{
