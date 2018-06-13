bebe49ff3a0272076ecbb5ae0d9b5826ce93f126
Ian Costanzo <ian@middleware360.com>
2018-06-13 15:19:35 -0700

    Initial version of BCeID web services

---
+++ cllc-interfaces/BCeID/BCeIDBusiness.cs
@@ -0,0 +1,32 @@
+using System.Runtime.Serialization;
+using System;
+using System.Globalization;
+
+namespace BCeIDWrapper
+{
+    public class BCeIDBusiness
+    {
+        public string contactEmail { get; set; }
+        public string contactPhone { get; set; }
+
+        public string individualFirstname { get; set; }
+        public string individualMiddlename { get; set; }
+        public string individualOtherMiddlename { get; set; }
+        public string individualSurname { get; set; }
+
+		public string businessTypeName { get; set; }
+		public string businessTypeDescription { get; set; }
+		public string businessTypeCode { get; set; }
+        public string businessTypeOther { get; set; }
+        public string legalName { get; set; }
+        public string businessNumber { get; set; }
+        public string incorporationNumber { get; set; }
+        public string jurisdictionOfIncorporation { get; set; }
+        public string addressLine1 { get; set; }
+        public string addressLine2 { get; set; }
+        public string addressCity { get; set; }
+        public string addressProv { get; set; }
+		public string addressPostal { get; set; }
+		public string addressCountry { get; set; }
+    }
+}
