5cf3103a5520832cc7d43ea13d8e95aedb6236f5
Moffat Sehudi <msehudi@gmail.com>
2018-06-27 10:13:03 -0700

    business-profile screens

---
+++ cllc-public-app/ClientApp/src/app/business-profile/tabs/shareholders-ucl-llc/shareholders-ucl-llc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shareholders-ucl-llc',
+  templateUrl: './shareholders-ucl-llc.component.html',
+  styleUrls: ['./shareholders-ucl-llc.component.css']
+})
+export class ShareholdersUclLlcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
