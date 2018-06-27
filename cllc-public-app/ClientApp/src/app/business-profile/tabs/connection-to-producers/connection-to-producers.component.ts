51fe0c52a49f66f1704a23fd197ed83a14d48389
Moffat Sehudi <msehudi@gmail.com>
2018-06-27 11:53:56 -0700

    business profile pages partial

--- cllc-public-app/ClientApp/src/app/business-profile/tabs/connection-to-producers/connection-to-producers.component.ts
+++ cllc-public-app/ClientApp/src/app/business-profile/tabs/connection-to-producers/connection-to-producers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-connection-to-producers',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connection-to-producers.component.css']
 })
 export class ConnectionToProducersComponent implements OnInit {
-
+  @Input() accountId: string;
+  @Input() businessType: string;
   constructor() { }
 
   ngOnInit() {
