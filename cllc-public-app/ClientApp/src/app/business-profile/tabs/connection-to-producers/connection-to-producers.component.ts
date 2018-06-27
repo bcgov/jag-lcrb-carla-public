1ebe47ab13ff6a3ee8b416c3d397d40d6f0221af
Moffat Sehudi <msehudi@gmail.com>
2018-06-27 10:13:03 -0700

    business-profile screens

---
+++ cllc-public-app/ClientApp/src/app/business-profile/tabs/connection-to-producers/connection-to-producers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-to-producers',
+  templateUrl: './connection-to-producers.component.html',
+  styleUrls: ['./connection-to-producers.component.css']
+})
+export class ConnectionToProducersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ cllc-public-app/ClientApp/src/app/business-profile/tabs/key-personel/key-personel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-key-personel',
+  templateUrl: './key-personel.component.html',
+  styleUrls: ['./key-personel.component.scss']
+})
+export class KeyPersonelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
