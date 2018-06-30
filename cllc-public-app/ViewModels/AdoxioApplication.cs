bd38fa6df933f3324bc05c5b803444a9d88e92bb
Rafael Ponce de Leon <rafael.poncedeleon@quartech.com>
2018-06-29 17:11:46 -0700

    BC-566

--- cllc-public-app/ViewModels/AdoxioApplication.cs
+++ cllc-public-app/ViewModels/AdoxioApplication.cs
@@ -7,18 +7,20 @@ namespace Gov.Lclb.Cllb.Public.ViewModels
 {
     public class AdoxioApplication
     {
-		public string id { get; set; } 
+		public string id { get; set; } //adoxio_applicationid
         public string name { get; set; }
         public string applyingPerson { get; set; }
         public string jobNumber { get; set; }
-        public string licenseType { get; set; }
+        public string licenseType { get; set; } //adoxio_licencetype
         public string establishmentName { get; set; }
 		public string establishmentaddressstreet { get; set; }
 		public string establishmentaddresscity { get; set; }
 		public string establishmentaddresspostalcode { get; set; }
 		public string establishmentAddress { get; set; }
         public string applicationStatus { get; set; }
+        public Adoxio_applicanttypecodes applicantType { get; set; } //adoxio_applicanttype
+        public GeneralYesNo registeredEstablishment { get; set; } //adoxio_registeredestablishment
 
-		public ViewModels.Account applicant { get; set; }
+        public ViewModels.Account applicant { get; set; }
     }
 }
