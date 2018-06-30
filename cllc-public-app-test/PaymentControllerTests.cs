a36172f82b45d4a57505c308e8552393667abf7d
Ian Costanzo <ian@anon-solutions.ca>
2018-06-30 16:03:39 -0700

    Added payment submit button and skelton of controller

---
+++ cllc-public-app-test/PaymentControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using Xunit;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Gov.Lclb.Cllb.Public.Test
+{
+	public class PaymentControllerTests : ApiIntegrationTestBaseWithLogin
+	{
+		public PaymentControllerTests(CustomWebApplicationFactory<Startup> factory)
+            : base(factory)
+		{ }
+
+        [Fact]
+        public async System.Threading.Tasks.Task TestNoAccessToAnonymousUser()
+        {
+            string service = "payment";
+            string id = "SomeRandomId";
+
+            // first confirm we are not logged in
+            await GetCurrentUserIsUnauthorized();
+
+            // try each GET, should return unauthorized
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/" + service + "/submit/" + id);
+            var response = await _client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            string _discard = await response.Content.ReadAsStringAsync();
+
+			request = new HttpRequestMessage(HttpMethod.Get, "/api/" + service + "/update/" + id);
+            response = await _client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            _discard = await response.Content.ReadAsStringAsync();
+
+			request = new HttpRequestMessage(HttpMethod.Get, "/api/" + service + "/verify/" + id);
+            response = await _client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            _discard = await response.Content.ReadAsStringAsync();
+        }
+        
+	}
+}
