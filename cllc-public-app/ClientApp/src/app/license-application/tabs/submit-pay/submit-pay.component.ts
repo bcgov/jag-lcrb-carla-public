67fa50c22c1f23b8e23dc402744e375e287a1937
Ian Costanzo <ian@anon-solutions.ca>
2018-06-30 18:31:08 -0700

    Updates to submit pay component

--- cllc-public-app/ClientApp/src/app/license-application/tabs/submit-pay/submit-pay.component.ts
+++ cllc-public-app/ClientApp/src/app/license-application/tabs/submit-pay/submit-pay.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PaymentDataService } from '../../../services/payment-data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-submit-pay',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit-pay.component.scss']
 })
 export class SubmitPayComponent implements OnInit {
+  @Input() applicationId: string;
+  busy: Subscription;
 
   constructor() { }
 
---
+++ cllc-public-app/ClientApp/src/app/services/payment-data.service.ts
@@ -0,0 +1,36 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from "@angular/http";
+import "rxjs/add/operator/toPromise";
+
+import { AdoxioApplication } from "../models/adoxio-application.model";
+
+@Injectable()
+   export class PaymentDataService {
+
+   apiPath = "api/account/";
+   submitPath = "submit/";
+
+   constructor(private http: Http) { }
+
+   getPaymentSubmissionUrl(id: string) {
+     let headers = new Headers();
+     headers.append("Content-Type", "application/json");
+
+     //call API
+     //console.log("===== PaymentService.submit: ", id);
+     return this.http.get(this.apiPath + this.submitPath + id, { headers: headers });
+   }
+
+   private handleError(error: Response | any) {
+     let errMsg: string;
+     if (error instanceof Response) {
+       const body = error.json() || "";
+       const err = body.error || JSON.stringify(body);
+       errMsg = `${error.status} - ${error.statusText || ""} ${err}`;
+     } else {
+       errMsg = error.message ? error.message : error.toString();
+     }
+     console.error(errMsg);
+     return Promise.reject(errMsg);
+   }
+}
