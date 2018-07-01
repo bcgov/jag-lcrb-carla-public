cdd71ee30826fa57b1ae770d3780c1d6f54d1f60
Ian Costanzo <ian@anon-solutions.ca>
2018-07-01 09:02:34 -0700

    Added payment re-direct confirmation component

---
+++ cllc-public-app/ClientApp/src/app/payment-confirmation/payment-confirmation.component.spec.ts
@@ -0,0 +1,9 @@
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { PaymentConfirmationComponent } from './payment-confirmation.component';
+
+let component: PaymentConfirmationComponent;
+let fixture: ComponentFixture<PaymentConfirmationComponent>;
+
+describe('payment-confirmation component', () => {
+});
