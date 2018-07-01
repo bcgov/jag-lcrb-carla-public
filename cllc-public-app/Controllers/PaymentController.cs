158418074b1360a0f27d3b8e7caccb38bc30e27b
Ian Costanzo <ian@anon-solutions.ca>
2018-07-01 10:05:24 -0700

    Payment confirmation with application id

--- cllc-public-app/Controllers/PaymentController.cs
+++ cllc-public-app/Controllers/PaymentController.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Gov.Lclb.Cllb.Interfaces;
 using Microsoft.Extensions.Logging;
 using Gov.Lclb.Cllb.Interfaces.Models;
+using Newtonsoft.Json;
 
 namespace Gov.Lclb.Cllb.Public.Controllers
 {
@@ -20,9 +21,7 @@ namespace Gov.Lclb.Cllb.Public.Controllers
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly ILogger _logger;
         private readonly IDynamicsClient _dynamicsClient;
-
-		Dictionary<string, string> redirectUrl;
-
+        
 		public PaymentController(Interfaces.Microsoft.Dynamics.CRM.System context, IConfiguration configuration, 
 		                         IHttpContextAccessor httpContextAccessor, ILoggerFactory loggerFactory, IDynamicsClient dynamicsClient)
         {
@@ -31,10 +30,6 @@ namespace Gov.Lclb.Cllb.Public.Controllers
             this._httpContextAccessor = httpContextAccessor;
             this._dynamicsClient = dynamicsClient;
 			_logger = loggerFactory.CreateLogger(typeof(PaymentController));
-
-			redirectUrl = new Dictionary<string, string>();
-			redirectUrl["url"] = "https://google.ca";
-			redirectUrl["key2"] = "value2";
         }
 
 		/// <summary>
@@ -45,9 +40,19 @@ namespace Gov.Lclb.Cllb.Public.Controllers
 		/// <param name="id">GUID of the Application to pay</param>
 		/// <returns></returns>
 		[HttpGet("submit/{id}")]
-		public async Task<JsonResult> GetPaymentUrl(string id)
+		public async Task<IActionResult> GetPaymentUrl(string id)
 		{
 			_logger.LogError("Called GetPaymentUrl(" + id + ")");
+			ViewModels.AdoxioApplication result = await GetDynamicsApplication(id);
+			if (result == null)
+			{
+				return NotFound();
+			}
+
+			Dictionary<string, string> redirectUrl;
+			redirectUrl = new Dictionary<string, string>();
+            redirectUrl["url"] = "https://google.ca?id=" + id;
+
 			return Json(redirectUrl);
 		}
 
@@ -60,10 +65,19 @@ namespace Gov.Lclb.Cllb.Public.Controllers
         /// <param name="id">GUID of the Application to pay</param>
         /// <returns></returns>
         [HttpGet("update/{id}")]
-		public async Task<JsonResult> UpdatePaymentStatus(string id)
+		public async Task<IActionResult> UpdatePaymentStatus(string id)
         {
-			_logger.LogError("Called UpdatePaymentStatus(" + id + ")");
-			return Json(redirectUrl);
+			ViewModels.AdoxioApplication result = await GetDynamicsApplication(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            Dictionary<string, string> redirectUrl;
+            redirectUrl = new Dictionary<string, string>();
+            redirectUrl["url"] = "https://google.ca?id=" + id;
+
+            return Json(redirectUrl);
         }
 
 		/// <summary>
@@ -76,11 +90,67 @@ namespace Gov.Lclb.Cllb.Public.Controllers
         /// <param name="id">GUID of the Application to pay</param>
         /// <returns></returns>
         [HttpGet("verify/{id}")]
-		public async Task<JsonResult> VerifyPaymentStatus(string id)
+		public async Task<IActionResult> VerifyPaymentStatus(string id)
         {
-			_logger.LogError("Called VerifyPaymentStatus(" + id + ")");
-			return Json(redirectUrl);
+			ViewModels.AdoxioApplication result = await GetDynamicsApplication(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            Dictionary<string, string> redirectUrl;
+            redirectUrl = new Dictionary<string, string>();
+            redirectUrl["url"] = "https://google.ca?id=" + id;
+
+            return Json(redirectUrl);
         }
 
+		private async Task<ViewModels.AdoxioApplication> GetDynamicsApplication(string id)
+        {
+            // get the current user.
+            string temp = _httpContextAccessor.HttpContext.Session.GetString("UserSettings");
+            UserSettings userSettings = JsonConvert.DeserializeObject<UserSettings>(temp);
+
+            _logger.LogError("Application id = " + id);
+            _logger.LogError("User id = " + userSettings.AccountId);
+
+            ViewModels.AdoxioApplication result = null;
+            var dynamicsApplication = await _dynamicsClient.GetApplicationById(Guid.Parse(id));
+            if (dynamicsApplication == null)
+            {
+                return null;
+            }
+            else
+            {
+                if (!CurrentUserHasAccessToApplicationOwnedBy(dynamicsApplication._adoxioApplicantValue))
+                {
+                    return null;
+                }
+                result = await dynamicsApplication.ToViewModel(_dynamicsClient);
+            }
+
+            return result;
+        }
+
+		/// <summary>
+        /// Verify whether currently logged in user has access to this account id
+        /// </summary>
+        /// <returns>boolean</returns>
+        private bool CurrentUserHasAccessToApplicationOwnedBy(string accountId)
+        {
+            // get the current user.
+            string temp = _httpContextAccessor.HttpContext.Session.GetString("UserSettings");
+            UserSettings userSettings = JsonConvert.DeserializeObject<UserSettings>(temp);
+
+            // For now, check if the account id matches the user's account.
+            // TODO there may be some account relationships in the future
+            if (userSettings.AccountId != null && userSettings.AccountId.Length > 0)
+            {
+                return userSettings.AccountId == accountId;
+            }
+
+            // if current user doesn't have an account they are probably not logged in
+            return false;
+        }
     }
 }
