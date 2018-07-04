6554a8afdd37b361c604c5b890bb38f3e0ddd804
Rafael Ponce de Leon <rafael.poncedeleon@quartech.com>
2018-07-04 12:56:23 -0700

    BC-480 application floor plan BC-566 aplication landing page

--- cllc-public-app/ClientApp/src/app/license-application/tabs/declaration/declaration.component.ts
+++ cllc-public-app/ClientApp/src/app/license-application/tabs/declaration/declaration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-declaration',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./declaration.component.scss']
 })
 export class DeclarationComponent implements OnInit {
+
+  @Input('accountId') accountId: string;
+  @Input('applicationId') applicationId: string;
 
   constructor() { }
 
