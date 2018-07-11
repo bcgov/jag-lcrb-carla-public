e30a4f99baaa2fff281c73fd72b2665ccb9d3da3
Moffat Sehudi <msehudi@gmail.com>
2018-07-10 17:38:57 -0700

    business profile autosave

--- cllc-public-app/ClientApp/src/app/business-profile/tabs/connection-to-producers/connection-to-producers.component.ts
+++ cllc-public-app/ClientApp/src/app/business-profile/tabs/connection-to-producers/connection-to-producers.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MatSnackBar } from '../../../../../node_modules/@angular/material';
-import { FormBuilder } from '../../../../../node_modules/@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { FormBuilder } from '@angular/forms';
 import { TiedHouseConnectionsDataService } from '../../../services/tied-house-connections-data.service';
-import { debug } from 'util';
 import { TiedHouseConnection } from '../../../models/tied-house-connection.model';
+import { auditTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-connection-to-producers',
@@ -36,6 +36,11 @@ export class ConnectionToProducersComponent implements OnInit {
       societyConnectionFederalProducerDetails: ['']
     });
 
+    this.form.valueChanges
+      .pipe(auditTime(2000)).subscribe(formData => {
+        this.save();
+      });
+
     this.tiedHouseService.getTiedHouse(this.accountId)
       .subscribe(res => {
         this._tiedHouseData = res.json();
@@ -46,7 +51,7 @@ export class ConnectionToProducersComponent implements OnInit {
   save() {
     const data = (<any>Object).assign(this._tiedHouseData, this.form.value);
     this.tiedHouseService.updateTiedHouse(data, data.id).subscribe(res => {
-      this.snackBar.open('Connections to producers have been saved', 'Success', { duration: 3500, extraClasses: ['red-snackbar'] });
+      // this.snackBar.open('Connections to producers have been saved', 'Success', { duration: 3500, extraClasses: ['red-snackbar'] });
     },
       err => {
         this.snackBar.open('Error saving Connections to producers', 'Fail', { duration: 3500, extraClasses: ['red-snackbar'] });
