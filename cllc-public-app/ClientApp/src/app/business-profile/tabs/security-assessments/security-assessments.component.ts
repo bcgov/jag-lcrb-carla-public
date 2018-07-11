2eae43b9a48e5d2e25264beef21b287e7a5fc144
Moffat Sehudi <msehudi@gmail.com>
2018-07-11 08:57:15 -0700

    organize business profile tabs

--- cllc-public-app/ClientApp/src/app/business-profile/tabs/security-assessments/security-assessments.component.ts
+++ cllc-public-app/ClientApp/src/app/business-profile/tabs/security-assessments/security-assessments.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input, ViewContainerRef, ViewChild } from '@angular/core';
-import { AdoxioLegalEntity } from '../models/adoxio-legalentities.model';
-import { AdoxioLegalEntityDataService } from "../services/adoxio-legal-entity-data.service";
 import { MatPaginator, MatTableDataSource, MatSort, MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { Subscription } from 'rxjs';
+import { AdoxioLegalEntity } from '../../../models/adoxio-legalentities.model';
+import { AdoxioLegalEntityDataService } from '../../../services/adoxio-legal-entity-data.service';
 
 @Component({
   selector: 'app-security-assessments',
@@ -36,7 +36,7 @@ export class SecurityAssessmentsComponent implements OnInit {
 
   getDirectorsAndOfficersAndShareholders() {
     let legalEntitiesList = [];
-    this.busyObsv = this.legalEntityDataservice.getLegalEntitiesbyPosition(this.parentLegalEntityId, "director-officer-shareholder")
+    this.busyObsv = this.legalEntityDataservice.getLegalEntitiesbyPosition(this.parentLegalEntityId, 'director-officer-shareholder')
       .subscribe((result) => {
         let data = result.json();
         data.forEach((entry) => {
@@ -54,7 +54,7 @@ export class SecurityAssessmentsComponent implements OnInit {
     let consentRequestList: string[] = [];
 
     this.dataSource.data.forEach((row) => {
-      //console.log("row values: ", row.id + " - " + row.sendConsentRequest + " - " + row.firstname);
+      // console.log("row values: ", row.id + " - " + row.sendConsentRequest + " - " + row.firstname);
       if (row.sendConsentRequest) {
         consentRequestList.push(row.id);
       }
@@ -77,9 +77,9 @@ export class SecurityAssessmentsComponent implements OnInit {
   private handleError(error: Response | any) {
     let errMsg: string;
     if (error instanceof Response) {
-      const body = error.json() || "";
+      const body = error.json() || '';
       const err = body || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ""} ${err}`;
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
