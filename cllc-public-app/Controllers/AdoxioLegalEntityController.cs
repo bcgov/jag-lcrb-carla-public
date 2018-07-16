b03cd25bc3208d9aa776af957524ba1b57e24dbb
George Walker <george@walkersoftware.ca>
2018-07-16 10:57:03 -0700

    Fix for filename display issue

--- cllc-public-app/Controllers/AdoxioLegalEntityController.cs
+++ cllc-public-app/Controllers/AdoxioLegalEntityController.cs
@@ -298,7 +298,7 @@ namespace Gov.Lclb.Cllb.Public.Controllers
                     {
                         ViewModels.FileSystemItem fileSystemItemVM = new ViewModels.FileSystemItem();
                         // remove the document type text from file name
-                        fileSystemItemVM.name = fileDetails.Name.Substring(0, fileDetails.Name.IndexOf("__"));
+                        fileSystemItemVM.name = fileDetails.Name.Substring(fileDetails.Name.IndexOf("__") + 2 );
                         // convert size from bytes (original) to KB
                         fileSystemItemVM.size = int.Parse(fileDetails.Length);
                         fileSystemItemVM.serverrelativeurl = fileDetails.ServerRelativeUrl;
