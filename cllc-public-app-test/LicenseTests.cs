d925f7cbb235e49970cdf7241e4072c0b4667ef1
Daniel Truong <daniel.t.truong@gmail.com>
2018-07-23 09:43:24 -0700

    Removing license controller tests for now

--- cllc-public-app-test/LicenseTests.cs
+++ cllc-public-app-test/LicenseTests.cs
@@ -1,46 +1,46 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Xunit;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.TestHost;
+// using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Linq;
+// using System.Net.Http;
+// using System.Threading.Tasks;
+// using Xunit;
 
-using System.Text;
-using Newtonsoft.Json;
-using System.Net;
-using Gov.Lclb.Cllb.Interfaces.Microsoft.Dynamics.CRM;
-using Gov.Lclb.Cllb.Public.Models;
-using Gov.Lclb.Cllb.Public.Utility;
+// using System.Text;
+// using Newtonsoft.Json;
+// using System.Net;
+// using Gov.Lclb.Cllb.Interfaces.Microsoft.Dynamics.CRM;
+// using Gov.Lclb.Cllb.Public.Models;
+// using Gov.Lclb.Cllb.Public.Utility;
 
-namespace Gov.Lclb.Cllb.Public.Test
-{
-    public class LicenseTest
-    {
-        [Fact]
-        public void GetLicensesByLicenceeTest()
-        {
+// namespace Gov.Lclb.Cllb.Public.Test
+// {
+//     public class LicenseTest
+//     {
+//         [Fact]
+//         public void GetLicensesByLicenceeTest()
+//         {
 
-        }
+//         }
 
-        [Fact]
-        public void GetCurrentUserDyanamicsApplicationsTest()
-        {
+//         [Fact]
+//         public void GetCurrentUserDyanamicsApplicationsTest()
+//         {
 
-        }
+//         }
 
-        [Fact]
-        public void GetDynamicsLicensesTest()
-        {
+//         [Fact]
+//         public void GetDynamicsLicensesTest()
+//         {
 
-        }
+//         }
 
-        [Fact]
-        public void GetDynamicsLicensesByIdTest()
-        {
+//         [Fact]
+//         public void GetDynamicsLicensesByIdTest()
+//         {
 
-        }
-    }
-}
+//         }
+//     }
+// }
