6913220911bda8158583b55ddc6a5ac730d61d6d
Daniel Truong <daniel.t.truong@gmail.com>
2018-07-30 12:01:57 -0700

    LCSD-18 Updated BCEID Confirmation

--- cllc-public-app/ClientApp/src/app/home/home.component.ts
+++ cllc-public-app/ClientApp/src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  window = window;
 
   constructor(private titleService: Title ) { }
 
