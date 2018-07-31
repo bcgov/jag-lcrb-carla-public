8ee430d16ec4429b405956f2817f4dd6e40fcce0
Daniel Truong <daniel.t.truong@gmail.com>
2018-07-31 11:11:35 -0700

    Added lite flag

--- cllc-public-app/ClientApp/src/app/home/home.component.ts
+++ cllc-public-app/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ClientConfigDataService } from '../services/client-config.service';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,15 @@ import { Title } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
   window = window;
+  isLiteVersion: boolean;
 
-  constructor(private titleService: Title ) { }
+  constructor(private titleService: Title, private clientConfigDataService: ClientConfigDataService) {}
 
   ngOnInit() {
     this.titleService.setTitle('Home - Liquor and Cannabis Regulation BranchB');
+    this.clientConfigDataService.getConfig().subscribe(data => {
+      // this.isLiteVersion = data.isLiteVersion;
+    });
   }
 
 }
