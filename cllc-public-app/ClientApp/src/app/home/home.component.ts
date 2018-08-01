f99498932b83a8f535f648b2069511a829427bf8
Rafael Ponce de Leon <Rafael.PonceDeLeon@quartech.com>
2018-08-01 15:18:21 -0700

    Wireframe changes- home and payment confirmation

--- cllc-public-app/ClientApp/src/app/home/home.component.ts
+++ cllc-public-app/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ClientConfigDataService } from '../services/client-config.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +11,16 @@ import { ClientConfigDataService } from '../services/client-config.service';
 export class HomeComponent implements OnInit {
   window = window;
   isLiteVersion: boolean;
+  isDataLoaded: boolean = false;
+  busy: Subscription;
 
   constructor(private titleService: Title, private clientConfigDataService: ClientConfigDataService) {}
 
   ngOnInit() {
-    this.titleService.setTitle('Home - Liquor and Cannabis Regulation BranchB');
-    this.clientConfigDataService.getConfig().subscribe(data => {
+    this.titleService.setTitle('Home - Liquor and Cannabis Regulation Branch');
+    this.busy = this.clientConfigDataService.getConfig().subscribe(data => {
       this.isLiteVersion = data.isLiteVersion;
+      this.isDataLoaded = true;
     });
   }
 
