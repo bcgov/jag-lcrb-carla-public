3b43bda1587013840db4da5db87202210493b76b
George Walker <george@walkersoftware.ca>
2018-08-08 15:04:31 -0700

    Develop (#491)
    *  BC service landing page (#489)
    
    * BC service landing page
    
    * Authorization header fix
    
    * remove unsed file
    
    * fix bceid logo link (#490)
    
    * BC service landing page
    
    * Authorization header fix
    
    * remove unsed file
    
    * fix icon image link

---
+++ cllc-public-app/ClientApp/src/app/lite/associates-dashboard/associates-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-associate-dashboard',
+  templateUrl: './associates-dashboard.component.html',
+  styleUrls: ['./associates-dashboard.component.css']
+})
+export class AssociatesDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
