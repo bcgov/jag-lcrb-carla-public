f38778027798998d123430145e1253db34768055
Moffat Sehudi <msehudi@gmail.com>
2018-08-08 13:10:32 -0700

    BC service landing page

---
+++ cllc-public-app/ClientApp/src/app/lite/associates-dashboard/associates-dashboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AssociatesDashboardComponent } from './associates-dashboard.component';
+
+describe('AssociatesDashboardComponent', () => {
+  let component: AssociatesDashboardComponent;
+  let fixture: ComponentFixture<AssociatesDashboardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AssociatesDashboardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AssociatesDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
