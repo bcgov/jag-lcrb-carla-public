5a70a0a289abd51d582d4164adc87f0fb30ac5fe
Rafael Ponce de Leon <Rafael.PonceDeLeon@quartech.com>
2018-08-13 17:05:21 -0700

    LCSD-90 - Logging Account Controller

---
+++ cllc-public-app/Utils/LoggingEvents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gov.Lclb.Cllb.Public.Utils
+{
+    public static class LoggingEvents
+    {
+        public const int HttpGet = 1000;
+        public const int HttpPost = 1001;
+        public const int HttpPut = 1002;
+        public const int HttpDelete = 1003;
+
+        public const int Get = 2000;
+
+        public const int NotFound = 4000;
+
+        public const int Error = 5000;
+        public const int BadRequest = 5001;
+    }
+}
