034cd448b5c21c66c98ed9a6bdc7db0fd368c13c
Moffat Sehudi <msehudi@gmail.com>
2018-08-27 13:30:04 -0700

    remove odata references

--- cllc-public-app/Models.Extensions/FileSystemItem.cs
+++ cllc-public-app/Models.Extensions/FileSystemItem.cs
@@ -42,59 +42,5 @@
             return result;
         }
 
-        /// <summary>
-        /// Copy values from a Dynamics legal entity to a view model.
-        /// </summary>
-        /// <param name="to"></param>
-        /// <param name="from"></param>
-        public static void CopyValues(this MS.FileServices.FileSystemItem to, ViewModels.FileSystemItem from)
-        {
-            to.Name = CombineNameDocumentType (from.name, from.documenttype);
-            to.Size = from.size;
-            to.TimeCreated = from.timecreated;
-            to.TimeLastModified = from.timelastmodified;            
-        }
-
-        /// <summary>
-        /// Convert a given voteQuestion to a ViewModel
-        /// </summary>        
-        public static ViewModels.FileSystemItem ToViewModel(this MS.FileServices.FileSystemItem fileSystemItem)
-        {
-            ViewModels.FileSystemItem result = null;
-            if (fileSystemItem != null)
-            {
-                result = new ViewModels.FileSystemItem();
-                if (fileSystemItem.Id != null)
-                {
-                    result.id = fileSystemItem.Id;
-                }
-
-                result.name = GetDocumentName(fileSystemItem.Name);
-                result.documenttype = GetDocumentType(fileSystemItem.Name);
-                result.size = fileSystemItem.Size;
-                result.timecreated = fileSystemItem.TimeCreated;
-                result.timelastmodified = fileSystemItem.TimeLastModified;
-
-            }            
-            return result;
-        }                   
-
-        public static MS.FileServices.FileSystemItem ToModel(this ViewModels.FileSystemItem fileSystemItem)
-        {
-            MS.FileServices.File result = null;
-            if (fileSystemItem != null)
-            {
-                result = new MS.FileServices.File()
-                {
-                    Id = fileSystemItem.id,
-                    Name = CombineNameDocumentType(fileSystemItem.name,fileSystemItem.documenttype),
-                    Size = fileSystemItem.size,
-                    TimeCreated = fileSystemItem.timecreated,
-                    TimeLastModified = fileSystemItem.timelastmodified
-                };               
-            }
-            
-            return result;
-        }
     }
 }
