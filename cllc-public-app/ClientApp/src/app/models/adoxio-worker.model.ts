87a3c250d61ed0a1fcd9893be5a63d6c40a4f1eb
Daniel Truong <daniel.t.truong@gmail.com>
2018-08-28 10:20:06 -0700

    Preparing for worker registration

---
+++ cllc-public-app/ClientApp/src/app/models/adoxio-worker.model.ts
@@ -0,0 +1,15 @@
+export interface AdoxioWorker { 
+    adoxio_isldbworker: boolean;
+    adoxio_firstname: string;
+    adoxio_middlename: string;
+    adoxio_lastname: string;
+    adoxio_dateofbirth: string;
+    adoxio_gender: string;
+    adoxio_birthplace: string;
+    adoxio_driverslicencenumber: string;
+    adoxio_bcidcardnumber: string;
+    adoxio_phonenumber: string;
+    adoxio_email: string;
+    adoxio_selfdisclosure: string;
+    adoxio_triggerphs: string;
+}
