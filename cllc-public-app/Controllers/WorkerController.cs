fe894c91cc24adbf249863788955753c3d17e00f
Daniel Truong <daniel.t.truong@gmail.com>
2018-08-29 15:20:22 -0700

    removed broken odata imports

--- cllc-public-app/Controllers/WorkerController.cs
+++ cllc-public-app/Controllers/WorkerController.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.OData.Client;
 using Newtonsoft.Json;
 using System;
 using System.Threading.Tasks;
