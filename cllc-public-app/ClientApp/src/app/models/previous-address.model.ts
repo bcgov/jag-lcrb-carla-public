9cb79d37a704348657622a77334a96247b5665a0
Daniel Truong <daniel.t.truong@gmail.com>
2018-08-30 11:56:59 -0700

    Removed references to Adoxio

---
+++ cllc-public-app/ClientApp/src/app/models/previous-address.model.ts
@@ -0,0 +1,10 @@
+export interface PreviousAddress { 
+    name: string;
+    streetaddress: string;
+    city: string;
+    provstate: string;
+    country: string;
+    postalcode: string;
+    fromdate: string;
+    todate: string;
+}
