e8ae7f6566e1b64222eb2476d3f8d4ad18f8d355
Moffat Sehudi <msehudi@gmail.com>
2018-09-07 12:16:07 -0700

    worker registration changes

---
+++ cllc-public-app/ClientApp/src/app/worker-registration/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class WorkerDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ cllc-public-app/ClientApp/src/app/worker-registration/user-confirmation/user-confirmation.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { User } from '../../models/user.model';
+import { DynamicsContact } from '../../models/dynamics-contact.model';
+import { UserDataService } from '../../services/user-data.service';
+import { AppState } from '../../app-state/models/app-state';
+import { Store } from '@ngrx/store';
+import { AliasDataService } from '../../services/alias-data.service';
+import { PreviousAddressDataService } from '../../services/previous-address-data.service';
+import { ContactDataService } from '../../services/contact-data.service';
+import { WorkerDataService } from '../../services/worker-data.service.';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-confirmation',
+  templateUrl: './user-confirmation.component.html',
+  styleUrls: ['./user-confirmation.component.scss']
+})
+export class UserConfirmationComponent implements OnInit {
+  @Input() currentUser: User;
+  @Output() reloadUser: EventEmitter<any> = new EventEmitter<any>();
+  busy: any;
+
+  constructor(private userDataService: UserDataService,
+    private store: Store<AppState>,
+    private aliasDataService: AliasDataService,
+    private previousAddressDataService: PreviousAddressDataService,
+    private contactDataService: ContactDataService,
+    private workerDataService: WorkerDataService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+
+  confirmContact(confirm: boolean) {
+    if (confirm) {
+      // create contact here
+      const contact = new DynamicsContact();
+      contact.fullname = this.currentUser.name;
+      contact.firstname = this.currentUser.firstname;
+      contact.lastname = this.currentUser.lastname;
+      contact.emailaddress1 = this.currentUser.email;
+      this.busy = this.contactDataService.createWorkerContact(contact).subscribe(res => {
+        this.reloadUser.emit(true);
+      }, error => alert('Failed to create contact'));
+    } else {
+      window.location.href = 'logout';
+    }
+  }
+}
