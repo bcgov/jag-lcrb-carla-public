7fd54f174a3a785317e5ee9621a1d70b94d16dab
George Walker <george@walkersoftware.ca>
2018-09-07 11:41:56 -0700

    Merge Spd sync into Develop (#564)
    * LCSD-64 skeleton for SPD PoC
    
    * LCSD-64 skeleton for SPD PoC (#479)
    
    * commented out authentication
    
    * spd sync partial
    
    * spd export updates
    
    * remove email
    
    * LCSD-182: spd export
    
    * spd sync fixes
    
    * LCSD-218 - build fixes
    
    * Remove extra files
    
    * Remove redundant items from build
    
    * Spd sync build fixes (#559)
    
    * LCSD-64 skeleton for SPD PoC
    
    * LCSD-218 - build fixes
    
    * Remove extra files
    
    * Remove redundant items from build
    
    * LCSD-218 Fix configuration keys for OpenShift build
    
    * Spd sync (#560)
    
    * LCSD-64 skeleton for SPD PoC
    
    * LCSD-218 - build fixes
    
    * Remove extra files
    
    * Remove redundant items from build
    
    * LCSD-218 Fix configuration keys for OpenShift build
    
    * LCSD-218 add health check
    
    * LCSD-218 OpenShift fixes
    
    * Resolve compiler warnings
    
    * LCSD-218 add diagnostic info
    
    * LCSD-218 - add extra logging to SPD Export
    
    * LCSD-218 Fix hard coded email server
    
    * LCSD-218 Fix naming convention
    
    * LCSD-218 update template
    
    * Spd sync (#562)
    
    * LCSD-64 skeleton for SPD PoC
    
    * LCSD-218 - build fixes
    
    * Remove extra files
    
    * Remove redundant items from build
    
    * LCSD-218 Fix configuration keys for OpenShift build
    
    * LCSD-218 add health check
    
    * LCSD-218 OpenShift fixes
    
    * Resolve compiler warnings
    
    * LCSD-218 add diagnostic info
    
    * LCSD-218 - add extra logging to SPD Export
    
    * LCSD-218 Fix hard coded email server
    
    * LCSD-218 Fix naming convention
    
    * LCSD-218 update template

---
+++ spd-sync/Controllers/SpdController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Hangfire;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace Gov.Lclb.Cllb.SpdSync.Controllers
+{
+    [Route("api/spd")]
+    public class SpdController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        private readonly string accessToken;
+        private readonly string baseUri;
+        private readonly ILogger _logger;
+
+        public SpdController(IConfiguration configuration, ILoggerFactory loggerFactory)
+        {
+            Configuration = configuration;
+            accessToken = Configuration["ACCESS_TOKEN"];
+            baseUri = Configuration["BASE_URI"];
+            _logger = loggerFactory.CreateLogger(typeof(SpdController));
+        }
+
+        /// <summary>
+        /// GET api/spd/send
+        /// Send export to SPD.
+        /// </summary>
+        /// <returns>OK if successful</returns>
+        [HttpGet("send")]
+        public ActionResult Send()
+        {
+            BackgroundJob.Enqueue(() => new SpdUtils(Configuration).SendExportJob(null));
+            _logger.LogInformation("Started send export job");
+            return Ok();
+        }
+
+        /// <summary>
+        /// GET api/apd/receive
+        /// Start a receive import job
+        /// </summary>
+        /// <returns>OK if successful</returns>
+        [HttpGet("receive")]
+        public ActionResult Receive()
+        {
+            // check the file drop for a file, and then process it.
+            _logger.LogInformation("Started receive import job");
+            return Ok();
+
+        }
+    }
+}
---
+++ spd-sync/LocalHangfireAuthorizationFilter.cs
@@ -0,0 +1,16 @@
+﻿using Hangfire.Dashboard;
+
+namespace Gov.Lclb.Cllb.SpdSync
+{
+    public class LocalHangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var httpContext = context.GetHttpContext();
+
+            // Allow all authenticated users to see the Dashboard (potentially dangerous).
+            //return httpContext.User.Identity.IsAuthenticated;
+            return true;
+        }
+    }
+}
