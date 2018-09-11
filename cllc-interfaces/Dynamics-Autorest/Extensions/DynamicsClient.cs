b0d7aeec338c5b7886f396d7640283aea65ef64c
George Walker <george@walkersoftware.ca>
2018-09-11 13:25:30 -0700

    LCSD-232 Interim commit, fix build errors

---
+++ cllc-interfaces/Dynamics-Autorest/Extensions/DynamicsClient.cs
@@ -0,0 +1,23 @@
+namespace Gov.Lclb.Cllb.Interfaces
+{
+    using Microsoft.Rest;
+    using Microsoft.Rest.Serialization;
+    using Models;
+    using Newtonsoft.Json;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+
+    /// <summary>
+    /// Auto Generated
+    /// </summary>
+    public partial class DynamicsClient : ServiceClient<DynamicsClient>, IDynamicsClient
+    {
+        /// <summary>
+        /// The base URI of the service.
+        /// </summary>
+        public System.Uri NativeBaseUri { get; set; }
+
+    }
+}
