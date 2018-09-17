ea2f206db62eff16da069bc52e27d09ae8488416
Moffat Sehudi <msehudi@gmail.com>
2018-09-17 15:28:08 -0700

    LCSD-206: use environment variables

--- one-stop-service/one-stop-service/Controllers/OneStopController.cs
+++ one-stop-service/one-stop-service/Controllers/OneStopController.cs
@@ -4,48 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.ServiceModel;
+using Microsoft.Extensions.Configuration;
+using Hangfire;
+using Gov.Lclb.Cllb.OneStopService;
 
 namespace one_stop_service.Controllers
 {
     [Route("api/[controller]")]
     public class OneStopController : Controller
     {
+        IConfiguration Configuration;
+        public OneStopController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         [Route("[action]")]
         public async Task<IActionResult> SendLicenceCreationMessage()
         {
-            var cred = new System.ServiceModel.Description.ClientCredentials() {
-
-            };
-            cred.UserName.UserName = "HubPartnerLCRB";
-            cred.UserName.Password = "LCRBtest@0904";
-            var serviceClient = new OneStopServiceReference.receiveFromPartner_PortTypeClient();
-            var basicHttpBinding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
-            basicHttpBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
-            serviceClient.Endpoint.Binding = basicHttpBinding;
-
-            using (new OperationContextScope(serviceClient.InnerChannel))
-            {
-                //Create message header containing the credentials
-                var header = new OneStopServiceReference.SoapSecurityHeader("", "HubPartnerLCRB", "LCRBtest@0904", "");
-                //Add the credentials message header to the outgoing request
-                OperationContext.Current.OutgoingMessageHeaders.Add(header);
-
-
-                //try
-                //{
-                    var version = await serviceClient.receiveFromPartnerAsync("");
-                //}
-                //catch (Exception ex)
-                //{
-                //    throw;
-                //}
-            }
-
-
-
-            
-
-
+            BackgroundJob.Enqueue(() => new OneStopUtils(Configuration).SendLicenceCreationMessage(null));
             return Ok();
         }
 
