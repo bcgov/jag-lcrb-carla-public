5f276d23ec83bb6d28ca2b8aae17d342ba458980
George Walker <george@walkersoftware.ca>
2018-09-17 15:26:19 -0700

    Worker Reg changes (#584)
    * LCSD-185 Improvements to initial data from BC Service Card
    
    * LCSD-185 BC-Service Card fixes
    
    * LCSD-185 fixes to worker registration
    
    * LCSD-185 BC Service card login sequence fixes
    
    * LCSD-185 fixes for worker reg form
    
    * LCSD-290 Worker Reg app form changes

--- cllc-public-app/Authentication/SiteminderAuthenticationHandler.cs
+++ cllc-public-app/Authentication/SiteminderAuthenticationHandler.cs
@@ -233,6 +233,7 @@ namespace Gov.Lclb.Cllb.Public.Authentication
                 IHostingEnvironment hostingEnv = (IHostingEnvironment)context.RequestServices.GetService(typeof(IHostingEnvironment));
 
                 UserSettings userSettings = new UserSettings();
+                
                 string userId = null;
 				string devCompanyId = null;
                 string siteMinderGuid = "";
@@ -528,6 +529,20 @@ namespace Gov.Lclb.Cllb.Public.Authentication
                         _logger.LogDebug("userSettings.ContactId:" + userSettings.ContactId);
                     }
                 }				
+
+                // add the worker settings if it is a new user.
+                if (userSettings.IsNewUserRegistration && userSettings.NewWorker == null)
+                {
+                    userSettings.NewWorker = new ViewModels.Worker();
+                    userSettings.NewWorker.CopyHeaderValues(context.Request.Headers);
+                }
+
+                // add the worker settings if it is a new user.
+                if (userSettings.IsNewUserRegistration && userSettings.NewContact == null)
+                {
+                    userSettings.NewContact = new ViewModels.Contact();
+                    userSettings.NewContact.CopyHeaderValues(context.Request.Headers);
+                }
 
                 // **************************************************
                 // Update user settings
