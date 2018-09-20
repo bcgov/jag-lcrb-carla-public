528bb39385b33fef59fea4424bd31d547c5d5f50
Moffat Sehudi <msehudi@gmail.com>
2018-09-20 13:38:16 -0700

    flatten folder structure

---
+++ one-stop-service/Controllers/OneStopController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ServiceModel;
+using Microsoft.Extensions.Configuration;
+using Hangfire;
+using Gov.Lclb.Cllb.OneStopService;
+
+namespace one_stop_service.Controllers
+{
+    [Route("api/[controller]")]
+    public class OneStopController : Controller
+    {
+        IConfiguration Configuration;
+        public OneStopController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [Route("[action]/{licenceGuild}")]
+        public async Task<IActionResult> SendLicenceCreationMessage(string licenceGuild)
+        {
+            BackgroundJob.Enqueue(() => new OneStopUtils(Configuration).SendLicenceCreationMessage(null, licenceGuild));
+            
+            return Ok();
+        }
+
+    }
+}
---
+++ one-stop-service/PingService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace Gov.Lclb.Cllb.OneStopService
+{
+    public class PingService : IPingService
+    {
+        public string Ping(string message)
+        {
+            return string.Join("", message.Reverse());
+        }
+    }
+
+    [ServiceContract]
+    public interface IPingService
+    {
+        [OperationContract]
+        string Ping(string message);
+    }
+}
