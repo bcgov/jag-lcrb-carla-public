a698c793f135ecb946b43a48c286b094ee7f0661
msehudi <msehudi@gmail.com>
2018-09-20 14:34:42 -0700

    OneStop updates (#594)
    * worker reg application loading spinners
    
    * worker reg application form cleanup
    
    * one  stop service updates
    
    * flatten folder structure
    
    * one stop updates
    
    * update gitignore
    
    * update service

---
+++ one-stop-service/ReceiveFromHubService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace Gov.Lclb.Cllb.OneStopService
+{
+    public class ReceiveFromHubService : IReceiveFromHubService
+    {
+        public string receiveFromHub(string inputXML)
+        {
+            return string.Join("", inputXML.Reverse());
+        }
+    }
+
+    [ServiceContract]
+    public interface IReceiveFromHubService
+    {
+        [OperationContract]
+        string receiveFromHub(string inputXML);
+    }
+}
