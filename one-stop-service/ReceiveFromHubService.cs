429e0f8653feeb4d34a53b23cd02299f73cf2c78
Moffat Sehudi <msehudi@gmail.com>
2018-09-21 15:32:40 -0700

    LCSD-318: parse response from OneStop

--- one-stop-service/ReceiveFromHubService.cs
+++ one-stop-service/ReceiveFromHubService.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.ServiceModel;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Gov.Lclb.Cllb.OneStopService
 {
@@ -10,9 +13,29 @@ namespace Gov.Lclb.Cllb.OneStopService
     {
         public string receiveFromHub(string inputXML)
         {
-            return string.Join("", inputXML.Reverse());
+            try
+            {
+                // deserialize the inputXML
+                var serializer = new XmlSerializer(typeof(SBNCreateProgramAccountResponse1));
+                SBNCreateProgramAccountResponse1 licenseData;
+                using (TextReader reader = new StringReader(inputXML))
+                {
+                    licenseData = (SBNCreateProgramAccountResponse1)serializer.Deserialize(reader);
+                }
+
+                //TODO: Update dynamics
+
+            }
+            catch (Exception e)
+            {
+                //TODO: Construct error message and return it
+            }
+
+            return "200";
+
         }
     }
+
 
     [ServiceContract]
     public interface IReceiveFromHubService
