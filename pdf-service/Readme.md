2e4d89a5093db1ff7e8d3471c420a277697ee016
George Walker <george@walkersoftware.ca>
2018-09-24 10:07:08 -0700

    LCSD-250 PDF Service

---
+++ pdf-service/Readme.md
@@ -0,0 +1,53 @@
+# PDF Service #
+------------
+
+The purpose of the PDF microservice is to generate PDF documents based on templates and JSON format data.  No database connection is used to generate the PDF.
+
+## Development ##
+
+This service is a Dotnet Core 2.1 application written using the Web SDK.  As such you can use an IDE such as Visual Studio or VS Code to edit the files.  
+
+## Testing ##
+
+This section describes concepts and tools necessary for testing the service.
+
+### Curl
+
+Curl is a command line tool for sending HTTP requests.  
+
+The website for Curl is https://curl.haxx.se
+
+###Authorization
+
+To obtain an Authorization token, access the following URL:
+
+<Microservice Base URL>/api/authentication/token?secret=<SECRET>
+
+Note that if your route is using a self signed certificate, you will need to use the -k parameter with Curl to disable certificate validation.
+
+Substitute <SECRET> with the value for the JWT Token Key for the environment.  The token endpoint will only provide a token if the secret provided matches that in the application configuration.
+
+The lengthy string of characters at the start of the response is the token, followed by an expires declaration.  Copy the first string and use that as your token.
+
+
+### JWT Authentication
+
+The PDF Microservice uses Json Web Token (JWT) authentication for the REST API server.
+
+JWT authentication is implemented using the "Authentication" header, consisting of "Bearer " plus the text of the token.
+
+To interact with a service using curl, specify the following command line:
+
+`curl --header "Authorization: Bearer <token>"` <rest of Curl command>
+
+Where token is a valid JWT token,  obtained through the Authorization endpoint.  Note that if you copy and paste the token from certain applications it may add Unicode characters; if you run into problems, copy from Notepad or another application that uses plaintext. 
+
+### Triggering the export
+
+Using the above JWT Authentication method, here is the request to start the export:
+
+`curl --header "Authorization: Bearer <TOKEN>" -d {input data} -k <URL to service>`
+
+
+
+  
---
+++ pdf-service/Startup.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdf
+{
+    public class Startup
+    {
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            if (!System.Diagnostics.Debugger.IsAttached)
+                builder.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            builder.AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // Enable Node Services
+            services.AddNodeServices();
+
+            services.AddMvc(config =>
+            {
+					
+                if (!string.IsNullOrEmpty(Configuration["JWT_TOKEN_KEY"]))
+                {
+                    var policy = new AuthorizationPolicyBuilder()
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                    config.Filters.Add(new AuthorizeFilter(policy));
+                }
+
+            });
+
+            // Other ConfigureServices() code...
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "JAG LCRB PDF Service", Version = "v1" });
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddDefaultTokenProviders();
+
+            //if (!string.IsNullOrEmpty(Configuration["JWT_TOKEN_KEY"]))
+            //{
+            //    // Configure JWT authentication
+            //    services.AddAuthentication(o =>
+            //    {
+            //        o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //        o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    }).AddJwtBearer(o =>
+            //    {
+            //        o.SaveToken = true;
+            //        o.RequireHttpsMetadata = false;
+            //        o.TokenValidationParameters = new TokenValidationParameters()
+            //        {
+            //            RequireExpirationTime = false,
+            //            ValidIssuer = Configuration["JWT_VALID_ISSUER"],
+            //            ValidAudience = Configuration["JWT_VALID_AUDIENCE"],
+            //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT_TOKEN_KEY"]))
+            //        };
+            //    });
+            //}
+            
+            // health checks. 
+            services.AddHealthChecks(checks =>
+            {
+                checks.AddValueTaskCheck("HTTP Endpoint", () => new
+                    ValueTask<IHealthCheckResult>(HealthCheckResult.Healthy("Ok")));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            //app.UseAuthentication();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JAG LCRB PDF Service");
+            });
+        }
+
+    }
+}
+
