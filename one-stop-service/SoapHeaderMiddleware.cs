ef00068b133ecd3606a7b62580d4cb102cf81f84
Moffat Sehudi <msehudi@gmail.com>
2018-10-02 12:31:16 -0700

    Add request logging

--- one-stop-service/SoapHeaderMiddleware.cs
+++ one-stop-service/SoapHeaderMiddleware.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Gov.Lclb.Cllb.OneStopService
@@ -10,20 +13,53 @@ namespace Gov.Lclb.Cllb.OneStopService
     public class SoapHeaderMiddleware
     {
         private readonly RequestDelegate _next;
+        private ILogger logger;
 
-        public SoapHeaderMiddleware(RequestDelegate next)
+        public SoapHeaderMiddleware(RequestDelegate next, ILogger logger)
         {
             _next = next;
+            this.logger = logger;
         }
 
         public async Task Invoke(HttpContext context)
         {
-            // Do work that doesn't write to the Response.
+            var requestLog = await FormatRequest(context.Request);
+            logger.LogInformation(requestLog);
             if (context.Request.Path.Value.Equals("/receiveFromHub") && string.IsNullOrEmpty(context.Request.Headers["SOAPAction"]))
             {
                 context.Request.Headers["SOAPAction"] = "http://tempuri.org/IReceiveFromHubService/receiveFromHub";
             }
             await _next(context);
+        }
+
+
+        private async Task<string> FormatRequest(HttpRequest request)
+        {
+            var body = request.Body;
+
+            //This line allows us to set the reader for the request back at the beginning of its stream.
+            request.EnableRewind();
+
+            //We now need to read the request stream.  First, we create a new byte[] with the same length as the request stream...
+            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+
+            //...Then we copy the entire request stream into the new buffer.
+            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+
+            //We convert the byte[] into a string using UTF8 encoding...
+            var bodyAsText = Encoding.UTF8.GetString(buffer);
+
+            //..and finally, assign the read body back to the request body, which is allowed because of EnableRewind()
+            request.Body = body;
+
+            var headers = "";
+
+            foreach(var kvPair in request.Headers.AsEnumerable())
+            {
+                headers += $"{kvPair.Key}: {kvPair.Value}\n";
+            }
+
+            return $">>>>>> START REQUEST lOG: \n Scheme: {request.Scheme} \n Path: {request.Host}{request.Path} \n QueryString: {request.QueryString} \n Body: {bodyAsText} \n Headers: {headers} \n <<<<<< END REQUEST lOG";
         }
     }
 
