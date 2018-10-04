ed9dbcb58fb08d18351513a301742e890eb8b66d
Moffat Sehudi <msehudi@gmail.com>
2018-10-04 15:47:27 -0700

    Log outgoing inputXML

--- cllc-interfaces/OneStopRestClient/OneStopRestClient.cs
+++ cllc-interfaces/OneStopRestClient/OneStopRestClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -9,17 +10,20 @@ namespace Gov.Lclb.Cllb.Interfaces
         public Uri BaseUri { get; set; }
         public string AuthorizationHeaderValue { get; set; }
         private readonly HttpClient httpClient = new HttpClient();
+        private readonly ILogger logger;
 
-        public OneStopRestClient(Uri baseUri, string authorizationHeaderValue)
+        public OneStopRestClient(Uri baseUri, string authorizationHeaderValue, ILogger logger)
         {
             BaseUri = baseUri;
             AuthorizationHeaderValue = authorizationHeaderValue;
             httpClient.DefaultRequestHeaders.Add("Authorization", authorizationHeaderValue);
+            this.logger = logger;
         }
 
         public async Task<string> receiveFromPartner(string inputXml)
         {
-            var url = $"{BaseUri}?inputXml={Uri.EscapeDataString(inputXml)}";
+            var url = $"{BaseUri}?inputXML={Uri.EscapeDataString(inputXml)}";
+            logger.LogInformation($"InputXML to send = {inputXml}");
             var response = await httpClient.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
