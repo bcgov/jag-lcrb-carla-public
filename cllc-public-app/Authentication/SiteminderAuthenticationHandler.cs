5217482d2b4dd4966e7ec294869bafa951805b5b
Moffat Sehudi <msehudi@gmail.com>
2018-10-12 09:55:29 -0700

    Fix authentication

--- cllc-public-app/Authentication/SiteminderAuthenticationHandler.cs
+++ cllc-public-app/Authentication/SiteminderAuthenticationHandler.cs
@@ -13,7 +13,7 @@ using Gov.Lclb.Cllb.Interfaces;
 using Gov.Lclb.Cllb.Public.Utils;
 
 namespace Gov.Lclb.Cllb.Public.Authentication
-{    
+{
     #region SiteMinder Authentication Options
     /// <summary>
     /// Options required for setting up SiteMinder Authentication
@@ -22,16 +22,16 @@ namespace Gov.Lclb.Cllb.Public.Authentication
     {
         // note that header keys are case insensitive, thus the reason why the keys are all lower case.
 
-		private const string ConstDevAuthenticationTokenKey = "DEV-USER";
-		private const string ConstDevBCSCAuthenticationTokenKey = "DEV-BCSC-USER";
-        private const string ConstDevDefaultUserId = "TMcTesterson";       
+        private const string ConstDevAuthenticationTokenKey = "DEV-USER";
+        private const string ConstDevBCSCAuthenticationTokenKey = "DEV-BCSC-USER";
+        private const string ConstDevDefaultUserId = "TMcTesterson";
         private const string ConstSiteMinderUserGuidKey = "smgov_userguid"; //deprecated -- smgov_useridentifier
         private const string ConstSiteMinderUserIdentifierKey = "smgov_useridentifier";
         private const string ConstSiteMinderUniversalIdKey = "sm_universalid";
         private const string ConstSiteMinderUserNameKey = "sm_user";
 
         //BCeId Values
-        private const string ConstSiteMinderBusinessGuidKey = "smgov_businessguid"; 
+        private const string ConstSiteMinderBusinessGuidKey = "smgov_businessguid";
         private const string ConstSiteMinderBusinessLegalNameKey = "smgov_businesslegalname";
 
         //BC Services Card
@@ -71,10 +71,10 @@ namespace Gov.Lclb.Cllb.Public.Authentication
             InactivegDbUserIdError = ConstInactivegDbUserIdError;
             InvalidPermissions = ConstInvalidPermissions;
             DevAuthenticationTokenKey = ConstDevAuthenticationTokenKey;
-			DevBCSCAuthenticationTokenKey = ConstDevBCSCAuthenticationTokenKey;
+            DevBCSCAuthenticationTokenKey = ConstDevBCSCAuthenticationTokenKey;
             DevDefaultUserId = ConstDevDefaultUserId;
             UnderageError = ConstUnderageError;
-        }        
+        }
 
         /// <summary>
         /// Default Scheme Name
@@ -163,7 +163,7 @@ namespace Gov.Lclb.Cllb.Public.Authentication
         /// </summary>
         public string DevAuthenticationTokenKey { get; set; }
 
-		/// <summary>
+        /// <summary>
         /// Development Environment Authentication Key
         /// </summary>
         public string DevBCSCAuthenticationTokenKey { get; set; }
@@ -197,7 +197,7 @@ namespace Gov.Lclb.Cllb.Public.Authentication
     /// </summary>
     public class SiteminderAuthenticationHandler : AuthenticationHandler<SiteMinderAuthOptions>
     {
-        private readonly ILogger _logger;        
+        private readonly ILogger _logger;
 
         /// <summary>
         /// Siteminder Authentication Constructir
@@ -209,7 +209,7 @@ namespace Gov.Lclb.Cllb.Public.Authentication
         public SiteminderAuthenticationHandler(IOptionsMonitor<SiteMinderAuthOptions> configureOptions, ILoggerFactory loggerFactory, UrlEncoder encoder, ISystemClock clock)
             : base(configureOptions, loggerFactory, encoder, clock)
         {
-            _logger = loggerFactory.CreateLogger(typeof(SiteminderAuthenticationHandler));                    
+            _logger = loggerFactory.CreateLogger(typeof(SiteminderAuthenticationHandler));
         }
 
         /// <summary>
@@ -220,10 +220,10 @@ namespace Gov.Lclb.Cllb.Public.Authentication
         {
             // get siteminder headers
             _logger.LogDebug("Parsing the HTTP headers for SiteMinder authentication credential");
-                  
+
             SiteMinderAuthOptions options = new SiteMinderAuthOptions();
             bool isDeveloperLogin = false;
-			bool isBCSCDeveloperLogin = false;
+            bool isBCSCDeveloperLogin = false;
             try
             {
                 ClaimsPrincipal principal;
@@ -233,9 +233,9 @@ namespace Gov.Lclb.Cllb.Public.Authentication
                 IHostingEnvironment hostingEnv = (IHostingEnvironment)context.RequestServices.GetService(typeof(IHostingEnvironment));
 
                 UserSettings userSettings = new UserSettings();
-                
+
                 string userId = null;
-				string devCompanyId = null;
+                string devCompanyId = null;
                 string siteMinderGuid = "";
                 string siteMinderBusinessGuid = "";
                 string siteMinderUserType = "";
@@ -265,42 +265,42 @@ namespace Gov.Lclb.Cllb.Public.Authentication
 
                     if (!string.IsNullOrEmpty(temp))
                     {
-						if (temp.Contains("::"))
-						{
-							var temp2 = temp.Split("::");
-							userId = temp2[0];
-							if (temp2.Length >= 2 )
-								devCompanyId = temp2[1];
-							else
-								devCompanyId = temp2[0];
-						}
-						else
-						{
-							userId = temp;
-							devCompanyId = temp;
-						}
+                        if (temp.Contains("::"))
+                        {
+                            var temp2 = temp.Split("::");
+                            userId = temp2[0];
+                            if (temp2.Length >= 2)
+                                devCompanyId = temp2[1];
+                            else
+                                devCompanyId = temp2[0];
+                        }
+                        else
+                        {
+                            userId = temp;
+                            devCompanyId = temp;
+                        }
                         isDeveloperLogin = true;
 
-						_logger.LogDebug("Got user from dev cookie = " + userId + ", company = " + devCompanyId);
-					} 
-					else 
-					{
+                        _logger.LogDebug("Got user from dev cookie = " + userId + ", company = " + devCompanyId);
+                    }
+                    else
+                    {
                         // same set of tests for a BC Services Card dev login
-						temp = context.Request.Cookies[options.DevBCSCAuthenticationTokenKey];
+                        temp = context.Request.Cookies[options.DevBCSCAuthenticationTokenKey];
 
                         if (string.IsNullOrEmpty(temp)) // could be an automated test user.
                         {
                             temp = context.Request.Headers["DEV-BCSC-USER"];
                         }
 
-						if (!string.IsNullOrEmpty(temp))
-						{
-							userId = temp;
-							isBCSCDeveloperLogin = true;
+                        if (!string.IsNullOrEmpty(temp))
+                        {
+                            userId = temp;
+                            isBCSCDeveloperLogin = true;
 
-							_logger.LogDebug("Got user from dev cookie = " + userId);
-						}
-					}
+                            _logger.LogDebug("Got user from dev cookie = " + userId);
+                        }
+                    }
                 }
 
                 // **************************************************
@@ -316,6 +316,16 @@ namespace Gov.Lclb.Cllb.Public.Authentication
                 {
                     //do nothing
                     _logger.LogDebug("No UserSettings found");
+                }
+
+                // is user authenticated - if so we're done
+                if ((userSettings.UserAuthenticated && string.IsNullOrEmpty(userId)) ||
+                    (userSettings.UserAuthenticated && !string.IsNullOrEmpty(userId) &&
+                     !string.IsNullOrEmpty(userSettings.UserId) && userSettings.UserId == userId))
+                {
+                    _logger.LogDebug("User already authenticated with active session: " + userSettings.UserId);
+                    principal = userSettings.AuthenticatedUser.ToClaimsPrincipal(options.Scheme, userSettings.UserType);
+                    return AuthenticateResult.Success(new AuthenticationTicket(principal, null, Options.Scheme));
                 }
 
                 string smgov_userdisplayname = context.Request.Headers["smgov_userdisplayname"];
@@ -377,19 +387,21 @@ namespace Gov.Lclb.Cllb.Public.Authentication
                     if (isDeveloperLogin)
                     {
                         _logger.LogError("Generating a Development user");
-						userSettings.BusinessLegalName = devCompanyId + " TestBusiness";
+                        userSettings.BusinessLegalName = devCompanyId + " TestBusiness";
                         userSettings.UserDisplayName = userId + " TestUser";
                         siteMinderGuid = GuidUtility.CreateIdForDynamics("contact", userSettings.UserDisplayName).ToString();
                         siteMinderBusinessGuid = GuidUtility.CreateIdForDynamics("account", userSettings.BusinessLegalName).ToString();
                         siteMinderUserType = "Business";
-					} else if (isBCSCDeveloperLogin) {
-						_logger.LogError("Generating a Development BC Services user");
-						userSettings.BusinessLegalName = null;
+                    }
+                    else if (isBCSCDeveloperLogin)
+                    {
+                        _logger.LogError("Generating a Development BC Services user");
+                        userSettings.BusinessLegalName = null;
                         userSettings.UserDisplayName = userId + " Associate";
                         siteMinderGuid = GuidUtility.CreateIdForDynamics("bcsc", userSettings.UserDisplayName).ToString();
-						siteMinderBusinessGuid = null;
+                        siteMinderBusinessGuid = null;
                         siteMinderUserType = "VerifiedIndividual";
-					}
+                    }
                 }
 
                 // Previously the code would do a database lookup here.  However there is no backing database for the users table now,
@@ -399,23 +411,13 @@ namespace Gov.Lclb.Cllb.Public.Authentication
                 userSettings.AuthenticatedUser = await _dynamicsClient.LoadUser(siteMinderGuid, context.Request.Headers, _logger);
                 _logger.LogDebug("After getting authenticated user = " + userSettings.GetJson());
 
-                
-                // is user authenticated - if so we're done
-                if ((userSettings.UserAuthenticated && string.IsNullOrEmpty(userId)) ||
-                    (userSettings.UserAuthenticated && !string.IsNullOrEmpty(userId) &&
-                     !string.IsNullOrEmpty(userSettings.UserId) && userSettings.UserId == userId))
-                {
-                    _logger.LogDebug("User already authenticated with active session: " + userSettings.UserId);
-                    principal = userSettings.AuthenticatedUser.ToClaimsPrincipal(options.Scheme, userSettings.UserType);
-                    return AuthenticateResult.Success(new AuthenticationTicket(principal, null, Options.Scheme));
-                }
 
                 // check that the potential new user is 19.
                 if (userSettings.AuthenticatedUser != null && userSettings.AuthenticatedUser.ContactId == null)
                 {
                     string rawBirthDate = context.Request.Headers[options.SiteMinderBirthDate];
                     // get the birthdate.
-                    if (DateTimeOffset.TryParse (rawBirthDate, out DateTimeOffset birthDate))
+                    if (DateTimeOffset.TryParse(rawBirthDate, out DateTimeOffset birthDate))
                     {
                         DateTimeOffset nineteenYears = DateTimeOffset.Now.AddYears(-19);
                         if (birthDate > nineteenYears)
@@ -463,53 +465,53 @@ namespace Gov.Lclb.Cllb.Public.Authentication
                     userSettings.ContactId = userSettings.AuthenticatedUser.ContactId.ToString();
 
                     if (siteMinderBusinessGuid != null) // BCeID user
-                    {                        
+                    {
                         var account = await _dynamicsClient.GetAccountBySiteminderBusinessGuid(siteMinderBusinessGuid);
                         if (account != null && account.Accountid != null)
                         {
                             userSettings.AccountId = account.Accountid;
                             userSettings.AuthenticatedUser.AccountId = Guid.Parse(account.Accountid);
                         }
-                    }                    
+                    }
                 }
 
-				if (!hostingEnv.IsProduction() && (isDeveloperLogin || isBCSCDeveloperLogin))
+                if (!hostingEnv.IsProduction() && (isDeveloperLogin || isBCSCDeveloperLogin))
                 {
                     _logger.LogError("DEV MODE Setting identity and creating session for: " + userId);
 
-					if (isDeveloperLogin)
-					{
-						userSettings.BusinessLegalName = devCompanyId + " TestBusiness";
-						userSettings.UserDisplayName = userId + " TestUser";
+                    if (isDeveloperLogin)
+                    {
+                        userSettings.BusinessLegalName = devCompanyId + " TestBusiness";
+                        userSettings.UserDisplayName = userId + " TestUser";
 
-						// add generated guids
-						userSettings.SiteMinderBusinessGuid = GuidUtility.CreateIdForDynamics("account", userSettings.BusinessLegalName).ToString();
-						userSettings.SiteMinderGuid = GuidUtility.CreateIdForDynamics("contact", userSettings.UserDisplayName).ToString();
-					}
-					else if (isBCSCDeveloperLogin)
-					{
-						userSettings.BusinessLegalName = null;
-						userSettings.UserDisplayName = userId + " Associate";
+                        // add generated guids
+                        userSettings.SiteMinderBusinessGuid = GuidUtility.CreateIdForDynamics("account", userSettings.BusinessLegalName).ToString();
+                        userSettings.SiteMinderGuid = GuidUtility.CreateIdForDynamics("contact", userSettings.UserDisplayName).ToString();
+                    }
+                    else if (isBCSCDeveloperLogin)
+                    {
+                        userSettings.BusinessLegalName = null;
+                        userSettings.UserDisplayName = userId + " Associate";
 
                         // add generated guids
                         userSettings.SiteMinderBusinessGuid = null;
-						userSettings.SiteMinderGuid = GuidUtility.CreateIdForDynamics("bcsc", userSettings.UserDisplayName).ToString();
-					}
+                        userSettings.SiteMinderGuid = GuidUtility.CreateIdForDynamics("bcsc", userSettings.UserDisplayName).ToString();
+                    }
 
                     if (userSettings.IsNewUserRegistration)
                     {
-						if (isDeveloperLogin)
-						{
-							// add generated guids
-							userSettings.AccountId = userSettings.SiteMinderBusinessGuid;
-							userSettings.ContactId = userSettings.SiteMinderGuid;
-						}
-						else if (isBCSCDeveloperLogin)
-						{
-							// set to null for now
+                        if (isDeveloperLogin)
+                        {
+                            // add generated guids
+                            userSettings.AccountId = userSettings.SiteMinderBusinessGuid;
+                            userSettings.ContactId = userSettings.SiteMinderGuid;
+                        }
+                        else if (isBCSCDeveloperLogin)
+                        {
+                            // set to null for now
                             userSettings.AccountId = null;
                             userSettings.ContactId = null;
-						}
+                        }
 
                         _logger.LogDebug("New user registration:" + userSettings.UserDisplayName);
                         _logger.LogDebug("userSettings.SiteMinderBusinessGuid:" + userSettings.SiteMinderBusinessGuid);
@@ -533,7 +535,7 @@ namespace Gov.Lclb.Cllb.Public.Authentication
                         _logger.LogDebug("userSettings.AccountId:" + userSettings.AccountId);
                         _logger.LogDebug("userSettings.ContactId:" + userSettings.ContactId);
                     }
-                }				
+                }
 
                 // add the worker settings if it is a new user.
                 if (userSettings.IsNewUserRegistration && userSettings.NewWorker == null)
@@ -564,6 +566,6 @@ namespace Gov.Lclb.Cllb.Public.Authentication
                 Console.WriteLine(exception);
                 throw;
             }
-        }        
-    }    
+        }
+    }
 }
