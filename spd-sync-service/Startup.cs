59d6c8568390b3b447b3de6fde07d6c30983ee02
Daniel Truong <daniel.t.truong@gmail.com>
2018-10-29 12:51:11 -0700

    Reverting jwt

--- spd-sync-service/Startup.cs
+++ spd-sync-service/Startup.cs
@@ -3,11 +3,9 @@ using Hangfire;
 using Hangfire.Console;
 using Hangfire.MemoryStorage;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.HealthChecks;
@@ -50,13 +48,13 @@ namespace Gov.Lclb.Cllb.SpdSync
         {
             services.AddMvc(config =>
             {
-                if (!string.IsNullOrEmpty(Configuration["JWT_TOKEN_KEY"]))
-                {
-                    var policy = new AuthorizationPolicyBuilder()
-                                 .RequireAuthenticatedUser()
-                                 .Build();
-                    config.Filters.Add(new AuthorizeFilter(policy));
-                }
+                // if (!string.IsNullOrEmpty(Configuration["JWT_TOKEN_KEY"]))
+                // {
+                //     var policy = new AuthorizationPolicyBuilder()
+                //                  .RequireAuthenticatedUser()
+                //                  .Build();
+                //     config.Filters.Add(new AuthorizeFilter(policy));
+                // }
             });
 
             // Other ConfigureServices() code...
