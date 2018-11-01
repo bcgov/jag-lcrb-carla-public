1db21b939568d2ba107ad17bc52b8718abc06bca
Daniel Truong <daniel.t.truong@gmail.com>
2018-11-01 16:21:53 -0700

    LCSD-492 Added business number

--- one-stop-service/LdbExport.cs
+++ one-stop-service/LdbExport.cs
@@ -150,6 +150,7 @@ namespace Gov.Lclb.Cllb.OneStopService
                 new KeyValuePair<string, string>("AdoxioLicenceprintname", "Licence Type"),
                 new KeyValuePair<string, string>("licence.AdoxioLicencee.Name", "Legal Name"),
                 new KeyValuePair<string, string>("AdoxioLicencenumber", "Licence Number"),
+                new KeyValuePair<string, string>("AdoxioBusinessnumber", "Business Number"),
                 new KeyValuePair<string, string>("AdoxioEffectivedate", "Effective Date"),
                 new KeyValuePair<string, string>("AdoxioExpirydate", "Expiry Date"),
                 new KeyValuePair<string, string>("AdoxioEstablishment.AdoxioName", "Establishment Name"),
