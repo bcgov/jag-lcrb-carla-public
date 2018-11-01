c671879ae67172b604047f4e01656842e962ea40
Daniel Truong <daniel.t.truong@gmail.com>
2018-11-01 16:14:09 -0700

    Added legal name to Ldbexport

--- one-stop-service/LdbExport.cs
+++ one-stop-service/LdbExport.cs
@@ -148,6 +148,7 @@ namespace Gov.Lclb.Cllb.OneStopService
             var result = new List<KeyValuePair<string, string>>
             {                
                 new KeyValuePair<string, string>("AdoxioLicenceprintname", "Licence Type"),
+                new KeyValuePair<string, string>("licence.AdoxioLicencee.Name", "Legal Name"),
                 new KeyValuePair<string, string>("AdoxioLicencenumber", "Licence Number"),
                 new KeyValuePair<string, string>("AdoxioEffectivedate", "Effective Date"),
                 new KeyValuePair<string, string>("AdoxioExpirydate", "Expiry Date"),
