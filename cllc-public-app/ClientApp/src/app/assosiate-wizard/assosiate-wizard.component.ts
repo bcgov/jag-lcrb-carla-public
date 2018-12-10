4bf841eaa8d90dfc47da652c991a25e9083e4fc6
Moffat Sehudi <msehudi@gmail.com>
2018-12-10 13:28:56 -0800

    LCSD-312

--- cllc-public-app/ClientApp/src/app/assosiate-wizard/assosiate-wizard.component.ts
+++ cllc-public-app/ClientApp/src/app/assosiate-wizard/assosiate-wizard.component.ts
@@ -3,14 +3,50 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-assosiate-wizard',
   templateUrl: './assosiate-wizard.component.html',
-  styleUrls: ['./assosiate-wizard.component.css']
+  styleUrls: ['./assosiate-wizard.component.scss']
 })
 export class AssosiateWizardComponent implements OnInit {
   currentQuestion = 'q1';
+  questionHistory: string[] = [];
   answerCollection: any = {};
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  getOrganizationSection(answer: string): string {
+    let nextQuestion = '';
+    switch (answer) {
+      case 'Public Corporation':
+        nextQuestion = 'q4a';
+        break;
+      case 'Private Corporation':
+        nextQuestion = 'q5a';
+        break;
+      case 'Limited Partnership':
+        nextQuestion = 'q6a';
+        break;
+      case 'General Partnership':
+        nextQuestion = 'q7a';
+        break;
+      case 'Limited Liability Partnership':
+        nextQuestion = 'q8a';
+        break;
+      case 'Society':
+        nextQuestion = 'q9a';
+        break;
+    }
+    return nextQuestion;
+  }
+
+  setNextQuestion(question: string) {
+    this.questionHistory.push(this.currentQuestion);
+    this.currentQuestion = question;
+  }
+
+  goToPrevious() {
+    this.currentQuestion = this.questionHistory.pop();
   }
 
 }
