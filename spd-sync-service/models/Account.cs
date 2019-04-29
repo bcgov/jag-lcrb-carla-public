4d3d8119546a13308c7b0525ff12d0cf8fb994f5
George Walker <george@walkersoftware.ca>
2019-04-29 16:03:38 -0700

    LCSD-1199 SPICE model

---
+++ spd-sync-service/models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpdSync.models
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+}
