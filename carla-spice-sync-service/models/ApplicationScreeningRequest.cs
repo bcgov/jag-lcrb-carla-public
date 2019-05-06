05b60dae2c1e18b5dcdd79ebd9a52e748ae19ceb
Brian Richter <brianorwhatever@gmail.com>
2019-05-06 13:55:39 -0700

    added application screenings

--- carla-spice-sync-service/models/ApplicationScreeningRequest.cs
+++ carla-spice-sync-service/models/ApplicationScreeningRequest.cs
@@ -12,25 +12,19 @@ namespace SpdSync.models
             Cannabis = 525840001,
             ESS = 525840000
         }
-
-        public string Name { get; set; }
-        public string JobNumber { get; set; }
-        public string ApplicantName { get; set; }
-        public string BCeIDNumber { get; set; }
-        public Address Address { get; set; }
-        public Contact ContactPerson { get; set; }
-        public Contact ApplyingPerson { get; set; }
-
         [JsonConverter(typeof(StringEnumConverter))]
         public Spice_ApplicantType ApplicantType { get; set; }
         public bool UrgentPriority { get; set; }
-
-        public DateTimeOffset DateSent { get; set; }
-
-        public Account Account { get; set; }
-
+        public string Name { get; set; }
+        public string RecordIdentifier { get; set; }
+        public string ApplicantName { get; set; }
+        public string BCeIDNumber { get; set; }
+        public Account ApplicantAccount { get; set; }
+        public Address BusinessAddress { get; set; }
         public Establishment Establishment { get; set; }
-
+        public Contact ContactPerson { get; set; }
+        public Contact ApplyingPerson { get; set; }
+        public DateTimeOffset DateSent { get; set; }
         public List<LegalEntity> Associates { get; set; }
     }
 }
