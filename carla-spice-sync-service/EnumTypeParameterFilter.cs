26c3e320e8487a9700b9d56b4bc306b0fd2f527d
George Walker <george@walkersoftware.ca>
2019-05-09 13:50:35 -0700

    Update Spice Sync

---
+++ carla-spice-sync-service/EnumTypeParameterFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Gov.Lclb.Cllb.SpdSync
+{
+    public class EnumTypeParameterFilter : IParameterFilter
+    {
+        public void Apply(IParameter parameter, ParameterFilterContext context)
+        {
+            var type = context.ApiParameterDescription.Type;
+
+            if (type.IsEnum)
+            {
+                parameter.Extensions.Add("x-ms-enum", new { name = type.Name, modelAsString = false });
+            }
+        }
+    }
+}
