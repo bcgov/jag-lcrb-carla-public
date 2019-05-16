64d86e56dff6e0cb87b24d61756a1311f45ced13
Moffat Sehudi <msehudi@gmail.com>
2019-05-16 13:11:30 -0700

    file rename

--- cllc-public-app/ClientApp/src/app/services/legal-entity-data.service.ts
+++ cllc-public-app/ClientApp/src/app/services/legal-entity-data.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { debounce, catchError } from 'rxjs/operators';
 import { DataService } from './data.service';
-import { LegalEntity } from '@appmodels/legal-entities.model';
+import { LegalEntity } from '@appmodels/legal-entity.model';
 
 @Injectable()
 export class LegalEntityDataService extends DataService {
