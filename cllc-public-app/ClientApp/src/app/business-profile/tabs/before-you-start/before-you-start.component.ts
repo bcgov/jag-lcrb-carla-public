7f2c78f0d5f5de6817f7e57976c75fa555b59f48
Moffat Sehudi <msehudi@gmail.com>
2019-05-23 08:36:19 -0700

    Marketer dashboard update

--- cllc-public-app/ClientApp/src/app/business-profile/tabs/before-you-start/before-you-start.component.ts
+++ cllc-public-app/ClientApp/src/app/business-profile/tabs/before-you-start/before-you-start.component.ts
@@ -1,5 +1,5 @@
 
-import {filter} from 'rxjs/operators';
+import {filter, takeWhile} from 'rxjs/operators';
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { UserDataService } from '../../../services/user-data.service';
 import { User } from '../../../models/user.model';
@@ -8,30 +8,27 @@ import { DynamicsDataService } from '../../../services/dynamics-data.service';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../app-state/models/app-state';
 import { Subscription } from 'rxjs';
+import { FormBase } from '@shared/form-base';
 
 @Component({
   selector: 'app-before-you-start',
   templateUrl: './before-you-start.component.html',
   styleUrls: ['./before-you-start.component.scss']
 })
-export class BeforeYouStartComponent implements OnInit, OnDestroy {
+export class BeforeYouStartComponent extends FormBase implements OnInit, OnDestroy {
   @Input() accountId: string;
   @Input() businessType: string;
-  subscriptions: Subscription[] = [];
 
-  constructor(private store: Store<AppState>, private route: ActivatedRoute) { }
+  constructor(private store: Store<AppState>, private route: ActivatedRoute) {
+    super();
+   }
 
   ngOnInit() {
-    const sub = this.store.select(state => state.currentAccountState).pipe(
-      filter(account => !!account))
+    this.store.select(state => state.currentAccountState)
+    .pipe(takeWhile(() => this.componentActive))
+    .pipe(filter(account => !!account))
       .subscribe(account => {
         this.businessType = account.currentAccount.businessType;
       });
-      this.subscriptions.push(sub);
-
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 }
