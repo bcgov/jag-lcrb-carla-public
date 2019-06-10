d99705b9d6ae4639e8aac2553d42ea7fdfd39a0d
Moffat Sehudi <msehudi@gmail.com>
2019-06-10 13:00:54 -0700

    save birthdate to contact

--- cllc-interfaces/Dynamics-Autorest/ModelExtensions/MicrosoftDynamicsCRMcontact.cs
+++ cllc-interfaces/Dynamics-Autorest/ModelExtensions/MicrosoftDynamicsCRMcontact.cs
@@ -1,17 +1,31 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace Gov.Lclb.Cllb.Interfaces.Models
 {
-    using Newtonsoft.Json;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Linq;
 
     public partial class MicrosoftDynamicsCRMcontact
     {
-        
+
         /// <summary>
         /// </summary>
         [JsonProperty(PropertyName = "parentcustomerid_account@odata.bind")]
         public string ParentCustomerIdAccountODataBind { get; set; }
 
+
+        /// <summary>
+        /// </summary>
+        [JsonProperty(PropertyName = "birthdate")]
+        [JsonConverter(typeof(DateFormatConverter), "yyyy-MM-dd")]
+        public System.DateTimeOffset? Birthdate { get; set; }
+
+    }
+
+    public class DateFormatConverter : IsoDateTimeConverter
+    {
+        public DateFormatConverter(string format)
+        {
+            DateTimeFormat = format;
+        }
     }
 }
