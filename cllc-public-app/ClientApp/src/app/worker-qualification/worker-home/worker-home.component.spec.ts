62f4cdb0c10bd9de4027b8a1dc7a2bffaaf5ffa9
Moffat Sehudi <msehudi@gmail.com>
2019-06-12 11:25:37 -0700

    more tests

--- cllc-public-app/ClientApp/src/app/worker-qualification/worker-home/worker-home.component.spec.ts
+++ cllc-public-app/ClientApp/src/app/worker-qualification/worker-home/worker-home.component.spec.ts
@@ -2,17 +2,38 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WorkerHomeComponent } from './worker-home.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { PolicyDocumentDataService } from '@app/services/policy-document-data.service';
+import { of } from 'rxjs';
+import { ActivatedRouteStub } from '@app/testing/activated-route-stub';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { PolicyDocument } from '@models/policy-document.model';
+import { PolicyDocumentComponent } from '@app/policy-document/policy-document.component';
+
+
+const PolicyDocumentDataServiceStub: Partial<PolicyDocumentDataService> = {
+  getPolicyDocument: () => of(null)
+};
+const activatedRouteStub: ActivatedRouteStub = new ActivatedRouteStub({});
+const dialogStub: Partial<MatDialog> = {};
 
 describe('WorkerHomeComponent', () => {
   let component: WorkerHomeComponent;
   let fixture: ComponentFixture<WorkerHomeComponent>;
 
   beforeEach(async(() => {
+    activatedRouteStub.data = of(null);
+
     TestBed.configureTestingModule({
-      declarations: [ WorkerHomeComponent ],
+      declarations: [WorkerHomeComponent, PolicyDocumentComponent],
+      providers: [
+        { provide: MatDialog, useValue: dialogStub },
+        { provide: ActivatedRoute, useValue: activatedRouteStub },
+        { provide: PolicyDocumentDataService, useValue: PolicyDocumentDataServiceStub }
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
