5f5b793cd28d68f41454fd7456b8f8b2986f3d54
Moffat Sehudi <msehudi@gmail.com>
2019-06-12 11:25:37 -0700

    more tests

--- cllc-public-app/ClientApp/src/app/insert/insert.component.ts
+++ cllc-public-app/ClientApp/src/app/insert/insert.component.ts
@@ -1,6 +1,7 @@
 import {
   Component, ComponentFactoryResolver, HostBinding, Input, OnInit, ReflectiveInjector,
-  ViewContainerRef, ViewChild } from '@angular/core';
+  ViewContainerRef, ViewChild
+} from '@angular/core';
 import { InsertService } from './insert.service';
 // each component which will be dynamically inserted must be imported here
 import { StaticComponent } from '../static/static.component';
@@ -19,51 +20,51 @@ export const INSERT_TYPES = {
 })
 export class InsertComponent implements OnInit {
   @ViewChild('container', { read: ViewContainerRef }) container: ViewContainerRef;
-  currentInsert : any = null;
+  currentInsert: any = null;
 
-  @Input() id : string;
-  @HostBinding('class.insert-hidden') hidden: boolean = true;
+  @Input() id: string;
+  @HostBinding('class.insert-hidden') hidden = true;
 
   constructor(
     private resolver: ComponentFactoryResolver,
     private insertService: InsertService) { }
 
   ngOnInit() {
-    if(this.id) {
+    if (this.id) {
       // register with global 'insert' service so the active component can update us
       this.insertService.registerInsert(this.id, this);
     }
   }
 
-  @Input() set componentSpec(spec: {type: string, inputs?: any, options?: any}) {
-    let compCls = spec && INSERT_TYPES[spec.type];
-    if(! compCls) {
+  @Input() set componentSpec(spec: { type: string, inputs?: any, options?: any }) {
+    const compCls = spec && INSERT_TYPES[spec.type];
+    if (!compCls) {
       this.hidden = true;
       return;
     }
 
     // Inputs need to be in the following format to be resolved properly
-    let inputProviders = Object.keys(spec.inputs).map((inputName) => {
-      return {provide: inputName, useValue: spec.inputs[inputName]};
+    const inputProviders = Object.keys(spec.inputs).map((inputName) => {
+      return { provide: inputName, useValue: spec.inputs[inputName] };
     });
-    let resolvedInputs = ReflectiveInjector.resolve(inputProviders);
+    const resolvedInputs = ReflectiveInjector.resolve(inputProviders);
 
     // We create an injector out of the data we want to pass down and this components injector
-    let injector = ReflectiveInjector.fromResolvedProviders(
+    const injector = ReflectiveInjector.fromResolvedProviders(
       resolvedInputs,
       this.container.parentInjector);
 
     // We create a factory out of the component we want to create
-    let factory = this.resolver.resolveComponentFactory(compCls);
+    const factory = this.resolver.resolveComponentFactory(compCls);
 
     // We create the component using the factory and the injector
-    let component = factory.create(injector);
+    const component = factory.create(injector);
 
     // We insert the component into the dom container
     this.container.insert(component.hostView);
 
     // Destroy the previously created component
-    if(this.currentInsert) {
+    if (this.currentInsert) {
       this.currentInsert.destroy();
     }
 
