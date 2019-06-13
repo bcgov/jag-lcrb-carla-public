064f17823feed7f28054769ae48357350dedb1ed
Moffat Sehudi <msehudi@gmail.com>
2019-06-13 10:01:12 -0700

    unit tests improvements

--- cllc-public-app/ClientApp/src/app/vote/vote.component.spec.ts
+++ cllc-public-app/ClientApp/src/app/vote/vote.component.spec.ts
@@ -1,10 +1,15 @@
-// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
 import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
-import { BrowserModule, By } from "@angular/platform-browser";
+import { BrowserModule, By } from '@angular/platform-browser';
 import { VoteComponent } from './vote.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { VoteDataService } from '@services/vote-data.service';
 
 let component: VoteComponent;
 let fixture: ComponentFixture<VoteComponent>;
+const coockieServiceStub: Partial<CookieService> = {};
+const voteDataServiceStub: Partial<VoteDataService> = {};
+
 
 describe('vote component', () => {
     beforeEach(async(() => {
@@ -12,8 +17,11 @@ describe('vote component', () => {
             declarations: [ VoteComponent ],
             imports: [ BrowserModule ],
             providers: [
+                { provide: VoteDataService, useValue: voteDataServiceStub },
+                { provide: CookieService, useValue: coockieServiceStub },
                 { provide: ComponentFixtureAutoDetect, useValue: true }
-            ]
+            ],
+            schemas: [NO_ERRORS_SCHEMA]
         });
         fixture = TestBed.createComponent(VoteComponent);
         component = fixture.componentInstance;
