34068b3416e21b2fcb4a2443c30ebd56a3dff05a
Moffat Sehudi <msehudi@gmail.com>
2019-06-19 09:41:48 -0700

    LCSD-1152 Update Kubernetes Health Checks

--- cllc-interfaces/Dynamics-Autorest/Extensions/DynamicsHealthCheck.cs
+++ cllc-interfaces/Dynamics-Autorest/Extensions/DynamicsHealthCheck.cs
@@ -4,7 +4,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Gov.Lclb.Cllb.Public.HealthCheck.Extension
+namespace Gov.Lclb.Cllb.Interfaces
 {
     public class DynamicsHealthCheck : IHealthCheck
     {
