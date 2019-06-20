1de4c36d02f1d7b8b9224ac0a2c6e321bcc1662d
Moffat Sehudi <msehudi@gmail.com>
2019-06-20 12:21:33 -0700

    filter json 200 response

--- cllc-public-app/CustomLogger.cs
+++ cllc-public-app/CustomLogger.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -16,14 +17,16 @@ namespace Gov.Lclb.Cllb.Public
     public class CllcConsoleLoggerProvider : ILoggerProvider
     {
         private readonly CllcConsoleLoggerConfiguration _config;
+        private readonly IHostingEnvironment _env;
         private readonly ConcurrentDictionary<string, CllcConsoleLogger> _loggers = new ConcurrentDictionary<string, CllcConsoleLogger>();
-        public CllcConsoleLoggerProvider(CllcConsoleLoggerConfiguration config)
+        public CllcConsoleLoggerProvider(CllcConsoleLoggerConfiguration config, IHostingEnvironment env)
         {
             _config = config;
+            _env = env;
         }
         public ILogger CreateLogger(string categoryName)
         {
-            return _loggers.GetOrAdd(categoryName, name => new CllcConsoleLogger(name, _config));
+            return _loggers.GetOrAdd(categoryName, name => new CllcConsoleLogger(name, _config, _env));
         }
         public void Dispose()
         {
@@ -35,10 +38,12 @@ namespace Gov.Lclb.Cllb.Public
         private static object _lock = new Object();
         private readonly string _name;
         private readonly CllcConsoleLoggerConfiguration _config;
-        public CllcConsoleLogger(string name, CllcConsoleLoggerConfiguration config)
+        private readonly IHostingEnvironment _env;
+        public CllcConsoleLogger(string name, CllcConsoleLoggerConfiguration config, IHostingEnvironment env)
         {
             _name = name;
             _config = config;
+            _env = env;
         }
         public IDisposable BeginScope<TState>(TState state)
         {
@@ -56,9 +61,12 @@ namespace Gov.Lclb.Cllb.Public
             }
             lock (_lock)
             {
-                if ((_config.EventId == 0 || _config.EventId == eventId.Id) && !state.ToString().Contains("cannabislicensing/hc"))
+                //filter log messages here (created to filter health check logs)
+                if ((_config.EventId == 0 || _config.EventId == eventId.Id) 
+                    && !state.ToString().Contains("/hc") 
+                    && (!state.ToString().Contains("200 application/json") || !_env.IsProduction()))
                 {
-                    Console.WriteLine($"{_name}:{logLevel.ToString()}: {formatter(state, exception)}");
+                    Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {logLevel.ToString()}:  {_name}:\n\t {formatter(state, exception)}");
                 }
             }
         }
