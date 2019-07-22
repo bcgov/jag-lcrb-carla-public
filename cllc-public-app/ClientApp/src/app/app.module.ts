14cfc50b7f3697bba30e272e398ac5605f56bc46
Moffat Sehudi <msehudi@gmail.com>
2019-07-22 16:34:51 -0700

    associate content partial

--- cllc-public-app/ClientApp/src/app/app.module.ts
+++ cllc-public-app/ClientApp/src/app/app.module.ts
@@ -58,7 +58,7 @@ import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { DynamicsDataService } from './services/dynamics-data.service';
 import { DynamicsFormComponent } from './dynamics-form/dynamics-form.component';
 import {
-  EditShareholdersComponent,
+  EditShareholdersComponent, ShareholderDialogComponent,
 } from './account-profile/tabs/shareholders/shareholders.component';
 import { FormViewerComponent } from './form-viewer/form-viewer.component';
 import { InsertComponent } from './insert/insert.component';
@@ -97,7 +97,7 @@ import { FinancialInformationComponent } from './account-profile/tabs/financial-
 import { AccountProfileSummaryComponent } from './account-profile/account-profile-summary/account-profile-summary.component';
 
 import { NgBusyModule } from 'ng-busy';
-import { KeyPersonnelComponent } from './account-profile/tabs/key-personnel/key-personnel.component';
+import { KeyPersonnelComponent, KeyPersonnelDialogComponent } from './account-profile/tabs/key-personnel/key-personnel.component';
 import { ConnectionToProducersComponent } from './account-profile/tabs/connection-to-producers/connection-to-producers.component';
 import { LicenseApplicationComponent } from './license-application/license-application.component';
 import { PaymentConfirmationComponent } from './payment-confirmation/payment-confirmation.component';
@@ -243,8 +243,10 @@ import { LegalEntityComponent } from './account-profile/tabs/legal-entity/legal-
     AppRemoveIfFeatureOffDirective,
     AssociateContentComponent,
     ConnectionToNonMedicalStoresComponent,
+    KeyPersonnelDialogComponent,
     AssociatePageComponent,
-    LegalEntityComponent
+    LegalEntityComponent,
+    ShareholderDialogComponent
   ],
   imports: [
     ChartsModule,
@@ -381,7 +383,9 @@ import { LegalEntityComponent } from './account-profile/tabs/legal-entity/legal-
   entryComponents: [
     ApplicationCancellationDialogComponent,
     DirectorAndOfficerPersonDialogComponent,
+    KeyPersonnelDialogComponent,
     WorkerHomeDialogComponent,
+    ShareholderDialogComponent,
   ],
   bootstrap: [AppComponent]
 })
