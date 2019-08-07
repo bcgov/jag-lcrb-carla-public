356f4e5c33e272b204e494662412cedf10cf82fd
George Walker <george@walkersoftware.ca>
2019-08-07 11:08:58 -0700

    Code cleanup, ensure that SharePointFileManager is not a singleton

--- cllc-public-app/Controllers/LogoutController.cs
+++ cllc-public-app/Controllers/LogoutController.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using Gov.Lclb.Cllb.Public.Authentication;
-using Gov.Lclb.Cllb.Public.Contexts;
+﻿using Gov.Lclb.Cllb.Public.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace Gov.Lclb.Cllb.Public.Controllers
 {
     [Route("[controller]")]
     public class LogoutController : Controller
     {
-        private readonly IConfiguration Configuration;
-        private readonly AppDbContext db;
+        private readonly IConfiguration Configuration;        
         private readonly IHostingEnvironment _env;
         private readonly SiteMinderAuthOptions _options = new SiteMinderAuthOptions();
 
-        public LogoutController(AppDbContext db, IConfiguration configuration, IHostingEnvironment env)
+        public LogoutController(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
-            _env = env;
-            this.db = db;            
+            _env = env;                        
         }
 
         [HttpGet]
