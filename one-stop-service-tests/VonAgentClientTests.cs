0e1c88bccac0ec107b6928154aedfbbaba580015
Brian Richter <brianorwhatever@gmail.com>
2019-08-14 13:03:51 -0700

    von agent tests

--- one-stop-service-tests/VonAgentClientTests.cs
+++ one-stop-service-tests/VonAgentClientTests.cs
@@ -16,20 +16,20 @@ namespace one_stop_service_tests
         public async Task TestCreateLicenceCredential()
         {
             var mockHttp = new MockHttpMessageHandler();
-            mockHttp.When("http://localhost/lcrb/issue-credential")
+            var request = mockHttp.When("http://localhost/lcrb/issue-credential")
                     .Respond("application/json", "[{'sucess': true, 'result': '123-123-123', 'served_by': 'django-83'}]");
             var httpClient = new HttpClient(mockHttp);
 
             var mock = new Mock<ILogger<VonAgentClient>>();
             ILogger<VonAgentClient> logger = mock.Object;
-            
+            var establishment = new MicrosoftDynamicsCRMadoxioEstablishment()
+            {
+                AdoxioName = "Cannabis Establishment"
+            };
             MicrosoftDynamicsCRMadoxioLicences licence = new MicrosoftDynamicsCRMadoxioLicences()
             {
                 AdoxioLicencenumber = "400",
-                AdoxioEstablishment = new MicrosoftDynamicsCRMadoxioEstablishment()
-                {
-                    AdoxioName = "Cannabis Establishment"
-                },
+                AdoxioEstablishment = establishment,
                 AdoxioEffectivedate = DateTime.UtcNow,
                 AdoxioExpirydate = DateTime.Parse("2019-12-27T07:50:11.455516-07:00"),
                 AdoxioEstablishmentaddressstreet = "159 Mary Jane Lane",
@@ -37,11 +37,11 @@ namespace one_stop_service_tests
                 AdoxioEstablishmentaddresspostalcode = "V9A4V1"
             };
 
-            var subject = new VonAgentClient(httpClient, logger, "cannabis", "1", "http://localhost");
+            var subject = new VonAgentClient(httpClient, logger, "cannabis", "1", "http://localhost/");
 
             await subject.CreateLicenceCredential(licence, "BC1234567");
 
-            
+            Assert.Equal(1, mockHttp.GetMatchCount(request));
         }
     }
 }
