b40f3e626aacd7d80c363edf40ab530652fee2ec
George Walker <george@walkersoftware.ca>
2019-08-26 16:05:35 -0700

    LCSD-1741 Extra logging

--- cllc-public-app/Utils/LoggingEvents.cs
+++ cllc-public-app/Utils/LoggingEvents.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Gov.Lclb.Cllb.Public.Utils
@@ -21,5 +23,44 @@ namespace Gov.Lclb.Cllb.Public.Utils
 
         public const int Error = 5000;
         public const int BadRequest = 5001;
+
+        public static string GetHeaders (HttpRequest request)
+        {
+            StringBuilder html = new StringBuilder();
+            html.AppendLine("<html>");
+            html.AppendLine("<body>");
+            html.AppendLine("<b>Request Headers:</b>");
+            html.AppendLine("<ul style=\"list-style-type:none\">");
+            foreach (var item in request.Headers)
+            {
+                html.AppendFormat("<li><b>{0}</b> = {1}</li>\r\n", item.Key, ExpandValue(item.Value));
+            }
+            html.AppendLine("</ul>");
+            html.AppendLine("</body>");
+            html.AppendLine("</html>");
+            return html.ToString();
+        }
+
+        /// <summary>
+        /// Utility function used to expand headers.
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        private static string ExpandValue(IEnumerable<string> values)
+        {
+            StringBuilder value = new StringBuilder();
+
+            foreach (string item in values)
+            {
+                if (value.Length > 0)
+                {
+                    value.Append(", ");
+                }
+                value.Append(item);
+            }
+            return value.ToString();
+        }
+
+
     }
 }
