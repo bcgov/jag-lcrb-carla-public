5817eb076d46a36b23d8c9d308a98c1e3fc4e82f
Brian Richter <brianorwhatever@gmail.com>
2019-09-12 13:32:44 -0700

    fixed SPD job id

--- carla-spice-sync-service/models/CompletedApplicationScreening.cs
+++ carla-spice-sync-service/models/CompletedApplicationScreening.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Gov.Lclb.Cllb.CarlaSpiceSync
 {
     public class CompletedApplicationScreening
     {
         public string RecordIdentifier { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
         public SpiceApplicationStatus Result { get; set; }
         public List<Associate> Associates { get; set; }
     }
