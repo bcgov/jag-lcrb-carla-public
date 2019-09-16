254d3951117619571d1337411b724a75adbe828a
Moffat Sehudi <msehudi@gmail.com>
2019-09-16 14:08:20 -0700

    product inventory package partial

---
+++ cllc-public-app/ClientApp/src/app/shared/product-inventory-packaged/product-inventory-packaged.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-inventory-packaged',
+  templateUrl: './product-inventory-packaged.component.html',
+  styleUrls: ['./product-inventory-packaged.component.scss']
+})
+export class ProductInventoryPackagedComponent implements OnInit {
+  @Input() displayedColumns = [ 'rowLabel', 'seeds', 'vegetativeCannabisPlants', 'freshCannabis', 'driedCannabis', 'ediblesSolids',
+    'ediblesNonSolids', 'inhaledExtracts', 'ingestedExtracts', 'otherExtracts', 'tropicals', 'other'];
+  @Input() dataSource = [{rowLabel: 'Quantity received - domestic', seeds: 2},
+  {rowLabel: 'Quantity received - returns', seeds: 1},
+  {rowLabel: 'Other', seeds: 20},
+];
+  @Input() showTotals = true;
+  @Input() totalsLabel = 'Total';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getTotalCost(colName: string) {
+    return this.dataSource.map(t => (t[colName] || 0)).reduce((acc, value) => acc + value, 0);
+  }
+
+}
