e8bdb4b30194e20377274e50e9ba9a406bfbd30c
Moffat Sehudi <msehudi@gmail.com>
2019-09-27 11:30:33 -0700

    LCSD-1927 Add store phone number and email to Plan Store Opening Page

--- cllc-public-app/Models.Extensions/License.cs
+++ cllc-public-app/Models.Extensions/License.cs
@@ -28,7 +28,10 @@ namespace Gov.Lclb.Cllb.Public.Models
             if (adoxioEstablishmentId != null)
             {
                 var establishment = dynamicsClient.Establishments.GetByKey(adoxioEstablishmentId.ToString());
+                adoxioLicenseVM.establishmentId = establishment.AdoxioEstablishmentid;
                 adoxioLicenseVM.establishmentName = establishment.AdoxioName;
+                adoxioLicenseVM.establishmentEmail = establishment.AdoxioEmail;
+                adoxioLicenseVM.establishmentPhone = establishment.AdoxioPhone;
                 adoxioLicenseVM.establishmentAddress = establishment.AdoxioAddressstreet
                                                     + ", " + establishment.AdoxioAddresscity
                                                     + " " + establishment.AdoxioAddresspostalcode;
