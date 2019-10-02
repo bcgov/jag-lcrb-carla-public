6e012115eac471181e83b65a71d9e0c92cc519ff
Moffat Sehudi <msehudi@gmail.com>
2019-10-02 15:24:48 -0700

    licensee changes partial

--- cllc-public-app/ClientApp/src/app/shared/licensee-tree/licensee-tree.component.ts
+++ cllc-public-app/ClientApp/src/app/shared/licensee-tree/licensee-tree.component.ts
@@ -1,142 +1,111 @@
-import { Component, OnInit, Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, merge } from 'rxjs';
-import { FlatTreeControl } from '@angular/cdk/tree';
-import { CollectionViewer, SelectionChange } from '@angular/cdk/collections';
-import { map } from 'rxjs/operators';
+import { Component, OnInit, Injectable, ViewChild, OnDestroy } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource, MatTree } from '@angular/material/tree';
+import { Store } from '@ngrx/store';
+import { AppState } from '@app/app-state/models/app-state';
+import { takeWhile } from 'rxjs/operators';
+import { Account } from '@models/account.model';
 
 
-
-
-export class DynamicFlatNode {
-  constructor(public item: string, public level = 1, public expandable = false,
-    public isLoading = false) { }
-}
 
 /**
- * Database for dynamic data. When expanding a node in the tree, the data source will need to fetch
- * the descendants data from the database.
+ * Food; data; with nested structure.
+ * Each; node; has; a; name; and; an; optiona; list; of; children.
  */
-export class DynamicDatabase {
-  dataMap = new Map<string, string[]>([
-    ['Fruits', ['Apple', 'Orange', 'Banana']],
-    ['Vegetables', ['Tomato', 'Potato', 'Onion']],
-    ['Apple', ['Fuji', 'Macintosh']],
-    ['Onion', ['Yellow', 'White', 'Purple']]
-  ]);
-
-  rootLevelNodes: string[] = ['Fruits', 'Vegetables'];
-
-  /** Initial data from database */
-  initialData(): DynamicFlatNode[] {
-    return this.rootLevelNodes.map(name => new DynamicFlatNode(name, 0, true));
-  }
-
-  getChildren(node: string): string[] | undefined {
-    return this.dataMap.get(node);
-  }
-
-  isExpandable(node: string): boolean {
-    return this.dataMap.has(node);
-  }
+interface FoodNode {
+  name: string;
+  newName?: string;
+  children?: FoodNode[];
+  isNew?: boolean;
+  deleted?: boolean;
+  edited?: boolean;
 }
-/**
- * File database, it can build a tree structured Json object from string.
- * Each node in Json object represents a file or a directory. For a file, it has filename and type.
- * For a directory, it has filename and children (a list of files or directories).
- * The input will be a json object string, and the output is a list of `FileNode` with nested
- * structure.
- */
-@Injectable()
-export class DynamicDataSource {
 
-  dataChange = new BehaviorSubject<DynamicFlatNode[]>([]);
-
-  get data(): DynamicFlatNode[] { return this.dataChange.value; }
-  set data(value: DynamicFlatNode[]) {
-    this._treeControl.dataNodes = value;
-    this.dataChange.next(value);
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Parent Company',
+    children: [
+      {
+        name: 'Shareholder Company Alpha',
+        children: [
+          {
+            name: 'Shareholder Company Beta',
+            children: [
+              { name: 'Shareholder George' },
+              { name: 'Shareholder George\'s Wife' },
+            ]
+          },
+          {
+            name: 'Shareholder Company Gamma',
+            children: [
+              { name: 'Shareholder Carlos' },
+              { name: 'Shareholder Carlos¿s Wife' },
+            ]
+          },
+        ]
+      },
+    ]
   }
+];
 
-  constructor(private _treeControl: FlatTreeControl<DynamicFlatNode>,
-    private _database: DynamicDatabase) { }
-
-  connect(collectionViewer: CollectionViewer): Observable<DynamicFlatNode[]> {
-    this._treeControl.expansionModel.onChange.subscribe(change => {
-      if ((change as SelectionChange<DynamicFlatNode>).added ||
-        (change as SelectionChange<DynamicFlatNode>).removed) {
-        this.handleTreeControl(change as SelectionChange<DynamicFlatNode>);
-      }
-    });
-
-    return merge(collectionViewer.viewChange, this.dataChange).pipe(map(() => this.data));
-  }
-
-  /** Handle expand/collapse behaviors */
-  handleTreeControl(change: SelectionChange<DynamicFlatNode>) {
-    if (change.added) {
-      change.added.forEach(node => this.toggleNode(node, true));
-    }
-    if (change.removed) {
-      change.removed.slice().reverse().forEach(node => this.toggleNode(node, false));
-    }
-  }
-
-  /**
-   * Toggle the node, remove from display list
-   */
-  toggleNode(node: DynamicFlatNode, expand: boolean) {
-    const children = this._database.getChildren(node.item);
-    const index = this.data.indexOf(node);
-    if (!children || index < 0) { // If no children, or cannot find the node, no op
-      return;
-    }
-
-    node.isLoading = true;
-
-    setTimeout(() => {
-      if (expand) {
-        const nodes = children.map(name =>
-          new DynamicFlatNode(name, node.level + 1, this._database.isExpandable(name)));
-        this.data.splice(index + 1, 0, ...nodes);
-      } else {
-        let count = 0;
-        for (let i = index + 1; i < this.data.length
-          && this.data[i].level > node.level; i++ , count++) { }
-        this.data.splice(index + 1, count);
-      }
-
-      // notify the change
-      this.dataChange.next(this.data);
-      node.isLoading = false;
-    }, 1000);
-  }
-}
+// TREE_DATA[0].children[0].children[0] = TREE_DATA[0];
 
 @Component({
   selector: 'app-licensee-tree',
   templateUrl: './licensee-tree.component.html',
   styleUrls: ['./licensee-tree.component.scss'],
-  providers: [DynamicDatabase]
 })
 export class LicenseeTreeComponent implements OnInit {
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+  @ViewChild('tree') tree: MatTree<any>;
+  componentActive = true;
+  account: Account;
 
-  constructor(database: DynamicDatabase) {
-    this.treeControl = new FlatTreeControl<DynamicFlatNode>(this.getLevel, this.isExpandable);
-    this.dataSource = new DynamicDataSource(this.treeControl, database);
-
-    this.dataSource.data = database.initialData();
+  constructor(private store: Store<AppState>) {
+    this.dataSource.data = TREE_DATA;
   }
 
-  treeControl: FlatTreeControl<DynamicFlatNode>;
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
 
-  dataSource: DynamicDataSource;
+  ngOnInit() {
+    this.store.select(state => state.currentAccountState.currentAccount)
+      .pipe(takeWhile(() => this.componentActive))
+      .subscribe(account => {
+        this.account = account;
+      });
+  }
 
-  getLevel = (node: DynamicFlatNode) => node.level;
+  editAssociate(node) {
+    node.name = node.name.toUpperCase();
+    node.edited = true;
+  }
 
-  isExpandable = (node: DynamicFlatNode) => node.expandable;
+  addAssociate(node) {
+    node.children = node.children || [];
+    node.children.push({
+      name: node.name + ' - child - ' + node.children.length,
+      isNew: true
+    });
+    this.refreshTree();
+  }
 
-  hasChild = (_: number, _nodeData: DynamicFlatNode) => _nodeData.expandable;
+  deleteAssociate(node) {
+    node.deleted = true;
+    const children = node.children || [];
+    children.forEach(element => {
+      this.deleteAssociate(element);
+    });
+    this.refreshTree();
+  }
 
-  ngOnInit() { }
+  refreshTree() {
+    const data = [...this.dataSource.data];
+    this.dataSource.data = [];
+    this.dataSource.data = data;
+  }
 
+  OnDestroy() {
+    this.componentActive = false;
+  }
 }
