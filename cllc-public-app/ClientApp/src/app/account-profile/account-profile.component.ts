305dee0f0cbaf65d6c6d483c332556da2b824284
Moffat Sehudi <msehudi@gmail.com>
2019-10-02 15:44:06 -0700

    LCSD-1959 Registration ID validation does not allow extra provincial companies

--- cllc-public-app/ClientApp/src/app/account-profile/account-profile.component.ts
+++ cllc-public-app/ClientApp/src/app/account-profile/account-profile.component.ts
@@ -241,7 +241,7 @@ export class AccountProfileComponent extends FormBase implements OnInit {
 
         if (this.account.isPrivateCorporation()) {
           this.form.get('businessProfile.bcIncorporationNumber')
-          .setValidators([Validators.pattern('^BC[0-9][0-9][0-9][0-9][0-9][0-9][0-9]$')]);
+          .setValidators([Validators.pattern('^[A-Za-z][A-Za-z][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$')]);
         } else if (this.account.businessType === 'Society') {
           this.form.get('businessProfile.bcIncorporationNumber')
           .setValidators([Validators.pattern('^S[0-9][0-9][0-9][0-9][0-9][0-9][0-9]$')]);
