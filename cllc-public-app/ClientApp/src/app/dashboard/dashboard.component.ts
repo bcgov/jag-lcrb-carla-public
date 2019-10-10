50d2f30b57dc6a1f5db1847c421a5131ab2a9efb
Moffat Sehudi <msehudi@gmail.com>
2019-10-10 10:00:59 -0700

    Licensee changes partial

--- cllc-public-app/ClientApp/src/app/dashboard/dashboard.component.ts
+++ cllc-public-app/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,11 @@ import { Store } from '@ngrx/store';
 import { takeWhile } from 'rxjs/operators';
 import { FormBase } from '@shared/form-base';
 import { Account } from '@models/account.model';
+import { Application } from '@models/application.model';
+import { ApplicationType, ApplicationTypeNames } from '@models/application-type.model';
+import { ApplicationDataService } from '@services/application-data.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,7 +19,10 @@ export class DashboardComponent extends FormBase implements OnInit {
   account: Account;
   indigenousNationModeActive: boolean;
 
-  constructor(private store: Store<AppState>) {
+  constructor(private store: Store<AppState>,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private applicationDataService: ApplicationDataService) {
     super();
   }
 
@@ -31,4 +39,24 @@ export class DashboardComponent extends FormBase implements OnInit {
         this.indigenousNationModeActive = active;
       });
   }
+
+  startLicenseeChangeApplication() {
+    const newLicenceApplicationData: Application = <Application>{
+      licenseType: 'Leadership Change',
+      applicantType: this.account.businessType,
+      applicationType: <ApplicationType>{ name: ApplicationTypeNames.Marketer },
+      account: this.account,
+    };
+
+    this.applicationDataService.createApplication(newLicenceApplicationData).subscribe(
+      data => {
+        this.router.navigateByUrl(`/account-profile/${data.id}`);
+      },
+      () => {
+        this.snackBar.open('Error starting a New Marketer Application', 'Fail', { duration: 3500, panelClass: ['red-snackbar'] });
+        console.log('Error starting a New Marketer Application');
+      }
+    );
+  }
+
 }
