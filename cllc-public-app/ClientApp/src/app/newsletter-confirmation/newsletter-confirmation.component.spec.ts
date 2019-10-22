80a940819c3ee1a939cd63669963057542fe41aa
Moffat Sehudi <msehudi@gmail.com>
2019-10-22 16:29:39 -0700

    Update angular unit tests

--- cllc-public-app/ClientApp/src/app/newsletter-confirmation/newsletter-confirmation.component.spec.ts
+++ cllc-public-app/ClientApp/src/app/newsletter-confirmation/newsletter-confirmation.component.spec.ts
@@ -6,6 +6,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { NewsletterDataService } from '@services/newsletter-data.service';
 import { ActivatedRouteStub } from '@app/testing/activated-route-stub';
 import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs/internal/observable/of';
 
 let component: NewsletterConfirmationComponent;
 let fixture: ComponentFixture<NewsletterConfirmationComponent>;
@@ -20,7 +21,7 @@ describe('newsletter-confirmation component', () => {
             providers: [
                 { provide: ComponentFixtureAutoDetect, useValue: true },
                 { provide: Router, useValue: routerSpy },
-                { provide: NewsletterDataService, useValue: {} },
+                { provide: NewsletterDataService, useValue: { verifyCode: () => of('')} },
                 { provide: ActivatedRoute, useValue: activatedRouteStub }
             ],
             schemas: [NO_ERRORS_SCHEMA]
@@ -29,7 +30,7 @@ describe('newsletter-confirmation component', () => {
         component = fixture.componentInstance;
     }));
 
-    it('should do something', async(() => {
+    it('should do create', async(() => {
         expect(true).toEqual(true);
     }));
 });
