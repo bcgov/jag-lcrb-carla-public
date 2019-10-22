c27820259dcee012933e88986b54c2f3f5982421
Moffat Sehudi <msehudi@gmail.com>
2019-10-22 16:29:39 -0700

    Update angular unit tests

--- cllc-public-app/ClientApp/src/app/app.component.ts
+++ cllc-public-app/ClientApp/src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent extends FormBase implements OnInit {
   isAssociate = false;
   account: Account;
 
-    constructor(
+  constructor(
     public dialog: MatDialog,
     private renderer: Renderer2,
     private router: Router,
@@ -41,11 +41,11 @@ export class AppComponent extends FormBase implements OnInit {
     private accountDataService: AccountDataService,
     public featureFlagService: FeatureFlagService,
     private versionInfoDataService: VersionInfoDataService,
-    ) {
+  ) {
     super();
     featureFlagService.featureOn('Maps')
       .subscribe(x => this.showMap = x);
-    
+
     this.isDevMode = isDevMode();
     this.router.events
       .pipe(takeWhile(() => this.componentActive))
@@ -76,31 +76,31 @@ export class AppComponent extends FormBase implements OnInit {
         this.businessProfiles = state.legalEntities;
       });
 
-    }
+  }
 
-    loadVersionInfo() {
-        this.versionInfoDataService.getVersionInfo()
-            .pipe(takeWhile(() => this.componentActive))
-            .subscribe((versionInfo: VersionInfo) => {
-                this.versionInfo = versionInfo;
-            });
-    }
+  loadVersionInfo() {
+    this.versionInfoDataService.getVersionInfo()
+      .pipe(takeWhile(() => this.componentActive))
+      .subscribe((versionInfo: VersionInfo) => {
+        this.versionInfo = versionInfo;
+      });
+  }
 
-    openVersionInfoDialog() {
-        // set dialogConfig settings
-        const dialogConfig = {
-            disableClose: true,
-            autoFocus: true,
-            width: '500px',
-            data: this.versionInfo
-        };
+  openVersionInfoDialog() {
+    // set dialogConfig settings
+    const dialogConfig = {
+      disableClose: true,
+      autoFocus: true,
+      width: '500px',
+      data: this.versionInfo
+    };
 
-        // open dialog, get reference and process returned data from dialog
-        const dialogRef = this.dialog.open(VersionInfoDialogComponent, dialogConfig);
-    }
+    // open dialog, get reference and process returned data from dialog
+    const dialogRef = this.dialog.open(VersionInfoDialogComponent, dialogConfig);
+  }
 
 
-    reloadUser() {
+  reloadUser() {
     this.store.select(state => state.currentUserState.currentUser)
       .pipe(takeWhile(() => this.componentActive))
       .subscribe((data: User) => {
@@ -122,11 +122,11 @@ export class AppComponent extends FormBase implements OnInit {
   }
 
   showBceidTermsOfUse(): boolean {
-    const result =  (this.currentUser
+    const result = (this.currentUser
       && this.currentUser.businessname
       && this.currentUser.isNewUser === true)
       || (this.account && !this.account.termsOfUseAccepted);
-      return result;
+    return result;
   }
 
   isIE10orLower() {
