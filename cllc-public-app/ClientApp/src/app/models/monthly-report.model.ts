cc7ab28ad3267d4bbcb541847132bf0e987b6d28
Brian Richter <brianorwhatever@gmail.com>
2019-11-01 16:45:51 -0700

    LCSD-2064 - Save and submit buttons work

--- cllc-public-app/ClientApp/src/app/models/monthly-report.model.ts
+++ cllc-public-app/ClientApp/src/app/models/monthly-report.model.ts
@@ -1,16 +1,22 @@
 import { InventorySalesReport } from './inventory-sales-report.model';
 
+export enum monthlyReportStatus {
+    Draft = 1,
+    Submitted = 845280001,
+    Closed = 845280002
+}
+
 export class MonthlyReport {
   monthlyReportId: string;
   licenseId: string;
   licenseNumber: string;
-  status: string;
   establishmentName: string;
   city: string;
   postalCode: string;
   reportingPeriodYear: string;
   reportingPeriodMonth: string;
 
+  statusCode: number;
   employeesManagement: number;
   employeesAdministrative: number;
   employeesSales: number;
