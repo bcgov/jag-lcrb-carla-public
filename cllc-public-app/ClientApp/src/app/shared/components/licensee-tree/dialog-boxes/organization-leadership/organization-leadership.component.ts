64be139542a79858b31ee4af196a6ce8474d946f
Moffat Sehudi <msehudi@gmail.com>
2019-11-13 16:29:51 -0800

    licensee changes updates

--- cllc-public-app/ClientApp/src/app/shared/components/licensee-tree/dialog-boxes/organization-leadership/organization-leadership.component.ts
+++ cllc-public-app/ClientApp/src/app/shared/components/licensee-tree/dialog-boxes/organization-leadership/organization-leadership.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { LicenseeChangeLog } from '@models/legal-entity-change.model';
+import { LicenseeChangeLog } from '@models/licensee-change-log.model';
 import { FormBase } from '@shared/form-base';
 
 @Component({
