0f19232956b2357d37f80ed390d95fd18b4dc659
Brian Richter <brianorwhatever@gmail.com>
2019-11-19 02:45:53 -0800

    Refactored Enums and Swagger fixes

---
+++ carla-spice-sync-service/models/enums/ApplicationSecurityClearanceStatus.cs
@@ -0,0 +1,57 @@
+// namespace Gov.Lclb.Cllb.CarlaSpiceSync
+// {
+//     public enum ApplicationSecurityClearanceStatus
+//     {
+//         NotSent = 845280000,
+//         Sent = 845280001,
+//         Sending = 845280007,
+//         Incomplete = 845280002,
+//         Withdrawn = 845280003,
+//         Passed = 845280004,
+//         Failed = 845280005
+//     }
+
+//     // public class TranslateStatus
+//     // {
+//     //     public static LCRBApplicationSecurityStatus? BusinessResultSpiceToLCRB(SpiceApplicationStatus status)
+//     //     {
+//     //         return status switch
+//     //         {
+//     //             SpiceApplicationStatus.FitAndProper => LCRBApplicationSecurityStatus.Passed,
+//     //             SpiceApplicationStatus.NotFitAndProper => LCRBApplicationSecurityStatus.Failed,
+//     //             SpiceApplicationStatus.Withdrawn => LCRBApplicationSecurityStatus.Withdrawn,
+//     //             _ => null,
+//     //         };
+//     //     }
+
+//     //     public static LCRBWorkerSecurityStatus? WorkerResultSpiceToLCRB(SpiceApplicationStatus status)
+//     //     {
+//     //         switch (status)
+//     //         {
+//     //             case SpiceApplicationStatus.Cleared:
+//     //                 return LCRBWorkerSecurityStatus.Pass;
+//     //             case SpiceApplicationStatus.NotCleared:
+//     //                 return LCRBWorkerSecurityStatus.Fail;
+//     //             case SpiceApplicationStatus.Withdrawn:
+//     //                 return LCRBWorkerSecurityStatus.Withdrawn;
+//     //             default:
+//     //                 return null;
+//     //         }
+//     //     }
+
+//     //     public static LCRBWorkerSecurityStatusCode? WorkerResultSpiceToLCRBStatusCode(SpiceApplicationStatus status)
+//     //     {
+//     //         switch (status)
+//     //         {
+//     //             case SpiceApplicationStatus.Cleared:
+//     //                 return LCRBWorkerSecurityStatusCode.Active;
+//     //             case SpiceApplicationStatus.NotCleared:
+//     //                 return LCRBWorkerSecurityStatusCode.Rejected;
+//     //             case SpiceApplicationStatus.Withdrawn:
+//     //                 return LCRBWorkerSecurityStatusCode.Withdrawn;
+//     //             default:
+//     //                 return null;
+//     //         }
+//     //     }
+//     // }
+// }
