6c52094d905e5891f86fd1596276cb8f6cb08f12
George Walker <george@walkersoftware.ca>
2019-12-23 18:24:13 -0800

    Adjust startup

--- file-manager-service/Program.cs
+++ file-manager-service/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using Serilog;
@@ -17,21 +19,31 @@ namespace Gov.Lclb.Cllb.Services.FileManager
 
         // Additional configuration is required to successfully run gRPC on macOS.
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.ConfigureKestrel(options =>
+            {
+                // Setup a HTTP/2 endpoint without TLS.
+                options.ListenLocalhost(8080, o => o.Protocols =
+                    HttpProtocols.Http2);
+            });
+            webBuilder.UseStartup<Startup>();
+            webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
 
-                    config.AddEnvironmentVariables();
-                })
-                .ConfigureLogging((hostingContext, logging) =>
+                config.AddEnvironmentVariables();
+            });
+            webBuilder.ConfigureLogging((hostingContext, logging) =>
                 {
                     logging.ClearProviders();
                     logging.SetMinimumLevel(LogLevel.Debug);
                     logging.AddDebug();
                     logging.AddEventSourceLogger();
-                })
-                .UseSerilog()
-                .UseStartup<Startup>();
+                });
+            webBuilder.UseSerilog();
+        });
+                
     }
 }
