6f309e00ba5d0ec065ca6f9acee8e1050807ed39
Tim Gunderson <tgunderson@gmail.com>
2020-01-08 12:52:34 -0800

    added licence event component

--- cllc-public-app/ClientApp/src/app/app-routing.module.ts
+++ cllc-public-app/ClientApp/src/app/app-routing.module.ts
@@ -34,6 +34,7 @@ import { FederalReportingComponent } from '@components/federal-reporting/federal
 import { ApplicationLicenseeChangesComponent } from '@components/applications/application-licensee-changes/application-licensee-changes.component';
 import { LicencesComponent } from '@components/licences/licences.component';
 import { ApplicationAndLicenceFeeComponent } from '@components/applications/application-and-licence-fee/application-and-licence-fee.component';
+import { LicenceEventComponent } from '@components/licences/licence-event/licence-event.component';
 
 
 const routes: Routes = [
@@ -46,6 +47,12 @@ const routes: Routes = [
     component: LicencesComponent,
     canActivate: [BCeidAuthGuard, FeatureGuard],
     data: { feature: 'FederalReporting' }
+  },
+  {
+    path: 'licence-event',
+    component: LicenceEventComponent,
+    canActivate: [BCeidAuthGuard],
+  
   },
   {
     path: 'federal-reporting/:licenceId',
