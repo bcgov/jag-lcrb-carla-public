d5aa65bd8aae844eff9cd23358d2c3ca5ed51819
Brian Richter <brianorwhatever@gmail.com>
2020-01-13 23:31:46 -0800

    Migrate Orgbook to separate service, Refactor gRPC

--- orgbook-service/OrgBookClient.cs
+++ orgbook-service/OrgBookClient.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Gov.Lclb.Cllb.OneStopService
+namespace Gov.Lclb.Cllb.OrgbookService
 {
     public class OrgBookClient
     {
