cb335005f11c327690f70af2b1d90b2df0a50140
Moffat Sehudi <msehudi@gmail.com>
2020-01-22 23:34:20 -0800

    Organization structure update

--- cllc-public-app/ClientApp/src/app/models/licensee-change-log.model.ts
+++ cllc-public-app/ClientApp/src/app/models/licensee-change-log.model.ts
@@ -42,10 +42,13 @@ export class LicenseeChangeLog {
   legalEntityId: string;
   parentLegalEntityId: string;
   parentLinceseeChangeLogId: string;
+  parentBusinessAccountId: string;
+  businessAccountId: string;
   children: LicenseeChangeLog[];
   parentLinceseeChangeLog: LicenseeChangeLog;
   interestPercentageNew: number;
   interestPercentageOld: number;
+  phsLink: string;
 
   isRoot: boolean; // This is only used on the client side
   isIndividual: boolean; // This is only used on the client side
@@ -149,6 +152,7 @@ export class LicenseeChangeLog {
   constructor(legalEntity: LegalEntity = null) {
     if (legalEntity) {
       this.legalEntityId = legalEntity.id;
+      this.businessAccountId = legalEntity.shareholderAccountId;
       this.businessType = legalEntity.legalentitytype;
       this.isIndividual = legalEntity.isindividual;
       this.parentLegalEntityId = legalEntity.parentLegalEntityId;
@@ -163,8 +167,11 @@ export class LicenseeChangeLog {
       this.isShareholderOld = legalEntity.isShareholder;
       this.isTrusteeNew = legalEntity.isTrustee;
       this.isTrusteeOld = legalEntity.isTrustee;
-      if (legalEntity.account) {
-        this.businessType = legalEntity.account.businessType;
+      if (legalEntity.isApplicant){
+        this.businessAccountId = legalEntity.accountId;
+      } else {
+        this.parentBusinessAccountId = legalEntity.accountId;
+        this.businessAccountId = legalEntity.shareholderAccountId;
       }
       this.numberofSharesNew = legalEntity.commonvotingshares;
       this.numberofSharesOld = legalEntity.commonvotingshares;
@@ -180,6 +187,7 @@ export class LicenseeChangeLog {
       this.dateofBirthOld = legalEntity.dateofbirth;
       this.titleNew = legalEntity.jobTitle;
       this.titleOld = legalEntity.jobTitle;
+      this.phsLink = legalEntity.phsLink;
     }
   }
 
