b7e7c96c489e26cadb967935a294665540bae65b
Moffat Sehudi <msehudi@gmail.com>
2020-02-11 10:59:55 -0800

    Fix angular tests

--- cllc-public-app/ClientApp/src/app/shared/components/org-structure/org-structure.component.spec.ts
+++ cllc-public-app/ClientApp/src/app/shared/components/org-structure/org-structure.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrgStructureComponent } from './org-structure.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('OrgStructureComponent', () => {
   let component: OrgStructureComponent;
@@ -9,6 +10,7 @@ describe('OrgStructureComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[ ReactiveFormsModule],
       declarations: [ OrgStructureComponent ],
       schemas: [NO_ERRORS_SCHEMA]
     })
