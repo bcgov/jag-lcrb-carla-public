4cefaa1e1532d1406fc800d6dfe76c4031d699b6
George Walker <george@walkersoftware.ca>
2020-02-12 16:43:54 -0800

    Adjust tests

--- cllc-public-app/ClientApp/karma.conf.js
+++ cllc-public-app/ClientApp/karma.conf.js
@@ -4,6 +4,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
+    singleRun: true,
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
