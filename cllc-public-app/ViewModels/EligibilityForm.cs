55b0da7e3e776d43fa99913b6533139ed97e3423
Brian Richter <brianorwhatever@gmail.com>
2020-02-25 15:07:55 -0800

    eligibility form backend

---
+++ cllc-public-app/ViewModels/EligibilityForm.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Converters;
+
+namespace Gov.Lclb.Cllb.Public.ViewModels
+{
+    public class EligibilityForm
+    {
+        public bool? IsConnectedToUnlicencedStore { get; set; }
+        public string NameLocationUnlicencedRetailer { get; set; }
+        public bool? IsRetailerStillOperating { get; set; }
+        public DateTimeOffset? DateOperationsCeased { get; set; }
+        public bool? IsInvolvedIllegalDistribution { get; set; }
+        public string NameLocationRetailer { get; set; }
+        public string IllegalDistributionInvolvementDetails { get; set; }
+        public bool? IsInvolvementContinuing { get; set; }
+        public DateTimeOffset? DateInvolvementCeased { get; set; }
+        public bool IsEligibilityCertified { get; set; }
+        public string EligibilitySignature { get; set; }
+        public DateTimeOffset? DateSignedOrDismissed { get; set; }
+    }
+}
