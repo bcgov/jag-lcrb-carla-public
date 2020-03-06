ffb616b34b4eaf0b386b9d71fa8c0349afefe84c
Elizabeth Wolfe <elizabeth.wolfe@gov.bc.ca>
2020-03-06 15:06:22 -0800

    Add in comments and complete all fields for CRSApplicationPrivateCorp.

--- functional-tests/bdd-tests/TestBaseCRS.cs
+++ functional-tests/bdd-tests/TestBaseCRS.cs
@@ -62,60 +62,81 @@ namespace bdd_tests
 
             ngDriver.WaitForAngular();
 
+            /* 
+            Page Title: Terms of Use
+            */
+
+            // select the acceptance checkbox
             NgWebElement termsOfUseCheckbox = ngDriver.FindElement(By.XPath("//input[@type='checkbox']"));
             termsOfUseCheckbox.Click();
 
+            // click on the Continue button
             NgWebElement continueButton = ngDriver.FindElement(By.XPath("//section[3]/button"));
             continueButton.Click();
 
+            /* 
+            Page Title: Please confirm the business or organization name associated to the Business BCeID.
+            */
+
+            // click on the Yes button
             NgWebElement confirmationButton = ngDriver.FindElement(By.XPath("//button"));
             confirmationButton.Click();
 
-            // Private Corporation
+            /* 
+            Page Title: Please confirm the organization type associated with the Business BCeID:
+            */
+
+            // if this is a private corporation, click the radio button
             if (businessType == "private corporation")
             {
                 NgWebElement privateCorporationRadio = ngDriver.FindElement(By.Name("InitialBusinessType"));
                 privateCorporationRadio.Click();
             }
 
-            // Public Corporation
+            // if this is a public corporation, click the radio button
             if (businessType == "public corporation")
             {
                 NgWebElement publicCorporationRadio = ngDriver.FindElement(By.XPath("(//input[@name='InitialBusinessType'])[2]"));
                 publicCorporationRadio.Click();
             }
 
-            // Sole Proprietorship
+            // if this is a sole proprietorship, click the radio button
             if (businessType == "sole proprietorship")
             {
                 NgWebElement soleProprietorshipRadio = ngDriver.FindElement(By.XPath("(//input[@name='InitialBusinessType'])[3]"));
                 soleProprietorshipRadio.Click();
             }
 
-            // Partnership
+            // if this is a partnership, click the radio button
             if (businessType == "partnership")
             {
                 NgWebElement partnershipRadio = ngDriver.FindElement(By.XPath("(//input[@name='InitialBusinessType'])[4]"));
                 partnershipRadio.Click();
             }
 
-            // Society
+            // if this is a society, click the radio button
             if (businessType == "society")
             {
                 NgWebElement societyRadio = ngDriver.FindElement(By.XPath("(//input[@name='InitialBusinessType'])[5]"));
                 societyRadio.Click();
             }
 
-            // Indigenous nation
+            // if this is an indigenous nation, click the radio button
             if (businessType == "indigenous nation")
             {
                 NgWebElement indigenousNationRadio = ngDriver.FindElement(By.XPath("(//input[@name='InitialBusinessType'])[6]"));
                 indigenousNationRadio.Click();
             }
 
+            // click on the Next button
             NgWebElement nextButton = ngDriver.FindElement(By.XPath("//button[contains(.,'Next')]"));
             nextButton.Click();
 
+            /* 
+            Page Title: Please confirm the name associated with the Business BCeID login provided.
+            */
+
+            // click on the Yes button
             NgWebElement confirmNameButton = ngDriver.FindElement(By.XPath("//button"));
             confirmNameButton.Click();
         }
@@ -129,6 +150,10 @@ namespace bdd_tests
 
             //browser sync - don't wait for Angular
             ngDriver.IgnoreSynchronization = true;
+
+            /* 
+            Page Title: Internet Payments Program (Bambora)
+            */
 
             driver.FindElementByName("trnCardNumber").SendKeys(testCC);
 
