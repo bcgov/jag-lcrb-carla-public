c2377699007d92c29b64a00dbf028890e48b9238
Moffat Sehudi <msehudi@gmail.com>
2020-03-19 12:21:31 -0700

    fix unit tests

--- cllc-public-app/ClientApp/src/app/components/licences/licence-renewal-steps/licence-renewal-steps.component.spec.ts
+++ cllc-public-app/ClientApp/src/app/components/licences/licence-renewal-steps/licence-renewal-steps.component.spec.ts
@@ -5,6 +5,8 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { FeatureFlagService } from '@services/feature-flag.service';
 import { of } from 'rxjs';
 import { MatIconModule } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { ActivatedRouteStub } from '@app/testing/activated-route-stub';
 
 describe('LicenceRenewalStepsComponent', () => {
   let component: LicenceRenewalStepsComponent;
@@ -15,7 +17,8 @@ describe('LicenceRenewalStepsComponent', () => {
       declarations: [LicenceRenewalStepsComponent],
       imports:[MatIconModule,],
       providers: [
-        { provide: FeatureFlagService, useValue: { featureOn: () => of(true) } }
+        { provide: FeatureFlagService, useValue: { featureOn: () => of(true) } },
+        { provide: ActivatedRoute, useValue: new ActivatedRouteStub() }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
