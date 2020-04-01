893a202bf33a2ee69b8deaee770c9f028c697d4c
ElizabethWolfe <ejwolfe@gmail.com>
2020-04-01 10:32:14 -0700

    Fix dates and wait issues; update multilevel to be e2e.

--- functional-tests/bdd-tests/MultiLevelCRSApplicationPrivateCorp.cs
+++ functional-tests/bdd-tests/MultiLevelCRSApplicationPrivateCorp.cs
@@ -16,17 +16,25 @@ using Xunit;
 Feature: MultiLevelCRSApplicationPrivateCorp
     As a logged in business user
     I want to submit a CRS Application for a private corporation
-    With multiple business shareholders
+    With multiple nested business shareholders
 
 Scenario: Start Application
     Given I am logged in to the dashboard as a private corporation
+    And the account is deleted
+    And I am logged in to the dashboard as a private corporation
     And I click on the Start Application button
     And I complete the eligibility disclosure
     And I review the account profile
     And I review the organization structure
-    And I add in multiple business shareholders
+    And I add in multiple nested business shareholders
     And I submit the organization information
-    Then I see the application page
+    And I complete the application
+    And I review the security screening requirements
+    And I click on the Pay for Application button
+    And I enter the payment information
+    And I return to the dashboard
+    And the account is deleted
+    Then I see the login page
 */
 
 namespace bdd_tests
@@ -36,6 +44,12 @@ namespace bdd_tests
     {
         [Given(@"I am logged in to the dashboard as a (.*)")]
         public void I_view_the_dashboard(string businessType)
+        {
+            CarlaLogin(businessType);
+        }
+
+        [And(@"I am logged in to the dashboard as a (.*)")]
+        public void And_I_view_the_dashboard(string businessType)
         {
             CarlaLogin(businessType);
         }
@@ -434,7 +448,7 @@ namespace bdd_tests
             business_shareholder_5();
         }
 
-        [Then(@"I submit the organization information")]
+        [And(@"I submit the organization information")]
         public void submit_org_info()
         {
             // click on Submit Organization Info button
@@ -442,6 +456,175 @@ namespace bdd_tests
             NgWebElement submitOrgInfoButton = ngDriver.FindElement(By.XPath("//button[contains(.,' SUBMIT ORGANIZATION INFORMATION')]"));
             System.Threading.Thread.Sleep(7000);
             submitOrgInfoButton.Click();
+        }
+
+        [And(@"I complete the application")]
+        public void I_complete_the_application()
+        {
+            /* 
+            Page Title: Submit the Cannabis Retail Store Application
+            */
+
+            // create application info
+            string estName = "Point Ellis Greenhouse";
+            string estAddress = "645 Tyee Rd";
+            string estCity = "Victoria";
+            string estPostal = "V9A6X5";
+            string estPID = "012345678";
+            string estEmail = "test@test.com";
+            string estPhone = "2505555555";
+            string conGiven = "Given";
+            string conSurname = "Surname";
+            string conRole = "CEO";
+            string conPhone = "2508888888";
+            string conEmail = "contact@email.com";
+
+            System.Threading.Thread.Sleep(7000);
+
+            // enter the establishment name
+            NgWebElement estabName = ngDriver.FindElement(By.Id("establishmentName"));
+            estabName.SendKeys(estName);
+
+            // enter the establishment address
+            NgWebElement estabAddress = ngDriver.FindElement(By.Id("establishmentAddressStreet"));
+            estabAddress.SendKeys(estAddress);
+
+            // enter the establishment city
+            NgWebElement estabCity = ngDriver.FindElement(By.Id("establishmentAddressCity"));
+            estabCity.SendKeys(estCity);
+
+            // enter the establishment postal code
+            NgWebElement estabPostal = ngDriver.FindElement(By.Id("establishmentAddressPostalCode"));
+            estabPostal.SendKeys(estPostal);
+
+            // enter the PID
+            NgWebElement estabPID = ngDriver.FindElement(By.Id("establishmentParcelId"));
+            estabPID.SendKeys(estPID);
+
+            // enter the store email
+            NgWebElement estabEmail = ngDriver.FindElement(By.Id("establishmentEmail"));
+            estabEmail.SendKeys(estEmail);
+
+            // enter the store phone number
+            NgWebElement estabPhone = ngDriver.FindElement(By.Id("establishmentPhone"));
+            estabPhone.SendKeys(estPhone);
+
+            // find the upload test files in the bdd-tests\upload_files folder
+            var environment = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(environment).Parent.FullName;
+            string projectDirectory2 = Directory.GetParent(projectDirectory).Parent.FullName;
+
+            // upload a store signage document
+            string signagePath = Path.Combine(projectDirectory2 + Path.DirectorySeparatorChar + "bdd-tests" + Path.DirectorySeparatorChar + "upload_files" + Path.DirectorySeparatorChar + "signage.pdf");
+            NgWebElement uploadSignage = ngDriver.FindElement(By.XPath("(//input[@type='file'])[2]"));
+            uploadSignage.SendKeys(signagePath);
+
+            // upload a valid interest document
+            string validInterestPath = Path.Combine(projectDirectory2 + Path.DirectorySeparatorChar + "bdd-tests" + Path.DirectorySeparatorChar + "upload_files" + Path.DirectorySeparatorChar + "valid_interest.pdf");
+            NgWebElement uploadValidInterest = ngDriver.FindElement(By.XPath("(//input[@type='file'])[6]"));
+            uploadValidInterest.SendKeys(validInterestPath);
+
+            // upload a floor plan document
+            string floorplanPath = Path.Combine(projectDirectory2 + Path.DirectorySeparatorChar + "bdd-tests" + Path.DirectorySeparatorChar + "upload_files" + Path.DirectorySeparatorChar + "floor_plan.pdf");
+            NgWebElement uploadFloorplan = ngDriver.FindElement(By.XPath("(//input[@type='file'])[8]"));
+            uploadFloorplan.SendKeys(floorplanPath);
+
+            // upload a site plan document
+            string sitePlanPath = Path.Combine(projectDirectory2 + Path.DirectorySeparatorChar + "bdd-tests" + Path.DirectorySeparatorChar + "upload_files" + Path.DirectorySeparatorChar + "site_plan.pdf");
+            NgWebElement uploadSitePlan = ngDriver.FindElement(By.XPath("(//input[@type='file'])[11]"));
+            uploadSitePlan.SendKeys(sitePlanPath);
+
+            // upload a financial integrity form
+            string finIntegrityPath = Path.Combine(projectDirectory2 + Path.DirectorySeparatorChar + "bdd-tests" + Path.DirectorySeparatorChar + "upload_files" + Path.DirectorySeparatorChar + "fin_integrity.pdf");
+            NgWebElement uploadFinIntegrity = ngDriver.FindElement(By.XPath("(//input[@type='file'])[15]"));
+            uploadFinIntegrity.SendKeys(finIntegrityPath);
+
+            // enter the first name of the application contact
+            NgWebElement contactGiven = ngDriver.FindElement(By.Id("contactPersonFirstName"));
+            contactGiven.SendKeys(conGiven);
+
+            // enter the last name of the application contact
+            NgWebElement contactSurname = ngDriver.FindElement(By.Id("contactPersonLastName"));
+            contactSurname.SendKeys(conSurname);
+
+            // enter the role of the application contact
+            NgWebElement contactRole = ngDriver.FindElement(By.CssSelector("input[formControlName=contactPersonRole]"));
+            contactRole.SendKeys(conRole);
+
+            // enter the phone number of the application contact
+            NgWebElement contactPhone = ngDriver.FindElement(By.CssSelector("input[formControlName=contactPersonPhone]"));
+            contactPhone.SendKeys(conPhone);
+
+            // enter the email of the application contact
+            NgWebElement contactEmail = ngDriver.FindElement(By.Id("contactPersonEmail"));
+            contactEmail.SendKeys(conEmail);
+
+            // click on the authorized to submit checkbox
+            NgWebElement authorizedSubmit = ngDriver.FindElement(By.Id("authorizedToSubmit"));
+            authorizedSubmit.Click();
+
+            // click on the signature agreement checkbox
+            NgWebElement signatureAgree = ngDriver.FindElement(By.Id("signatureAgreement"));
+            signatureAgree.Click();
+
+            // click on the Submit button
+            NgWebElement submit_button = ngDriver.FindElement(By.XPath("//button[contains(.,'SUBMIT')]"));
+            submit_button.Click();
+        }
+
+        [And(@"I review the security screening requirements")]
+        public void review_security_screening_reqs()
+        {
+            /* 
+            Page Title: Security Screening Requirements
+                      : placeholder for future testing
+            */
+        }
+
+        [And(@"I click on the Pay for Application button")]
+        public void click_on_pay()
+        {
+            NgWebElement pay_button = ngDriver.FindElement(By.XPath("//button[contains(.,'Pay for Application')]"));
+            pay_button.Click();
+        }
+
+        [And(@"I enter the payment information")]
+        public void enter_payment_info()
+        {
+            MakePayment();
+        }
+
+        [And(@"I return to the dashboard")]
+        public void return_to_dashboard()
+        {
+            /* 
+            Page Title: Payment Approved
+            */
+
+            // confirm that payment receipt is for $7,500.00
+            Assert.True(ngDriver.FindElement(By.XPath("/html/body/app-root/div/div/div/main/div/app-payment-confirmation/mat-card/div/div[1]/div/div/table/tr[6]/td[2][text()='$7,500.00']")).Displayed);
+
+            string retDash = "Return to Dashboard";
+
+            // click on the Return to Dashboard link
+            NgWebElement returnDash = ngDriver.FindElement(By.LinkText(retDash));
+            returnDash.Click();
+        }
+
+        [And(@"the account is deleted")]
+        public void Delete_my_account()
+        {
+            this.CarlaDeleteCurrentAccount();
+        }
+
+        [Then(@"I see the login page")]
+        public void I_see_login()
+        {
+            /* 
+            Page Title: Apply for a cannabis licence
+            */
+
+            Assert.True(ngDriver.FindElement(By.XPath("//a[text()='Log In']")).Displayed);
         }
 
         public void business_shareholder_2()
