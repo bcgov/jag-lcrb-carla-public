643b04ceb2c7ee5a9440a4ff0d56f479178d0c92
George Walker <george@walkersoftware.ca>
2020-04-08 13:17:32 -0700

    Word sanitizer

---
+++ cllc-public-app-test/SanitizeUtilityTests.cs
@@ -0,0 +1,20 @@
+﻿
+using Gov.Lclb.Cllb.Public.Utils;
+using Xunit;
+
+namespace Gov.Lclb.Cllb.Public.Test
+{
+    public class SanitizeUtilityTest
+    {
+        [Fact]
+        public void TestSanitize()
+        {
+            string input =    "Joe_personal123123";
+            string expected = "J**_p*******123123";
+            
+            string resultingData = WordSanitizer.Sanitize(input);
+            Assert.Equal(expected, resultingData );
+        }
+    }
+}
+
