8295d6964ea8e751282425b5f151737a29714532
ElizabethWolfe <ejwolfe@gmail.com>
2020-04-09 07:49:28 -0700

    Tune up CRS licence test.

--- functional-tests/bdd-tests/LicenceCRSPrivateCorp.cs
+++ functional-tests/bdd-tests/LicenceCRSPrivateCorp.cs
@@ -93,7 +93,8 @@ namespace bdd_tests
             uiCalendar2.Click();
 
             // enter the reason for the opening date
-            NgWebElement uiReasonDate = ngDriver.FindElement(By.XPath("/html/body/app-root/div/div/div/main/div/app-application-and-licence-fee/div/div[2]/div[2]/section[2]/div/app-field[2]/section/div/section/textarea"));
+            NgWebElement uiReasonDate = ngDriver.FindElement(By.XPath("//textarea"));
+            //NgWebElement uiReasonDate = ngDriver.FindElement(By.XPath("/html/body/app-root/div/div/div/main/div/app-application-and-licence-fee/div/div[2]/div[2]/section[2]/div/app-field[2]/section/div/section/textarea"));
             uiReasonDate.SendKeys(reasonDay);
        
             NgWebElement paymentButton = ngDriver.FindElement(By.XPath("//button[contains(.,' PAY LICENCE FEE AND RECEIVE LICENCE')]"));
@@ -102,8 +103,13 @@ namespace bdd_tests
             // pay the licence fee
             MakePayment();
 
-            // return to the Licences tab
-            uiLicenceFee.Click();
+            System.Threading.Thread.Sleep(7000);
+
+            string licencesLink = "Licences";
+
+            // click on the Licences link
+            NgWebElement uiLicences = ngDriver.FindElement(By.LinkText(licencesLink));
+            uiLicences.Click();
         }
 
         [And(@"I plan the store opening")]
@@ -171,7 +177,15 @@ namespace bdd_tests
             NgWebElement saveButton = ngDriver.FindElement(By.XPath("//button[contains(.,'SAVE')]"));
             saveButton.Click();
 
-            //todo: confirm next page after save
+            /* 
+            Page Title: Welcome to Liquor and Cannabis Licensing
+            */
+
+            string licencesLink = "Licences";
+
+            // click on the Licences link
+            NgWebElement uiLicences = ngDriver.FindElement(By.LinkText(licencesLink));
+            uiLicences.Click();
         }
 
         [And(@"I request a store relocation")]
@@ -330,6 +344,13 @@ namespace bdd_tests
             */
 
             Assert.True (ngDriver.FindElement(By.XPath("//body[contains(.,'Federal Reporting')]")).Displayed);
+            
+            // return to the Licences tab
+            string licencesLink = "Licences";
+
+            // click on the Licences link
+            NgWebElement uiLicences = ngDriver.FindElement(By.LinkText(licencesLink));
+            uiLicences.Click();
         }
 
         [And(@"I request a transfer of ownership")]
@@ -350,9 +371,7 @@ namespace bdd_tests
             Page Title: Transfer Your Cannabis Retail Store Licence
             */
 
-            /* Note: Functionality not fully available */
-
-            // search for the proposed licensee
+            // TODO: search for the proposed licensee
 
             // click on consent to licence transfer checkbox
             NgWebElement consentToTransfer = ngDriver.FindElement(By.XPath("/html/body/app-root/div/div/div/main/div/app-application-ownership-transfer/div/div[2]/div[2]/section[5]/app-field/section/div/section/section/input"));
@@ -369,23 +388,8 @@ namespace bdd_tests
             // click on submit transfer button
             NgWebElement submitTransferButton = ngDriver.FindElement(By.XPath("//button[contains(.,' SUBMIT TRANSFER')]"));
             submitTransferButton.Click();
-        }
 
-        [And(@"I show the store as open on the map")]
-        public void show_store_open_on_map()
-        {
-            /* 
-            Page Title: Licences
-            Subtitle:   Cannabis Retail Store Licences
-            */
-            
-            string showOpenOnMap = "Show Store as Open on Map";
-
-            // click on the Transfer Ownership link
-            NgWebElement uiShowOpenOnMap = ngDriver.FindElement(By.LinkText(showOpenOnMap));
-            uiShowOpenOnMap.Click();
-
-            // next steps?
+            // TODO: determine next steps
         }
 
         [And(@"I request a third party operator")]
@@ -406,19 +410,48 @@ namespace bdd_tests
             Page Title: Assigning Third Party Operator for Cannabis Retail Store Licence
             */
 
-            // select the business name of third party operator - TODO
+            // TODO: select the business name of third party operator
 
             // click on authorized to submit checkbox
-            NgWebElement authorizedToSubmit = ngDriver.FindElement(By.XPath("/html/body/app-root/div/div/div/main/div/app-application-third-party-operator/div/div[2]/div[2]/div/app-field[2]/section/div/section/section/input"));
+            NgWebElement authorizedToSubmit = ngDriver.FindElement(By.XPath("//input[@type='checkbox']"));
             authorizedToSubmit.Click();
 
             // click on signature agreement checkbox
-            NgWebElement signatureAgreement = ngDriver.FindElement(By.XPath("/html/body/app-root/div/div/div/main/div/app-application-third-party-operator/div/div[2]/div[2]/div/app-field[2]/section/div/section/section/input"));
+            NgWebElement signatureAgreement = ngDriver.FindElement(By.XPath("(//input[@type='checkbox'])[2]"));
             signatureAgreement.Click();
 
             // click on submit button
             NgWebElement submitButton = ngDriver.FindElement(By.XPath("//button[contains(.,' SUBMIT')]"));
             submitButton.Click();
+        }
+
+        [And(@"I show the store as open on the map")]
+        public void show_store_open_on_map()
+        {
+            /* 
+            Page Title: Licences
+            Subtitle:   Cannabis Retail Store Licences
+            */
+
+            string showOpenOnMap = "Show Store as Open on Map";
+
+            // click on the Transfer Ownership link
+            NgWebElement uiShowOpenOnMap = ngDriver.FindElement(By.LinkText(showOpenOnMap));
+            uiShowOpenOnMap.Click();
+
+            // TODO: next steps?
+
+            /* 
+            Page Title: Apply for a cannabis licence
+            */
+
+            System.Threading.Thread.Sleep(7000);
+
+            string dashboard = "Dashboard";
+
+            // click on the Dashboard link
+            NgWebElement uiDashboard = ngDriver.FindElement(By.LinkText(dashboard));
+            uiDashboard.Click();
         }
 
         [Then(@"the requested applications are visible on the dashboard")]
@@ -434,10 +467,10 @@ namespace bdd_tests
             // confirm that a name or branding change request is displayed
             Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'Name or Branding Change')]")).Displayed);
 
-            // confirm that a transfer of ownership request is displayed
+            // TODO: confirm that a transfer of ownership request is displayed
             Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,' ')]")).Displayed);
 
-            // confirm that a third party operator request is displayed
+            // TODO: confirm that a third party operator request is displayed
             Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,' ')]")).Displayed);
         }
     }
