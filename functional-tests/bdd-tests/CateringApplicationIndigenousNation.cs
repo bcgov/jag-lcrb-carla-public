0f209ff371daf76fbd98e9d8c47f20d43ad89567
ElizabethWolfe <ejwolfe@gmail.com>
2020-04-23 14:24:28 -0700

    Add in feature flag check for liquor.

--- functional-tests/bdd-tests/CateringApplicationIndigenousNation.cs
+++ functional-tests/bdd-tests/CateringApplicationIndigenousNation.cs
@@ -38,9 +38,22 @@ namespace bdd_tests
     [FeatureFile("./CateringApplication_indigenousnation.feature")]
     public sealed class CateringApplicationIndigenousNation : TestBase
     {
+        public void CheckFeatureFlagsLiquor()
+        {
+            string feature_flags = configuration["featureFlags"];
+
+            // navigate to the feature flags page
+            driver.Navigate().GoToUrl($"{baseUri}{feature_flags}");
+
+            // confirm that the LiquorOne flag is enabled during this test
+            Assert.True(driver.FindElement(By.XPath("//body[contains(.,'LiquorOne')]")).Displayed);
+        }
+
         [Given(@"I am logged in to the dashboard as an (.*)")]
         public void I_view_the_dashboard(string businessType)
         {
+            CheckFeatureFlagsLiquor();
+
             CarlaLoginNoCheck();
         }
 
