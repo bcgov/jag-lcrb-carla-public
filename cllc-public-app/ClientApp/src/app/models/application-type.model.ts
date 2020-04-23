9b71687b89391c8c599f86caa19fa84c2b7aef93
George Walker <george@walkersoftware.ca>
2020-04-22 20:40:13 -0700

    Add form reference to the portal

--- cllc-public-app/ClientApp/src/app/models/application-type.model.ts
+++ cllc-public-app/ClientApp/src/app/models/application-type.model.ts
@@ -24,6 +24,7 @@ export class ApplicationType {
   showDeclarations: boolean;
   showLiquorDeclarations: boolean;
   establishmetNameIsReadOnly: boolean;
+  formReference: string;
   showDescription1: boolean;
 
   storeContactInfo: FormControlState;
