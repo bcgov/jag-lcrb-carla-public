8b5182b0673e6f8528dd56c6783c8c8de9718b2c
ElizabethWolfe <ejwolfe@gmail.com>
2020-04-30 11:53:24 -0700

    Refactor catering org structure.

--- functional-tests/bdd-tests/CateringApplicationPublicCorp.cs
+++ functional-tests/bdd-tests/CateringApplicationPublicCorp.cs
@@ -85,58 +85,7 @@ namespace bdd_tests
         [And(@"I review the organization structure")]
         public void I_continue_to_organization_review()
         {
-            /* 
-            Page Title: [client name] Detailed Organization Information
-            */
-
-            // find the upload test file in the bdd-tests\upload_files folder
-            var environment = Environment.CurrentDirectory;
-            string projectDirectory = Directory.GetParent(environment).Parent.FullName;
-            string projectDirectory2 = Directory.GetParent(projectDirectory).Parent.FullName;
-
-            // upload NOA form
-            string NOAPath = Path.Combine(projectDirectory2 + Path.DirectorySeparatorChar + "bdd-tests" + Path.DirectorySeparatorChar + "upload_files" + Path.DirectorySeparatorChar + "signage.pdf");
-            NgWebElement uploadNOA = ngDriver.FindElement(By.XPath("(//input[@type='file'])[2]"));
-            uploadNOA.SendKeys(NOAPath);
-
-            /********** Key Personnel **********/
-
-            // create the key personnel data
-            string keyPersonnelFirst = "Jane";
-            string keyPersonnelLast = "Bond";
-            string keyPersonnelTitle = "Adventurer";
-            string keyPersonnelEmail = "jane@bond.com";
-
-            // open key personnel form
-            NgWebElement openKeyPersonnelForm = ngDriver.FindElement(By.XPath("//div[@id='cdk-step-content-0-1']/app-application-licensee-changes/div/section/app-org-structure/div[3]/section/app-associate-list/div/button"));
-            openKeyPersonnelForm.Click();
-
-            // enter key personnel first name
-            NgWebElement openKeyPersonnelFirst = ngDriver.FindElement(By.XPath("//input[@type='text']"));
-            openKeyPersonnelFirst.SendKeys(keyPersonnelFirst);
-
-            // enter key personnel last name
-            NgWebElement openKeyPersonnelLast = ngDriver.FindElement(By.XPath("(//input[@type='text'])[2]"));
-            openKeyPersonnelLast.SendKeys(keyPersonnelLast);
-
-            // select key personnel role
-            NgWebElement openKeyPersonnelRole = ngDriver.FindElement(By.XPath("//input[@type='checkbox']"));
-            openKeyPersonnelRole.Click();
-
-            // enter key personnel title
-            NgWebElement openKeyPersonnelTitle = ngDriver.FindElement(By.XPath("(//input[@type='text'])[3]"));
-            openKeyPersonnelTitle.SendKeys(keyPersonnelTitle);
-
-            // enter key personnel email
-            NgWebElement openKeyPersonnelEmail = ngDriver.FindElement(By.XPath("(//input[@type='text'])[4]"));
-            openKeyPersonnelEmail.SendKeys(keyPersonnelEmail);
-
-            // select key person DOB
-            NgWebElement openKeyPersonnelDOB = ngDriver.FindElement(By.XPath("(//input[@type='text'])[5]"));
-            openKeyPersonnelDOB.Click();
-
-            NgWebElement openKeyPersonnelDOB1 = ngDriver.FindElement(By.XPath("//*[@id='mat-datepicker-1']/div/mat-month-view/table/tbody/tr[2]/td[1]/div"));
-            openKeyPersonnelDOB1.Click();
+            CateringOrgStructure();
         }
 
         [And(@"I submit the organization structure")]
