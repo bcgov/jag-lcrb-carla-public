f1d28da7dd54fa8437a1535b8ec059e855d089f3
Brian Richter <brianorwhatever@gmail.com>
2020-05-06 15:01:51 -0700

    remove IE support entirely

---
+++ cllc-public-app/ClientApp/src/app/components/incompatible-browser/incompatible-browser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incompatible-browser',
+  templateUrl: './incompatible-browser.component.html'
+})
+
+export class IncompatibleBrowserComponent {
+  constructor() {
+  }
+}
