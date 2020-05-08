8b9cc60a428fa21795910d69c809bd929bb8cbf3
ElizabethWolfe <ejwolfe@gmail.com>
2020-05-08 10:19:12 -0700

    Remove feature flag secret.

--- functional-tests/bdd-tests/MultiLevelCRSApplicationPrivateCorp.cs
+++ functional-tests/bdd-tests/MultiLevelCRSApplicationPrivateCorp.cs
@@ -44,10 +44,8 @@ namespace bdd_tests
     {
         public void CheckFeatureFlagsCannabis()
         {
-            string feature_flags = configuration["featureFlags"];
-
             // navigate to the feature flags page
-            driver.Navigate().GoToUrl($"{baseUri}{feature_flags}");
+            driver.Navigate().GoToUrl($"{baseUri}api/features");
 
             // confirm that the CRS-Renewal flag is enabled during this test
             Assert.True(driver.FindElement(By.XPath("//body[contains(.,'CRS-Renewal')]")).Displayed);
