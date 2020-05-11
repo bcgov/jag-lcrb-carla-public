6da78e23e9d69308a36f3789832aae450c00c22d
ElizabethWolfe <ejwolfe@gmail.com>
2020-05-11 16:00:30 -0700

    Drafts of account creation files.

---
+++ functional-tests/account-creation-tests/CreateCRSApplicationSociety.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: Create_CRSApplication_society
+    As a logged in business user
+    I want to submit a CRS Application for a society
+    To be used as test data
+
+Scenario: Start Application
+    Given I am logged in to the dashboard as a society
+    And the account is deleted
+    And I am logged in to the dashboard as a society
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile
+    And I review the organization structure
+    And I submit the organization structure
+    And I complete the Cannabis Retail Store application
+    And I review the security screening requirements
+    And I click on the Pay for Application button
+    And I enter the payment information
+    And I return to the dashboard
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./Create_CRSApplication_society.feature")]
+    public sealed class CreateCRSApplicationSociety : TestBase
+    {
+        public void CheckFeatureFlagsCannabis()
+        {
+            // navigate to the feature flags page
+            driver.Navigate().GoToUrl($"{baseUri}api/features");
+
+            // confirm that the CRS-Renewal flag is enabled during this test
+            Assert.True(driver.FindElement(By.XPath("//body[contains(.,'CRS-Renewal')]")).Displayed);
+        }
+
+        [Given(@"I am logged in to the dashboard as a (.*)")]
+        public void I_view_the_dashboard(string businessType)
+        {
+            CheckFeatureFlagsCannabis();
+            
+            CarlaLoginNoCheck();
+        }
+
+        [And(@"I am logged in to the dashboard as a (.*)")]
+        public void And_I_view_the_dashboard(string businessType)
+        {
+            CarlaLogin(businessType);
+        }
+
+        [And(@"I click on the Start Application button for a Cannabis Retail Store")]
+        public void I_start_application()
+        {
+            /* 
+            Page Title: Welcome to Cannabis Licensing
+            */
+
+            NgWebElement startApp_button = ngDriver.FindElement(By.XPath("//button[text()='START APPLICATION']"));
+            startApp_button.Click();
+        }
+
+        [And(@"I complete the eligibility disclosure")]
+        public void complete_eligibility_disclosure()
+        {
+            CRSEligibilityDisclosure();
+        }
+
+        [And(@"I review the account profile")]
+        public void review_account_profile()
+        {
+            ReviewAccountProfile();
+        }
+
+        [And(@"I review the organization structure")]
+        public void I_continue_to_organization_review()
+        {
+            ReviewOrgStructure();
+        }
+
+
+        [And(@"I submit the organization structure")]
+        public void submit_org_structure()
+        {
+            SubmitOrgInfoButton();
+        }
+
+        [And(@"I complete the Cannabis Retail Store application")]
+        public void I_complete_the_application()
+        {
+            CRSApplication();
+        }
+
+        [And(@"I review the security screening requirements")]
+        public void review_security_screening_reqs()
+        {
+            /* 
+            Page Title: Security Screening Requirements
+            */
+
+            ReviewSecurityScreening();
+        }
+
+        [And(@"I click on the Pay for Application button")]
+        public void click_on_pay()
+        {
+            NgWebElement pay_button = ngDriver.FindElement(By.XPath("//button[contains(.,'Pay for Application')]"));
+            pay_button.Click();
+        }
+
+        [And(@"I enter the payment information")]
+        public void enter_payment_info()
+        {
+            MakePayment();
+        }
+
+        [And(@"I return to the dashboard")]
+        public void return_to_dashboard()
+        {
+            CRSReturnToDashboard();
+        }
+
+        [And(@"the account is deleted")]
+        public void Delete_my_account()
+        {
+            this.CarlaDeleteCurrentAccount();
+        }
+
+        [Then(@"I see the login page")]
+        public void I_see_login()
+        {
+            /* 
+            Page Title: Apply for a cannabis licence
+            */
+
+            Assert.True (ngDriver.FindElement(By.XPath("//a[text()='Log In']")).Displayed);
+        }
+    }
+}
---
+++ functional-tests/account-creation-tests/Create_CateringApplication_society.feature
@@ -0,0 +1,19 @@
+﻿Feature: Create_CateringApplication_society
+    As a logged in business user
+    I want to submit a Catering Application for a society
+    To be used as test data
+
+Scenario: Start Application
+    Given I am logged in to the dashboard as a society
+    And the account is deleted
+    And I am logged in to the dashboard as a society
+    And I click on the Start Application button for Catering
+    And I review the account profile
+    And I review the organization structure
+    And I submit the organization structure
+    And I complete the Catering application
+    And I click on the Submit button
+    And I click on the Pay for Application button
+    And I enter the payment information
+    And I return to the dashboard
+    Then I see the login page
