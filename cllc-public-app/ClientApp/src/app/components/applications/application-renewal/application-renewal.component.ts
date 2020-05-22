ab542fdbd651903cc31ce501d73302d6322c1a38
Moffat Sehudi <msehudi@gmail.com>
2020-05-22 12:07:39 -0700

    LCSD-3055 for renewal application

--- cllc-public-app/ClientApp/src/app/components/applications/application-renewal/application-renewal.component.ts
+++ cllc-public-app/ClientApp/src/app/components/applications/application-renewal/application-renewal.component.ts
@@ -24,20 +24,29 @@ import { FeatureFlagService } from '@services/feature-flag.service';
 import { LicenseDataService } from '@app/services/license-data.service';
 import { UPLOAD_FILES_MODE, ApplicationCancellationDialogComponent } from '@components/dashboard/applications-and-licences/applications-and-licences.component';
 
-
-
-const ServiceHours = [
-  // '00:00', '00:15', '00:30', '00:45', '01:00', '01:15', '01:30', '01:45', '02:00', '02:15', '02:30', '02:45', '03:00',
-  // '03:15', '03:30', '03:45', '04:00', '04:15', '04:30', '04:45', '05:00', '05:15', '05:30', '05:45', '06:00', '06:15',
-  // '06:30', '06:45', '07:00', '07:15', '07:30', '07:45', '08:00', '08:15', '08:30', '08:45',
-  '09:00', '09:15', '09:30',
-  '09:45', '10:00', '10:15', '10:30', '10:45', '11:00', '11:15', '11:30', '11:45', '12:00', '12:15', '12:30', '12:45',
-  '13:00', '13:15', '13:30', '13:45', '14:00', '14:15', '14:30', '14:45', '15:00', '15:15', '15:30', '15:45', '16:00',
-  '16:15', '16:30', '16:45', '17:00', '17:15', '17:30', '17:45', '18:00', '18:15', '18:30', '18:45', '19:00', '19:15',
-  '19:30', '19:45', '20:00', '20:15', '20:30', '20:45', '21:00', '21:15', '21:30', '21:45', '22:00', '22:15', '22:30',
-  '22:45', '23:00'
-  // , '23:15', '23:30', '23:45'
-];
+const ValidationErrorMap = {
+  renewalCriminalOffenceCheck: 'Please answer question 1',
+  renewalUnreportedSaleOfBusiness: 'Please answer question 2',
+  renewalBusinessType: 'Please answer question 3',
+  renewalTiedhouse: 'Please answer question 4',
+  tiedhouseFederalInterest: 'Please answer question 5',
+  renewalOrgLeadership: 'Please answer question 6',
+  renewalkeypersonnel: 'Please answer question 7',
+  renewalShareholders: 'Please answer question 8',
+  renewalOutstandingFines: 'Please answer question 9',
+  renewalBranding: 'Please answer question 10',
+  renewalSignage: 'Please answer question 11',
+  renewalEstablishmentAddress: 'Please answer question 12',
+  renewalValidInterest: 'Please answer question 13',
+  renewalZoning: 'Please answer question 14',
+  renewalFloorPlan: 'Please answer question 15',
+  contactPersonFirstName: 'Please enter the business contact\'s first name',
+  contactPersonLastName: 'Please enter the business contact\'s last name',
+  contactPersonEmail: 'Please enter the business contact\'s email address',
+  contactPersonPhone: 'Please enter the business contact\'s 10-digit phone number',
+  authorizedToSubmit: 'Please affirm that you are authorized to submit the application',
+  signatureAgreement: 'Please affirm that all of the information provided for this application is true and complete',
+};
 
 class ApplicationHTMLContent {
   title: string;
@@ -65,7 +74,6 @@ export class ApplicationRenewalComponent extends FormBase implements OnInit {
   validationMessages: any[];
   showValidationMessages: boolean;
   submittedApplications = 8;
-  ServiceHours = ServiceHours;
   tiedHouseFormData: TiedHouseConnection;
   possibleProblematicNameWarning = false;
   htmlContent: ApplicationHTMLContent = <ApplicationHTMLContent>{};
@@ -131,7 +139,7 @@ export class ApplicationRenewalComponent extends FormBase implements OnInit {
       renewalZoning: ['', Validators.required],
       // #15
       renewalFloorPlan: ['', Validators.required],
-      
+
       contactPersonFirstName: ['', Validators.required],
       contactPersonLastName: ['', Validators.required],
       contactPersonRole: [''],
@@ -311,18 +319,9 @@ export class ApplicationRenewalComponent extends FormBase implements OnInit {
   }
 
   isValid(): boolean {
-    // mark controls as touched
-    for (const c in this.form.controls) {
-      if (typeof (this.form.get(c).markAsTouched) === 'function') {
-        this.form.get(c).markAsTouched();
-      }
-    }
+    this.markConstrolsAsTouched(this.form);
     this.showValidationMessages = false;
-    this.validationMessages = [];
-
-    if (!this.form.valid) {
-      this.validationMessages.push('Some required fields have not been completed');
-    }
+    this.validationMessages = this.listControlsWithErrors(this.form, ValidationErrorMap);
     return this.form.valid;
   }
 
