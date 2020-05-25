de212b596fa7bd6a0c6bfee56a618eb1e78d3737
Moffat Sehudi <msehudi@gmail.com>
2020-05-25 16:00:53 -0700

    Fix Typo

--- cllc-public-app/ClientApp/src/app/app-routing.module.ts
+++ cllc-public-app/ClientApp/src/app/app-routing.module.ts
@@ -48,7 +48,7 @@ import { ApplicationThirdPartyOperatorComponent } from '@components/applications
 import { CancelThirdPartyOperatorComponent } from '@components/applications/cancel-third-party-operator/cancel-third-party-operator.component';
 import { ApplicationCovidTemporaryExtensionComponent } from '@components/applications/application-covid-temporary-extension/application-covid-temporary-extension.component';
 import { CovidConfirmationComponent } from '@components/applications/application-covid-temporary-extension/covid-confirmation/covid-confirmation.component';
-import { TerminateTOPRelationshipComponent } from '@components/applications/terminate-tpo-relationship/terminate-tpo-relationship.component';
+import { TerminateTPORelationshipComponent } from '@components/applications/terminate-tpo-relationship/terminate-tpo-relationship.component';
 
 
 const routes: Routes = [
@@ -201,7 +201,7 @@ const routes: Routes = [
   },
   {
     path: 'terminate-third-party-operator/:licenceId',
-    component: TerminateTOPRelationshipComponent,
+    component: TerminateTPORelationshipComponent,
     canActivate: [BCeidAuthGuard]
   },
   {
