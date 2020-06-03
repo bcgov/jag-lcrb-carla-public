50ab8f3c4dc95a9c2c5337258b09120c9105e434
ElizabethWolfe <ejwolfe@gmail.com>
2020-06-03 13:22:21 -0700

    Refactor log in.

--- functional-tests/account-creation-tests/CreateCateringApplicationIndigenousNation.cs
+++ functional-tests/account-creation-tests/CreateCateringApplicationIndigenousNation.cs
@@ -49,7 +49,7 @@ namespace bdd_tests
         }
 
         [And(@"I am logged in to the dashboard as an (.*)")]
-        public void And_I_view_the_dashboard(string businessType)
+        public void And_I_view_the_dashboard_IN(string businessType)
         {
             CarlaLoginWithUser(businessType);
         }
--- functional-tests/bdd-tests/TestBase.cs
+++ functional-tests/bdd-tests/TestBase.cs
@@ -2567,5 +2567,11 @@ namespace bdd_tests
         {
             MakePayment();
         }
+
+        [And(@"I am logged in to the dashboard as a (.*)")]
+        public void And_I_view_the_dashboard(string businessType)
+        {
+            CarlaLogin(businessType);
+        }
     }
 }
