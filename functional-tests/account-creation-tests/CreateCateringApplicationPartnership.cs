b4656f0d8c51769cfe0985693717bfd0f91f232d
ElizabethWolfe <ejwolfe@gmail.com>
2020-06-06 09:13:25 -0700

    Clean up account creation tests.

--- functional-tests/account-creation-tests/CreateCateringApplicationPartnership.cs
+++ functional-tests/account-creation-tests/CreateCateringApplicationPartnership.cs
@@ -38,11 +38,10 @@ namespace bdd_tests
     [FeatureFile("./Create_CateringApplication_partnership.feature")]
     public sealed class CreateCateringApplicationPartnership : TestBase
     {      
-
         [Given(@"I am logged in to the dashboard as a (.*)")]
         public void I_view_the_dashboard(string businessType)
         {
-            //CheckFeatureFlagsLiquor();
+            CheckFeatureFlagsLiquor();
 
             CarlaLoginNoCheck();
         }
--- functional-tests/account-creation-tests/CreateCateringApplicationPrivateCorp.cs
+++ functional-tests/account-creation-tests/CreateCateringApplicationPrivateCorp.cs
@@ -38,11 +38,10 @@ namespace bdd_tests
     [FeatureFile("./Create_CateringApplication_privatecorp.feature")]
     public sealed class CreateCateringApplicationPrivateCorp : TestBase
     {
-
         [Given(@"I am logged in to the dashboard as a (.*)")]
         public void I_view_the_dashboard(string businessType)
         {
-            //CheckFeatureFlagsLiquor();
+            CheckFeatureFlagsLiquor();
 
             CarlaLoginNoCheck();
         }
