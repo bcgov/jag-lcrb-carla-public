2718909a3885cc8cc89223d36804e327e2ba48ed
ElizabethWolfe <ejwolfe@gmail.com>
2020-06-08 17:27:04 -0700

    Refactor licence fee.

--- functional-tests/bdd-tests/E2ECateringApplicationLicenceIndigenousNationNameBrandingChange.cs
+++ functional-tests/bdd-tests/E2ECateringApplicationLicenceIndigenousNationNameBrandingChange.cs
@@ -33,7 +33,7 @@ Scenario: Pay First Year Catering Licence and Submit Name Branding Change
     And I return to the dashboard
     And the application is approved
     And I click on the Licences tab for Catering
-    And I pay the licensing fee
+    And I pay the licensing fee for Catering
     And I click on the licence download link
     And I request a valid store name or branding change
     And the account is deleted
@@ -58,13 +58,6 @@ namespace bdd_tests
         public void And_I_view_the_dashboard_IN(string businessType)
         {
             CarlaLogin(businessType);
-        }
-
-
-        [And(@"I pay the licensing fee")]
-        public void click_pay_first_year_licensing_fee()
-        {
-            PayCateringLicenceFee();
         }
     }
 }
