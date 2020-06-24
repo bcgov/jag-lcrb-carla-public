8a64278ef04139d8d73155f0dcf559ca08fa58b0
ElizabethWolfe <ejwolfe@gmail.com>
2020-06-24 13:18:41 -0700

    Change re org structure test.

--- functional-tests/bdd-tests/TestBase.cs
+++ functional-tests/bdd-tests/TestBase.cs
@@ -3154,6 +3154,23 @@ namespace bdd_tests
         }
 
 
+        [And(@"the org structure is correct after payment")]
+        public void OrgStructureCorrectPayment()
+        {
+            // confirm that first individual is in correct positions
+            Assert.True(ngDriver.FindElement(By.XPath("//app-org-structure/div/div[4]/section/app-associate-list/div/table/tr[1]/td[1]/span[contains(.,'Same1')]")).Displayed);
+            Assert.True(ngDriver.FindElement(By.XPath("//app-org-structure/div/div[5]/section[1]/app-associate-list/div/table/tr/td[1]/span[contains(.,'Same1')]")).Displayed);
+
+            // confirm that second individual is in correct positions
+            Assert.True(ngDriver.FindElement(By.XPath("//*[@id='cdk-accordion-child-0']/div/section/app-org-structure/div/div[4]/section/app-associate-list/div/table/tr[1]/td[1]/span[contains(.,'Same2')]")).Displayed);
+            Assert.True(ngDriver.FindElement(By.XPath("//*[@id='cdk-accordion-child-0']/div/section/app-org-structure/div/div[5]/section[1]/app-associate-list/div/table/tr[1]/td[1]/span[contains(.,'Same2')]")).Displayed);
+
+            // confirm that third individual is in correct positions
+            Assert.True(ngDriver.FindElement(By.XPath("//*[@id='cdk-accordion-child-0']/div/section/app-org-structure/div/div[4]/section/app-associate-list/div/table/tr[2]/td[1]/span[contains(.,'Same3')]")).Displayed);
+            Assert.True(ngDriver.FindElement(By.XPath("//*[@id='cdk-accordion-child-0']/div/section/app-org-structure/div/div[5]/section[1]/app-associate-list/div/table/tr[2]/td[1]/span[contains(.,'Same3')]")).Displayed);
+        }
+
+
         [And(@"I remove the latest director and shareholder")]
         public void RemoveLatestDirectorShareholder()
         {
