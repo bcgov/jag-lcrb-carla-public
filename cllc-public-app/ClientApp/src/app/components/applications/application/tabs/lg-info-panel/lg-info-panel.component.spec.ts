5038f8caecf28ac22c847a5b5ec47f5914aea9a9
Tim Gunderson <tgunderson@gmail.com>
2020-06-26 15:39:19 -0700

    lg info panel component

---
+++ cllc-public-app/ClientApp/src/app/components/applications/application/tabs/lg-info-panel/lg-info-panel.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LgInfoPanelComponent } from './lg-info-panel.component';
+
+describe('LgInfoPanelComponent', () => {
+  let component: LgInfoPanelComponent;
+  let fixture: ComponentFixture<LgInfoPanelComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LgInfoPanelComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LgInfoPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
