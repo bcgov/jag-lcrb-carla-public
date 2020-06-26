288c489fe24b53b452a9f4f45ac6d773444b2ac8
Tim Gunderson <tgunderson@gmail.com>
2020-06-26 15:38:40 -0700

    additional PIDS section

---
+++ cllc-public-app/ClientApp/src/app/components/applications/application/tabs/additional-pids/additional-pids.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdditionalPidsComponent } from './additional-pids.component';
+
+describe('AdditionalPidsComponent', () => {
+  let component: AdditionalPidsComponent;
+  let fixture: ComponentFixture<AdditionalPidsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AdditionalPidsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdditionalPidsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
