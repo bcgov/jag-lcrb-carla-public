e4f858f495dec64a701143878c26f1e8b51a8156
Tim Gunderson <tgunderson@gmail.com>
2020-06-26 15:38:40 -0700

    additional PIDS section

---
+++ cllc-public-app/ClientApp/src/app/components/applications/application/tabs/additional-pids/additional-pids.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Application } from '@models/application.model';
+import { ApplicationDataService } from '@services/application-data.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBase } from '@shared/form-base';
+
+@Component({
+  selector: 'app-additional-pids',
+  templateUrl: './additional-pids.component.html',
+  styleUrls: ['./additional-pids.component.scss']
+})
+export class AdditionalPidsComponent extends FormBase implements OnInit {
+  @Input() application: Application;
+  validationMessages: string[];
+  form: FormGroup;
+
+  constructor(private applicationDataService: ApplicationDataService,
+    private fb: FormBuilder) {
+      super();
+     }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      hasMultiplePIDs: ['', []],
+      PIDList: ['', []]
+    });
+    this.form.patchValue(this.application);
+  }
+
+}
