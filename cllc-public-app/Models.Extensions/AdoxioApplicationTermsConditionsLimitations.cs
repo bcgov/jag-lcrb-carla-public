5dccb9d58b51be2f9965b99953352f7ac4f4271c
Brian Richter <brianorwhatever@gmail.com>
2020-07-02 16:23:09 -0700

    terms and conditions added to licences dashboard

---
+++ cllc-public-app/Models.Extensions/AdoxioApplicationTermsConditionsLimitations.cs
@@ -0,0 +1,30 @@
+﻿using Gov.Lclb.Cllb.Interfaces.Models;
+using Gov.Lclb.Cllb.Public.ViewModels;
+
+namespace Gov.Lclb.Cllb.Public.Models
+{
+    /// <summary>
+    /// ViewModel transforms.
+    /// </summary>
+    public static class AdoxioApplicationTermsConditionsLimitations
+    {  
+        public static ViewModels.TermsAndConditions ToViewModel(this MicrosoftDynamicsCRMadoxioApplicationtermsconditionslimitation term)
+        {
+            ViewModels.TermsAndConditions result = null;
+            if (term != null)
+            {
+                result = new ViewModels.TermsAndConditions();
+                if (term.AdoxioApplicationtermsconditionslimitationid != null)
+                {
+                    result.Id = term.AdoxioApplicationtermsconditionslimitationid.ToString();
+                }
+
+                result.LicenceId = term._adoxioLicenceValue;
+                result.Content = term.AdoxioTermsandconditions;
+            }
+            return result;
+        }
+
+    }
+
+}
