928c9b0c2400ad38cc1ba8e19655bfe7b8f72d1f
Tim Gunderson <tgunderson@gmail.com>
2020-07-07 13:47:13 -0700

    additional config

--- cllc-public-app/ViewModels/ApplicationTypeContent.cs
+++ cllc-public-app/ViewModels/ApplicationTypeContent.cs
@@ -13,6 +13,13 @@ namespace Gov.Lclb.Cllb.Public.ViewModels
         LocalGovernmentApproval = 845280005,
         FloorPlan = 845280006,
         SitePlan = 845280007,
+        SitePhotos = 845280008,
+        ValidInterest = 845280009,
+        LetterOfIntent = 845280010,
+        Zoning = 845280011,
+        ServiceArea = 845280012,
+        OutdoorArea = 845280013,
+        CapacityArea = 845280014,
         
     
     }
