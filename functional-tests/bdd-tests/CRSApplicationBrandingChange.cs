2c5ccc4ce00974342672883a1f134d039256db80
ElizabethWolfe <ejwolfe@gmail.com>
2020-07-12 09:03:32 -0700

    Rename files to clarify purpose.

--- functional-tests/bdd-tests/CRSApplicationBrandingChange.cs
+++ functional-tests/bdd-tests/CRSApplicationBrandingChange.cs
@@ -1,4 +1,19 @@
-﻿Feature: CRSApplicationNameBrandingChange
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: CRSApplicationNameBrandingChange
     As a logged in business user
     I want to submit a CRS Application for different business types
     And request a valid name or branding change for the approved application
@@ -116,3 +131,23 @@ Scenario: Sole Proprietorship CRS Name Branding Change
     And I request a valid store name or branding change for Cannabis
     And the account is deleted
     Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./CRSApplicationBrandingChange.feature")]
+    public sealed class CRSApplicationBrandingChange : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as an (.*)")]
+        public void I_view_the_dashboard_IN(string businessType)
+        {
+            CarlaLogin(businessType);
+        }
+
+        [Given(@"I am logged in to the dashboard as a (.*)")]
+        public void I_view_the_dashboard(string businessType)
+        {
+            CarlaLogin(businessType);
+        }
+    }
+}
