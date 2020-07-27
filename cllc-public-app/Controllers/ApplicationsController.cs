78f54cdd04b24a28ac305d64d6b7ce7474df0877
George Walker <george@walkersoftware.ca>
2020-07-27 11:22:07 -0700

    Fix for invalid licence sub category on create

--- cllc-public-app/Controllers/ApplicationsController.cs
+++ cllc-public-app/Controllers/ApplicationsController.cs
@@ -732,6 +732,13 @@ namespace Gov.Lclb.Cllb.Public.Controllers
             // copy received values to Dynamics Application
             adoxioApplication.CopyValues(item);
             adoxioApplication.AdoxioApplicanttype = (int?)item.ApplicantType;
+
+            // fix for an invalid licence sub category
+            if (adoxioApplication.AdoxioLicencesubcategory == 0)
+            {
+                adoxioApplication.AdoxioLicencesubcategory = null;
+            }
+
             try
             {
                 // set license type relationship 
