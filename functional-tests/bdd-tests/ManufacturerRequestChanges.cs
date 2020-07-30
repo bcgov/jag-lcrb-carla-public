2fa115510162a8b1108fbfe6925aa0d3fa9b3e29
ElizabethWolfe <ejwolfe@gmail.com>
2020-07-30 08:56:45 -0700

    Draft Gherkin for mfg licence tests.

---
+++ functional-tests/bdd-tests/ManufacturerRequestChanges.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: ManufacturerRequestChanges.feature
+    As a logged in business user
+    I want to request changes for a manufacturer licence
+
+@e2e @privatecorporation @manufacturer @winery
+Scenario: Private Corporation Winery Transfer Licence
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Manufacturer Licence
+    And I complete the Manufacturer application for a winery
+    And I enter the payment information
+    And I confirm the payment receipt for a Manufacturer Licence application
+    And I return to the dashboard
+    And the application is approved
+    And I pay the licensing fee for a Manufacturer application
+    And I request a transfer of ownership
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./ManufacturerRequestChanges.feature")]
+    public sealed class ManufacturerRequestChanges : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void I_view_the_dashboard(string businessType)
+        {
+            CarlaLogin(businessType);
+        }
+    }
+}
