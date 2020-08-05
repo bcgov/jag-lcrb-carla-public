39aeba3a5962db02ecc7bae37261e3ef556601bf
ElizabethWolfe <ejwolfe@gmail.com>
2020-08-05 13:35:43 -0700

    Tidy up.

--- functional-tests/smoke-tests/CRSApplicationPrivateCorp.cs
+++ functional-tests/smoke-tests/CRSApplicationPrivateCorp.cs
@@ -26,8 +26,8 @@ Scenario: Start Application
     And I review the organization structure for a private corporation
     And I click on the Submit Organization Information button
     And I complete the Cannabis Retail Store application for a private corporation
-    And I review the security screening requirements for a private corporation
-    And I click on the Pay for Application button
+    # And I review the security screening requirements for a private corporation
+    # And I click on the Pay for Application button
     And I enter the payment information
     And I confirm the payment receipt for a Cannabis Retail Store application
     And the account is deleted
