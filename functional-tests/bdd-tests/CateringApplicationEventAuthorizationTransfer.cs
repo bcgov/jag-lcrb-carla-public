878ff4bbe01bca71b55b9de5fb51ddb69d83f533
ElizabethWolfe <ejwolfe@gmail.com>
2020-08-07 14:42:25 -0700

    Uncomment Gherkin step re LGIN.

--- functional-tests/bdd-tests/CateringApplicationEventAuthorizationTransfer.cs
+++ functional-tests/bdd-tests/CateringApplicationEventAuthorizationTransfer.cs
@@ -13,7 +13,7 @@ using System.IO;
 using Xunit;
 
 /*
-  Feature: CateringApplicationEventAuthorizationTransfer
+   Feature: CateringApplicationEventAuthorizationTransfer
     As a logged in business user
     I want to pay the first year catering licence fee
     And submit an event authorization and transfer of ownership request for different business types
@@ -123,6 +123,46 @@ using Xunit;
     And I pay the licensing fee for Catering
     And I request an event authorization
     And I request a transfer of ownership
+    And the account is deleted
+    Then I see the login page
+
+ @e2e @catering @privatecorporation @validation
+ Scenario: Validation for Event Authorization 
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Catering
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the Submit Organization Information button
+    And I complete the Catering application
+    And I click on the Submit button
+    And I click on the Pay for Application button
+    And I enter the payment information
+    And the application is approved
+    And I pay the licensing fee for Catering
+    And I click on the Licences tab
+    And I click on the event authorization link
+    And I do not complete the application correctly
+    And the expected validation errors are thrown for an event authorization
+    And the account is deleted
+    Then I see the login page
+
+ @e2e @catering @privatecorporation @validation
+ Scenario: Validation for Catering Transfer of Ownership
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Catering
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the Submit Organization Information button
+    And I complete the Catering application
+    And I click on the Submit button
+    And I click on the Pay for Application button
+    And I enter the payment information
+    And the application is approved
+    And I pay the licensing fee for Catering
+    And I click on the Licences tab
+    And I click on the transfer of ownership link
+    And I do not complete the application correctly
+    And the expected validation errors are thrown for a Catering transfer of ownership
     And the account is deleted
     Then I see the login page
 */
