662415b6172ea68dc788001dd566fb4ce7773a0d
ElizabethWolfe <ejwolfe@gmail.com>
2020-08-10 08:17:04 -0700

    Refactor link methods.

--- functional-tests/bdd-tests/CRSApplicationStoreChanges.cs
+++ functional-tests/bdd-tests/CRSApplicationStoreChanges.cs
@@ -31,9 +31,11 @@ Scenario: Indigenous Nation Cannabis Store Changes
     And I enter the payment information
     And the application is approved
     And I pay the licensing fee for Cannabis
-    And I request a Cannabis store relocation
+    And I request a store relocation for Cannabis
     And I request a structural change
     And I request a transfer of ownership
+    And I confirm the relocation request is displayed on the dashboard
+    And I confirm the structural change request is displayed on the dashboard
     And the account is deleted
     Then I see the login page
 
@@ -50,9 +52,11 @@ Scenario: Partnership Cannabis Store Changes
     And I enter the payment information
     And the application is approved
     And I pay the licensing fee for Cannabis
-    And I request a Cannabis store relocation
+    And I request a store relocation for Cannabis
     And I request a structural change
     And I request a transfer of ownership
+    And I confirm the relocation request is displayed on the dashboard
+    And I confirm the structural change request is displayed on the dashboard
     And the account is deleted
     Then I see the login page
 
@@ -69,9 +73,11 @@ Scenario: Private Corporation Cannabis Store Changes
     And I enter the payment information
     And the application is approved
     And I pay the licensing fee for Cannabis
-    And I request a Cannabis store relocation
+    And I request a store relocation for Cannabis
     And I request a structural change
     And I request a transfer of ownership
+    And I confirm the relocation request is displayed on the dashboard
+    And I confirm the structural change request is displayed on the dashboard
     And the account is deleted
     Then I see the login page
 
@@ -88,9 +94,11 @@ Scenario: Public Corporation Cannabis Store Changes
     And I enter the payment information
     And the application is approved
     And I pay the licensing fee for Cannabis
-    And I request a Cannabis store relocation
+    And I request a store relocation for Cannabis
     And I request a structural change
     And I request a transfer of ownership
+    And I confirm the relocation request is displayed on the dashboard
+    And I confirm the structural change request is displayed on the dashboard
     And the account is deleted
     Then I see the login page
 
@@ -107,9 +115,11 @@ Scenario: Society Cannabis Store Changes
     And I enter the payment information
     And the application is approved
     And I pay the licensing fee for Cannabis
-    And I request a Cannabis store relocation
+    And I request a store relocation for Cannabis
     And I request a structural change
     And I request a transfer of ownership
+    And I confirm the relocation request is displayed on the dashboard
+    And I confirm the structural change request is displayed on the dashboard
     And the account is deleted
     Then I see the login page
 
@@ -126,9 +136,73 @@ Scenario: Sole Proprietorship Cannabis Store Changes
     And I enter the payment information
     And the application is approved
     And I pay the licensing fee for Cannabis
-    And I request a Cannabis store relocation
+    And I request a store relocation for Cannabis
     And I request a structural change
     And I request a transfer of ownership
+    And I confirm the relocation request is displayed on the dashboard
+    And I confirm the structural change request is displayed on the dashboard
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @privatecorporation @validation
+Scenario: Validation for CRS Store Relocation
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the Submit Organization Information button
+    And I complete the Cannabis Retail Store application for a private corporation
+    And I click on the Pay for Application button
+    And I enter the payment information
+    And the application is approved
+    And I pay the licensing fee for Cannabis
+    And I click on the Licences tab
+    And I click on the relocation application link
+    And I click on the Continue to Application button
+    And I do not complete the application correctly
+    And the expected validation errors are thrown for a store relocation application
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @privatecorporation @validation
+Scenario: Validation for CRS Structural Change
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the Submit Organization Information button
+    And I complete the Cannabis Retail Store application for a private corporation
+    And I click on the Pay for Application button
+    And I enter the payment information
+    And the application is approved
+    And I pay the licensing fee for Cannabis
+    And I click on the Licences tab
+    And I click on the link for structural change 
+    And I click on the Continue to Application button
+    And I do not complete the application correctly
+    And the expected validation errors are thrown for a structural change application
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @privatecorporation @validation
+Scenario: Validation for CRS Transfer of Ownership
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the Submit Organization Information button
+    And I complete the Cannabis Retail Store application for a private corporation
+    And I click on the Pay for Application button
+    And I enter the payment information
+    And the application is approved
+    And I pay the licensing fee for Cannabis
+    And I click on the Licences tab
+    And I click on the link for transfer of ownership
+    And I do not complete the application correctly
+    And the expected validation errors are thrown for a CRS transfer of ownership
     And the account is deleted
     Then I see the login page
 */
