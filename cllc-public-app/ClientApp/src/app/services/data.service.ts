13ded37b142a6286580960ab2461c62ccfa0f37f
George Walker <george@walkersoftware.ca>
2020-08-21 11:22:30 -0700

    LCSD-3620 payment improvements

--- cllc-public-app/ClientApp/src/app/services/data.service.ts
+++ cllc-public-app/ClientApp/src/app/services/data.service.ts
@@ -8,6 +8,25 @@ export class DataService {
     'Content-Type': 'application/json'
   });
 
+  handleErrorWith503(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    if (error.status === 503) {
+      return throwError(
+        '503');
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
 
   handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
