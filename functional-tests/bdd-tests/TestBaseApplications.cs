03fa77a185f0e8b467b0ce2e9321cc22d8faf3e0
ElizabethWolfe <ejwolfe@gmail.com>
2020-08-21 12:45:00 -0700

    Automation for structural alteration test.

--- functional-tests/bdd-tests/TestBaseApplications.cs
+++ functional-tests/bdd-tests/TestBaseApplications.cs
@@ -3569,6 +3569,69 @@ namespace bdd_tests
         }
 
 
+        [And(@"I request structural alterations to an approved lounge or special events area")]
+        public void StructuralAlterations()
+        {
+            /* 
+            Page Title: Licences
+            */
+
+            string structuralAlterations = "Structural Alterations to an Approved Lounge or Special Events Area";
+
+            // click on the Structural Alterations Application link
+            NgWebElement uiStructuralAlterations = ngDriver.FindElement(By.LinkText(structuralAlterations));
+            uiStructuralAlterations.Click();
+
+            ContinueToApplicationButton();
+
+            // create test data
+            string outdoorAreaDescription = "Sample outdoor area description";
+            string outdoorAreaCapacity = "10";
+            string capacityAreaOccupants = "20";
+
+            // find the upload test files in the bdd-tests\upload_files folder
+            var environment = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(environment).Parent.FullName;
+            string projectDirectory2 = Directory.GetParent(projectDirectory).Parent.FullName;
+
+            // upload the floor plan
+            string floorplanPath = Path.Combine(projectDirectory2 + Path.DirectorySeparatorChar + "bdd-tests" + Path.DirectorySeparatorChar + "upload_files" + Path.DirectorySeparatorChar + "floor_plan.pdf");
+            NgWebElement uiUploadFloorplan = ngDriver.FindElement(By.XPath("(//input[@type='file'])[2]"));
+            uiUploadFloorplan.SendKeys(floorplanPath);
+
+            // add outside area
+            NgWebElement uiOutdoorArea = ngDriver.FindElement(By.CssSelector("[formcontrolname='outsideAreas'] button"));
+            uiOutdoorArea.Click();
+
+            // enter the outdooor area description
+            NgWebElement uiOutdoorAreaDescription = ngDriver.FindElement(By.CssSelector("[formcontrolname='outsideAreas'] input[formcontrolname='areaLocation']"));
+            uiOutdoorAreaDescription.SendKeys(outdoorAreaDescription);
+
+            // enter the outdoor area occupant load
+            NgWebElement uiOutdoorAreaOccupantLoad = ngDriver.FindElement(By.CssSelector("[formcontrolname='outsideAreas'] input[formcontrolname='capacity']"));
+            uiOutdoorAreaOccupantLoad.SendKeys(outdoorAreaCapacity);
+
+            // enter capacity area occupant load
+            NgWebElement uiCapacityAreaOccupantLoad = ngDriver.FindElement(By.CssSelector("[formgroupname='capacityArea'] input[formcontrolname='capacity']"));
+            uiCapacityAreaOccupantLoad.SendKeys(capacityAreaOccupants);
+
+            // select the authorized to submit checkbox
+            NgWebElement uiAuthorizedToSubmit = ngDriver.FindElement(By.CssSelector("input[formcontrolname='authorizedToSubmit'][type='checkbox']"));
+            uiAuthorizedToSubmit.Click();
+
+            // select the signature agreement checkbox
+            NgWebElement uiSignatureAgreement = ngDriver.FindElement(By.CssSelector("input[formcontrolname='signatureAgreement'][type='checkbox']"));
+            uiSignatureAgreement.Click();
+
+            // click on the Submit & Pay button
+            ClickOnSubmitButton();
+
+            MakePayment();
+
+            System.Threading.Thread.Sleep(3000);
+
+        }
+
         [And(@"I request a location change")]
         public void LocationChange()
         {
