d0e3566bd4008820535a079f4e95738b74b1480c
ElizabethWolfe <ejwolfe@gmail.com>
2020-08-27 12:35:32 -0700

    Housekeeping.

---
+++ functional-tests/bdd-tests/TestBaseLGApproval.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+namespace bdd_tests
+{
+    public abstract partial class TestBase : Feature, IDisposable
+    {
+        [And(@"I approve the application for a (.*)")]
+        public void ApproveApplication(string applicationType)
+        {
+            if (applicationType == "picnic area")
+            {
+
+            }
+        }
+
+
+        [And(@"I read the response from the local government")]
+        public void ReadResponse()
+        {
+
+        }
+    }
+}
