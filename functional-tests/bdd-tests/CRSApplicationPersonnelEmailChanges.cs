96f391e8494932452d3cf8406e2b8e7f28dfd1e8
ElizabethWolfe <ejwolfe@gmail.com>
2020-09-01 12:50:15 -0700

    Split personnel tests.

--- functional-tests/bdd-tests/CRSApplicationPersonnelEmailChanges.cs
+++ functional-tests/bdd-tests/CRSApplicationPersonnelEmailChanges.cs
@@ -13,13 +13,13 @@ using System.IO;
 using Xunit;
 
 /*
-Feature: CRSApplicationPersonnelNameChanges
+Feature: CRSApplicationPersonnelEmailChanges
     As a logged in business user
     I want to submit a CRS Application for different business types
-    And request personnel email and name changes for the approved application
+    And request personnel email changes for the approved application
 
 @e2e @cannabis @partnership @crsemail
-Scenario: Partnership CRS Personnel Name Changes
+Scenario: Partnership CRS Personnel Email Changes
     Given I am logged in to the dashboard as a partnership
     And I click on the Start Application button for a Cannabis Retail Store
     And I complete the eligibility disclosure
@@ -32,15 +32,12 @@ Scenario: Partnership CRS Personnel Name Changes
     And the application is approved
     And I pay the licensing fee for Cannabis
     And I click on the link for Dashboard
-    And I request a personnel name change for a partnership
-    And I confirm the correct personnel name change fee for a Cannabis licence
-    And I click on the link for Dashboard
-    And I confirm that the director name has been updated
+    And I change a personnel email address for a partnership
     And the account is deleted
     Then I see the login page
 
 @e2e @cannabis @privatecorporation @crsemail
-Scenario: Private Corporation CRS Personnel Name Changes
+Scenario: Private Corporation CRS Personnel Email Changes
     Given I am logged in to the dashboard as a private corporation
     And I click on the Start Application button for a Cannabis Retail Store
     And I complete the eligibility disclosure
@@ -53,15 +50,12 @@ Scenario: Private Corporation CRS Personnel Name Changes
     And the application is approved
     And I pay the licensing fee for Cannabis
     And I click on the link for Dashboard
-    And I request a personnel name change for a private corporation
-    And I confirm the correct personnel name change fee for a Cannabis licence
-    And I click on the link for Dashboard
-    And I confirm that the director name has been updated
+    And I change a personnel email address for a private corporation
     And the account is deleted
     Then I see the login page
 
 @e2e @cannabis @publiccorporation @crsemailpubcorp
-Scenario: Public Corporation CRS Personnel Name Changes
+Scenario: Public Corporation CRS Personnel Email Changes
     Given I am logged in to the dashboard as a public corporation
     And I click on the Start Application button for a Cannabis Retail Store
     And I complete the eligibility disclosure
@@ -74,15 +68,12 @@ Scenario: Public Corporation CRS Personnel Name Changes
     And the application is approved
     And I pay the licensing fee for Cannabis
     And I click on the link for Dashboard
-    And I request a personnel name change for a public corporation
-    And I confirm the correct personnel name change fee for a Cannabis licence
-    And I click on the link for Dashboard
-    And I confirm that the director name has been updated
+    And I change a personnel email address for a public corporation
     And the account is deleted
     Then I see the login page
 
 @e2e @cannabis @society @crsemail2
-Scenario: Society CRS Personnel Name Changes
+Scenario: Society CRS Personnel Email Changes
     Given I am logged in to the dashboard as a society
     And I click on the Start Application button for a Cannabis Retail Store
     And I complete the eligibility disclosure
@@ -95,15 +86,12 @@ Scenario: Society CRS Personnel Name Changes
     And the application is approved
     And I pay the licensing fee for Cannabis
     And I click on the link for Dashboard
-    And I request a personnel name change for a society
-    And I confirm the correct personnel name change fee for a Cannabis licence
-    And I click on the link for Dashboard
-    And I confirm that the director name has been updated
+    And I change a personnel email address for a society
     And the account is deleted
     Then I see the login page
 
 @e2e @cannabis @soleproprietorship @crsemail
-Scenario: Sole Proprietorship CRS Personnel Name Changes
+Scenario: Sole Proprietorship CRS Personnel Email Changes
     Given I am logged in to the dashboard as a sole proprietorship
     And I click on the Start Application button for a Cannabis Retail Store
     And I complete the eligibility disclosure
@@ -116,18 +104,15 @@ Scenario: Sole Proprietorship CRS Personnel Name Changes
     And the application is approved
     And I pay the licensing fee for Cannabis
     And I click on the link for Dashboard
-    And I request a personnel name change for a sole proprietorship
-    And I confirm the correct personnel name change fee for a Cannabis licence
-    And I click on the link for Dashboard
-    And I confirm that the director name has been updated
+    And I change a personnel email address for a sole proprietorship
     And the account is deleted
     Then I see the login page
 */
 
 namespace bdd_tests
 {
-    [FeatureFile("./CRSApplicationPersonnelNameChanges.feature")]
-    public sealed class CRSApplicationPersonnelNameChanges : TestBase
+    [FeatureFile("./CRSApplicationPersonnelEmailChanges.feature")]
+    public sealed class CRSApplicationPersonnelEmailChanges : TestBase
     {
         [Given(@"I am logged in to the dashboard as a(.*)")]
         public void LogInToDashboard(string businessType)
