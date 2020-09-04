30b7b7835bab2462bace912b022600209d7e75a8
ElizabethWolfe <ejwolfe@gmail.com>
2020-09-04 10:05:21 -0700

    Draft of combined Catering and CRSApp test.

---
+++ functional-tests/bdd-tests/CombinedCateringCRSApplication.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: CRSApplication.feature
+    As a logged in business user
+    I want to submit a CRS Application for different business types
+
+@e2e @cannabis @indigenousnation @crsapp
+Scenario: Indigenous Nation CRS Application
+    Given I am logged in to the dashboard as an indigenous nation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for an indigenous nation
+    And I review the organization structure for an indigenous nation
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Retail Store application for an indigenous nation
+    And I review the security screening requirements for an indigenous nation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Retail Store application
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @partnership @crsapp
+Scenario: Partnership CRS Application
+    Given I am logged in to the dashboard as a partnership
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a partnership
+    And I review the organization structure for a partnership
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Retail Store application for a partnership
+    And I review the security screening requirements for a partnership
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Retail Store application
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @privatecorporation @crsapp
+Scenario: Private Corporation CRS Application
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Retail Store application for a private corporation
+    And I review the security screening requirements for a private corporation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Retail Store application
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @publiccorporation @crsapp
+Scenario: Public Corporation CRS Application
+    Given I am logged in to the dashboard as a public corporation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a public corporation
+    And I review the organization structure for a public corporation
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Retail Store application for a public corporation
+    And I review the security screening requirements for a public corporation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Retail Store application
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @society @crsapp
+Scenario: Society CRS Application
+    Given I am logged in to the dashboard as a society
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a society
+    And I review the organization structure for a society
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Retail Store application for a society
+    And I review the security screening requirements for a society
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Retail Store application
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @soleproprietorship @crsapp
+Scenario: Sole Proprietorship CRS Application
+    Given I am logged in to the dashboard as a sole proprietorship
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a sole proprietorship
+    And I review the organization structure for a sole proprietorship
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Retail Store application for a sole proprietorship
+    And I review the security screening requirements for a sole proprietorship
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Retail Store application
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @localgovernment @crsapp
+Scenario: Local Government CRS Application
+    Given I am logged in to the dashboard as a local government
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a local government
+    And I review the organization structure for a local government
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Retail Store application for a local government
+    And I review the security screening requirements for a local government
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Retail Store application
+    And the account is deleted
+    Then I see the login page
+
+@crsapp @validation @privatecorporation
+Scenario: Validation for Private Corporation CRS Application 
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the button for Submit Organization Information
+    And I do not complete the application correctly
+    And the expected validation errors are thrown for a Cannabis application
+    And the account is deleted
+    Then I see the login page
+
+@crsapp @validation indigenousnation
+Scenario: Validation for IN CRS Application
+    Given I am logged in to the dashboard as an indigenous nation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for an indigenous nation
+    And I review the organization structure for an indigenous nation
+    And I click on the button for Submit Organization Information
+    And I do not complete the application correctly
+    And the expected validation errors are thrown for an indigenous nation Cannabis application
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./CRSApplication.feature")]
+    public sealed class CRSApplication : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
---
+++ functional-tests/bdd-tests/CombinedCateringCRSApplication.feature
@@ -0,0 +1,30 @@
+﻿Feature: CombinedCateringCRSApplications.feature
+    As a logged in business user
+    I want to submit a CRS and Catering application for the same account
+
+@e2e @cannabis @privatecorporation @crsapp
+Scenario: Private Corporation CRS Application
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Retail Store application for a private corporation
+    And I review the security screening requirements for a private corporation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Retail Store application
+    And I click on the Dashboard tab
+    And I click on the Start Application button for Catering
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the button for Submit Organization Information
+    And I complete the Catering application
+    And I click on the Submit button
+    And I review the security screening requirements for a private corporation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
+    And the account is deleted
+    Then I see the login page
