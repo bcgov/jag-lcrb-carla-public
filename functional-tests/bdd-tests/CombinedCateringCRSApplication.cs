eb66c6893dc2155ed749fc40f21cc721109660cd
ElizabethWolfe <ejwolfe@gmail.com>
2020-09-04 10:08:52 -0700

    Housekeeping.

--- functional-tests/bdd-tests/CombinedCateringCRSApplication.cs
+++ functional-tests/bdd-tests/CombinedCateringCRSApplication.cs
@@ -13,41 +13,9 @@ using System.IO;
 using Xunit;
 
 /*
-Feature: CRSApplication.feature
+Feature: CombinedCateringCRSApplications.feature
     As a logged in business user
-    I want to submit a CRS Application for different business types
-
-@e2e @cannabis @indigenousnation @crsapp
-Scenario: Indigenous Nation CRS Application
-    Given I am logged in to the dashboard as an indigenous nation
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for an indigenous nation
-    And I review the organization structure for an indigenous nation
-    And I click on the button for Submit Organization Information
-    And I complete the Cannabis Retail Store application for an indigenous nation
-    And I review the security screening requirements for an indigenous nation
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And I confirm the payment receipt for a Cannabis Retail Store application
-    And the account is deleted
-    Then I see the login page
-
-@e2e @cannabis @partnership @crsapp
-Scenario: Partnership CRS Application
-    Given I am logged in to the dashboard as a partnership
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a partnership
-    And I review the organization structure for a partnership
-    And I click on the button for Submit Organization Information
-    And I complete the Cannabis Retail Store application for a partnership
-    And I review the security screening requirements for a partnership
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And I confirm the payment receipt for a Cannabis Retail Store application
-    And the account is deleted
-    Then I see the login page
+    I want to submit a CRS and Catering application for the same account
 
 @e2e @cannabis @privatecorporation @crsapp
 Scenario: Private Corporation CRS Application
@@ -62,104 +30,25 @@ Scenario: Private Corporation CRS Application
     And I click on the button for Pay for Application
     And I enter the payment information
     And I confirm the payment receipt for a Cannabis Retail Store application
-    And the account is deleted
-    Then I see the login page
-
-@e2e @cannabis @publiccorporation @crsapp
-Scenario: Public Corporation CRS Application
-    Given I am logged in to the dashboard as a public corporation
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a public corporation
-    And I review the organization structure for a public corporation
-    And I click on the button for Submit Organization Information
-    And I complete the Cannabis Retail Store application for a public corporation
-    And I review the security screening requirements for a public corporation
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And I confirm the payment receipt for a Cannabis Retail Store application
-    And the account is deleted
-    Then I see the login page
-
-@e2e @cannabis @society @crsapp
-Scenario: Society CRS Application
-    Given I am logged in to the dashboard as a society
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a society
-    And I review the organization structure for a society
-    And I click on the button for Submit Organization Information
-    And I complete the Cannabis Retail Store application for a society
-    And I review the security screening requirements for a society
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And I confirm the payment receipt for a Cannabis Retail Store application
-    And the account is deleted
-    Then I see the login page
-
-@e2e @cannabis @soleproprietorship @crsapp
-Scenario: Sole Proprietorship CRS Application
-    Given I am logged in to the dashboard as a sole proprietorship
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a sole proprietorship
-    And I review the organization structure for a sole proprietorship
-    And I click on the button for Submit Organization Information
-    And I complete the Cannabis Retail Store application for a sole proprietorship
-    And I review the security screening requirements for a sole proprietorship
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And I confirm the payment receipt for a Cannabis Retail Store application
-    And the account is deleted
-    Then I see the login page
-
-@e2e @cannabis @localgovernment @crsapp
-Scenario: Local Government CRS Application
-    Given I am logged in to the dashboard as a local government
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a local government
-    And I review the organization structure for a local government
-    And I click on the button for Submit Organization Information
-    And I complete the Cannabis Retail Store application for a local government
-    And I review the security screening requirements for a local government
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And I confirm the payment receipt for a Cannabis Retail Store application
-    And the account is deleted
-    Then I see the login page
-
-@crsapp @validation @privatecorporation
-Scenario: Validation for Private Corporation CRS Application 
-    Given I am logged in to the dashboard as a private corporation
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
+    And I click on the Dashboard tab
+    And I click on the Start Application button for Catering
     And I review the account profile for a private corporation
     And I review the organization structure for a private corporation
     And I click on the button for Submit Organization Information
-    And I do not complete the application correctly
-    And the expected validation errors are thrown for a Cannabis application
-    And the account is deleted
-    Then I see the login page
-
-@crsapp @validation indigenousnation
-Scenario: Validation for IN CRS Application
-    Given I am logged in to the dashboard as an indigenous nation
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for an indigenous nation
-    And I review the organization structure for an indigenous nation
-    And I click on the button for Submit Organization Information
-    And I do not complete the application correctly
-    And the expected validation errors are thrown for an indigenous nation Cannabis application
+    And I complete the Catering application
+    And I click on the Submit button
+    And I review the security screening requirements for a private corporation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
     And the account is deleted
     Then I see the login page
 */
 
 namespace bdd_tests
 {
-    [FeatureFile("./CRSApplication.feature")]
-    public sealed class CRSApplication : TestBase
+    [FeatureFile("./CombinedCateringCRSApplications.feature")]
+    public sealed class CombinedCateringCRSApplications : TestBase
     {
         [Given(@"I am logged in to the dashboard as a(.*)")]
         public void LogInToDashboard(string businessType)
