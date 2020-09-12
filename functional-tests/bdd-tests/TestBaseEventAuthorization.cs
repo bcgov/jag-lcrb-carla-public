eade79d1f753ed416335f215870234ad695dc2cd
ElizabethWolfe <ejwolfe@gmail.com>
2020-09-12 15:07:39 -0700

    Add in sleep re header loading.

--- functional-tests/bdd-tests/TestBaseEventAuthorization.cs
+++ functional-tests/bdd-tests/TestBaseEventAuthorization.cs
@@ -236,6 +236,8 @@ namespace bdd_tests
 
             Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'Licences')]")).Displayed);
 
+            System.Threading.Thread.Sleep(4000);
+
             // click on the Event History bar
             NgWebElement uiExpandEventHistory = ngDriver.FindElement(By.CssSelector(".mat-expansion-panel #mat-expansion-panel-header-2[role='button']"));
             uiExpandEventHistory.Click();
