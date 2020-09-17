da4f56e6a667ae4177ee8c5b8db96f02c9e76f42
Elizabeth Wolfe <elizabeth.wolfe@gov.bc.ca>
2020-09-17 10:09:22 -0700

    Review security screening tests.

---
+++ functional-tests/bdd-tests/ReviewSecurityScreening.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./ReviewSecurityScreening.feature")]
+    public sealed class ReviewSecurityScreening : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LoggedInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
