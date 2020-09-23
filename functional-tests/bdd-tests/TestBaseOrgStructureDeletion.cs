e3c6ebcb76c28218bce149382eefa0356cb7061b
ElizabethWolfe <ejwolfe@gmail.com>
2020-09-23 07:03:30 -0700

    Draft of org structure deletion tests.

---
+++ functional-tests/bdd-tests/TestBaseOrgStructureDeletion.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+namespace bdd_tests
+{
+    public abstract partial class TestBase : Feature, IDisposable
+    {
+ 
+    }
+}
