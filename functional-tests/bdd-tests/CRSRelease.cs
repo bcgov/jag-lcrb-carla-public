49064f78194b6f3fa75221f7414e4dc0a94b8285
ElizabethWolfe <ejwolfe@gmail.com>
2020-09-28 08:44:49 -0700

    Fix automation for CRS release test.

--- functional-tests/bdd-tests/CRSRelease.cs
+++ functional-tests/bdd-tests/CRSRelease.cs
@@ -30,6 +30,9 @@ Scenario: Check CRS Release status
     And I click on the button for Pay for Application
     And I enter the payment information
     And I confirm the payment receipt for a Cannabis Retail Store application
+    And the application is approved
+    And I pay the licensing fee for Cannabis
+    And I click on the Licences tab
     And I click on the button for CRS terms and conditions
     And the correct terms and conditions are displayed for CRS
     And I request a valid store name or branding change for Cannabis
