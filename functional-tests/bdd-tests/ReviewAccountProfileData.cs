73e82ccb2fd1d1a2328acb6c0c613265ee4a93a4
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-01 15:26:09 -0700

    Update tests and add try catch.

--- functional-tests/bdd-tests/ReviewAccountProfileData.cs
+++ functional-tests/bdd-tests/ReviewAccountProfileData.cs
@@ -1,11 +1,27 @@
-﻿Feature: ReviewAccountProfileValidation
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: ReviewAccountProfileValidation
     As a logged in business user
     I want to confirm the validation messages for the account profile
 
 @e2e @cannabis @privatecorporation @reviewaccount
 Scenario: Validation for Private Corporation Review Account Profile
     Given I am logged in to the dashboard as a private corporation
-    And I click on the link for Edit Account Profile
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
     And I click on the Submit button
     And the expected validation errors are thrown for an account profile
     And the account is deleted
@@ -14,7 +30,8 @@ Scenario: Validation for Private Corporation Review Account Profile
 @e2e @cannabis @partnership @reviewaccount
 Scenario: Validation for Partnership Review Account Profile
     Given I am logged in to the dashboard as a partnership
-    And I click on the link for Edit Account Profile
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
     And I click on the Submit button
     And the expected validation errors are thrown for an account profile
     And the account is deleted
@@ -23,7 +40,8 @@ Scenario: Validation for Partnership Review Account Profile
 @e2e @cannabis @publiccorporation @reviewaccount
 Scenario: Validation for Public Corporation Review Account Profile
     Given I am logged in to the dashboard as a public corporation
-    And I click on the link for Edit Account Profile
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
     And I click on the Submit button
     And the expected validation errors are thrown for an account profile
     And the account is deleted
@@ -32,7 +50,8 @@ Scenario: Validation for Public Corporation Review Account Profile
 @e2e @cannabis @society @reviewaccount
 Scenario: Validation for Society Review Account Profile
     Given I am logged in to the dashboard as a society
-    And I click on the link for Edit Account Profile
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
     And I click on the Submit button
     And the expected validation errors are thrown for an account profile
     And the account is deleted
@@ -41,7 +60,8 @@ Scenario: Validation for Society Review Account Profile
 @e2e @cannabis @soleproprietorship @reviewaccount
 Scenario: Validation for Sole Proprietorship Review Account Profile
     Given I am logged in to the dashboard as a sole proprietorship
-    And I click on the link for Edit Account Profile
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
     And I click on the Submit button   
     And the expected validation errors are thrown for an account profile
     And the account is deleted
@@ -50,7 +70,8 @@ Scenario: Validation for Sole Proprietorship Review Account Profile
  @e2e @cannabis @indigenousnation @reviewaccount
  Scenario: Validation for Indigenous Nation Review Account Profile
     Given I am logged in to the dashboard as an indigenous nation
-    And I click on the link for Edit Account Profile
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
     And I click on the Submit button   
     And the expected validation errors are thrown for an account profile
     And the account is deleted
@@ -59,8 +80,36 @@ Scenario: Validation for Sole Proprietorship Review Account Profile
  @e2e @cannabis @localgovernment @reviewaccount
  Scenario: Validation for Local Government Review Account Profile
     Given I am logged in to the dashboard as a local government
-    And I click on the link for Edit Account Profile
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
     And I click on the Submit button   
     And the expected validation errors are thrown for an account profile
     And the account is deleted
     Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./ReviewAccountProfileValidation.feature")]
+    [Collection("Cannabis")]
+    public sealed class ReviewAccountProfileValidation : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LoggedInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
