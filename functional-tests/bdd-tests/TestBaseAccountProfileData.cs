36675f74c32b1795809427cb433f3359b7ad91b0
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-05 10:54:41 -0700

    Add connection checks.

--- functional-tests/bdd-tests/TestBaseAccountProfileData.cs
+++ functional-tests/bdd-tests/TestBaseAccountProfileData.cs
@@ -22,7 +22,7 @@ namespace bdd_tests
     {
         [And(@"the correct data is displayed for a(.*)")]
         public void AccountProfileData(string bizType)
-        {           
+        {
             // check legal name field is populated
             NgWebElement uiLegalName = ngDriver.FindElement(By.CssSelector("div:nth-of-type(2) > div:nth-of-type(1) > div > div > div:nth-of-type(1) > app-field:nth-of-type(1) > section > div > section > input"));
             string fieldValueLegalName = uiLegalName.GetAttribute("value");
@@ -169,16 +169,54 @@ namespace bdd_tests
             NgWebElement uiEmailAddress1 = ngDriver.FindElement(By.CssSelector("input[formcontrolname='emailaddress1']"));
             Assert.True(uiEmailAddress1.GetAttribute("value") == "automated@test.com");
 
-            // check financial interest details are correct
-            NgWebElement uiLiquorFinancialInterestDetails = ngDriver.FindElement(By.CssSelector("textarea[formcontrolname='liquorFinancialInterestDetails']"));
-            Assert.True(uiLiquorFinancialInterestDetails.GetAttribute("value") == "Details of the financial interest (automated test).");
-
             // check indigenous nation connection details are correct
-            if (bizType == "n indigenous nation account profile")
+            if ((bizType == "n indigenous nation account profile") || (bizType == " local government account profile"))
             {
                 NgWebElement uiINConnectionToFederalProducerDetails = ngDriver.FindElement(By.CssSelector("textarea[formcontrolname='iNConnectionToFederalProducerDetails']"));
                 Assert.True(uiINConnectionToFederalProducerDetails.GetAttribute("value") == "Name and details of federal producer (automated test) for IN/local government.");
             }
+
+            // check partnership connection details are correct
+            if (bizType == " partnership")
+            {
+                NgWebElement uiDetailsFederalProducer2 = ngDriver.FindElement(By.CssSelector("textarea[formcontrolname='partnersConnectionFederalProducerDetails']"));
+                Assert.True(uiDetailsFederalProducer2.GetAttribute("value") == "The name of the federal producer and details of the connection (partnership).");
+            }
+
+            // check federal producer connection to corporation details are correct
+            if ((bizType == " partnership") || (bizType == " private corporation") || (bizType == " sole proprietorship") || (bizType == " university"))
+            {
+                NgWebElement uiDetailsFederalProducer3 = ngDriver.FindElement(By.CssSelector("textarea[formcontrolname='federalProducerConnectionToCorpDetails']"));
+                Assert.True(uiDetailsFederalProducer3.GetAttribute("value") == "Name and details of federal producer connection to corporation.");
+            }
+
+            // check corporation connection to federal producer details are correct
+            if ((bizType == " private corporation") || (bizType == " public corporation") || (bizType == " sole proprietorship") || (bizType == " university"))
+            {
+                NgWebElement uiDetailsFederalProducer2 = ngDriver.FindElement(By.CssSelector("textarea[formcontrolname='corpConnectionFederalProducerDetails']"));
+                Assert.True(uiDetailsFederalProducer2.GetAttribute("value") == "The name of the federal producer and details of the connection.");
+            }
+
+            // check public corporation shareholder and family relationship details are correct
+            if (bizType == " public corporation")
+            {
+                NgWebElement uiShareholderDetails = ngDriver.FindElement(By.CssSelector("textarea[formcontrolname='share20PlusConnectionProducerDetails']"));
+                Assert.True(uiShareholderDetails.GetAttribute("value") == "Details of shareholder relationship.");
+
+                NgWebElement uiFamilyConnectionDetails = ngDriver.FindElement(By.CssSelector("textarea[formcontrolname='share20PlusFamilyConnectionProducerDetail']"));
+                Assert.True(uiFamilyConnectionDetails.GetAttribute("value") == "Details of family relationship (automated test).");
+            }
+
+            // check society federal producer details are correct
+            if (bizType == " society")
+            {
+                NgWebElement uiSocietyConnectionDetails = ngDriver.FindElement(By.CssSelector("textarea[formcontrolname='societyConnectionFederalProducerDetails']"));
+                Assert.True(uiSocietyConnectionDetails.GetAttribute("value") == "Details of society/federal producer relationship.");
+            }
+
+            // check financial interest details are correct
+            NgWebElement uiLiquorFinancialInterestDetails = ngDriver.FindElement(By.CssSelector("textarea[formcontrolname='liquorFinancialInterestDetails']"));
+            Assert.True(uiLiquorFinancialInterestDetails.GetAttribute("value") == "Details of the financial interest (automated test).");
         }
     }
 }
