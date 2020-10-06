37336eb216bbf124c185aadc3ff0c5ed9c510ee4
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-06 09:44:12 -0700

    Fix workflow for org structure validation tests.

--- functional-tests/bdd-tests/ReviewOrgStructureValidation.cs
+++ functional-tests/bdd-tests/ReviewOrgStructureValidation.cs
@@ -20,9 +20,7 @@ Feature: ReviewOrgStructureValidation
 @e2e @cannabis @partnership @orgstructure
 Scenario: Validation for Partnership Org Structure
     Given I am logged in to the dashboard as a partnership
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a partnership
+    And I click on the Complete Organization Information button
     And I click on the button for Submit Organization Information
     And the expected validation errors are thrown for a partnership org structure
     And the account is deleted
@@ -31,9 +29,7 @@ Scenario: Validation for Partnership Org Structure
 @e2e @cannabis @privatecorporation @orgstructure
 Scenario: Validation for Private Corporation Org Structure
     Given I am logged in to the dashboard as a private corporation
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a private corporation
+    And I click on the Complete Organization Information button
     And I click on the button for Submit Organization Information
     And the expected validation errors are thrown for a private corporation org structure
     And the account is deleted
@@ -42,9 +38,7 @@ Scenario: Validation for Private Corporation Org Structure
 @e2e @cannabis @publiccorporation @orgstructure
 Scenario: Validation for Public Corporation Org Structure
     Given I am logged in to the dashboard as a public corporation
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a public corporation
+    And I click on the Complete Organization Information button
     And I click on the button for Submit Organization Information
     And the expected validation errors are thrown for a public corporation org structure
     And the account is deleted
@@ -53,9 +47,7 @@ Scenario: Validation for Public Corporation Org Structure
 @e2e @cannabis @society @orgstructure
 Scenario: Validation for Society Org Structure
     Given I am logged in to the dashboard as a society
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a society
+    And I click on the Complete Organization Information button
     And I click on the button for Submit Organization Information
     And the expected validation errors are thrown for a society org structure
     And the account is deleted
@@ -64,9 +56,7 @@ Scenario: Validation for Society Org Structure
 @e2e @cannabis @soleproprietorship @orgstructure
 Scenario: Validation for Sole Proprietorship Org Structure
     Given I am logged in to the dashboard as a sole proprietorship
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a sole proprietorship
+    And I click on the Complete Organization Information button
     And I click on the button for Submit Organization Information
     And the expected validation errors are thrown for a sole proprietorship org structure
     And the account is deleted
@@ -76,9 +66,7 @@ Scenario: Validation for Sole Proprietorship Org Structure
 # @e2e @cannabis @indigenousnation @orgstructure
 # Scenario: Validation for Indigenous Nation Org Structure
 #    Given I am logged in to the dashboard as an indigenous nation
-#    And I click on the Start Application button for a Cannabis Retail Store
-#    And I complete the eligibility disclosure
-#    And I review the account profile for an indigenous nation
+#    And I click on the Complete Organization Information button
 #    And I click on the button for Submit Organization Information
 #    And the expected validation errors are thrown for an indigenous nation org structure
 #    And the account is deleted
@@ -88,9 +76,7 @@ Scenario: Validation for Sole Proprietorship Org Structure
 # @e2e @cannabis @localgovernment @orgstructure
 # Scenario: Validation for Local Government Org Structure
 #    Given I am logged in to the dashboard as a local government
-#    And I click on the Start Application button for a Cannabis Retail Store
-#    And I complete the eligibility disclosure
-#    And I review the account profile for a local government
+#    And I click on the Complete Organization Information button
 #    And I click on the button for Submit Organization Information
 #    And the expected validation errors are thrown for a local government org structure
 #    And the account is deleted
