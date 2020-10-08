8c940acfd2aa9a25f20d2005c095764461c0152a
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-08 10:28:41 -0700

    Fix Gherkin.

--- functional-tests/bdd-tests/ManufacturerApplication.cs
+++ functional-tests/bdd-tests/ManufacturerApplication.cs
@@ -26,6 +26,7 @@ Scenario: Private Corporation Winery Manufacturer Application
     And I click on the button for Submit Organization Information
     And I complete the Manufacturer application for a winery
     And I review the security screening requirements for a private corporation
+    And I click on the button for Pay for Application
     And I enter the payment information
     And I confirm the payment receipt for a Manufacturer Licence application
     And the account is deleted
@@ -40,6 +41,7 @@ Scenario: Private Corporation Distillery Manufacturer Application
     And I click on the button for Submit Organization Information
     And I complete the Manufacturer application for a distillery
     And I review the security screening requirements for a private corporation
+    And I click on the button for Pay for Application
     And I enter the payment information
     And I confirm the payment receipt for a Manufacturer Licence application
     And the account is deleted
@@ -54,6 +56,7 @@ Scenario: Private Corporation Brewery Manufacturer Application
     And I click on the button for Submit Organization Information
     And I complete the Manufacturer application for a brewery
     And I review the security screening requirements for a private corporation
+    And I click on the button for Pay for Application
     And I enter the payment information
     And I confirm the payment receipt for a Manufacturer Licence application
     And the account is deleted
@@ -68,6 +71,7 @@ Scenario: Private Corporation Co-packer Manufacturer Application
     And I click on the button for Submit Organization Information
     And I complete the Manufacturer application for a co-packer
     And I review the security screening requirements for a private corporation
+    And I click on the button for Pay for Application
     And I enter the payment information
     And I confirm the payment receipt for a Manufacturer Licence application
     And the account is deleted
