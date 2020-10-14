e1a4cfeabb2647c2a77815e000cabd0efa205204
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-14 08:46:28 -0700

    Work on Manufacturer validation tests.

--- functional-tests/bdd-tests/ManufacturerPicnicAreaEndorsement.cs
+++ functional-tests/bdd-tests/ManufacturerPicnicAreaEndorsement.cs
@@ -88,28 +88,6 @@ Scenario: Co-packer Picnic Area Endorsement Application
     And I request a picnic area endorsement
     And the account is deleted
     Then I see the login page
-
-@e2e @privatecorporation @manufacturer @validation
-Scenario: Validation for Manufacturer Picnic Area Endorsement
-    Given I am logged in to the dashboard as a private corporation
-    And I click on the Start Application button for a Manufacturer Licence
-    And I review the account profile for a private corporation
-    And I review the organization structure for a private corporation
-    And I click on the button for Submit Organization Information
-    And I complete the Manufacturer application for a winery
-     And I review the security screening requirements for a private corporation
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And I confirm the payment receipt for a Manufacturer Licence application
-    And the application is approved
-    And I pay the licensing fee for a Manufacturer application
-    And I click on the Licences tab
-    And I click on the link for Picnic Area Endorsement Application
-    And I click on the Continue to Application button
-    And I do not complete the application correctly
-    And the expected validation errors are thrown for a picnic area endorsement
-    And the account is deleted
-    Then I see the login page
 */
 
 namespace bdd_tests
