e55ca5f82897b4742d9c38a722b8e9787c5b3f9e
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-14 08:46:28 -0700

    Work on Manufacturer validation tests.

---
+++ functional-tests/bdd-tests/ValidationManufacturer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./ValidationManufacturer.feature")]
+    [Collection("Liquor")]
+    public sealed class ValidationManufacturer : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
