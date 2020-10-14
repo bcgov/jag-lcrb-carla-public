903752431b976b300eb5e3e7266467ba06640f49
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-14 09:49:57 -0700

    Group Catering validation together.

--- functional-tests/bdd-tests/CateringApplicationLicenseeRepresentative.cs
+++ functional-tests/bdd-tests/CateringApplicationLicenseeRepresentative.cs
@@ -34,26 +34,6 @@ using Xunit;
     And I request a licensee representative
     And the account is deleted
     Then I see the login page
-
- @e2e @licenseerep @validation
- Scenario: Validation for Licensee Representative Request
-    Given I am logged in to the dashboard as a private corporation
-    And I click on the Start Application button for Catering
-    And I review the account profile for a private corporation
-    And I review the organization structure for a private corporation
-    And I click on the button for Submit Organization Information
-    And I complete the Catering application
-    And I click on the Submit button
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And the application is approved
-    And I pay the licensing fee for Catering
-    And I click on the Licences tab
-    And I click on the link for Add Licensee Representative
-    And I do not complete the application correctly
-    And the expected validation errors are thrown for a licensee representative
-    And the account is deleted
-    Then I see the login page
 */
 
 namespace bdd_tests
