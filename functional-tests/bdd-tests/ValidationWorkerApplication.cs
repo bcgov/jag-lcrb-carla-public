3765831a87c94adc1b731df463ffbf5c2b8ff197
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-15 07:33:03 -0700

    Split out worker validation test.

--- functional-tests/bdd-tests/ValidationWorkerApplication.cs
+++ functional-tests/bdd-tests/ValidationWorkerApplication.cs
@@ -13,31 +13,31 @@ using System.IO;
 using Xunit;
 
 /*
-Feature: WorkerApplication
+Feature: ValidationWorkerApplication
     As a logged in worker applicant
     I want to submit a cannabis worker application
 
-@e2e @worker
-Scenario: Worker Application
+@e2e @worker @validation
+Scenario: Validation for Worker Application
     Given I login with no terms
     And the account is deleted
     And I am logged in to the dashboard
     And I click on my name
+    And I do not complete Step 1 of the application
+    And the Step 1 error messages are displayed
     And I complete Step 1 of the application
-    And I complete Step 2 of the application
+    And I do not complete Step 2 of the application
     And I click on the Submit & Pay button
-    And I enter the payment information
-    And I return to the dashboard
-    And the dashboard has a new status
+    And the Step 2 error messages are displayed
     And the account is deleted
     Then I see the login page
 */
 
 namespace bdd_tests
 {
-    [FeatureFile("./WorkerApplication.feature")]
+    [FeatureFile("./ValidationWorkerApplication.feature")]
     [Collection("General")]
-    public sealed class WorkerApplication : TestBaseWorker
+    public sealed class ValidationWorkerApplication : TestBaseWorker
     {
 
         [Given(@"I login with no terms")]
