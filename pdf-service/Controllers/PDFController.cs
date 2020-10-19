6d1cc894ef65e1001c9136266472e92184a1cdbd
George Walker <george@walkersoftware.ca>
2020-10-19 13:06:40 -0700

    Adjustments to PDF view

--- pdf-service/Controllers/PDFController.cs
+++ pdf-service/Controllers/PDFController.cs
@@ -50,7 +50,7 @@ namespace PDF.Controllers
                 _generatePdf.SetConvertOptions(new ConvertOptions
                 {
                     PageSize = Size.Letter,
-                    PageMargins = new Margins(5,5,5,5)
+                    PageMargins = new Margins(5,0,5,15)
                 });
                 
                 var pdf = await _generatePdf.GetPdfViewInHtml(html);
