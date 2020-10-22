af49d0c50678d3b46a41357456bd56f096456050
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-22 08:47:06 -0700

    Draft of Catering renewal test.

---
+++ functional-tests/bdd-tests/CateringApplicationLicensingRenewal.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: CateringApplicationLicensingRenewal
+    As a logged in business user
+    I want to pay the first year licensing fee for an approved Catering Application
+    And renew the licence
+
+@e2e @cannabis @privatecorporation @licencerenewal
+Scenario: Negative Catering Licence Renewal
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Catering
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the button for Submit Organization Information
+    And I complete the Catering application
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And the application is approved
+    And I pay the licensing fee for Cannabis
+    And the expiry date is changed to today
+    And I renew the licence with negative responses
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @privatecorporation @licencerenewal
+Scenario: Positive Catering Licence Renewal
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Catering
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I complete the Catering application
+    And I complete the Cannabis Retail Store application for a private corporation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And the application is approved
+    And I pay the licensing fee for Cannabis
+    And the expiry date is changed to today
+    And I renew the licence with positive responses
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./CateringApplicationLicensingRenewal.feature")]
+    [Collection("Cannabis")]
+    public sealed class CateringApplicationLicensingRenewal : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LoggedInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
---
+++ functional-tests/bdd-tests/CateringApplicationLicensingRenewal.feature
@@ -0,0 +1,38 @@
+﻿Feature: CateringApplicationLicensingRenewal
+    As a logged in business user
+    I want to pay the first year licensing fee for an approved Catering Application
+    And renew the licence
+
+@e2e @cannabis @privatecorporation @licencerenewal
+Scenario: Negative Catering Licence Renewal
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Catering
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the button for Submit Organization Information
+    And I complete the Catering application
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And the application is approved
+    And I pay the licensing fee for Cannabis
+    And the expiry date is changed to today
+    And I renew the licence with negative responses
+    And the account is deleted
+    Then I see the login page
+
+@e2e @cannabis @privatecorporation @licencerenewal
+Scenario: Positive Catering Licence Renewal
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Catering
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I complete the Catering application
+    And I complete the Cannabis Retail Store application for a private corporation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And the application is approved
+    And I pay the licensing fee for Cannabis
+    And the expiry date is changed to today
+    And I renew the licence with positive responses
+    And the account is deleted
+    Then I see the login page
