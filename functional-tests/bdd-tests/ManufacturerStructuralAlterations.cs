169dbc77a4e1253e871d13e83035757673d80e14
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-22 08:12:41 -0700

    Add dashboard check for mfg structural change.

--- functional-tests/bdd-tests/ManufacturerStructuralAlterations.cs
+++ functional-tests/bdd-tests/ManufacturerStructuralAlterations.cs
@@ -32,6 +32,8 @@ Scenario: Winery Structural Alterations to an Approved Lounge or Special Events 
     And the application is approved
     And I pay the licensing fee for a Manufacturer application
     And I request structural alterations to an approved lounge or special events area
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
     And the account is deleted
     Then I see the login page
 
@@ -50,6 +52,8 @@ Scenario: Brewery Structural Alterations to an Approved Lounge or Special Events
     And the application is approved
     And I pay the licensing fee for a Manufacturer application
     And I request structural alterations to an approved lounge or special events area
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
     And the account is deleted
     Then I see the login page
 
@@ -68,6 +72,8 @@ Scenario: Distillery Structural Alterations to an Approved Lounge or Special Eve
     And the application is approved
     And I pay the licensing fee for a Manufacturer application
     And I request structural alterations to an approved lounge or special events area
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
     And the account is deleted
     Then I see the login page
 
@@ -86,6 +92,8 @@ Scenario: Co-packer Structural Alterations to an Approved Lounge or Special Even
     And the application is approved
     And I pay the licensing fee for a Manufacturer application
     And I request structural alterations to an approved lounge or special events area
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
     And the account is deleted
     Then I see the login page
 */
