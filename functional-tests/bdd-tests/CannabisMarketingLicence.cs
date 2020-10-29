a5c1c6dd2211dd2b3b76119bbc79caca61516e74
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-29 14:17:06 -0700

    Draft of Cannabis Marketing Licence tests.

---
+++ functional-tests/bdd-tests/CannabisMarketingLicence.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./CannabisMarketingLicence.feature")]
+    [Collection("Liquor")]
+    public sealed class CannabisMarketingLicence : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
