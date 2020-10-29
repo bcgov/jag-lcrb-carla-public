053e703430de78381045e417410fe79e6ddedd9b
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-29 10:22:54 -0700

    Draft pdf verification scenario.

--- functional-tests/bdd-tests/ManufacturerRelease.cs
+++ functional-tests/bdd-tests/ManufacturerRelease.cs
@@ -13,85 +13,84 @@ using System.IO;
 using Xunit;
 
 /*
-Feature: CateringRelease
+Feature: ManufacturerRelease
     As a logged in business user
-    I want to confirm that the Catering functionality is ready for release
+    I want to confirm that the Manufacturer functionality is ready for release
 
-@validation @privatecorporation @release
-Scenario: Private Corporation Catering Release
+@e2e @privatecorporation @release @winery
+Scenario: Private Corporation Manufacturer Release 
     Given I am logged in to the dashboard as a private corporation
-    And I click on the Start Application button for Catering
+    And I click on the Start Application button for a Manufacturer Licence
     And I review the account profile for a private corporation
     And I review the organization structure for a private corporation
     And I click on the button for Submit Organization Information
-    And I complete the Catering application
-    And I click on the Submit button
+    And I complete the Manufacturer application for a winery
     And I review the security screening requirements for a private corporation
     And I click on the button for Pay for Application
     And I enter the payment information
+    And I confirm the payment receipt for a Manufacturer Licence application
     And the application is approved
-    And I click on the Licences tab
-    And I pay the licensing fee for Catering
-    And I click on the button for Catering terms and conditions
-    And the correct terms and conditions are displayed for Catering
+    And I pay the licensing fee for a Manufacturer application
     And I click on the link for Download Licence
-    And I request a valid store name or branding change for Catering
+    And I request a valid store name or branding change for Manufacturing
     And I click on the Licences tab
-    And I request a licensee representative
-    And I click on the link for Dashboard
-    And I request a personnel name change for a private corporation
-    And I click on the link for Dashboard
-    And I confirm that the director name has been updated
-    And I click on the Licences tab
-    And I request a store relocation for Catering
-    And I click on the Licences tab
-    And I request an event authorization that doesn't require approval
-    And the event history is updated correctly for an application without approval
-    # And I click on the Licences tab
-    # And I request a transfer of ownership
     And the expiry date is changed to today
-    And I renew the licence with positive responses for Catering
+    And I renew the licence with positive responses for a winery
     And I click on the Licences tab
+    And I request a facility structural change
+    And I click on the Licences tab
+    And I request a location change
+    And I click on the Licences tab
+    And I request a lounge area endorsement
+    And I click on the Licences tab
+    And I request an on-site store endorsement
+    And I click on the Licences tab
+    And I request a picnic area endorsement
+    And I click on the Licences tab
+    And I request a special event area endorsement
+    And I click on the Licences tab
+    And I request structural alterations to an approved lounge or special events area
     And I request a third party operator
+    And I request a transfer of ownership
     And the account is deleted
     Then I see the login page
 
-@validation @soleproprietorship @release
-Scenario: Sole Proprietorship Catering Release
+@e2e @soleproprietorship @release @winery
+Scenario: Sole Proprietorship Manufacturer Release 
     Given I am logged in to the dashboard as a sole proprietorship
-    And I click on the Start Application button for Catering
+    And I click on the Start Application button for a Manufacturer Licence
     And I review the account profile for a sole proprietorship
     And I review the organization structure for a sole proprietorship
     And I click on the button for Submit Organization Information
-    And I complete the Catering application
-    And I click on the Submit button
+    And I complete the Manufacturer application for a winery
     And I review the security screening requirements for a sole proprietorship
     And I click on the button for Pay for Application
     And I enter the payment information
+    And I confirm the payment receipt for a Manufacturer Licence application
     And the application is approved
-    And I click on the Licences tab
-    And I pay the licensing fee for Catering
-    And I click on the button for Catering terms and conditions
-    And the correct terms and conditions are displayed for Catering
+    And I pay the licensing fee for a Manufacturer application
     And I click on the link for Download Licence
-    And I request a valid store name or branding change for Catering
+    And I request a valid store name or branding change for Manufacturing
     And I click on the Licences tab
-    And I request a licensee representative
-    And I click on the link for Dashboard
-    And I request a personnel name change for a sole proprietorship
-    And I click on the link for Dashboard
-    And I confirm that the director name has been updated
-    And I click on the Licences tab
-    And I request a store relocation for Catering
-    And I click on the Licences tab
-    And I request an event authorization that doesn't require approval
-    And the event history is updated correctly for an application without approval
-    # And I click on the Licences tab
-    # And I request a transfer of ownership
     And the expiry date is changed to today
-    And I renew the licence with positive responses for Catering
+    And I renew the licence with positive responses for a winery
+    And I click on the Licences tab
+    And I request a facility structural change
+    And I click on the Licences tab
+    And I request a location change
+    And I click on the Licences tab
+    And I request a lounge area endorsement
+    And I click on the Licences tab
+    And I request an on-site store endorsement
+    And I click on the Licences tab
+    And I request a picnic area endorsement
+    And I click on the Licences tab
+    And I request a special event area endorsement
+    And I click on the Licences tab
+    And I request structural alterations to an approved lounge or special events area
     And I click on the Licences tab
     And I request a third party operator
+    And I request a transfer of ownership
     And the account is deleted
     Then I see the login page
 */
