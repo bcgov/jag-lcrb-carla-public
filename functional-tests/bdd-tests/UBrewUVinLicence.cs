91fa8f387d4e91980ec4030bc8f3c9bade90f979
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-29 16:05:57 -0700

    Draft of UBrew/UVin test cases.

---
+++ functional-tests/bdd-tests/UBrewUVinLicence.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: UBrewUVinLicence
+    As a logged in business user
+    I want to submit a UBrew / UVin Licence for different business types
+
+@e2e @ubrewuvin @indigenousnation 
+Scenario: Indigenous Nation UBrew / UVin Application
+    Given I am logged in to the dashboard as an indigenous nation
+    And I click on the Start Application button for a UBrew UVin application
+    And I review the account profile for an indigenous nation
+    And I review the organization structure for an indigenous nation
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Marketing application for an indigenous nation
+    And I click on the Submit button
+    And I review the security screening requirements for an indigenous nation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@e2e @ubrewuvin @partnership 
+Scenario: Partnership UBrew / UVin Application
+    Given I am logged in to the dashboard as a partnership
+    And I click on the Start Application button for a UBrew UVin application
+    And I review the account profile for a partnership
+    And I review the organization structure for a partnership
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Marketing application for a partnership
+    And I click on the Submit button
+    And I review the security screening requirements for a partnership
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@e2e @ubrewuvin @privatecorporation 
+Scenario: Private Corporation UBrew / UVin Application
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a UBrew UVin application
+    And I review the account profile for a private corporation
+    And I review the organization structure for a private corporation
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Marketing application for a private corporation
+    And I click on the Submit button
+    And I review the security screening requirements for a private corporation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@e2e @ubrewuvin @publiccorporation 
+Scenario: Public Corporation UBrew / UVin Application
+    Given I am logged in to the dashboard as a public corporation
+    And I click on the Start Application button for a UBrew UVin application
+    And I review the account profile for a public corporation
+    And I review the organization structure for a public corporation
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Marketing application for a public corporation
+    And I click on the Submit button
+    And I review the security screening requirements for a public corporation
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@e2e @ubrewuvin @society 
+Scenario: Society UBrew / UVin Application
+    Given I am logged in to the dashboard as a society
+    And I click on the Start Application button for a UBrew UVin application
+    And I review the account profile for a society
+    And I review the organization structure for a society
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Marketing application for a society
+    And I click on the Submit button
+    And I review the security screening requirements for a society
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@e2e @ubrewuvin @soleproprietorship 
+Scenario: Sole Proprietorship UBrew / UVin Application
+    Given I am logged in to the dashboard as a sole proprietorship
+    And I click on the Start Application button for a UBrew UVin application
+    And I review the account profile for a sole proprietorship
+    And I review the organization structure for a sole proprietorship
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Marketing application for a sole proprietorship
+    And I click on the Submit button
+    And I review the security screening requirements for a sole proprietorship
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@e2e @ubrewuvin @localgovernment 
+Scenario: Local Government UBrew / UVin Application
+    Given I am logged in to the dashboard as a local government
+    And I click on the Start Application button for a UBrew UVin application
+    And I review the account profile for a local government
+    And I review the organization structure for a local government
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Marketing application for a local government
+    And I click on the Submit button
+    And I review the security screening requirements for a local government
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@e2e @ubrewuvin @university 
+Scenario: University UBrew / UVin Application
+    Given I am logged in to the dashboard as a university
+    And I click on the Start Application button for a UBrew UVin application
+    And I review the account profile for a university
+    And I review the organization structure for a university
+    And I click on the button for Submit Organization Information
+    And I complete the Cannabis Marketing application for a university
+    And I click on the Submit button
+    And I review the security screening requirements for a university
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Catering application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./CannabisMarketingLicence.feature")]
+    [Collection("Liquor")]
+    public sealed class CannabisMarketingLicence : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
