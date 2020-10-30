7f19d7a35d55f084343ccf0eeff293bef45f138a
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-30 15:35:49 -0700

    Housekeeping.

--- functional-tests/bdd-tests/TestBaseUBrewUVinApplication.cs
+++ functional-tests/bdd-tests/TestBaseUBrewUVinApplication.cs
@@ -31,46 +31,63 @@ namespace bdd_tests
             string storeEmail = "auto@test.com";
             string storePhone = "2222222222";
             string contactTitle = "Brewmaster";
+            string contactPhone = "3333333333";
+            string contactEmail = "contact@email.com";
 
             // enter the establishment name
-            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
-            uiProposedLicensee.SendKeys(establishmentName);
+            NgWebElement uiEstablishmentName = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentName']"));
+            uiEstablishmentName.SendKeys(establishmentName);
 
             // enter the street address
-            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
-            uiProposedLicensee.SendKeys(streetAddress);
+            NgWebElement uiEstablishmentAddressStreet = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentAddressStreet']"));
+            uiEstablishmentAddressStreet.SendKeys(streetAddress);
 
             // enter the city
-            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
-            uiProposedLicensee.SendKeys(city);
+            NgWebElement uiEstablishmentAddressCity = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentAddressCity']"));
+            uiEstablishmentAddressCity.SendKeys(city);
 
             // enter the postal code
-            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
-            uiProposedLicensee.SendKeys(postalCode);
+            NgWebElement uiEstablishmentAddressPostalCode = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentAddressPostalCode']"));
+            uiEstablishmentAddressPostalCode.SendKeys(postalCode);
 
             // enter the PID
-            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
-            uiProposedLicensee.SendKeys(PID);
+            NgWebElement uiEstablishmentParcelId = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentParcelId']"));
+            uiEstablishmentParcelId.SendKeys(PID);
 
             // enter the store email
-            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
-            uiProposedLicensee.SendKeys(storeEmail);
+            NgWebElement uiEstablishmentEmail = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentEmail']"));
+            uiEstablishmentEmail.SendKeys(storeEmail);
 
             // enter the store phone
-            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
-            uiProposedLicensee.SendKeys(storePhone);
+            NgWebElement uiEstablishmentPhone = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentPhone']"));
+            uiEstablishmentPhone.SendKeys(storePhone);
 
             // upload the signage document
+            FileUpload("signage.pdf", "(//input[@type='file'])[2]");
 
-            // select
+            // select owner business checkbox
+            NgWebElement uiIsOwnerBusiness = ngDriver.FindElement(By.CssSelector("mat-checkbox#mat-checkbox-1[formcontrolname='isOwnerBusiness']"));
+            uiIsOwnerBusiness.Click();
 
-            // select
+            // select has valid interest checkbox
+            NgWebElement uiHasValidInterest = ngDriver.FindElement(By.CssSelector("mat-checkbox#mat-checkbox-2[formcontrolname='hasValidInterest']"));
+            uiHasValidInterest.Click();
 
-            // select
+            // select will have valid interest checkbox
+            NgWebElement uiWillhaveValidInterest = ngDriver.FindElement(By.CssSelector("mat-checkbox#mat-checkbox-3[formcontrolname='willhaveValidInterest']"));
+            uiWillhaveValidInterest.Click();
 
             // enter the contact title
-            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
-            uiProposedLicensee.SendKeys(contactTitle);
+            NgWebElement uiContactPersonRole = ngDriver.FindElement(By.CssSelector("input[formcontrolname='contactPersonRole']"));
+            uiContactPersonRole.SendKeys(contactTitle);
+
+            // enter the contact phone number
+            NgWebElement uiContactPersonPhone = ngDriver.FindElement(By.CssSelector("input[formcontrolname='contactPersonPhone']"));
+            uiContactPersonPhone.SendKeys(contactPhone);
+
+            // enter the contact email address
+            NgWebElement uiContactPersonEmail = ngDriver.FindElement(By.CssSelector("input[formcontrolname='contactPersonEmail']"));
+            uiContactPersonEmail.SendKeys(contactEmail);
 
             // click on authorize signature checkbox
             NgWebElement uiAuthorizeSignature = ngDriver.FindElement(By.CssSelector("input[formcontrolname='authorizedToSubmit']"));
@@ -79,10 +96,6 @@ namespace bdd_tests
             // click on signature agreement checkbox
             NgWebElement uiSignatureAgreement = ngDriver.FindElement(By.CssSelector("input[formcontrolname='signatureAgreement'][type='checkbox']"));
             uiSignatureAgreement.Click();
-
-
-            Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'test inspection')]")).Displayed);
-
         }
     }
 }
