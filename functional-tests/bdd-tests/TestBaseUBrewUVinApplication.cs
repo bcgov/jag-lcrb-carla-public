8e9b0483961505a4a03311e939326a9a42da4ad1
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-30 14:57:42 -0700

    UBrew automation - WIP.

---
+++ functional-tests/bdd-tests/TestBaseUBrewUVinApplication.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+namespace bdd_tests
+{
+    public abstract partial class TestBase : Feature, IDisposable
+    {
+        [And(@"I complete the UBrew / UVin application for (.*)")]
+        public void UBrewUVinApplication()
+        {
+            Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'test inspection')]")).Displayed);
+
+        }
+    }
+}
