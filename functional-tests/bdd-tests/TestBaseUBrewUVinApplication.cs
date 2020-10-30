e5512090c028df7d6815708a3ce3cd8c34d69c20
ElizabethWolfe <ejwolfe@gmail.com>
2020-10-30 15:10:42 -0700

    Remove unnecessary biz types for cannabis mktg.

--- functional-tests/bdd-tests/TestBaseUBrewUVinApplication.cs
+++ functional-tests/bdd-tests/TestBaseUBrewUVinApplication.cs
@@ -23,6 +23,64 @@ namespace bdd_tests
         [And(@"I complete the UBrew / UVin application for (.*)")]
         public void UBrewUVinApplication()
         {
+            string establishmentName = "Point Ellis Greenhouse";
+            string streetAddress = "645 Tyee Road";
+            string city = "Victoria";
+            string postalCode = "V9A6X5";
+            string PID = "999999999";
+            string storeEmail = "auto@test.com";
+            string storePhone = "2222222222";
+            string contactTitle = "Brewmaster";
+
+            // enter the establishment name
+            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
+            uiProposedLicensee.SendKeys(establishmentName);
+
+            // enter the street address
+            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
+            uiProposedLicensee.SendKeys(streetAddress);
+
+            // enter the city
+            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
+            uiProposedLicensee.SendKeys(city);
+
+            // enter the postal code
+            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
+            uiProposedLicensee.SendKeys(postalCode);
+
+            // enter the PID
+            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
+            uiProposedLicensee.SendKeys(PID);
+
+            // enter the store email
+            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
+            uiProposedLicensee.SendKeys(storeEmail);
+
+            // enter the store phone
+            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
+            uiProposedLicensee.SendKeys(storePhone);
+
+            // upload the signage document
+
+            // select
+
+            // select
+
+            // select
+
+            // enter the contact title
+            NgWebElement uiProposedLicensee = ngDriver.FindElement(By.CssSelector("input[formcontrolname='autocompleteInput']"));
+            uiProposedLicensee.SendKeys(contactTitle);
+
+            // click on authorize signature checkbox
+            NgWebElement uiAuthorizeSignature = ngDriver.FindElement(By.CssSelector("input[formcontrolname='authorizedToSubmit']"));
+            uiAuthorizeSignature.Click();
+
+            // click on signature agreement checkbox
+            NgWebElement uiSignatureAgreement = ngDriver.FindElement(By.CssSelector("input[formcontrolname='signatureAgreement'][type='checkbox']"));
+            uiSignatureAgreement.Click();
+
+
             Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'test inspection')]")).Displayed);
 
         }
