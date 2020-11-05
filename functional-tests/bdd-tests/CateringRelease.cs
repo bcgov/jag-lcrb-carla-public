1682957a80458475b609765a94fd0963897f0680
ElizabethWolfe <ejwolfe@gmail.com>
2020-11-05 12:36:39 -0800

    Update release test tags.

--- functional-tests/bdd-tests/CateringRelease.cs
+++ functional-tests/bdd-tests/CateringRelease.cs
@@ -17,7 +17,7 @@ Feature: CateringRelease
     As a logged in business user
     I want to confirm that the Catering functionality is ready for release
 
-@validation @privatecorporation @release
+@validation @privatecorporation @cateringrelease
 Scenario: Private Corporation Catering Release
     Given I am logged in to the dashboard as a private corporation
     And I click on the Start Application button for Catering
@@ -57,7 +57,7 @@ Scenario: Private Corporation Catering Release
     And the account is deleted
     Then I see the login page
 
-@validation @soleproprietorship @release
+@validation @soleproprietorship @cateringrelease
 Scenario: Sole Proprietorship Catering Release
     Given I am logged in to the dashboard as a sole proprietorship
     And I click on the Start Application button for Catering
