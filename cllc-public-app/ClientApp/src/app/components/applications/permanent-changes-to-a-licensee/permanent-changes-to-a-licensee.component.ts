72545cad7a7f9b834ac3500b93230d4befec5623
Moffat Sehudi <msehudi@gmail.com>
2020-11-05 09:54:34 -0800

    LCSD-4174 partial

--- cllc-public-app/ClientApp/src/app/components/applications/permanent-changes-to-a-licensee/permanent-changes-to-a-licensee.component.ts
+++ cllc-public-app/ClientApp/src/app/components/applications/permanent-changes-to-a-licensee/permanent-changes-to-a-licensee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from '@models/account.model';
 
 @Component({
   selector: 'app-permanent-changes-to-a-licensee',
@@ -6,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./permanent-changes-to-a-licensee.component.scss']
 })
 export class PermanentChangesToALicenseeComponent implements OnInit {
+
+  changeList = [
+    {
+      name: 'Internal Transfer of Shared',
+      availbleTo: ['PrivateCorporation', 'LimitedLiabilityPartnership']
+    },
+    {
+      name: 'External Transfer of Shared',
+      availbleTo: ['PrivateCorporation', 'LimitedLiabilityPartnership']
+    },
+    {
+      name: 'Change of Directors or Officers',
+      availbleTo: ['PrivateCorporation', 'PublicCorporation', 'Society']
+    },
+    {
+      name: 'Name Change, licensee -- corporation',
+      availbleTo: ['PrivateCorporation', 'PublicCorporation']
+    },
+    {
+      name: 'Name Change, licensee -- partnership',
+      availbleTo: ['GeneralPartnership', 'LimitedLiabilityPartnership', 'Society']
+    },
+    {
+      name: 'Name Change, licensee -- society',
+      availbleTo: ['Society']
+    },
+    {
+      name: 'Name Change, person',
+      availbleTo: ['PrivateCorporation', 'PublicCorporation', 'GeneralPartnership',
+        'LimitedLiabilityPartnership', 'IndigenousNation', 'LocalGovernment'
+      ]
+    },
+    {
+      name: 'Addition of receiver or executor',
+      availbleTo: ['PrivateCorporation', 'PublicCorporation', 'GeneralPartnership',
+        'LimitedLiabilityPartnership', 'Society']
+    }
+  ];
 
   constructor() { }
 
