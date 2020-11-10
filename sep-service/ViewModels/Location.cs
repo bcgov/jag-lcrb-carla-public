42b474c57ddba4702101c019e1665432d6c891b8
George Walker <george@walkersoftware.ca>
2020-11-10 14:16:46 -0800

    SEP initial model

---
+++ sep-service/ViewModels/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sep_service.ViewModels
+{
+    public class Location
+    {
+        public Address Address { get; set; }
+        public EventDate EventDate { get; set; }
+        public string LocationDescription { get; set; }
+        public string LocationName { get; set; }
+        public int MaxGuests { get; set; }
+    }
+}
