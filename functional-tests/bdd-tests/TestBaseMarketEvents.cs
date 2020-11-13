bead54664cd0d9d6ac6ced2c1abe8b99dc479091
ElizabethWolfe <ejwolfe@gmail.com>
2020-11-13 13:45:49 -0800

    Draft of change of hours tests - WIP.

--- functional-tests/bdd-tests/TestBaseMarketEvents.cs
+++ functional-tests/bdd-tests/TestBaseMarketEvents.cs
@@ -476,7 +476,7 @@ namespace bdd_tests
                     Assert.Contains("mat-checkbox-checked", uiSaturday3.GetAttribute("class"));
 
                     // confirm selected week of the month
-                    NgWebElement uiWeekOfMonth4 = ngDriver.FindElement(By.CssSelector("[formcontrolname='weekOfMonth'] mat-radio-button#mat-radio-5"));
+                    NgWebElement uiWeekOfMonth4 = ngDriver.FindElement(By.CssSelector("[formcontrolname='weekOfMonth'] mat-radio-button#mat-radio-10"));
                     Assert.Contains("mat-radio-checked", uiWeekOfMonth4.GetAttribute("class"));
                 }
             }
