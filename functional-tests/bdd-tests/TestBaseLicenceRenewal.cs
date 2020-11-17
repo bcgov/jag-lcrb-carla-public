df7dfcbf5c660cdb9ae0d89b5f7dcf36d7681f16
ElizabethWolfe <ejwolfe@gmail.com>
2020-11-17 12:33:23 -0800

    Update logic and tags.

--- functional-tests/bdd-tests/TestBaseLicenceRenewal.cs
+++ functional-tests/bdd-tests/TestBaseLicenceRenewal.cs
@@ -448,39 +448,32 @@ namespace bdd_tests
             ClickLicencesTab();
 
             // reload Licences page as needed
-            /*
+            
             for (int i = 0; i < 5; i++)
             {
-
-                try
-                {
-                    if (ngDriver.FindElement(By.XPath("//body[contains(.,'Active')]")).Displayed)
-                    {
-                        break;
-                    }
-                }
-                catch (Exception) 
+                if ((ngDriver.FindElement(By.XPath("//body[contains(.,'Active')]")).Displayed) == false)
                 {
                     ngDriver.Navigate().Refresh();
                     System.Threading.Thread.Sleep(2000);
-                } 
+                }
             }
+            
+            /*
+            ngDriver.Navigate().Refresh();
+            System.Threading.Thread.Sleep(2000);
+
+            ngDriver.Navigate().Refresh();
+            System.Threading.Thread.Sleep(2000);
+
+            ngDriver.Navigate().Refresh();
+            System.Threading.Thread.Sleep(2000);
+
+            ngDriver.Navigate().Refresh();
+            System.Threading.Thread.Sleep(2000);
+
+            ngDriver.Navigate().Refresh();
+            System.Threading.Thread.Sleep(2000);
             */
-
-            ngDriver.Navigate().Refresh();
-            System.Threading.Thread.Sleep(2000);
-
-            ngDriver.Navigate().Refresh();
-            System.Threading.Thread.Sleep(2000);
-
-            ngDriver.Navigate().Refresh();
-            System.Threading.Thread.Sleep(2000);
-
-            ngDriver.Navigate().Refresh();
-            System.Threading.Thread.Sleep(2000);
-
-            ngDriver.Navigate().Refresh();
-            System.Threading.Thread.Sleep(2000);
 
             // confirm that Renew Licence messages are no longer displayed
             Assert.True(ngDriver.FindElement(By.XPath("//body[not(contains(.,'Renew Licence'))]")).Displayed);
