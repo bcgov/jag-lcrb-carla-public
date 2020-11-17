a4e149539c9239b8c74ab8fdb2669788a63ab0cc
George Walker <george@walkersoftware.ca>
2020-11-17 11:59:30 -0800

    Remove certificate check for payment gateway

--- cllc-public-app/Startup.cs
+++ cllc-public-app/Startup.cs
@@ -313,18 +313,24 @@ namespace Gov.Lclb.Cllb.Public
 
             // add BC Express Pay (Bambora) service
             services.AddHttpClient<IBCEPService, BCEPService>()
-                .AddPolicyHandler(GetRetryPolicy())
-                .AddPolicyHandler(GetCircuitBreakerPolicy());
+                .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+                {
+                    ClientCertificateOptions = ClientCertificateOption.Manual,
+                    ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) =>  // bypass certificate check.
+                        {
+                            return true;
+                        }
+                });
+                
 
             // add the PDF client.
             services.AddHttpClient<IPdfService, PdfService>()
-                .AddPolicyHandler(GetRetryPolicy())
-                .AddPolicyHandler(GetCircuitBreakerPolicy());
+                .AddPolicyHandler(GetRetryPolicy());
 
             // add the GeoCoder Client.
-            services.AddHttpClient<IGeocoderService, GeocoderService>()                
-                .AddPolicyHandler(GetRetryPolicy())
-                .AddPolicyHandler(GetCircuitBreakerPolicy());
+            services.AddHttpClient<IGeocoderService, GeocoderService>()
+                .AddPolicyHandler(GetRetryPolicy());
 
             // add the file manager.
             string fileManagerURI = _configuration["FILE_MANAGER_URI"];
