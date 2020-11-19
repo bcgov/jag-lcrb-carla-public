1a80bbd8ac334ab9cd06544bd49fdc8e7c25beee
ElizabethWolfe <ejwolfe@gmail.com>
2020-11-19 10:12:16 -0800

    Create files for manage offsite storage tests.

---
+++ functional-tests/bdd-tests/ManageOffsiteStorage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./ManageOffsiteStorage.feature")]
+    [Collection("Liquor")]
+    public sealed class ManageOffsiteStorage : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
