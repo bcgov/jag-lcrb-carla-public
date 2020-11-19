0a4bc308c5faba0b73b79d2cdc58488fa4ded783
ElizabethWolfe <ejwolfe@gmail.com>
2020-11-19 11:40:51 -0800

    Updates to offsite storage test.

--- functional-tests/bdd-tests/ManageOffsiteStorage.cs
+++ functional-tests/bdd-tests/ManageOffsiteStorage.cs
@@ -17,7 +17,7 @@ Feature: ManageOffsiteStorage
     As a logged in business user
     I want to manage offsite storage on the Licences & Authorizations tab
 
-@e2e @privatecorporation @offsitestore
+@e2e @privatecorporation @offsitestorage
 Scenario: Private Corporation Manage Offsite Storage
     Given I am logged in to the dashboard as a private corporation
     And I click on the Start Application button for a Manufacturer Licence
@@ -34,6 +34,7 @@ Scenario: Private Corporation Manage Offsite Storage
     And I click on the Licences tab
     And I click on the link for Manage Off-Site Storage
     And I complete the offsite storage application
+    And I click on the Submit button
     # Confirm the expected result
     And the account is deleted
     Then I see the login page
