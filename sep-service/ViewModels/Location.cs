c6729119bf500a2ae9be1f39787b013a1434d8e2
George Walker <george@walkersoftware.ca>
2020-11-23 20:21:18 -0800

    Add settings

--- sep-service/ViewModels/Location.cs
+++ sep-service/ViewModels/Location.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SepService.ViewModels
 {
+    public enum LocationSetting
+    {
+        Indoor = 845280000,
+        Outdoor  = 845280001,
+        Both = 845280002
+    }
+
     public class Location
     {
         public Address Address { get; set; }
@@ -14,5 +24,12 @@ namespace SepService.ViewModels
         public int MaxGuests { get; set; }
         public bool MinorPresent { get; set; }
         public int? NumberMinors { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(LocationSetting))]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public LocationSetting Setting { get; set;}
+        
+
     }
 }
