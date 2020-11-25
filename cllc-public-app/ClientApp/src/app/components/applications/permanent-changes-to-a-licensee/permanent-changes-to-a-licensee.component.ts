391953098486f3ecc5d0dc377aaed80f6d1a0c9f
Moffat Sehudi <msehudi@gmail.com>
2020-11-25 09:04:45 -0800

    LCSD-4282: Payment Boxes

--- cllc-public-app/ClientApp/src/app/components/applications/permanent-changes-to-a-licensee/permanent-changes-to-a-licensee.component.ts
+++ cllc-public-app/ClientApp/src/app/components/applications/permanent-changes-to-a-licensee/permanent-changes-to-a-licensee.component.ts
@@ -1,13 +1,18 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AppState } from '@app/app-state/models/app-state';
+import { UPLOAD_FILES_MODE } from '@components/licences/licences.component';
 import { Account } from '@models/account.model';
 import { ApplicationLicenseSummary } from '@models/application-license-summary.model';
 import { Application } from '@models/application.model';
 import { Store } from '@ngrx/store';
 import { ApplicationDataService } from '@services/application-data.service';
+import { PaymentDataService } from '@services/payment-data.service';
 import { FormBase } from '@shared/form-base';
-import { filter } from 'rxjs/operators';
+import { Observable, of, forkJoin, merge, combineLatest } from 'rxjs';
+import { catchError, delay, filter, mergeMap, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-permanent-changes-to-a-licensee',
@@ -21,6 +26,14 @@ export class PermanentChangesToALicenseeComponent extends FormBase implements On
   cannabisLicences: ApplicationLicenseSummary[] = [];
   account: Account;
   businessType: string;
+  saveComplete: any;
+  submitApplicationInProgress: boolean;
+  proceedToSecurityScreeningInProgress: boolean;
+  busyPromise: Promise<void>;
+  showValidationMessages: boolean;
+  savedFormData: any;
+  invoiceType: any;
+  dataLoaded: boolean;
 
   get hasLiquor(): boolean {
     return this.liquorLicences.length > 0;
@@ -34,6 +47,10 @@ export class PermanentChangesToALicenseeComponent extends FormBase implements On
   form: FormGroup;
 
   constructor(private applicationDataService: ApplicationDataService,
+    private paymentDataService: PaymentDataService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar,
     private fb: FormBuilder,
     private store: Store<AppState>) {
     super();
@@ -43,19 +60,15 @@ export class PermanentChangesToALicenseeComponent extends FormBase implements On
         this.account = account;
         this.changeList = masterChangeList.filter(item => !!item.availableTo.find(bt => bt === account.businessType));
       });
+
+    combineLatest([this.route.paramMap, route.queryParams])
+      .subscribe(([pmap, { trnId, SessionKey }]) => {
+        this.invoiceType = pmap.get('invoiceType');
+      });
   }
 
   ngOnInit(): void {
-    const defaultLicences = {
-      liquor: [],
-      cannabis: []
-    };
-
     this.form = this.fb.group({
-      firstNameOld: [''],
-      firstNameNew: [''],
-      lastNameOld: [''],
-      lastNameNew: [''],
       csInternalTransferOfShares: [''],
       csExternalTransferOfShares: [''],
       csChangeOfDirectorsOrOfficers: [''],
@@ -64,6 +77,10 @@ export class PermanentChangesToALicenseeComponent extends FormBase implements On
       csNameChangeLicenseeSociety: [''],
       csNameChangeLicenseePerson: [''],
       csAdditionalReceiverOrExecutor: [''],
+      firstNameOld: [''],
+      firstNameNew: [''],
+      lastNameOld: [''],
+      lastNameNew: [''],
       description2: [''],
       description3: [''],
     });
@@ -73,10 +90,140 @@ export class PermanentChangesToALicenseeComponent extends FormBase implements On
         this.liquorLicences = licences.filter(item => item.licenceTypeCategory === 'Liquor');
         this.cannabisLicences = licences.filter(item => item.licenceTypeCategory === 'Cannabis');
         this.application = application;
+        if ((this.invoiceType === 'primary' && !this.application.primaryInvoicePaid) ||
+          (this.invoiceType === 'secondary' && !this.application.secondaryInvoicePaid)) {
+          this.paymentDataService.verifyPaymentURI(this.invoiceType + 'Invoice', this.application.id)
+            .subscribe(res => {
+              this.dataLoaded = true;
+              debugger;
+            });
+        }else{
+          this.dataLoaded = true;
+        }
+        debugger;
+        if (this.application.primaryInvoicePaid || this.application.secondaryInvoicePaid) {
+          this.form.disable();
+        }
         this.form.patchValue(application);
       });
 
   }
+
+  /**
+ * Save form data
+ * @param showProgress
+ */
+  save(showProgress: boolean = false, appData: Application = <Application>{}): Observable<[boolean, Application]> {
+    const saveData = this.form.value;
+
+    return this.applicationDataService.updateApplication({
+      ...this.application,
+      ...this.form.value,
+      ...appData
+    })
+      .pipe(takeWhile(() => this.componentActive))
+      .pipe(catchError(() => {
+        this.snackBar.open('Error saving Application', 'Fail', { duration: 3500, panelClass: ['red-snackbar'] });
+        const res: [boolean, Application] = [false, null];
+        return of(res);
+      }))
+      .pipe(mergeMap((data) => {
+        if (showProgress === true) {
+          this.snackBar.open('Application has been saved', 'Success', { duration: 2500, panelClass: ['green-snackbar'] });
+        }
+        const res: [boolean, Application] = [true, <Application>data];
+        return of(res);
+      }));
+  }
+
+  isValid(): boolean {
+    return true;
+  }
+
+  /**
+   * Submit the application for payment
+   * */
+  submit_application(invoiceType: 'primary' | 'secondary') {
+
+    // Only save if the data is valid
+    if (this.isValid()) {
+      this.submitApplicationInProgress = true;
+      this.save(!this.application.applicationType.isFree, <Application>{ invoiceTrigger: 1 }) // trigger invoice generation when saving
+        .pipe(takeWhile(() => this.componentActive))
+        .subscribe(([saveSucceeded, app]) => {
+          if (saveSucceeded) {
+            // payment is required
+            if (app) {
+              this.submitPayment(invoiceType)
+                .subscribe(res => {
+                  this.saveComplete.emit(true);
+                  this.submitApplicationInProgress = false;
+                });
+            }
+            // else if (app) {
+            //   // mark application as complete
+            //   this.save(!this.application.applicationType.isFree, <Application>{ isApplicationComplete: 'Yes' })
+            //     .subscribe(res => {
+            //       this.saveComplete.emit(true);
+            //       // however we need to redirect if the application is Free
+            //       if (this.application.applicationType.isFree) {
+            //         this.snackBar.open('Application submitted', 'Success', { duration: 2500, panelClass: ['green-snackbar'] });
+            //         this.router.navigateByUrl('/dashboard');
+            //       }
+            //     });
+            // }
+          } else if (this.application.applicationType.isFree) { // show error message the save failed and the application is free
+            this.snackBar.open('Error saving Application', 'Fail', { duration: 3500, panelClass: ['red-snackbar'] });
+            this.submitApplicationInProgress = false;
+          }
+        });
+    } else {
+      this.showValidationMessages = true;
+    }
+  }
+
+
+  private proceedToSecurityScreening() {
+    //send event to move to the next step of the multi-step
+    if (this.isValid()) { // Only proceed if the data is valid
+      this.proceedToSecurityScreeningInProgress = true;
+      this.busyPromise = this.save(true)
+        .toPromise()
+        .then(([saveSucceeded, app]) => {
+          this.proceedToSecurityScreeningInProgress = false;
+          if (saveSucceeded) {
+            this.saveComplete.next(true);
+          } else {
+            this.snackBar.open('Error saving Application', 'Fail', { duration: 3500, panelClass: ['red-snackbar'] });
+          }
+        });
+    } else {
+      this.showValidationMessages = true;
+    }
+  }
+
+  /**
+   * Redirect to payment processing page (Express Pay / Bambora service)
+   * */
+  private submitPayment(invoiceType: 'primary' | 'secondary') {
+    debugger;
+    let payMethod = this.paymentDataService.getPaymentURI('primaryInvoice', this.application.id);
+    if (invoiceType === 'secondary') {
+      payMethod = this.paymentDataService.getPaymentURI('secondaryInvoice', this.application.id);
+    }
+    return payMethod
+      .pipe(takeWhile(() => this.componentActive))
+      .pipe(mergeMap(jsonUrl => {
+        debugger;
+        window.location.href = jsonUrl['url'];
+        return jsonUrl['url'];
+      }, (err: any) => {
+        if (err._body === 'Payment already made') {
+          this.snackBar.open('Application payment has already been made.', 'Fail', { duration: 3500, panelClass: ['red-snackbar'] });
+        }
+      }));
+  }
+
 }
 
 const masterChangeList = [
