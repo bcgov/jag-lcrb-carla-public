c4d3fd4b21b49ccf6d4c6c7dbcd5fab920f50304
George Walker <george@walkersoftware.ca>
2020-12-04 13:38:07 -0800

    Undo change to UserSettings

--- cllc-public-app/Authentication/UserSettings.cs
+++ cllc-public-app/Authentication/UserSettings.cs
@@ -139,8 +139,13 @@ namespace Gov.Lclb.Cllb.Public.Authentication
         /// <returns></returns>
         public static UserSettings ReadUserSettings(HttpContext context)
         {
-            UserSettings userSettings = UserSettings.CreateFromHttpContext(_httpContextAccessor);
-            return userSettings;
+            UserSettings userSettings = new UserSettings();
+
+            if (context.Session.GetString("UserSettings") == null) return userSettings;
+
+            string settingsTemp = context.Session.GetString("UserSettings");
+
+            return !string.IsNullOrEmpty(settingsTemp) ? CreateFromJson(settingsTemp) : userSettings;
         }
 
     }
