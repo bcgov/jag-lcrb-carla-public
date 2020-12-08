a4d7521534bbd6b3ea764c6c5b89270297dc5573
ElizabethWolfe <ejwolfe@gmail.com>
2020-12-08 06:56:54 -0800

    Add JS.

--- functional-tests/bdd-tests/TestBaseEventAuthorization.cs
+++ functional-tests/bdd-tests/TestBaseEventAuthorization.cs
@@ -221,16 +221,12 @@ namespace bdd_tests
                 // click on the Save For Later button
                 NgWebElement uiSaveForLater = ngDriver.FindElement(By.CssSelector(".btn-primary:nth-child(1)"));
                 uiSaveForLater.Click();
-
-                //System.Threading.Thread.Sleep(4000);
             }
             else
             {
                 // click on the Submit button
                 NgWebElement uiSubmit = ngDriver.FindElement(By.CssSelector(".btn-primary~ .btn-primary+ .btn-primary"));
                 uiSubmit.Click();
-
-                //System.Threading.Thread.Sleep(4000);
             }
         }
 
