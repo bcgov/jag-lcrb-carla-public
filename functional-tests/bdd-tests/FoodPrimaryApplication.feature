363a8f5b4c85a209a26ed7875d13a63cea3655a0
ElizabethWolfe <ejwolfe@gmail.com>
2020-12-10 12:31:19 -0800

    Food primary and housekeeping.

--- functional-tests/bdd-tests/FoodPrimaryApplication.feature
+++ functional-tests/bdd-tests/FoodPrimaryApplication.feature
@@ -7,8 +7,6 @@ Scenario: Partnership Food Primary Application
     Given I am logged in to the dashboard as a partnership
     And I click on the Start Application button for Food Primary
     And I review the account profile for a partnership
-    And I review the organization structure for a partnership
-    And I click on the button for Submit Organization Information
     And I complete the Food Primary application
     And I click on the Submit button
     And I review the security screening requirements for a partnership
@@ -25,8 +23,6 @@ Scenario: Private Corporation Food Primary Application
     Given I am logged in to the dashboard as a private corporation
     And I click on the Start Application button for Food Primary
     And I review the account profile for a private corporation
-    And I review the organization structure for a private corporation
-    And I click on the button for Submit Organization Information
     And I complete the Food Primary application
     And I click on the Submit button
     And I review the security screening requirements for a private corporation
@@ -43,8 +39,6 @@ Scenario: Public Corporation Food Primary Application
     Given I am logged in to the dashboard as a public corporation
     And I click on the Start Application button for Food Primary
     And I review the account profile for a public corporation
-    And I review the organization structure for a public corporation
-    And I click on the button for Submit Organization Information
     And I complete the Food Primary application
     And I click on the Submit button
     And I review the security screening requirements for a public corporation
@@ -61,8 +55,6 @@ Scenario: Society Food Primary Application
     Given I am logged in to the dashboard as a society
     And I click on the Start Application button for Food Primary
     And I review the account profile for a society
-    And I review the organization structure for a society
-    And I click on the button for Submit Organization Information
     And I complete the Food Primary application
     And I click on the Submit button
     And I review the security screening requirements for a society
--- functional-tests/bdd-tests/TestBaseFoodPrimaryApplication.cs
+++ functional-tests/bdd-tests/TestBaseFoodPrimaryApplication.cs
@@ -24,10 +24,83 @@ namespace bdd_tests
         public void CompleteFoodPrimaryApplication()
         {
             /* 
-            Page Title: 
+            Page Title: Food Primary Licence Application
             */
 
+            // create test data
+            string estName = "Point Ellis Greenhouse";
+            string estAddress = "645 Tyee Rd";
+            string estCity = "Victoria";
+            string estPostal = "V9A 6X5";
+            string estPID = "012345678";
 
+            string estEmail = "test@test.com";
+            string estPhone = "2505555555";
+            string conGiven = "Given";
+            string conSurname = "Surname";
+            string conRole = "CEO";
+            string conPhone = "2508888888";
+            string conEmail = "contact@email.com";
+            string indigenousNation = "Cowichan Tribes";
+            string policeJurisdiction = "RCMP Shawnigan Lake";
+
+
+            // upload the central securities register
+            FileUpload("central_securities_register.pdf", "(//input[@type='file'])[3]");
+
+            // upload supporting business documentation
+            FileUpload("distribution_plan.pdf", "(//input[@type='file'])[6]");
+
+            // upload register directors and officers
+            FileUpload("todo.pdf", "(//input[@type='file'])[9]");
+
+            // upload shareholders holding less than 10% interest
+            FileUpload("todo.pdf", "(//input[@type='file'])[12]");
+
+            // enter the establishment name
+            NgWebElement uiEstabName = ngDriver.FindElement(By.Id("establishmentName"));
+            uiEstabName.SendKeys(estName);
+
+            // enter the establishment address
+            NgWebElement uiEstabAddress = ngDriver.FindElement(By.Id("establishmentAddressStreet"));
+            uiEstabAddress.SendKeys(estAddress);
+
+            // enter the establishment city
+            NgWebElement uiEstabCity = ngDriver.FindElement(By.Id("establishmentAddressCity"));
+            uiEstabCity.SendKeys(estCity);
+
+            // enter the establishment postal code
+            NgWebElement uiEstabPostal = ngDriver.FindElement(By.Id("establishmentAddressPostalCode"));
+            uiEstabPostal.SendKeys(estPostal);
+
+            // enter the PID
+            NgWebElement uiEstabPID = ngDriver.FindElement(By.Id("establishmentParcelId"));
+            uiEstabPID.SendKeys(estPID);
+
+            // upload zoning document
+            FileUpload("proof_of_zoning.pdf", "(//input[@type='file'])[15]");
+
+            // search for and select the indigenous nation
+            NgWebElement uiIndigenousNation = ngDriver.FindElement(By.CssSelector("input[formcontrolname='indigenousNation']"));
+            uiIndigenousNation.SendKeys(indigenousNation);
+
+            NgWebElement uiIndigenousNation2 = ngDriver.FindElement(By.CssSelector("#mat-option-0 span"));
+            uiIndigenousNation2.Click();
+
+            // search for and select the police jurisdiction
+            NgWebElement uiPoliceJurisdiction = ngDriver.FindElement(By.CssSelector("input[formcontrolname='policeJurisdiction']"));
+            uiPoliceJurisdiction.SendKeys(policeJurisdiction);
+
+            NgWebElement uiPoliceJurisdiction2 = ngDriver.FindElement(By.CssSelector("#mat-option-2 span"));
+            uiPoliceJurisdiction2.Click();
+
+            // upload signage document
+            FileUpload("signage.pdf", "(//input[@type='file'])[17]");
+
+            // upload floor plan
+            FileUpload("floor_plan.pdf", "(//input[@type='file'])[20]");
+
+            // select hours of sale - TODO
 
             // retrieve the current URL to get the application ID (needed downstream)
             string URL = ngDriver.Url;
