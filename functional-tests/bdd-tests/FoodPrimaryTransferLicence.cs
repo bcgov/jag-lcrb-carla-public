ec324455f8803a828b5374dba9c36f67f6e4ee2d
ElizabethWolfe <ejwolfe@gmail.com>
2020-12-11 12:28:56 -0800

    Draft FP licence transfer test.

---
+++ functional-tests/bdd-tests/FoodPrimaryTransferLicence.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: FoodPrimaryTransferLicence
+    As a logged in business user
+    I want to transfer a Food Primary licence for different business types
+
+@foodprimarylicencetransfer @partnership 
+Scenario: Partnership Food Primary Transfer Licence
+    Given I am logged in to the dashboard as a partnership
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a partnership
+    And I complete the Food Primary application for a partnership
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee 
+    And I request a transfer of ownership
+    And the account is deleted
+    Then I see the login page
+
+@foodprimarylicencetransfer @privatecorporation
+Scenario: Private Corporation Food Primary Transfer Licence
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a private corporation
+    And I complete the Food Primary application for a private corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee 
+    And I request a transfer of ownership
+    And the account is deleted
+    Then I see the login page
+
+@foodprimarylicencetransfer @publiccorporation
+Scenario: Public Corporation Food Primary Transfer Licence
+    Given I am logged in to the dashboard as a public corporation
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a public corporation
+    And I complete the Food Primary application for a public corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee 
+    And I request a transfer of ownership
+    And the account is deleted
+    Then I see the login page
+
+@foodprimarylicencetransfer @society
+Scenario: Society Food Primary Transfer Licence
+    Given I am logged in to the dashboard as a society
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a society
+    And I complete the Food Primary application for a society
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee
+    And I request a transfer of ownership
+    And the account is deleted
+    Then I see the login page
+
+@foodprimarylicencetransfer @soleproprietorship
+Scenario: Sole Proprietorship Food Primary Transfer Licence
+    Given I am logged in to the dashboard as a sole proprietorship
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a sole proprietorship
+    And I review the organization structure for a sole proprietorship
+    And I click on the button for Submit Organization Information
+    And I complete the Food Primary application for a sole proprietorship
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee 
+    And I request a transfer of ownership
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./FoodPrimaryTransferLicence.feature")]
+    [Collection("Liquor")]
+    public sealed class FoodPrimaryTransferLicence : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLiquorThree();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
