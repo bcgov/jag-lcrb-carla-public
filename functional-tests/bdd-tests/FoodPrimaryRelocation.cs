14fce59577bbdab28836393f59729607af56575a
ElizabethWolfe <ejwolfe@gmail.com>
2020-12-11 12:58:15 -0800

    Draft FP patio and relocation tests.

---
+++ functional-tests/bdd-tests/FoodPrimaryRelocation.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: FoodPrimaryRelocation
+    As a logged in business user
+    I want to submit a relocation request for a Food Primary licence
+
+@foodprimaryrelocation @partnership 
+Scenario: Partnership Food Primary Relocation Application
+    Given I am logged in to the dashboard as a partnership
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a partnership
+    And I complete the Food Primary application for a partnership
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee 
+    And I request a store relocation for Cannabis
+    And I click on the link for Dashboard
+    And I confirm the relocation request is displayed on the dashboard
+    And the account is deleted
+    Then I see the login page
+
+@foodprimaryrelocation @privatecorporation
+Scenario: Private Corporation Food Primary Relocation Application
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a private corporation
+    And I complete the Food Primary application for a private corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee 
+    And I request a store relocation for Cannabis
+    And I click on the link for Dashboard
+    And I confirm the relocation request is displayed on the dashboard
+    And the account is deleted
+    Then I see the login page
+
+@foodprimaryrelocation @publiccorporation
+Scenario: Public Corporation Food Primary Relocation Application
+    Given I am logged in to the dashboard as a public corporation
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a public corporation
+    And I complete the Food Primary application for a public corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee 
+    And I request a store relocation for Cannabis
+    And I click on the link for Dashboard
+    And I confirm the relocation request is displayed on the dashboard
+    And the account is deleted
+    Then I see the login page
+
+@foodprimaryrelocation @society
+Scenario: Society Food Primary Relocation Application
+    Given I am logged in to the dashboard as a society
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a society
+    And I complete the Food Primary application for a society
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee
+    And I request a store relocation for Cannabis
+    And I click on the link for Dashboard
+    And I confirm the relocation request is displayed on the dashboard
+    And the account is deleted
+    Then I see the login page
+
+@foodprimaryrelocation @soleproprietorship
+Scenario: Sole Proprietorship Food Primary Relocation Application
+    Given I am logged in to the dashboard as a sole proprietorship
+    And I click on the Start Application button for Food Primary
+    And I review the account profile for a sole proprietorship
+    And I review the organization structure for a sole proprietorship
+    And I click on the button for Submit Organization Information
+    And I complete the Food Primary application for a sole proprietorship
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Food Primary application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the application is approved
+    And I pay the licensing fee 
+    And I request a store relocation for Cannabis
+    And I click on the link for Dashboard
+    And I confirm the relocation request is displayed on the dashboard
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./FoodPrimaryRelocation.feature")]
+    [Collection("Liquor")]
+    public sealed class FoodPrimaryRelocation : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLiquorThree();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
