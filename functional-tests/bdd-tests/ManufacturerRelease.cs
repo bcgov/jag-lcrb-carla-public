3f8fc8f87e6aec03927ee11a0eb09e5493883e78
ElizabethWolfe <ejwolfe@gmail.com>
2020-12-16 18:32:25 -0800

    Add sole prop mfg release #2.

--- functional-tests/bdd-tests/ManufacturerRelease.cs
+++ functional-tests/bdd-tests/ManufacturerRelease.cs
@@ -121,6 +121,47 @@ Scenario: Sole Proprietorship Manufacturer Release #1
     And the account is deleted
     Then I see the login page
 
+@manufacturer @winery @release 
+Scenario: Sole Proprietorship Manufacturer Release #2
+    Given I am logged in to the dashboard as a sole proprietorship
+    And I click on the Start Application button for a Manufacturer Licence
+    And I review the account profile for a sole proprietorship
+    And I review the organization structure for a sole proprietorship
+    And I click on the button for Submit Organization Information
+    And I complete the Manufacturer application for a winery
+    And I review the security screening requirements for a sole proprietorship
+    And I click on the button for Pay for Application
+    And I enter the payment information
+    And I confirm the payment receipt for a Manufacturer Licence application
+    And the application is approved
+    And I pay the licensing fee
+    And I click on the Licences tab
+    And I request an on-site store endorsement
+    And I click on the Licences tab
+    And the on-site endorsement application is approved
+    And I request a picnic area endorsement
+    And I click on the Licences tab
+    And I request a special event area endorsement
+    And I click on the Licences tab
+    And I request structural alterations to an approved lounge or special events area
+    And I click on the Licences tab
+    And I complete the change hours application for a lounge area within service hours
+    And I click on the Submit button
+    And I enter the payment information
+    And I click on the Licences tab
+    # And I click on the link for Manage Off-Site Storage
+    # And I complete the offsite storage application
+    # And I click on the Submit button
+    And I request a market event for one date only
+    And I click on the market event submit button
+    And I click on the Licences tab
+    # And I click on the event history for markets
+    # And I click on the link for Approved
+    # And the market event data is correct for a one day event
+    And I request a third party operator
+    And I request a transfer of ownership
+    And the account is deleted
+    Then I see the login page
 */
 
 namespace bdd_tests
