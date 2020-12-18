e4cc570c178624e88e016ec3dd9be84171f91c0b
George Walker <george@walkersoftware.ca>
2020-12-18 13:26:08 -0800

    Revert soap class changes

--- one-stop-service/OneStop/Util/CRAContactType.cs
+++
@@ -1,7 +0,0 @@
-﻿/// <remarks/>
-[System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.7.2558.0")]
-[System.SerializableAttribute()]
-[System.Diagnostics.DebuggerStepThroughAttribute()]
-[System.ComponentModel.DesignerCategoryAttribute("code")]
-public partial class CRAContactType : CRAOtherPartyType {
-}
