7c58807b25de63f409160b66021386ab32b87dd7
Moffat Sehudi <msehudi@gmail.com>
2020-12-24 09:57:01 -0800

    LCSD-4536, LCSD-4631 and LCSD-4383

--- cllc-interfaces/Dynamics-Autorest/Extensions/DynamicsClient.cs
+++ cllc-interfaces/Dynamics-Autorest/Extensions/DynamicsClient.cs
@@ -271,7 +271,7 @@ namespace Gov.Lclb.Cllb.Interfaces
                 // fetch from Dynamics.
                 result = Accounts.GetByKey(accountid: id, expand: expand);
             }
-            catch (HttpOperationException)
+            catch (HttpOperationException ex)
             {
                 result = null;
             }
