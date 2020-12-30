3c8ccc99be4484d79a6c132a442d7dfb2f49b8e5
Alejandro Sanchez <emailforasr@gmail.com>
2020-12-29 17:15:00 -0800

    Add notices table component skeleton

---
+++ cllc-public-app/ClientApp/src/app/components/tables/notices-table/notices-table/notices-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notices-table',
+  templateUrl: './notices-table.component.html',
+  styleUrls: ['./notices-table.component.scss']
+})
+export class NoticesTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
