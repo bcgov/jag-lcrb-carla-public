e5ae14a574e547a3ed4a5ab935f7117fb78356b0
George Walker <george@walkersoftware.ca>
2020-12-29 16:28:08 -0800

    LCSD-4505 Dormancy

--- one-stop-service/OneStop/ChangeStatus.cs
+++ one-stop-service/OneStop/ChangeStatus.cs
@@ -165,25 +165,40 @@ namespace Gov.Jag.Lcrb.OneStopService.OneStop
                     programAccountStatus.programAccountStatusCode = OneStopUtils.PROGRAM_ACCOUNT_STATUS_CODE_CLOSED; // 02
                     //  programAccountReasonCode = 111
                     break;
+
                 case OneStopHubStatusChange.Expired:
                     programAccountStatus.programAccountStatusCode = OneStopUtils.PROGRAM_ACCOUNT_STATUS_CODE_CLOSED;
                     // programAccountReasonCode = 112 (expired)
                     break;
+
                 case OneStopHubStatusChange.Renewed:
                     programAccountStatus.programAccountStatusCode = OneStopUtils.PROGRAM_ACCOUNT_STATUS_CODE_CLOSED;
                     // programAccountReasonCode = 112 (expired)
                     break;
+
                 case OneStopHubStatusChange.Suspended:
                     programAccountStatus.programAccountStatusCode = OneStopUtils.PROGRAM_ACCOUNT_STATUS_CODE_SUSPENDED;
                     // programAccountReasonCode = 114 (expired)
                     break;
+
                 case OneStopHubStatusChange.SuspensionEnded:
                     programAccountStatus.programAccountStatusCode = OneStopUtils.PROGRAM_ACCOUNT_STATUS_CODE_ACTIVE;
                     // programAccountReasonCode = null
                     break;
+
                 case OneStopHubStatusChange.TransferComplete:
                     programAccountStatus.programAccountStatusCode = OneStopUtils.PROGRAM_ACCOUNT_STATUS_CODE_CLOSED;
                     // programAccountReasonCode = 113 (transfer)
+                    break;
+
+                case OneStopHubStatusChange.EnteredDormancy:
+                    programAccountStatus.programAccountStatusCode = OneStopUtils.PROGRAM_ACCOUNT_STATUS_CODE_SUSPENDED;
+                    // programAccountReasonCode = 115 (dormant)
+                    break;
+
+                case OneStopHubStatusChange.DormancyEnded:
+                    programAccountStatus.programAccountStatusCode = OneStopUtils.PROGRAM_ACCOUNT_STATUS_CODE_ACTIVE;
+                    // programAccountReasonCode = null
                     break;
             }
 
