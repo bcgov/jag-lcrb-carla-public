4757bf212eac8ab7d92ab0072476b7c9f7d89bd6
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-01 07:22:17 -0800

    Draft of Notices test.

---
+++ functional-tests/bdd-tests/Notices.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: Notices
+    As a logged in business user
+    I want to view notices on the Notices tab
+
+@notices
+Scenario: Notices Download (Private Corporation)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the link for Notices
+    And I click on the Notices file
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./Notices.feature")]
+    [Collection("Liquor")]
+    public sealed class Notices : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
---
+++ functional-tests/bdd-tests/Notices.feature
@@ -0,0 +1,11 @@
+﻿Feature: Notices
+    As a logged in business user
+    I want to view notices on the Notices tab
+
+@notices
+Scenario: Notices Download (Private Corporation)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the link for Notices
+    And I click on the Notices file
+    And the account is deleted
+    Then I see the login page
