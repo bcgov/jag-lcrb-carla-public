deaaa6d012d0457c9f72d757d35e5e78cdcb2370
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-04 15:09:23 -0800

    Update automation for OneStop.

--- functional-tests/bdd-tests/TestBaseOneStop.cs
+++ functional-tests/bdd-tests/TestBaseOneStop.cs
@@ -23,7 +23,7 @@ namespace bdd_tests
         [Given(@"I click on the Swagger link for OneStop")]
         public void ClickOnSwaggerLink()
         {
-            ngDriver.Navigate().GoToUrl($"https://one-stop-testing-b7aa30-dev.apps.silver.devops.gov.bc.ca");
+            ngDriver.Navigate().GoToUrl($"https://one-stop-testing-b7aa30-dev.apps.silver.devops.gov.bc.ca/swagger");
         }
 
 
@@ -32,61 +32,63 @@ namespace bdd_tests
         {
             if (buttonType == "Authorize button")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector("button.authorize"));
                 uiSwaggerButton.Click();
             }
 
             if (buttonType == "Close button")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector("button.btn-done"));
                 uiSwaggerButton.Click();
             }
 
             if (buttonType == "Get button for SendChangeAddress")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector("span:nth-child(1) #operations-OneStop-OneStop_GET .opblock-summary-method"));
                 uiSwaggerButton.Click();
             }
 
             if (buttonType == "Get button for SendChangeName")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(".no-margin span:nth-child(2) #operations-OneStop-OneStop_GET .opblock-summary-method"));
                 uiSwaggerButton.Click();
             }
 
             if (buttonType == "Get button for SendChangeStatus")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector("span:nth-child(3) .opblock-summary-method"));
                 uiSwaggerButton.Click();
             }
 
             if (buttonType == "Get button for SendLicenceCreationMessage")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector("span:nth-child(4) .opblock-summary-method"));
                 uiSwaggerButton.Click();
             }
 
             if (buttonType == "Get button for SendProgramAccountDetailsBroadcastMessage")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector("span:nth-child(5) .opblock-summary-method"));
                 uiSwaggerButton.Click();
             }
 
             if (buttonType == "Get button for LdbExport")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector("span:nth-child(6) .opblock-summary-method"));
                 uiSwaggerButton.Click();
             }
 
+            // TODO: need to distinguish between 6 visible buttons
             if (buttonType == "Try it out button")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.XPath("//*[@id='operations-OneStop-OneStop_GET']/div[2]/div/div[1]/div[1]/div[2]/button"));
                 uiSwaggerButton.Click();
             }
 
+            // TODO: need to distinguish between 6 visible buttons
             if (buttonType == "Execute button")
             {
-                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.CssSelector(""));
+                NgWebElement uiSwaggerButton = ngDriver.FindElement(By.XPath("//*[@id='operations-OneStop-OneStop_GET']/div[2]/div/div[2]/button"));
                 uiSwaggerButton.Click();
             }
         }
@@ -95,7 +97,8 @@ namespace bdd_tests
         [And(@"I enter the licence GUID (.*)")]
         public void EnterLicenceGUID(string licenceGUID)
         {
-            NgWebElement uiEnterGUID = ngDriver.FindElement(By.CssSelector(""));
+            // TODO: need to distinguish between 6 visible textboxes
+            NgWebElement uiEnterGUID = ngDriver.FindElement(By.XPath("//*[@id='operations-OneStop-OneStop_GET']/div[2]/div/div[1]/div[2]/div/table/tbody/tr/td[2]/input"));
             uiEnterGUID.SendKeys(licenceGUID);
         }
 
