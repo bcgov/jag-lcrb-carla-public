585f07666bc3f99a17c3de7599efb3be3b197339
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-05 09:00:29 -0800

    Update feature file.

--- functional-tests/bdd-tests/OneStopSwagger.cs
+++ functional-tests/bdd-tests/OneStopSwagger.cs
@@ -23,9 +23,9 @@ Scenario: OneStop Send Change of Address
     And I click on the Authorize button
     And I click on the Close button
     And I click on the Get button for SendChangeAddress
-    And I click on the Try it out button
+    And I click on the Try it out button for SendChangeAddress 
     And I enter the licence GUID 31b08509-909b-ea11-b818-00505683fbf4
-    And I click on the Execute button
+    And I click on the Execute button for SendChangeAddress
     Then the correct 200 response is displayed
 
 @onestopswagger
@@ -34,9 +34,9 @@ Scenario: OneStop Send Change of Name
     And I click on the Authorize button
     And I click on the Close button
     And I click on the Get button for SendChangeName
-    And I click on the Try it out button
+    And I click on the Try it out button for SendChangeName
     And I enter the licence GUID 31b08509-909b-ea11-b818-00505683fbf4
-    And I click on the Execute button
+    And I click on the Execute button for SendChangeName
     Then the correct 200 response is displayed
 
 @onestopswagger
@@ -45,9 +45,9 @@ Scenario: OneStop Send Change of Status
     And I click on the Authorize button
     And I click on the Close button
     And I click on the Get button for SendChangeStatus
-    And I click on the Try it out button
+    And I click on the Try it out button for SendChangeStatus
     And I enter the licence GUID 31b08509-909b-ea11-b818-00505683fbf4
-    And I click on the Execute button
+    And I click on the Execute button for SendChangeStatus
     Then the correct 200 response is displayed
 
 @onestopswagger
@@ -56,9 +56,9 @@ Scenario: OneStop Send Licence Creation Message
     And I click on the Authorize button
     And I click on the Close button
     And I click on the Get button for SendLicenceCreationMessage
-    And I click on the Try it out button
+    And I click on the Try it out button for SendLicenceCreationMessage
     And I enter the licence GUID 31b08509-909b-ea11-b818-00505683fbf4
-    And I click on the Execute button
+    And I click on the Execute button for SendLicenceCreationMessage
     Then the correct 200 response is displayed
 
 @onestopswagger
@@ -67,9 +67,9 @@ Scenario: OneStop Send Program Account Details Broadcast
     And I click on the Authorize button
     And I click on the Close button
     And I click on the Get button for SendProgramAccountDetailsBroadcastMessage
-    And I click on the Try it out button
+    And I click on the Try it out button for SendProgramAccountDetailsBroadcastMessage
     And I enter the licence GUID 31b08509-909b-ea11-b818-00505683fbf4
-    And I click on the Execute button
+    And I click on the Execute button for SendProgramAccountDetailsBroadcastMessage
     Then the correct 200 response is displayed   
    
 @onestopswagger
@@ -78,10 +78,10 @@ Scenario: OneStop LDB Export
     And I click on the Authorize button
     And I click on the Close button
     And I click on the Get button for LdbExport
-    And I click on the Try it out button
+    And I click on the Try it out button for LdbExport
     And I enter the licence GUID 31b08509-909b-ea11-b818-00505683fbf4
-    And I click on the Execute button
-    Then the correct 200 response is displayed         
+    And I click on the Execute button for LdbExport
+    Then the correct 200 response is displayed          
 */
 
 namespace bdd_tests
