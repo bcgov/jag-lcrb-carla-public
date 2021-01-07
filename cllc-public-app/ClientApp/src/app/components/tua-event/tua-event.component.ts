bf1bdcd18a42442e737ad5dee7abfe24b502f799
Alejandro Sanchez <emailforasr@gmail.com>
2021-01-06 17:39:59 -0800

    Basic routing to TUA event form

---
+++ cllc-public-app/ClientApp/src/app/components/tua-event/tua-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tua-event',
+  templateUrl: './tua-event.component.html',
+  styleUrls: ['./tua-event.component.scss']
+})
+export class TuaEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
