2b1a9fea6cbbed0e7b4e613cc8905c7b4e646b61
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-08 14:06:13 -0800

    Update Gherkin.

--- functional-tests/bdd-tests/FoodPrimaryRelocation.cs
+++ functional-tests/bdd-tests/FoodPrimaryRelocation.cs
@@ -29,7 +29,6 @@ Scenario: Food Primary Relocation Application (Private Corporation)
     And I click on the Dashboard tab
     And the dashboard status is updated as Application Under Review
     And the application is approved
-    # And I pay the licensing fee 
     And I click on the link for Relocation Application
     And I request a relocation application
     And I click on the link for Dashboard
