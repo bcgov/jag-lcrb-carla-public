326c606f1c8948d7a3b16e10e551da9c47241ccf
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-08 13:29:08 -0800

    Fix checkboxes.

--- functional-tests/bdd-tests/TestBaseMfgApplication.cs
+++ functional-tests/bdd-tests/TestBaseMfgApplication.cs
@@ -301,11 +301,11 @@ namespace bdd_tests
             uiContactEmail.SendKeys(contactEmail);
 
             // select the authorized to submit checkbox
-            NgWebElement uiAuthorizedToSubmit = ngDriver.FindElement(By.CssSelector("input[formcontrolname='authorizedToSubmit'][type='checkbox']"));
+            NgWebElement uiAuthorizedToSubmit = ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='authorizedToSubmit']"));
             uiAuthorizedToSubmit.Click();
 
             // select the signature agreement checkbox
-            NgWebElement uiSignatureAgreement = ngDriver.FindElement(By.CssSelector("input[formcontrolname='signatureAgreement'][type='checkbox']"));
+            NgWebElement uiSignatureAgreement = ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='signatureAgreement']"));
             uiSignatureAgreement.Click();
 
             // retrieve the current URL to get the application ID (needed downstream)
