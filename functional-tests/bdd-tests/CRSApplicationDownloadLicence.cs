6aadebd92e5b42fad9a7c4a85a9c04c3dfed3353
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-11 08:16:37 -0800

    Update Gherkin.

--- functional-tests/bdd-tests/CRSApplicationDownloadLicence.cs
+++ functional-tests/bdd-tests/CRSApplicationDownloadLicence.cs
@@ -57,7 +57,7 @@ namespace bdd_tests
 
             CheckFeatureFlagsSecurityScreening();
 
-            CheckFeatureLEConnections();
+            //CheckFeatureLEConnections();
 
             IgnoreSynchronizationFalse();
 
