733b0978d202c49e7f0566337c09790f18f10740
George Walker <george@walkersoftware.ca>
2021-01-12 16:54:57 -0800

    Adjust redirector

--- landing-page/ClientApp/src/app/models/license-type.model.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationType } from './application-type.model';
-
-export class LicenseType {
-  id: string;
-  code: string;
-  name: string;
-  allowedActions: ApplicationType[];
-}
--- landing-page/ClientApp/src/app/models/vote-question.model.ts
+++
@@ -1,9 +0,0 @@
-import { VoteOption } from "./vote-option.model";
-export class VoteQuestion {
-  id: string;
-  title: string;
-  question: string;
-  slug: string;  
-  options: VoteOption[];
-  constructor() { }
-}
