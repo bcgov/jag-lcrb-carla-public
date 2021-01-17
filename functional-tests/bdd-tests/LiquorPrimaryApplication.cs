3ab63ff96889d0516ec926b7007916fb07b6270d
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-17 08:46:06 -0800

    Draft Liquor Primary Gherkin.

---
+++ functional-tests/bdd-tests/LiquorPrimaryApplication.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: LiquorPrimaryApplication
+    As a logged in business user
+    I want to submit Liquor Primary Applications for different manufacturer types
+
+@liquorprimaryapp 
+Scenario: DEV Liquor Primary Application
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Liquor Primary Licence
+    And I review the account profile for a private corporation
+    And I complete the Liquor application
+    And I enter the payment information
+    And I confirm the payment receipt for a Liquor Primary Licence application
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./LiquorPrimaryApplication.feature")]
+    [Collection("Liquor")]
+    public sealed class LiquorPrimaryApplication : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            // CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
