883edeeafed75f0b29ac46b1c9bf912c1bbd6475
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-17 09:31:10 -0800

    Fix Gherkin.

--- functional-tests/bdd-tests/LiquorPrimaryApplication.cs
+++ functional-tests/bdd-tests/LiquorPrimaryApplication.cs
@@ -23,6 +23,7 @@ Scenario: DEV Liquor Primary Application (Private Corporation)
     And I click on the Start Application button for a Liquor Primary Licence
     And I review the account profile for a private corporation
     And I complete the Liquor Primary application for a private corporation
+    And I click on the Submit button
     And I enter the payment information
     And I confirm the payment receipt for a Liquor Primary Licence application
     And I click on the Dashboard tab
