bea3c562523c1be24a0713a53ee0283ca2919e81
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-21 07:52:03 -0800

    Clean up mess.

--- functional-tests/bdd-tests/MfgRelease.cs
+++ functional-tests/bdd-tests/MfgRelease.cs
@@ -1,4 +1,19 @@
-﻿Feature: NewFeature
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: NewFeature
     As a logged in business user
     I want to submit a Catering Application for different business types
 
@@ -68,3 +83,34 @@ Scenario: DEV Manufacturer Release #2 (Winery/Private Corporation)
     And I request a transfer of ownership
     And the account is deleted
     Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./MfgRelease.feature")]
+    [Collection("Liquor")]
+    public sealed class MfgRelease : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            // CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
