4d40db54ac63eb75dc446ebdb2d6be0f7b8f7d48
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-25 12:51:09 -0800

    Draft of permanent change to licensee test.

---
+++ functional-tests/bdd-tests/PermanentChangeToLicensee.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: PermanentChangeToLicensee
+    As a logged in business user
+    I want to submit a licensee changes for different licence types
+
+@cannabis @licenseechanges
+Scenario: DEV Licencee Changes (Private Corporation)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Cannabis Retail Store
+    And I complete the eligibility disclosure
+    And I review the account profile for a private corporation
+    And I complete the Cannabis Retail Store application for a private corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And the application is approved
+    And I click on the Dashboard tab
+    And I click on the button for Submit a Change
+    And I complete the Permanent Change to a Licensee application
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./PermanentChangeToLicensee.feature")]
+    [Collection("Cannabis")]
+    public sealed class PermanentChangeToLicensee : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsMaps();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            // CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
---
+++ functional-tests/bdd-tests/TestBasePermanentChangeLicensee.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+namespace bdd_tests
+{
+    public abstract partial class TestBase : Feature, IDisposable
+    {
+        [And(@"I complete the Permanent Change to a Licensee application")]
+        public void PermanentChangeLicensee(string appType)
+        {
+            /* 
+            Page Title: Permanent Change to a Licensee
+            */
+
+            // click on Internal Transfer of Shares
+            NgWebElement uiInternalTransferOfShares = ngDriver.FindElement(By.CssSelector("#mat-checkbox-4.mat-checkbox"));
+            uiInternalTransferOfShares.Click();
+
+            // click on External Transfer of Shares
+            NgWebElement uiExternalTransferOfShares = ngDriver.FindElement(By.CssSelector("#mat-checkbox-5.mat-checkbox"));
+            uiExternalTransferOfShares.Click();
+
+            // click on Change of Directors or Officers
+            NgWebElement uiChangeOfDirectorsOrOfficers = ngDriver.FindElement(By.CssSelector("#mat-checkbox-6.mat-checkbox"));
+            uiChangeOfDirectorsOrOfficers.Click();
+
+            // click on Name Change, Licensee -- Corporation
+            NgWebElement uiNameChangeLicenseeCorporation = ngDriver.FindElement(By.CssSelector("#mat-checkbox-7.mat-checkbox"));
+            uiNameChangeLicenseeCorporation.Click();
+
+            // click on Name Change, Person
+            NgWebElement uiNameChangePerson = ngDriver.FindElement(By.CssSelector("#mat-checkbox-8.mat-checkbox"));
+            uiNameChangePerson.Click();
+
+            // click on Addition of Receiver or Executor
+            NgWebElement uiAdditionOfReceiverOrExecutor = ngDriver.FindElement(By.CssSelector("#mat-checkbox-9.mat-checkbox"));
+            uiAdditionOfReceiverOrExecutor.Click();
+
+            if (appType == "Catering application")
+            {
+                // upload Central Securities Register forms
+                FileUpload("central_securities_register.pdf", "(//input[@type='file'])[3]");
+
+                // upload supporting business documentation
+                FileUpload("business_plan.pdf", "(//input[@type='file'])[6]");
+
+                // upload shareholders holding less than 10% interest
+                FileUpload("shareholders_less_10_interest.pdf", "(//input[@type='file'])[9]");
+
+                // upload Cannabis Associate Security Screening Forms
+                FileUpload("valid_interest.pdf", "(//input[@type='file'])[12]");
+
+                // upload Financial Integrity Documents
+                FileUpload("fin_integrity.pdf", "(//input[@type='file'])[15]");
+            }
+
+            if (appType == "CRS application")
+            {
+                // upload Cannabis Associate Security Screening Forms
+                FileUpload("valid_interest.pdf", "(//input[@type='file'])[]");
+
+                // upload Financial Integrity Documents
+                FileUpload("fin_integrity.pdf", "(//input[@type='file'])[]");
+            }
+
+            // select the authorized to submit checkbox
+            NgWebElement uiAuthorizedToSubmit = ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='authorizedToSubmit']"));
+            uiAuthorizedToSubmit.Click();
+
+            // select the signature agreement checkbox
+            NgWebElement uiSignatureAgreement = ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='signatureAgreement']"));
+            uiSignatureAgreement.Click();
+        }
+    }
+}
