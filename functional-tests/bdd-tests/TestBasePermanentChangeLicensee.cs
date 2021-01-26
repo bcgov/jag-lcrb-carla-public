ab4aba63506a1b4d6e95c484f576c7670d655a98
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-26 14:53:17 -0800

    Redo automation logic.

--- functional-tests/bdd-tests/TestBasePermanentChangeLicensee.cs
+++ functional-tests/bdd-tests/TestBasePermanentChangeLicensee.cs
@@ -29,71 +29,73 @@ namespace bdd_tests
             Page Title: Permanent Change to a Licensee
             */
 
-            if (appType != "Catering application for a partnership")
+            /*
+            // click on Internal Transfer of Shares
+            NgWebElement uiInternalTransferOfShares = ngDriver.FindElement(By.CssSelector("#mat-checkbox-3.mat-checkbox"));
+            uiInternalTransferOfShares.Click();
+
+            // click on External Transfer of Shares
+            NgWebElement uiExternalTransferOfShares = ngDriver.FindElement(By.CssSelector("#mat-checkbox-4.mat-checkbox"));
+            uiExternalTransferOfShares.Click();
+
+            // click on Change of Directors or Officers
+            NgWebElement uiChangeOfDirectorsOrOfficers = ngDriver.FindElement(By.CssSelector("#mat-checkbox-5.mat-checkbox"));
+            uiChangeOfDirectorsOrOfficers.Click();
+
+            // click on Name Change, Licensee -- Corporation
+            NgWebElement uiNameChangeLicenseeCorporation = ngDriver.FindElement(By.CssSelector("#mat-checkbox-6.mat-checkbox"));
+            uiNameChangeLicenseeCorporation.Click();
+
+            // click on Name Change, Person
+            NgWebElement uiNameChangePerson = ngDriver.FindElement(By.CssSelector("#mat-checkbox-7.mat-checkbox"));
+            uiNameChangePerson.Click();
+
+            // click on Addition of Receiver or Executor
+            NgWebElement uiAdditionOfReceiverOrExecutor = ngDriver.FindElement(By.CssSelector("#mat-checkbox-8.mat-checkbox"));
+            uiAdditionOfReceiverOrExecutor.Click();
+            */
+
+            switch (appType)
             {
-
-                // click on Internal Transfer of Shares
-                NgWebElement uiInternalTransferOfShares = ngDriver.FindElement(By.CssSelector("#mat-checkbox-3.mat-checkbox"));
-                uiInternalTransferOfShares.Click();
-
-                // click on External Transfer of Shares
-                NgWebElement uiExternalTransferOfShares = ngDriver.FindElement(By.CssSelector("#mat-checkbox-4.mat-checkbox"));
-                uiExternalTransferOfShares.Click();
-
-                // click on Change of Directors or Officers
-                NgWebElement uiChangeOfDirectorsOrOfficers = ngDriver.FindElement(By.CssSelector("#mat-checkbox-5.mat-checkbox"));
-                uiChangeOfDirectorsOrOfficers.Click();
-
-                // click on Name Change, Licensee -- Corporation
-                NgWebElement uiNameChangeLicenseeCorporation = ngDriver.FindElement(By.CssSelector("#mat-checkbox-6.mat-checkbox"));
-                uiNameChangeLicenseeCorporation.Click();
-
-                // click on Name Change, Person
-                NgWebElement uiNameChangePerson = ngDriver.FindElement(By.CssSelector("#mat-checkbox-7.mat-checkbox"));
-                uiNameChangePerson.Click();
-
-                // click on Addition of Receiver or Executor
-                NgWebElement uiAdditionOfReceiverOrExecutor = ngDriver.FindElement(By.CssSelector("#mat-checkbox-8.mat-checkbox"));
-                uiAdditionOfReceiverOrExecutor.Click();
-            }
-
-            if (appType == "Catering application for a private corporation")
-            {
-                // upload Central Securities Register forms
-                FileUpload("central_securities_register.pdf", "(//input[@type='file'])[3]");
-
-                // upload supporting business documentation
-                FileUpload("business_plan.pdf", "(//input[@type='file'])[6]");
-
-                // upload shareholders holding less than 10% interest
-                FileUpload("shareholders_less_10_interest.pdf", "(//input[@type='file'])[9]");
-
-                // upload Cannabis Associate Security Screening Forms
-                FileUpload("valid_interest.pdf", "(//input[@type='file'])[12]");
-
-                // upload Financial Integrity Documents
-                FileUpload("fin_integrity.pdf", "(//input[@type='file'])[15]");
-            }
-
-            if (appType == "Catering application for a partnership")
-            {
-                // upload Personal History Summary ocuments
-                FileUpload("fin_integrity.pdf", "(//input[@type='file'])[15]");
-            }
-
-            if (appType == "Catering application for a sole proprietorship")
-            { }
-
-            if (appType == "Catering application for a society")
-            { }
-
-            if (appType == "CRS application for a private corporation")
-            {
-                // upload Cannabis Associate Security Screening Forms
-                FileUpload("valid_interest.pdf", "(//input[@type='file'])[]");
-
-                // upload Financial Integrity Documents
-                FileUpload("fin_integrity.pdf", "(//input[@type='file'])[]");
+                case "Catering application for a society":
+                    // click on Change of Directors or Officers
+                    NgWebElement uiChangeOfDirectorsOrOfficers = ngDriver.FindElement(By.CssSelector("#mat-checkbox-3.mat-checkbox"));
+                    uiChangeOfDirectorsOrOfficers.Click();
+                    // click on Name Change, Licensee -- Society
+                    NgWebElement uiNameChangeLicenseeCorporation = ngDriver.FindElement(By.CssSelector("#mat-checkbox-4.mat-checkbox"));
+                    uiNameChangeLicenseeCorporation.Click();
+                    // click on Name Change, Person
+                    NgWebElement uiNameChangePerson = ngDriver.FindElement(By.CssSelector("#mat-checkbox-5.mat-checkbox"));
+                    uiNameChangePerson.Click();
+                    // click on Addition of Receiver or Executor
+                    NgWebElement uiAdditionOfReceiverOrExecutor = ngDriver.FindElement(By.CssSelector("#mat-checkbox-6.mat-checkbox"));
+                    uiAdditionOfReceiverOrExecutor.Click();
+                    break;
+                case "Catering application for a private corporation":
+                    // upload Central Securities Register forms
+                    FileUpload("central_securities_register.pdf", "(//input[@type='file'])[3]");
+                    // upload supporting business documentation
+                    FileUpload("business_plan.pdf", "(//input[@type='file'])[6]");
+                    // upload shareholders holding less than 10% interest
+                    FileUpload("shareholders_less_10_interest.pdf", "(//input[@type='file'])[9]");
+                    // upload Cannabis Associate Security Screening Forms
+                    FileUpload("valid_interest.pdf", "(//input[@type='file'])[12]");
+                    // upload Financial Integrity Documents
+                    FileUpload("fin_integrity.pdf", "(//input[@type='file'])[15]");
+                    break;
+                case "Catering application for a partnership":
+                    // upload Personal History Summary ocuments
+                    FileUpload("fin_integrity.pdf", "(//input[@type='file'])[15]");
+                    break;
+                case "Catering application for a sole proprietorship":
+                    // TODO
+                    break;
+                case "CRS application for a private corporation":
+                    // upload Cannabis Associate Security Screening Forms
+                    FileUpload("valid_interest.pdf", "(//input[@type='file'])[]");
+                    // upload Financial Integrity Documents
+                    FileUpload("fin_integrity.pdf", "(//input[@type='file'])[]");
+                    break;
             }
 
             // select the authorized to submit checkbox
