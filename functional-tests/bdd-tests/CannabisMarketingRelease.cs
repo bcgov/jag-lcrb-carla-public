8e7c511f880ef0b10f301c96742bec5e732fbada
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-26 09:01:35 -0800

    Add release test for mktg.

---
+++ functional-tests/bdd-tests/CannabisMarketingRelease.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: CannabisMarketingRelease
+    As a logged in business user
+    I want to run a release test for a Cannabis Marketing Licence
+
+@cannabismktg @release
+Scenario: DEV Cannabis Marketing Release (Private Corporation)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Cannabis Marketing Licence
+    And I review the account profile for a private corporation
+    And I complete the Cannabis Marketing application for a private corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Marketing Licence
+    And I click on the Dashboard tab
+    And the application is approved
+    And I click on the Licences tab
+    And I click on the link for Download Licence
+    And the licence is successfully downloaded
+    And I request a transfer of ownership
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./CannabisMarketingRelease.feature")]
+    [Collection("Liquor")]
+    public sealed class CannabisMarketingRelease : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
