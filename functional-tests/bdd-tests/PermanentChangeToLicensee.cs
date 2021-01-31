828bf03cd4bde9dcdb518e2359c53cadde579373
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-31 08:53:37 -0800

    Remove CRS scenario.

--- functional-tests/bdd-tests/PermanentChangeToLicensee.cs
+++ functional-tests/bdd-tests/PermanentChangeToLicensee.cs
@@ -15,28 +15,9 @@ using Xunit;
 /*
 Feature: PermanentChangeToLicensee
     As a logged in business user
-    I want to submit a licensee changes for different licence types
+    I want to submit a licensee changes for different business types
 
-@cannabis @licenseechanges @release
-Scenario: DEV CRS Licensee Changes (Private Corporation)
-    Given I am logged in to the dashboard as a private corporation
-    And I click on the Start Application button for a Cannabis Retail Store
-    And I complete the eligibility disclosure
-    And I review the account profile for a private corporation
-    And I complete the Cannabis Retail Store application for a private corporation
-    And I click on the Submit button
-    And I enter the payment information
-    And the application is approved
-    And I click on the Licences tab
-    And I pay the licensing fee 
-    And I click on the Dashboard tab
-    And I click on the button for Submit a Change
-    And I complete the Permanent Change to a Licensee application for a CRS application for a private corporation
-    And I click on the Submit button
-    And the account is deleted
-    Then I see the login page
-
-@catering @licenseechanges
+@catering @licenseechanges @release
 Scenario: DEV Catering Licensee Changes (Private Corporation)
     Given I am logged in to the dashboard as a private corporation
     And I click on the Start Application button for Catering
