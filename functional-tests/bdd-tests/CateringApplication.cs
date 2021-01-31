80e9fd4f7526c13ebcbe03c3dea424937eff1323
ElizabethWolfe <ejwolfe@gmail.com>
2021-01-31 09:53:52 -0800

    Housekeeping.

--- functional-tests/bdd-tests/CateringApplication.cs
+++ functional-tests/bdd-tests/CateringApplication.cs
@@ -17,42 +17,6 @@ Feature: CateringApplication
     As a logged in business user
     I want to submit a Catering Application for different business types
 
-@cateringapp @partnership 
-Scenario: UAT Catering Application (Partnership)
-    Given I am logged in to the dashboard as a partnership
-    And I click on the Start Application button for Catering
-    And I review the account profile for a partnership
-    And I review the organization structure for a partnership
-    And I click on the button for Submit Organization Information
-    And I complete the Catering application
-    And I click on the secondary Submit button
-    And I review the security screening requirements for a partnership
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And I confirm the payment receipt for a Catering application
-    And I click on the Dashboard tab
-    And the dashboard status is updated as Application Under Review
-    And the account is deleted
-    Then I see the login page
-
-@cateringapp @privatecorporation
-Scenario: UAT Catering Application (Private Corporation)
-    Given I am logged in to the dashboard as a private corporation
-    And I click on the Start Application button for Catering
-    And I review the account profile for a private corporation
-    And I review the organization structure for a private corporation
-    And I click on the button for Submit Organization Information
-    And I complete the Catering application
-    And I click on the secondary Submit button
-    And I review the security screening requirements for a private corporation
-    And I click on the button for Pay for Application
-    And I enter the payment information
-    And I confirm the payment receipt for a Catering application
-    And I click on the Dashboard tab
-    And the dashboard status is updated as Application Under Review
-    And the account is deleted
-    Then I see the login page
-
 @cateringapp @privatecorporation
 Scenario: DEV Catering Application (Private Corporation)
     Given I am logged in to the dashboard as a private corporation
@@ -67,17 +31,13 @@ Scenario: DEV Catering Application (Private Corporation)
     And the account is deleted
     Then I see the login page
 
-@cateringapp @publiccorporation
-Scenario: UAT Catering Application (Public Corporation)
-    Given I am logged in to the dashboard as a public corporation
+@cateringapp @society
+Scenario: DEV Catering Application (Society)
+    Given I am logged in to the dashboard as a society
     And I click on the Start Application button for Catering
-    And I review the account profile for a public corporation
-    And I review the organization structure for a public corporation
-    And I click on the button for Submit Organization Information
+    And I review the account profile for a society
     And I complete the Catering application
-    And I click on the secondary Submit button
-    And I review the security screening requirements for a public corporation
-    And I click on the button for Pay for Application
+    And I click on the Submit button
     And I enter the payment information
     And I confirm the payment receipt for a Catering application
     And I click on the Dashboard tab
@@ -85,16 +45,13 @@ Scenario: UAT Catering Application (Public Corporation)
     And the account is deleted
     Then I see the login page
 
-@cateringapp @society
-Scenario: UAT Catering Application (Society)
-    Given I am logged in to the dashboard as a society
+@cateringapp @partnership 
+Scenario: DEV Catering Application (Partnership)
+    Given I am logged in to the dashboard as a partnership
     And I click on the Start Application button for Catering
-    And I review the account profile for a society
-    And I review the organization structure for a society
-    And I click on the button for Submit Organization Information
+    And I review the account profile for a partnership
     And I complete the Catering application
-    And I click on the secondary Submit button
-    And I review the security screening requirements for a society
+    And I click on the Submit button
     And I click on the button for Pay for Application
     And I enter the payment information
     And I confirm the payment receipt for a Catering application
@@ -104,22 +61,108 @@ Scenario: UAT Catering Application (Society)
     Then I see the login page
 
 @cateringapp @soleproprietorship
-Scenario: UAT Catering Application (Sole Proprietorship)
+Scenario: DEV Catering Application (Sole Proprietorship)
     Given I am logged in to the dashboard as a sole proprietorship
     And I click on the Start Application button for Catering
     And I review the account profile for a sole proprietorship
-    And I review the organization structure for a sole proprietorship
-    And I click on the button for Submit Organization Information
     And I complete the Catering application
-    And I click on the secondary Submit button
-    And I review the security screening requirements for a sole proprietorship
-    And I click on the button for Pay for Application
+    And I click on the Submit button
     And I enter the payment information
     And I confirm the payment receipt for a Catering application
     And I click on the Dashboard tab
     And the dashboard status is updated as Application Under Review
     And the account is deleted
     Then I see the login page
+
+#@cateringapp @partnership 
+#Scenario: UAT Catering Application (Partnership)
+#    Given I am logged in to the dashboard as a partnership
+#    And I click on the Start Application button for Catering
+#    And I review the account profile for a partnership
+#    And I review the organization structure for a partnership
+#    And I click on the button for Submit Organization Information
+#    And I complete the Catering application
+#    And I click on the secondary Submit button
+#    And I review the security screening requirements for a partnership
+#    And I click on the button for Pay for Application
+#    And I enter the payment information
+#    And I confirm the payment receipt for a Catering application
+#    And I click on the Dashboard tab
+#    And the dashboard status is updated as Application Under Review
+#    And the account is deleted
+#    Then I see the login page
+
+#@cateringapp @privatecorporation
+#Scenario: UAT Catering Application (Private Corporation)
+#    Given I am logged in to the dashboard as a private corporation
+#    And I click on the Start Application button for Catering
+#    And I review the account profile for a private corporation
+#    And I review the organization structure for a private corporation
+#    And I click on the button for Submit Organization Information
+#    And I complete the Catering application
+#    And I click on the secondary Submit button
+#    And I review the security screening requirements for a private corporation
+#    And I click on the button for Pay for Application
+#    And I enter the payment information
+#    And I confirm the payment receipt for a Catering application
+#    And I click on the Dashboard tab
+#    And the dashboard status is updated as Application Under Review
+#    And the account is deleted
+#    Then I see the login page
+
+#@cateringapp @publiccorporation
+#Scenario: UAT Catering Application (Public Corporation)
+#    Given I am logged in to the dashboard as a public corporation
+#    And I click on the Start Application button for Catering
+#    And I review the account profile for a public corporation
+#    And I review the organization structure for a public corporation
+#    And I click on the button for Submit Organization Information
+#    And I complete the Catering application
+#    And I click on the secondary Submit button
+#    And I review the security screening requirements for a public corporation
+#    And I click on the button for Pay for Application
+#    And I enter the payment information
+#    And I confirm the payment receipt for a Catering application
+#    And I click on the Dashboard tab
+#    And the dashboard status is updated as Application Under Review
+#    And the account is deleted
+#    Then I see the login page
+
+#@cateringapp @society
+#Scenario: UAT Catering Application (Society)
+#    Given I am logged in to the dashboard as a society
+#    And I click on the Start Application button for Catering
+#    And I review the account profile for a society
+#    And I review the organization structure for a society
+#    And I click on the button for Submit Organization Information
+#    And I complete the Catering application
+#    And I click on the secondary Submit button
+#    And I review the security screening requirements for a society
+#    And I click on the button for Pay for Application
+#    And I enter the payment information
+#    And I confirm the payment receipt for a Catering application
+#    And I click on the Dashboard tab
+#    And the dashboard status is updated as Application Under Review
+#    And the account is deleted
+#    Then I see the login page
+
+#@cateringapp @soleproprietorship
+#Scenario: UAT Catering Application (Sole Proprietorship)
+#    Given I am logged in to the dashboard as a sole proprietorship
+#    And I click on the Start Application button for Catering
+#    And I review the account profile for a sole proprietorship
+#    And I review the organization structure for a sole proprietorship
+#    And I click on the button for Submit Organization Information
+#    And I complete the Catering application
+#    And I click on the secondary Submit button
+#    And I review the security screening requirements for a sole proprietorship
+#    And I click on the button for Pay for Application
+#    And I enter the payment information
+#    And I confirm the payment receipt for a Catering application
+#    And I click on the Dashboard tab
+#    And the dashboard status is updated as Application Under Review
+#    And the account is deleted
+#    Then I see the login page
 */
 
 namespace bdd_tests
