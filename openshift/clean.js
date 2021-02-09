9f3b8bb21202d1fa815b177f89214e9722408a56
George Walker <george@walkersoftware.ca>
2021-02-09 10:35:02 -0800

    fix typo

--- openshift/clean.js
+++ openshift/clean.js
@@ -2,7 +2,8 @@
 
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('student.json');
-let student = JSON.parse(rawdata);
-
 var args = process.argv.slice(2);
+
+let rawdata = fs.readFileSync(args[0]);
+let template = JSON.parse(rawdata);
+
