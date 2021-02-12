3064fe38c3994f876cb2e874c0d56aba51858aaf
ElizabethWolfe <ejwolfe@gmail.com>
2021-02-12 08:53:08 -0800

    Automation.

--- functional-tests/bdd-tests/LiquorPrimaryStructuralChangeNoCapacity.cs
+++ functional-tests/bdd-tests/LiquorPrimaryStructuralChangeNoCapacity.cs
@@ -36,7 +36,7 @@ Scenario: Liquor Primary Structural Change No Capacity (Private Corporation)
     And I click on the Licences tab
     And I pay the licensing fee
     And I click on the link for Structural Change Application (No Capacity Increase)
-    # TODO
+    And I request a no capacity structural change
     And the account is deleted
     Then I see the login page
 */
