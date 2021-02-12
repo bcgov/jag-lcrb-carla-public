6677dcfdb9897ca8b084c302cfd1faa52dde86e8
ElizabethWolfe <ejwolfe@gmail.com>
2021-02-12 08:46:00 -0800

    Automation.

---
+++ functional-tests/bdd-tests/TestBaseStructuralChangeNoCapacityIncrease.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+namespace bdd_tests
+{
+    public abstract partial class TestBase : Feature, IDisposable
+    {
+        [And(@"I request a structural change")]
+        public void RequestStructuralChange()
+        {
+            /* 
+            Page Title: Licences & Authorizations
+            Subtitle:   Cannabis Retail Store Licences
+            */
+
+            string structuralChange = "Request a Structural Change";
+
+            // click on the request structural change link
+            NgWebElement uiStructuralChange = ngDriver.FindElement(By.LinkText(structuralChange));
+            uiStructuralChange.Click();
+
+            /* 
+            Page Title: Please Review the Account Profile
+            */
+
+            ContinueToApplicationButton();
+
+            /* 
+            Page Title: Submit the Cannabis Retail Store Structural Change Application
+            */
+
+            // create test data
+            string description = "Test automation outline of the proposed change.";
+
+            // enter the description of the change
+            NgWebElement uiDescriptionOfChange = ngDriver.FindElement(By.Id("description1"));
+            uiDescriptionOfChange.SendKeys(description);
+
+            // select not visible from outside checkbox
+            NgWebElement uiVisibleFromOutside = ngDriver.FindElement(By.CssSelector(".mat-checkbox-inner-container"));
+            JavaScriptClick(uiVisibleFromOutside);
+
+            // upload a floor plan document
+            FileUpload("floor_plan.pdf", "(//input[@type='file'])[2]");
+
+            // upload a site plan
+            FileUpload("site_plan.pdf", "(//input[@type='file'])[5]");
+
+            // select authorizedToSubmit checkbox
+            NgWebElement uiAuthorizedToSubmit = ngDriver.FindElement(By.Id("authorizedToSubmit"));
+            uiAuthorizedToSubmit.Click();
+
+            // select signatureAgreement checkbox
+            NgWebElement uiSignatureAgreement = ngDriver.FindElement(By.Id("signatureAgreement"));
+            uiSignatureAgreement.Click();
+
+            // click on the Submit & Pay button
+            ClickOnSubmitButton();
+
+            // pay for the structural change application
+            MakePayment();
+
+            /* 
+            Page Title: Payment Approved
+            */
+
+            ClickLicencesTab();
+        }
+
+
+        [And(@"I confirm the structural change request is displayed on the dashboard")]
+        public void RequestedStructuralChangeOnDashboard()
+        {
+            /* 
+            Page Title: Welcome to Liquor and Cannabis Licensing
+            */
+
+            // confirm that a structural change request is displayed
+            Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'Structural Change')]")).Displayed);
+        }
+    }
+}
