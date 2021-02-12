ef04584978fa19fc395612d6fe2ac16495fa2cbc
ElizabethWolfe <ejwolfe@gmail.com>
2021-02-11 19:40:50 -0800

    LP automations - WIP.

--- functional-tests/bdd-tests/LiquorPrimaryNewOutdoorPatio.cs
+++ functional-tests/bdd-tests/LiquorPrimaryNewOutdoorPatio.cs
@@ -35,7 +35,10 @@ Scenario: Liquor Primary New Outdoor Patio (Private Corporation)
     And the application is approved
     And I click on the Licences tab
     And I pay the licensing fee
-    # TODO
+    And I click on the link for New Outdoor Patio
+    And I request a new outdoor patio application
+    And I click on the Submit button
+    And I enter the payment information
     And the account is deleted
     Then I see the login page
 */
