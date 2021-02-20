9a6f47609ba3b7ca95f6f3805f976166c8e029bd
ElizabethWolfe <ejwolfe@gmail.com>
2021-02-19 17:26:03 -0800

    Delete org structure tests.

--- functional-tests/bdd-tests/UBrewUVinLicenceNameChange.cs
+++ functional-tests/bdd-tests/UBrewUVinLicenceNameChange.cs
@@ -18,7 +18,7 @@ Feature: UBrewUVinLicenceNameChange
     I want to submit a UBrew / UVin Licence name change application for different business types
 
 @ubrewuvinnamechange @partnership 
-Scenario: DEV UBrew / UVin Application Establishment Name Change (Partnership)
+Scenario: UBrew / UVin Application Establishment Name Change (Partnership)
     Given I am logged in to the dashboard as a partnership
     And I click on the Start Application button for a UBrew UVin application
     And I review the account profile for a partnership
@@ -39,7 +39,7 @@ Scenario: DEV UBrew / UVin Application Establishment Name Change (Partnership)
     Then I see the login page
 
 @ubrewuvinnamechange @privatecorporation 
-Scenario: DEV UBrew / UVin Application Establishment Name Change (Private Corporation)
+Scenario: UBrew / UVin Application Establishment Name Change (Private Corporation)
     Given I am logged in to the dashboard as a private corporation
     And I click on the Start Application button for a UBrew UVin application
     And I review the account profile for a private corporation
@@ -60,7 +60,7 @@ Scenario: DEV UBrew / UVin Application Establishment Name Change (Private Corpor
     Then I see the login page
 
 @ubrewuvinnamechange @publiccorporation 
-Scenario: DEV UBrew / UVin Application Establishment Name Change (Public Corporation)
+Scenario: UBrew / UVin Application Establishment Name Change (Public Corporation)
     Given I am logged in to the dashboard as a public corporation
     And I click on the Start Application button for a UBrew UVin application
     And I review the account profile for a public corporation
@@ -81,7 +81,7 @@ Scenario: DEV UBrew / UVin Application Establishment Name Change (Public Corpora
     Then I see the login page
 
 @ubrewuvinnamechange @soleproprietorship 
-Scenario: DEV UBrew / UVin Application Establishment Name Change (Sole Proprietorship)
+Scenario: UBrew / UVin Application Establishment Name Change (Sole Proprietorship)
     Given I am logged in to the dashboard as a sole proprietorship
     And I click on the Start Application button for a UBrew UVin application
     And I review the account profile for a sole proprietorship
@@ -100,106 +100,6 @@ Scenario: DEV UBrew / UVin Application Establishment Name Change (Sole Proprieto
     And the dashboard status is updated as Application Under Review
     And the account is deleted
     Then I see the login page
-
-#@ubrewuvinnamechange @partnership 
-#Scenario: UAT UBrew / UVin Application Establishment Name Change (Partnership)
-#    Given I am logged in to the dashboard as a partnership
-#    And I click on the Start Application button for a UBrew UVin application
-#    And I review the account profile for a partnership
-#    And I review the organization structure for a partnership
-#    And I click on the button for Submit Organization Information
-#    And I complete the UBrew / UVin application for a partnership
-#    And I click on the Submit button
-#    And I review the security screening requirements for a partnership
-#    And I click on the button for Pay for Application
-#    And I enter the payment information
-#    And I confirm the payment receipt for a UBrew / UVin application
-#    And I click on the Dashboard tab
-#    And the dashboard status is updated as Application Under Review
-#    And the application is approved
-#    And I click on the Licences tab
-#    And I pay the licensing fee 
-#    And I click on the Licences tab
-#    And I request a valid store name or branding change for UBrew
-#    And I click on the Dashboard tab
-#    And the dashboard status is updated as Application Under Review
-#    And the account is deleted
-#    Then I see the login page
-#
-#@ubrewuvinnamechange @privatecorporation 
-#Scenario: UAT UBrew / UVin Application Establishment Name Change (Private Corporation)
-#    Given I am logged in to the dashboard as a private corporation
-#    And I click on the Start Application button for a UBrew UVin application
-#    And I review the account profile for a private corporation
-#    And I review the organization structure for a private corporation
-#    And I click on the button for Submit Organization Information
-#    And I complete the UBrew / UVin application for a private corporation
-#    And I click on the Submit button
-#    And I review the security screening requirements for a private corporation
-#    And I click on the button for Pay for Application
-#    And I enter the payment information
-#    And I confirm the payment receipt for a UBrew / UVin application
-#    And I click on the Dashboard tab
-#    And the dashboard status is updated as Application Under Review
-#    And the application is approved
-#    And I click on the Licences tab
-#    And I pay the licensing fee 
-#    And I click on the Licences tab
-#    And I request a valid store name or branding change for UBrew
-#    And I click on the Dashboard tab
-#    And the dashboard status is updated as Application Under Review
-#    And the account is deleted
-#    Then I see the login page
-#
-#@ubrewuvinnamechange @publiccorporation 
-#Scenario: UAT UBrew / UVin Application Establishment Name Change (Public Corporation)
-#    Given I am logged in to the dashboard as a public corporation
-#    And I click on the Start Application button for a UBrew UVin application
-#    And I review the account profile for a public corporation
-#    And I review the organization structure for a public corporation
-#    And I click on the button for Submit Organization Information
-#    And I complete the UBrew / UVin application for a public corporation
-#    And I click on the Submit button
-#    And I review the security screening requirements for a public corporation
-#    And I click on the button for Pay for Application
-#    And I enter the payment information
-#    And I confirm the payment receipt for a UBrew / UVin application
-#    And I click on the Dashboard tab
-#    And the dashboard status is updated as Application Under Review
-#    And the application is approved
-#    And I click on the Licences tab
-#    And I pay the licensing fee 
-#    And I click on the Licences tab
-#    And I request a valid store name or branding change for UBrew
-#    And I click on the Dashboard tab
-#    And the dashboard status is updated as Application Under Review
-#    And the account is deleted
-#    Then I see the login page
-#
-#@ubrewuvinnamechange @soleproprietorship 
-#Scenario: UAT UBrew / UVin Application Establishment Name Change (Sole Proprietorship)
-#    Given I am logged in to the dashboard as a sole proprietorship
-#    And I click on the Start Application button for a UBrew UVin application
-#    And I review the account profile for a sole proprietorship
-#    And I review the organization structure for a sole proprietorship
-#    And I click on the button for Submit Organization Information
-#    And I complete the UBrew / UVin application for a sole proprietorship
-#    And I click on the Submit button
-#    And I review the security screening requirements for a sole proprietorship
-#    And I click on the button for Pay for Application
-#    And I enter the payment information
-#    And I confirm the payment receipt for a UBrew / UVin application
-#    And I click on the Dashboard tab
-#    And the dashboard status is updated as Application Under Review
-#    And the application is approved
-#    And I click on the Licences tab
-#    And I pay the licensing fee 
-#    And I click on the Licences tab
-#    And I request a valid store name or branding change for UBrew
-#    And I click on the Dashboard tab
-#    And the dashboard status is updated as Application Under Review
-#    And the account is deleted
-#    Then I see the login page
 */
 
 namespace bdd_tests
