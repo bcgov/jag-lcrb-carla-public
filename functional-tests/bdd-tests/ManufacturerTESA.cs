46e441452344462b54d7eedd0689da36a2535622
ElizabethWolfe <ejwolfe@gmail.com>
2021-02-22 12:17:26 -0800

    Add LG approval for TESA.

--- functional-tests/bdd-tests/ManufacturerTESA.cs
+++ functional-tests/bdd-tests/ManufacturerTESA.cs
@@ -31,6 +31,14 @@ Scenario: Manufacturer TESA Application (Winery)
     And I click on the Continue to Application button
     And I complete the TESA application
     And I click on the Submit button
+    And I log in as local government for Parksville
+    And I click on the link for Applications for Review
+    And I click on the link for Review Application
+    And I specify my contact details as the approving authority
+    And I click on the Submit button
+    And I log in as a return user
+    And I click on the link for Complete Application
+    And I review the local government response for a liquor primary licence
     And the account is deleted
     Then I see the login page
 
@@ -48,6 +56,14 @@ Scenario: Manufacturer TESA Application (Brewery)
     And I click on the Continue to Application button
     And I complete the TESA application
     And I click on the Submit button
+    And I log in as local government for Parksville
+    And I click on the link for Applications for Review
+    And I click on the link for Review Application
+    And I specify my contact details as the approving authority
+    And I click on the Submit button
+    And I log in as a return user
+    And I click on the link for Complete Application
+    And I review the local government response for a liquor primary licence
     And the account is deleted
     Then I see the login page
 
@@ -65,6 +81,14 @@ Scenario: Manufacturer TESA Application (Distillery)
     And I click on the Continue to Application button
     And I complete the TESA application
     And I click on the Submit button
+    And I log in as local government for Parksville
+    And I click on the link for Applications for Review
+    And I click on the link for Review Application
+    And I specify my contact details as the approving authority
+    And I click on the Submit button
+    And I log in as a return user
+    And I click on the link for Complete Application
+    And I review the local government response for a liquor primary licence
     And the account is deleted
     Then I see the login page
 
@@ -82,6 +106,14 @@ Scenario: Manufacturer TESA Application (Co-packer)
     And I click on the Continue to Application button
     And I complete the TESA application
     And I click on the Submit button
+    And I log in as local government for Parksville
+    And I click on the link for Applications for Review
+    And I click on the link for Review Application
+    And I specify my contact details as the approving authority
+    And I click on the Submit button
+    And I log in as a return user
+    And I click on the link for Complete Application
+    And I review the local government response for a liquor primary licence
     And the account is deleted
     Then I see the login page
 */
