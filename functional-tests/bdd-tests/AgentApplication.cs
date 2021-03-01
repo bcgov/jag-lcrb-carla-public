d722b75aa3e20284c55c157a0953eb6ff3ab863b
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-01 14:48:30 -0800

    Start Agent test.

---
+++ functional-tests/bdd-tests/AgentApplication.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: CannabisMarketingApplication
+    As a logged in business user
+    I want to submit a Cannabis Marketing application for different business types
+
+@cannabismktg @privatecorporation
+Scenario: Cannabis Marketing Application (Private Corporation)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Cannabis Marketing Licence
+    And I review the account profile for a private corporation
+    And I complete the Cannabis Marketing application for a private corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Marketing Licence
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@cannabismktg @society 
+Scenario: Cannabis Marketing Application (Society)
+    Given I am logged in to the dashboard as a society
+    And I click on the Start Application button for a Cannabis Marketing Licence
+    And I review the account profile for a society
+    And I complete the Cannabis Marketing application for a society
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Marketing Licence
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@cannabismktg @partnership 
+Scenario: Cannabis Marketing Application (Partnership)
+    Given I am logged in to the dashboard as a partnership
+    And I click on the Start Application button for a Cannabis Marketing Licence
+    And I review the account profile for a partnership
+    And I complete the Cannabis Marketing application for a partnership
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Marketing Licence
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+
+@cannabismktg @soleproprietorship 
+Scenario: Cannabis Marketing Application (Sole Proprietorship)
+    Given I am logged in to the dashboard as a sole proprietorship
+    And I click on the Start Application button for a Cannabis Marketing Licence
+    And I review the account profile for a sole proprietorship
+    And I complete the Cannabis Marketing application for a sole proprietorship
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for a Cannabis Marketing Licence
+    And I click on the Dashboard tab
+    And the dashboard status is updated as Application Under Review
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./CannabisMarketingApplication.feature")]
+    [Collection("Liquor")]
+    public sealed class CannabisMarketingApplication : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
