59b2a49e84907ec4f5ae789cb09f2996266857cd
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-02 11:41:40 -0800

    Automation for rural relocation test.

--- functional-tests/bdd-tests/TestBaseRuralRelocationApplication.cs
+++ functional-tests/bdd-tests/TestBaseRuralRelocationApplication.cs
@@ -35,64 +35,89 @@ namespace bdd_tests
             string proposedPostalCode = "V9A 6X5";
             string proposedPID = "111111111";
 
+            string resortDescription = "Sample resort description.";
+            string numberOfResidents = "18";
+            string otherBusinesses = "Sample other businesses.";
+            string legalOwners = "Sample legal owners.";
+            string business = "Sample business.";
+
             // upload proof of zoning 
             FileUpload("proof_of_zoning.pdf", "(//input[@type='file'])[3]");
 
+            // select 'Yes' for 'Is the proposed RLRS located in a rural community with no other RLRS?'
+            NgWebElement uiIsRlrsLocatedInRuralCommunityAloneYes = ngDriver.FindElement(By.Id("mat-button-toggle-127-button"));
+            JavaScriptClick(uiIsRlrsLocatedInRuralCommunityAloneYes);
 
+            // select 'Yes' for 'Is the proposed RLRS located in a tourist destination resort with no other RLRS?'
+            NgWebElement uiIsRlrsLocatedAtTouristDestinationAloneYes = ngDriver.FindElement(By.Id("mat-button-toggle-130-button"));
+            JavaScriptClick(uiIsRlrsLocatedAtTouristDestinationAloneYes);
 
+            // enter resort community description
+            NgWebElement uiRlrsResortCommunityDescription = ngDriver.FindElement(By.CssSelector("textarea#rlrsResortCommunityDescription"));
+            uiRlrsResortCommunityDescription.SendKeys(resortDescription);
 
+            // select 'Yes' for 'Is there year-round all-weather road access to the community?'
+            NgWebElement uiHasYearRoundAllWeatherRoadAccessYes = ngDriver.FindElement(By.Id("mat-button-toggle-133-button"));
+            JavaScriptClick(uiHasYearRoundAllWeatherRoadAccessYes);
 
+            // select 'Yes' for 'Does your general store operate seasonally?'
+            NgWebElement uiDoesGeneralStoreOperateSeasonallyYes = ngDriver.FindElement(By.Id("mat-button-toggle-136-button"));
+            JavaScriptClick(uiDoesGeneralStoreOperateSeasonallyYes);
 
+            // enter the number of residents
+            NgWebElement uiNumberOfResidents = ngDriver.FindElement(By.CssSelector("input#surroundingResidentsOfRlrs"));
+            uiNumberOfResidents.SendKeys(numberOfResidents);
 
-            // enter the proposed address
-            NgWebElement uiProposedAddress = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentAddressStreet']"));
-            uiProposedAddress.SendKeys(proposedAddress);
+            // select 'Yes' for 'Is the proposed RLRS located at least 10 km, by all-weather road, from another RLRS, LRS, or GLS?'
+            NgWebElement uiIsRlrsAtLeast10kmFromAnotherStoreYes = ngDriver.FindElement(By.Id("mat-button-toggle-139-button"));
+            JavaScriptClick(uiIsRlrsAtLeast10kmFromAnotherStoreYes);
 
-            // enter the proposed city
-            NgWebElement uiProposedCity = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentAddressCity']"));
-            uiProposedCity.SendKeys(proposedCity);
+            // enter the other businesses
+            NgWebElement uiOtherBusinesses = ngDriver.FindElement(By.CssSelector("textarea#otherBusinessesDetails"));
+            uiOtherBusinesses.SendKeys(otherBusinesses);
 
-            // enter the proposed postal code
-            NgWebElement uiProposedPostalCode = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentAddressPostalCode']"));
-            uiProposedPostalCode.SendKeys(proposedPostalCode);
+            // select 'Yes' for 'Is the applicant the legal and beneficial owner of the general store?'
+            NgWebElement uiIsApplicantOwnerOfStoreYes = ngDriver.FindElement(By.Id("mat-button-toggle-142-button"));
+            JavaScriptClick(uiIsApplicantOwnerOfStoreYes);
 
-            // enter the proposed PID
-            NgWebElement uiProposedPID = ngDriver.FindElement(By.CssSelector("input[formcontrolname='establishmentParcelId']"));
-            uiProposedPID.SendKeys(proposedPID);
+            // enter the legal owners
+            NgWebElement uiLegalOwners = ngDriver.FindElement(By.CssSelector("textarea#legalAndBeneficialOwnersOfStore"));
+            uiLegalOwners.SendKeys(legalOwners);
 
-            // upload the signage document
-            FileUpload("signage.pdf","(//input[@type='file'])[2]");
+            // select 'Yes' for 'Is the applicant a franchisee or otherwise affiliated with another business?'
+            NgWebElement uiIsApplicantFranchiseOrAffiliatedYes = ngDriver.FindElement(By.Id("mat-button-toggle-145-button"));
+            JavaScriptClick(uiIsApplicantFranchiseOrAffiliatedYes);
 
-            // upload the floor plan
-            FileUpload("floor_plan.pdf","(//input[@type='file'])[5]");
+            // enter the business
+            NgWebElement uiBusiness = ngDriver.FindElement(By.CssSelector("input#franchiseOrAffiliatedBusiness"));
+            uiBusiness.SendKeys(business);
 
-            // upload the site plan
-            FileUpload("site_plan.pdf","(//input[@type='file'])[8]");
+            // upload signage
+            FileUpload("signage.pdf", "(//input[@type='file'])[5]");
 
-            // upload the exterior photos
-            FileUpload("exterior_photos.jpg","(//input[@type='file'])[11]");
+            // upload floor plan
+            FileUpload("floor_plan.pdf", "(//input[@type='file'])[8]");
 
-            // select 'Yes' for proposed LRS site located within a grocery store
-            NgWebElement uiProposedSiteInGrocery = ngDriver.FindElement(By.CssSelector("#mat-button-toggle-1 button#mat-button-toggle-1-button"));
-            JavaScriptClick(uiProposedSiteInGrocery);
+            // upload site plan
+            FileUpload("site_plan.pdf", "(//input[@type='file'])[11]");
 
-            // upload grocery declaration document
-            FileUpload("grocery_declaration.pdf","(//input[@type='file'])[15]");
+            // upload exterior photos
+            FileUpload("exterior_photos.pdf", "(//input[@type='file'])[14]");
 
             // select the owner checkbox
-            NgWebElement uiOwner = ngDriver.FindElement(By.CssSelector(".mat-checkbox[formcontrolname='isOwnerBusiness']"));
-            uiOwner.Click();
+            NgWebElement uiOwnerCheckbox = ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='isOwnerBusiness']"));
+            uiOwnerCheckbox.Click();
 
-            // select the valid interest checkbox
-            NgWebElement uiValidInterest = ngDriver.FindElement(By.CssSelector(".mat-checkbox[formcontrolname='hasValidInterest']"));
-            uiValidInterest.Click();
+            // select the owner's valid interest checkbox
+            NgWebElement uiValidInterestCheckbox = ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='hasValidInterest']"));
+            uiValidInterestCheckbox.Click();
 
-            // select the future valid interest checkbox
-            NgWebElement uiFutureValidInterest = ngDriver.FindElement(By.CssSelector("mat-checkbox#mat-checkbox-4[formcontrolname='willhaveValidInterest']"));
-            uiFutureValidInterest.Click();
+            // select the zoning checkbox
+            NgWebElement uiZoningCheckbox = ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='willHaveValidInterest']"));
+            uiZoningCheckbox.Click();
 
-            // upload valid interest document
-            FileUpload("valid_interest.pdf","(//input[@type='file'])[18]");
+            // upload proof of valid interest
+            FileUpload("valid_interest.pdf", "(//input[@type='file'])[18]");
 
             // select the authorized to submit checkbox
             NgWebElement uiAuthorizedToSubmit = ngDriver.FindElement(By.CssSelector("input[formcontrolname='authorizedToSubmit'][type='checkbox']"));
@@ -101,19 +126,6 @@ namespace bdd_tests
             // select the signature agreement checkbox
             NgWebElement uiSignatureAgreement = ngDriver.FindElement(By.CssSelector("input[formcontrolname='signatureAgreement'][type='checkbox']"));
             uiSignatureAgreement.Click();
-
-            // click on the submit for LG/IN review button
-            ClickOnSubmitButton();
-        }
-
-
-        [And(@"an LRS licence has been created")]
-        public void LRSApproval()
-        {
-            System.Threading.Thread.Sleep(4000);
-
-            string testWines = "login/token/TestWines";
-            ngDriver.Navigate().GoToUrl($"{baseUri}{testWines}");
         }
     }
 }
