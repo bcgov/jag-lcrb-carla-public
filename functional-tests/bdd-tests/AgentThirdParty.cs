42d4424232a2e53f08a6f4752ecbb9aff2f5d7b1
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-03 12:28:14 -0800

    Agent 3rd party.

---
+++ functional-tests/bdd-tests/AgentThirdParty.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: AgentThirdParty
+    As a logged in business user
+    I want to request a third party operator application for an Agent 
+
+@agent @privatecorporation
+Scenario: Agent Third Party (Private Corporation)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for an Agent Licence
+    And I review the account profile for a private corporation
+    And I complete the Agent Licence application for a private corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for an Agent Licence
+    And the application is approved
+    And I click on the Licences tab
+    And I request a third party operator
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./AgentThirdParty.feature")]
+    [Collection("Liquor")]
+    public sealed class AgentThirdParty : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
