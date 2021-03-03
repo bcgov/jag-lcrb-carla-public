61571b9fbc4dabfdbe69e284504042ed561cbcb9
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-03 07:14:56 -0800

    Draft Agent licence test.

---
+++ functional-tests/bdd-tests/AgentDownloadLicence.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: AgentDownloadLicence
+    As a logged in business user
+    I want to download an Agent licence
+
+@agent @privatecorporation
+Scenario: Agent Download Licence (Private Corporation)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for an Agent Licence
+    And I review the account profile for a private corporation
+    And I complete the Agent Licence application for a private corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And I confirm the payment receipt for an Agent Licence
+    And the application is approved
+    And I click on the Licences tab
+    And I click on the link for Download Licence
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./AgentDownloadLicence.feature")]
+    [Collection("Liquor")]
+    public sealed class AgentDownloadLicence : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
