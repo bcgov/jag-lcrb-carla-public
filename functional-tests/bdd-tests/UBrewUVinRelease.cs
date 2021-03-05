e32296f98adc790f00062356f451cb219c8ccdd2
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-05 14:04:49 -0800

    Update tags.

--- functional-tests/bdd-tests/UBrewUVinRelease.cs
+++ functional-tests/bdd-tests/UBrewUVinRelease.cs
@@ -18,7 +18,7 @@ Feature: UBrewUVinRelease
     I want to submit a UBrew / UVin Licence application 
     And complete change requests 
 
-@ubrewuvin @release1
+@ubrewuvin @release4
 Scenario: UBrew / UVin Application Release Test (Private Corporation)
     Given I am logged in to the dashboard as a private corporation
     And I click on the Start Application button for a UBrew UVin application
