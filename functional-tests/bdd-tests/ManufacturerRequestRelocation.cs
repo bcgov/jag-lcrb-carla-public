a949981956d6b8b23ed8605700fcb033426b532b
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-10 13:42:07 -0800

    Rename files.

--- functional-tests/bdd-tests/ManufacturerRequestRelocation.cs
+++ functional-tests/bdd-tests/ManufacturerRequestRelocation.cs
@@ -1,4 +1,19 @@
-﻿Feature: ManufacturerRelocationChange
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: ManufacturerRequestRelocation
     As a logged in business user
     I want to request a relocation change for a manufacturer licence
 
@@ -65,3 +80,30 @@ Scenario: Relocation Change Application (Co-packer)
     And the dashboard status is updated as Application Under Review
     And the account is deleted
     Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./ManufacturerRequestRelocation.feature")]
+    [Collection("Liquor")]
+    public sealed class ManufacturerRequestRelocation : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
