b4e57aee0294f328eb6d10ba1e375381b90d7b03
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-10 13:43:46 -0800

    Rename files.

--- functional-tests/bdd-tests/ManufacturerStructuralAlterationsToAnApprovedLoungeOrSpecialEventsArea.cs
+++ functional-tests/bdd-tests/ManufacturerStructuralAlterationsToAnApprovedLoungeOrSpecialEventsArea.cs
@@ -1,4 +1,19 @@
-﻿Feature: ManufacturerStructuralAlterations
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: ManufacturerStructuralAlterationsToAnApprovedLoungeOrSpecialEventsArea
     As a logged in business user
     I want to request structural alterations to an approved lounge or special events area for a manufacturer licence
 
@@ -57,3 +72,30 @@ Scenario: Structural Alterations (Co-packer)
     And I request structural alterations to an approved lounge or special events area
     And the account is deleted
     Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./ManufacturerStructuralAlterationsToAnApprovedLoungeOrSpecialEventsArea.feature")]
+    [Collection("Liquor")]
+    public sealed class ManufacturerStructuralAlterationsToAnApprovedLoungeOrSpecialEventsArea : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
