f8c725469be4ef83070d5fc251f45e3ea0573ed3
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-11 13:13:30 -0800

    Update automation.

--- functional-tests/bdd-tests/TestBaseTermsAndConditions.cs
+++ functional-tests/bdd-tests/TestBaseTermsAndConditions.cs
@@ -47,7 +47,9 @@ namespace bdd_tests
 
             if (licenceType == "a CRS licence")
             {
-                Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'')]")).Displayed);
+                Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'This licence is subject to the terms and conditions specified in the restriction or approval letter(s) and those contained in the Cannabis Retail Store Handbook, which may be amended from time to time.')]")).Displayed);
+                Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'Packaged cannabis may only be sold within the service area outlined in blue on the LCRB approved floor plan, unless otherwise endorsed or approved by the LCRB.')]")).Displayed);
+                Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'The establishment may be open anytime between the hours of 9 a.m. and 11 p.m., subject to further restriction by the local government or Indigenous nation.')]")).Displayed);
             }
         }
     }
