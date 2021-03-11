a6a838da094cea5a57c374b9870149b05cd7d788
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-11 13:36:13 -0800

    Update automation.

--- functional-tests/bdd-tests/TestBaseTermsAndConditions.cs
+++ functional-tests/bdd-tests/TestBaseTermsAndConditions.cs
@@ -51,8 +51,6 @@ namespace bdd_tests
                 Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'Packaged cannabis may only be sold within the service area outlined in blue on the LCRB approved floor plan, unless otherwise endorsed or approved by the LCRB.')]")).Displayed);
                 Assert.True(ngDriver.FindElement(By.XPath("//body[contains(.,'The establishment may be open anytime between the hours of 9 a.m. and 11 p.m., subject to further restriction by the local government or Indigenous nation.')]")).Displayed);
             }
-
-
         }
     }
 }
