6b0a8f4c5c02fd20a98a4016eab043ce1ac577b8
George Walker <george@walkersoftware.ca>
2021-03-12 13:17:01 -0800

    docker entrypoint

--- orgbook-issuer-agent/issuer_controller/docker-entrypoint.sh
+++ orgbook-issuer-agent/issuer_controller/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export APP_NAME=${APP_NAME:-app}
-export APP_MODULE=${APP_MODULE:-${APP_NAME}:${APP_NAME}}
+export APP_MODULE=${APP_MODULE:-src.${APP_NAME}:${APP_NAME}}
 export HOST_IP=${HOST_IP:-0.0.0.0}
 export HOST_PORT=${HOST_PORT:-5000}
 export WORKER_CONNECTIONS=${WORKER_CONNECTIONS:-1000}
