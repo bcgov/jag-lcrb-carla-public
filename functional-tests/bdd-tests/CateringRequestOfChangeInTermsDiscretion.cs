557f6fe9b191bc861c071d3347935316c80c2e32
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-11 18:10:53 -0800

    Draft of request of change.

---
+++ functional-tests/bdd-tests/CateringRequestOfChangeInTermsDiscretion.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: CateringRequestOfChangeInTermsDiscretion
+    As a logged in business user
+    I want to apply for a Request of Change in Terms and Conditions/Request for Discretion
+
+@privatecorporation @cateringlicencedownload
+Scenario: Catering Licence Request of Change in Terms and Conditions/Request for Discretion (Private Corporation)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Catering
+    And I review the account profile for a private corporation
+    And I complete the Catering application for a private corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And the application is approved
+    And I click on the Licences tab
+    And I pay the licensing fee 
+    And I click on the link for Request of Change in Terms and Conditions/Request for Discretion
+    #And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./CateringRequestOfChangeInTermsDiscretion.feature")]
+    [Collection("Liquor")]
+    public sealed class CateringRequestOfChangeInTermsDiscretion : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
---
+++ functional-tests/bdd-tests/CateringRequestOfChangeInTermsDiscretion.feature
@@ -0,0 +1,18 @@
+﻿Feature: CateringRequestOfChangeInTermsDiscretion
+    As a logged in business user
+    I want to apply for a Request of Change in Terms and Conditions/Request for Discretion
+
+@privatecorporation @cateringlicencedownload
+Scenario: Catering Licence Request of Change in Terms and Conditions/Request for Discretion (Private Corporation)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for Catering
+    And I review the account profile for a private corporation
+    And I complete the Catering application for a private corporation
+    And I click on the Submit button
+    And I enter the payment information
+    And the application is approved
+    And I click on the Licences tab
+    And I pay the licensing fee 
+    And I click on the link for Request of Change in Terms and Conditions/Request for Discretion
+    #And the account is deleted
+    Then I see the login page
