a6e1fb7f0e9a1ad7ea48b8442e25e0544ad5c83d
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-12 08:55:14 -0800

    Mfg temp extension.

---
+++ functional-tests/bdd-tests/ManufacturerTemporaryExtension.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using Protractor;
+using System;
+using Xunit.Gherkin.Quick;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using Xunit;
+
+/*
+Feature: ManufacturerTemporaryExtension
+    As a logged in business user
+    I want to request a temporary extension for a Liquor Primary Application
+
+@manufacturer @winery 
+Scenario: Manufacturer Temporary Extension (Winery)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Manufacturer Licence
+    And I review the account profile for a private corporation
+    And I complete the Manufacturer application for a winery
+    And I enter the payment information
+    And I confirm the payment receipt for a Manufacturer Licence application
+    And the application is approved
+    And I pay the licensing fee 
+    And I click on the link for Temporary Extension of Licensed Area
+    And I submit a liquor primary temporary extension of licensed area application
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./ManufacturerTemporaryExtension.feature")]
+    [Collection("Liquor")]
+    public sealed class ManufacturerTemporaryExtension : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
