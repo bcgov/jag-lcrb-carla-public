f4047815f9c8fe1207d5a533cd5ad320098a1c8f
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-12 06:42:09 -0800

    Remove unnecessary biz types.

--- functional-tests/bdd-tests/UBrewUVinDownloadLicence.cs
+++ functional-tests/bdd-tests/UBrewUVinDownloadLicence.cs
@@ -15,26 +15,7 @@ using Xunit;
 /*
 Feature: UBrewUVinDownloadLicence
     As a logged in business user
-    I want to download a UBrew / UVin licence for different business types
-
-@ubrewuvinlicencedownload @partnership 
-Scenario: UBrew / UVin Application Licence Download (Partnership)
-    Given I am logged in to the dashboard as a partnership
-    And I click on the Start Application button for a UBrew UVin application
-    And I review the account profile for a partnership
-    And I complete the UBrew / UVin application for a partnership
-    And I click on the Submit button
-    And I enter the payment information
-    And I confirm the payment receipt for a UBrew / UVin application
-    And I click on the Dashboard tab
-    And the dashboard status is updated as Application Under Review
-    And the application is approved
-    And I click on the Licences tab
-    And I pay the licensing fee 
-    And I click on the Licences tab
-    And I click on the link for Download Licence
-    And the account is deleted
-    Then I see the login page
+    I want to download a UBrew / UVin licence
 
 @ubrewuvinlicencedownload @privatecorporation 
 Scenario: UBrew / UVin Application Licence Download (Private Corporation)
@@ -42,44 +23,6 @@ Scenario: UBrew / UVin Application Licence Download (Private Corporation)
     And I click on the Start Application button for a UBrew UVin application
     And I review the account profile for a private corporation
     And I complete the UBrew / UVin application for a private corporation
-    And I click on the Submit button
-    And I enter the payment information
-    And I confirm the payment receipt for a UBrew / UVin application
-    And I click on the Dashboard tab
-    And the dashboard status is updated as Application Under Review
-    And the application is approved
-    And I click on the Licences tab
-    And I pay the licensing fee 
-    And I click on the Licences tab
-    And I click on the link for Download Licence
-    And the account is deleted
-    Then I see the login page
-
-@ubrewuvinlicencedownload @publiccorporation 
-Scenario: UBrew / UVin Application Licence Download (Public Corporation)
-    Given I am logged in to the dashboard as a public corporation
-    And I click on the Start Application button for a UBrew UVin application
-    And I review the account profile for a public corporation
-    And I complete the UBrew / UVin application for a public corporation
-    And I click on the Submit button
-    And I enter the payment information
-    And I confirm the payment receipt for a UBrew / UVin application
-    And I click on the Dashboard tab
-    And the dashboard status is updated as Application Under Review
-    And the application is approved
-    And I click on the Licences tab
-    And I pay the licensing fee 
-    And I click on the Licences tab
-    And I click on the link for Download Licence
-    And the account is deleted
-    Then I see the login page
-
-@ubrewuvinlicencedownload @soleproprietorship 
-Scenario: UBrew / UVin Application Licence Download (Sole Proprietorship)
-    Given I am logged in to the dashboard as a sole proprietorship
-    And I click on the Start Application button for a UBrew UVin application
-    And I review the account profile for a sole proprietorship
-    And I complete the UBrew / UVin application for a sole proprietorship
     And I click on the Submit button
     And I enter the payment information
     And I confirm the payment receipt for a UBrew / UVin application
