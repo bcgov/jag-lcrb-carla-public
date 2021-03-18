3d91a90e644efa44a610740a433a65b9d38faf7f
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-18 13:57:57 -0700

    Refactor.

--- functional-tests/bdd-tests/FoodPrimaryRelease.cs
+++ functional-tests/bdd-tests/FoodPrimaryRelease.cs
@@ -37,6 +37,7 @@ Scenario: Food Primary Release Test #1 (Private Corporation)
     And I enter the payment information
     And I click on the Licences tab
     And I click on the link for Patron Participation Entertainment Endorsement
+    And I click on the Continue to Application button
     And I request a Patron Participation Entertainment Endorsement application
     And I click on the Submit button
     And I click on the Licences tab
