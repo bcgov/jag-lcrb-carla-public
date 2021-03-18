7ccb7a51da7fc96ff1503c86f48eb9bf28e8c0f3
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-18 07:59:17 -0700

    Remove unnecessary click.

--- functional-tests/bdd-tests/CRSRelease.cs
+++ functional-tests/bdd-tests/CRSRelease.cs
@@ -32,6 +32,7 @@ Scenario: CRS Release (Private Corporation)
     And I renew the licence with positive responses for Cannabis
     And I click on the Licences tab
     And I request a store relocation for Cannabis
+    And I click on the Licences tab
     And I request a structural change
     And I request a transfer of ownership for Cannabis
     And the account is deleted
@@ -63,6 +64,7 @@ Scenario: CRS Release (Sole Proprietorship)
     And I renew the licence with positive responses for Cannabis
     And I click on the Licences tab
     And I request a store relocation for Cannabis
+    And I click on the Licences tab
     And I request a structural change
     And I request a transfer of ownership for Cannabis
     And the account is deleted
