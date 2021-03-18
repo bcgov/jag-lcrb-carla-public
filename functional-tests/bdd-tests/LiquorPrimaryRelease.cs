50515b3d8d106eab306f452cbdacf991e61795df
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-18 12:53:30 -0700

    Refactor.

--- functional-tests/bdd-tests/LiquorPrimaryRelease.cs
+++ functional-tests/bdd-tests/LiquorPrimaryRelease.cs
@@ -42,6 +42,7 @@ Scenario: Liquor Primary Release Test #1 (Private Corporation)
     # And I enter the payment information
     # And I click on the Licences tab
     # And I click on the link for Catering Endorsement Application
+    # And I click on the Continue to Application button
     # And I request a catering endorsement application
     # And I click on the Submit button
     # And I enter the payment information
