894cb65413d8ef2a177898e352a6159d11e6a50b
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-22 12:39:33 -0700

    Fix Gherkin.

--- functional-tests/bdd-tests/FoodPrimaryRelease.cs
+++ functional-tests/bdd-tests/FoodPrimaryRelease.cs
@@ -29,9 +29,6 @@ Scenario: Food Primary Release Test #1 (Private Corporation)
     And I click on the Licences tab
     And I click on the link for Download Licence
     And I confirm the terms and conditions for a Food Primary licence
-    And I click on the link for Request of Change in Terms and Conditions/Request for Discretion
-    And I click on the Continue to Application button
-    And I request a change in terms and conditions application
     And I click on the Submit button
     And I enter the payment information
     And I click on the link for Add Licensee Representative
