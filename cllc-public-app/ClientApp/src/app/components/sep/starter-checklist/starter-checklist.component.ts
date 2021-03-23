a3368c3b6cdfe47a9f259e7bf9b1bf5c1b56c529
Moffat Sehudi <msehudi@gmail.com>
2021-03-22 17:06:50 -0700

    LCSD-5291 build SEP authenticated dashboard partial

---
+++ cllc-public-app/ClientApp/src/app/components/sep/starter-checklist/starter-checklist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-starter-checklist',
+  templateUrl: './starter-checklist.component.html',
+  styleUrls: ['./starter-checklist.component.scss']
+})
+export class StarterChecklistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
