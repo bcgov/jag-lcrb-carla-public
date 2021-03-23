5390dbbd28d20f76be0e304a0711db7e4e37ff7c
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-23 10:05:27 -0700

    Add LP change hrs test.

--- functional-tests/bdd-tests/TestBaseChangeHours.cs
+++ functional-tests/bdd-tests/TestBaseChangeHours.cs
@@ -135,8 +135,92 @@ namespace bdd_tests
         [And(@"I complete the change hours application for liquor service within service hours")]
         public void RequestLiquorChangeHours(string hoursType)
         {
-            ContinueToApplicationButton();
+            // select the proposed new hours
+            var uiServiceHoursSundayOpen =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursSundayOpen'] option[value='09:00']"));
+            uiServiceHoursSundayOpen.Click();
 
+            var uiServiceHoursSundayClose =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursSundayClose'] option[value='21:00']"));
+            uiServiceHoursSundayClose.Click();
+
+            var uiServiceHoursMondayOpen =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursMondayOpen'] option[value='11:00']"));
+            uiServiceHoursMondayOpen.Click();
+
+            var uiServiceHoursMondayClose =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursMondayClose'] option[value='23:00']"));
+            uiServiceHoursMondayClose.Click();
+
+            var uiServiceHoursTuesdayOpen =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursTuesdayOpen'] option[value='09:15']"));
+            uiServiceHoursTuesdayOpen.Click();
+
+            var uiServiceHoursTuesdayClose =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursTuesdayClose'] option[value='10:30']"));
+            uiServiceHoursTuesdayClose.Click();
+
+            var uiServiceHoursWednesdayOpen =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursWednesdayOpen'] option[value='12:30']"));
+            uiServiceHoursWednesdayOpen.Click();
+
+            var uiServiceHoursWednesdayClose =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursWednesdayClose'] option[value='21:30']"));
+            uiServiceHoursWednesdayClose.Click();
+
+            var uiServiceHoursThursdayOpen =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursThursdayOpen'] option[value='14:30']"));
+            uiServiceHoursThursdayOpen.Click();
+
+            var uiServiceHoursThursdayClose =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursThursdayClose'] option[value='19:00']"));
+            uiServiceHoursThursdayClose.Click();
+
+            var uiServiceHoursFridayOpen =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursFridayOpen'] option[value='17:00']"));
+            uiServiceHoursFridayOpen.Click();
+
+            var uiServiceHoursFridayClose =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursFridayClose'] option[value='19:45']"));
+            uiServiceHoursFridayClose.Click();
+
+            var uiServiceHoursSaturdayOpen =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursSaturdayOpen'] option[value='09:45']"));
+            uiServiceHoursSaturdayOpen.Click();
+
+            var uiServiceHoursSaturdayClose =
+                ngDriver.FindElement(
+                    By.CssSelector("[formcontrolname='serviceHoursSaturdayClose'] option[value='20:00']"));
+            uiServiceHoursSaturdayClose.Click();
+
+            // select the authorized to submit checkbox
+            var uiAuthorizedToSubmit =
+                ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='authorizedToSubmit']"));
+            uiAuthorizedToSubmit.Click();
+
+            // select the signature agreement checkbox
+            var uiSignatureAgreement =
+                ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='signatureAgreement']"));
+            uiSignatureAgreement.Click();
+        }
+
+
+        [And(@"I complete the change hours application for liquor service outside service hours")]
+        public void RequestLiquorChangeHoursOutside(string hoursType)
+        {
             // select the proposed new hours
             var uiServiceHoursSundayOpen =
                 ngDriver.FindElement(
