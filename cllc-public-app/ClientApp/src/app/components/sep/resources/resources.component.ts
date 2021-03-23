d4092995b1407d38db5f38a56d8b77eb4e0f7405
Moffat Sehudi <msehudi@gmail.com>
2021-03-23 15:27:43 -0700

    Sep update

--- cllc-public-app/ClientApp/src/app/components/sep/resources/resources.component.ts
+++ cllc-public-app/ClientApp/src/app/components/sep/resources/resources.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { StarterChecklistComponent } from '@components/sep/starter-checklist/starter-checklist.component';
 
 @Component({
   selector: 'app-resources',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResourcesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openChecklist() {
+    const dialogConfig = {
+      disableClose: true,
+      autoFocus: true,
+      width: "500px",
+      data: {
+        showStartApp: false
+      }
+    };
+
+    // open dialog, get reference and process returned data from dialog
+    const dialogRef = this.dialog.open(StarterChecklistComponent, dialogConfig);
+    dialogRef.afterClosed()
+      .subscribe(cancelApplication => {
+
+      });
   }
 
 }
