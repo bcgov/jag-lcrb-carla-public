10224909f9e5132a9c9891ebaa8d6bd7113e71af
ElizabethWolfe <ejwolfe@gmail.com>
2021-03-24 06:36:42 -0700

    Mfg take home test.

---
+++ functional-tests/bdd-tests/ManufacturerRequestTakeHomeSamplingEvent.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using Xunit.Gherkin.Quick;
+
+/*
+Feature: ManufacturerRequestTakeHomeSamplingEvent
+    As a logged in business user
+    I want to request a take home sampling event for a manufacturer licence
+
+@manufacturer @winery @mfglicencedownload
+Scenario: Manufacturer Download Licence (Winery)
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the Start Application button for a Manufacturer Licence
+    And I review the account profile for a private corporation
+    And I complete the Manufacturer application for a winery
+    And I enter the payment information
+    And I confirm the payment receipt for a Manufacturer Licence application
+    And the application is approved
+    And I click on the Licences tab
+    And I pay the licensing fee 
+    And I click on the Licences tab
+    And I click on the link for Request Take Home Sampling Event Authorization
+    And I complete the Take Home Sampling Event Authorization request
+    And I click on the secondary Submit button
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./ManufacturerRequestTakeHomeSamplingEvent.feature")]
+    [Collection("Liquor")]
+    public sealed class ManufacturerRequestTakeHomeSamplingEvent : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorTwo();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
