5855ce364e5ab39e4ee92100f32870d1ba687bb4
ElizabethWolfe <ejwolfe@gmail.com>
2021-04-01 20:24:24 -0700

    Update toggle selection.

--- functional-tests/bdd-tests/TestBaseCateringApplication.cs
+++ functional-tests/bdd-tests/TestBaseCateringApplication.cs
@@ -142,10 +142,12 @@ namespace bdd_tests
                 for (var i = 0; i < 50; i++)
                     try
                     {
-                        var names = ngDriver.FindElements(By.Id("mat-button-toggle-109-button"));
+                        var names = ngDriver.FindElements(By.CssSelector("mat-button-toggle-group[id='previousApplication']"));
                         if (names.Count > 0)
                         {
                             uiPreviousLicenceYes = names[0];
+                            var toggleButton = uiPreviousLicenceYes.FindElements(By.TagName("mat-button-toggle-button"))[0];
+                            toggleButton.Click();
                             break;
                         }
                     }
