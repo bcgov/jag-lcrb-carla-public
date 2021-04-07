e77ca415b877f1f3e0a707abdd7796b1d3f346c5
ElizabethWolfe <ejwolfe@gmail.com>
2021-04-07 09:39:05 -0700

    Draft of log in flow tests.

---
+++ functional-tests/bdd-tests/LoginFlow.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using Xunit.Gherkin.Quick;
+
+/*
+
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./**.feature")]
+    [Collection("Liquor")]
+    public sealed class LoginFlow : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
