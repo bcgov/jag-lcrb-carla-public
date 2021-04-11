5a24a7825cc4d9c172444b80894766a9c6aef6af
ElizabethWolfe <ejwolfe@gmail.com>
2021-04-11 08:18:30 -0700

    Update tags.

--- functional-tests/bdd-tests/FoodPrimaryRelease.cs
+++ functional-tests/bdd-tests/FoodPrimaryRelease.cs
@@ -6,7 +6,7 @@ Feature: FoodPrimaryRelease
     As a logged in business user
     I want to run a release test for a Food Primary licence
 
-@foodprimary @privatecorporation
+@foodprimary @privatecorporation @release8
 Scenario: Food Primary Release Test #1 (Private Corporation)
     Given I am logged in to the dashboard as a private corporation
     And I click on the Start Application button for Food Primary
