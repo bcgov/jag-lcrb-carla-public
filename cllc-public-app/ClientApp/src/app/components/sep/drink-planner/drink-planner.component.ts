b243dcb4ccc2454e00d21b6c6bc947f1a2f1f0ea
Alejandro Sanchez <emailforasr@gmail.com>
2021-04-12 20:56:46 -0700

    LCSD-5290 Drink calculator logic and styling fixes

--- cllc-public-app/ClientApp/src/app/components/sep/drink-planner/drink-planner.component.ts
+++ cllc-public-app/ClientApp/src/app/components/sep/drink-planner/drink-planner.component.ts
@@ -12,10 +12,11 @@ import drinkPlannerDefaults, { DrinkSettings, HOURS_OF_LIQUOR_SERVICE, SERVINGS_
 export class DrinkPlannerComponent extends FormBase implements OnInit {
 
   @Input()
-  settings: Array<DrinkSettings> = drinkPlannerDefaults;
+  configuration: Array<DrinkSettings> = drinkPlannerDefaults;
 
   // component state
   busy: Subscription;
+  state: { [key: string]: DrinkSettings } = {};
 
   // drink planner form
   form = this.fb.group({
@@ -32,10 +33,27 @@ export class DrinkPlannerComponent extends FormBase implements OnInit {
 
   ngOnInit() {
     let initialValues = {};
-    for (const item of this.settings) {
+    for (const item of this.configuration) {
+      this.state[item.group] = item;
       initialValues[item.group] = item.defaultPercentage;
     }
     this.form.patchValue(initialValues);
   }
 
+  get totalServings(): number {
+    const { hours, guests } = this.form.value as { hours: number; guests: number };
+    return (hours / HOURS_OF_LIQUOR_SERVICE * guests * SERVINGS_PER_PERSON);
+  }
+
+  getServings(group: string): number {
+    const percentage: number = this.form.get(group).value;
+    return this.totalServings * percentage / 100;
+  }
+
+  getStorage(group: string): number {
+    const config = this.state[group];
+    const servings = this.getServings(group);
+    const storageUnits = servings * config.servingSizeMl / config.storageSizeMl;
+    return storageUnits > 0 && storageUnits < 1 ? 1 : storageUnits;
+  }
 }
