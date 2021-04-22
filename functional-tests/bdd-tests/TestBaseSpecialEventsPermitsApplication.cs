1949572f3d975762f42e7ff07e635d53eed637d0
ElizabethWolfe <ejwolfe@gmail.com>
2021-04-22 08:08:36 -0700

    SEP automation.

--- functional-tests/bdd-tests/TestBaseSpecialEventsPermitsApplication.cs
+++ functional-tests/bdd-tests/TestBaseSpecialEventsPermitsApplication.cs
@@ -7,22 +7,22 @@ namespace bdd_tests
 {
     public abstract partial class TestBase : Feature, IDisposable
     {
-        [And(@"the correct data is displayed for a(.*)")]
-        public void AccountProfileData(string bizType)
+        [And(@"I complete the special events permits applicant info")]
+        public void SpecialEventsPermtsApplicantInfo()
         {
             /* 
-            Page Title: Account Profile
+            Page Title: Applicant Info
             */
 
-            // check legal name field is populated
-            var uiLegalName = ngDriver.FindElement(By.CssSelector(
-                "div:nth-of-type(2) > div:nth-of-type(1) > div > div > div:nth-of-type(1) > app-field:nth-of-type(1) > section > div > section > input"));
-            var fieldValueLegalName = uiLegalName.GetAttribute("value");
-            Assert.True(fieldValueLegalName != null);
+            string eventName = "SEP test event";
 
-            // check business number is correct
-            var uiBusinessNumber = ngDriver.FindElement(By.CssSelector("input[formcontrolname='businessNumber']"));
-            Assert.True(uiBusinessNumber.GetAttribute("value") == "111111111");
+            // enter the event name
+            var uiEventName = ngDriver.FindElement(By.CssSelector("input[formcontrolname='eventName']"));
+            uiEventName.SendKeys(eventName);
+
+            // click on the terms and conditions checkbox
+            var uiTermsAndConditions = ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='agreeToTnC']"));
+            uiTermsAndConditions.Click();
         }
     }
 }
