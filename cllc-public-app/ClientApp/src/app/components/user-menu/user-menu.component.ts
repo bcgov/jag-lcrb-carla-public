a5b2170fc8caf29bab09089550578fd3fe823ffc
Alejandro Sanchez <emailforasr@gmail.com>
2021-04-21 17:24:12 -0700

    LCSD-5516 Style fixes

--- cllc-public-app/ClientApp/src/app/components/user-menu/user-menu.component.ts
+++ cllc-public-app/ClientApp/src/app/components/user-menu/user-menu.component.ts
@@ -1,20 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { User } from '@models/user.model';
-import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
+import { faUserCircle, faBuilding } from "@fortawesome/free-regular-svg-icons";
 import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-user-menu',
   templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.scss']
+  styleUrls: ['./user-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class UserMenuComponent implements OnInit {
   // icons
   faUserCircle = faUserCircle;
+  faBuilding = faBuilding;
   faChevronDown = faChevronDown;
   faChevronUp = faChevronUp;
 
   @Input() currentUser: User;
+
+  get userIcon() {
+    if (this.currentUser?.userType == 'Business') {
+      return this.faBuilding;
+    }
+    return this.faUserCircle;
+  }
 
   constructor() { }
 
