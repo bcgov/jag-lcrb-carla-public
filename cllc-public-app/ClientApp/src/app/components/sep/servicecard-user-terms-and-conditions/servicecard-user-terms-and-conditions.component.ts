7767e708b3f20f507b07c8774873a1d52ebf5e9c
Alejandro Sanchez <emailforasr@gmail.com>
2021-04-22 17:17:18 -0700

    LCSD-5493 Contact creation when logging into SEP dashboard

---
+++ cllc-public-app/ClientApp/src/app/components/sep/servicecard-user-terms-and-conditions/servicecard-user-terms-and-conditions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servicecard-user-terms-and-conditions',
+  templateUrl: './servicecard-user-terms-and-conditions.component.html',
+  styleUrls: ['./servicecard-user-terms-and-conditions.component.scss']
+})
+export class ServicecardUserTermsAndConditionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
