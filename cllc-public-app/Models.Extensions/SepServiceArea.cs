6d6de2372aa7dbfa86d00d627ed9357ecf969b1f
Moffat Sehudi <msehudi@gmail.com>
2021-04-22 17:34:01 -0700

    SEP dynamics models partial

---
+++ cllc-public-app/Models.Extensions/SepServiceArea.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Gov.Lclb.Cllb.Interfaces.Models;
+
+namespace Gov.Lclb.Cllb.Public.Models
+{
+    /// <summary>
+    /// ViewModel transforms.
+    /// </summary>
+    public static class SepServiceAreExtensions
+    {
+        /// <summary>
+        /// Convert a given voteQuestion to a ViewModel
+        /// </summary>
+        public static ViewModels.SepServiceArea ToViewModel(this MicrosoftDynamicsCRMadoxioSpecialeventlicencedarea serviceArea)
+        {
+            ViewModels.SepServiceArea result = null;
+            if (serviceArea != null)
+            {
+                result = new ViewModels.SepServiceArea()
+                {
+                    Id = serviceArea.AdoxioSpecialeventlicencedareaid,
+                    SepLocationId = serviceArea._adoxioSpecialeventlocationidValue,
+                    SpecialEventId = serviceArea._adoxioSpecialeventidValue,
+                    MinorPresent = serviceArea.AdoxioMinorpresent,
+                    LicencedAreaMaxNumberOfGuests = serviceArea.AdoxioLicencedareamaxnumberofguests,
+                    MaximumNumberOfGuests = serviceArea.AdoxioMaximumnumberofguests,
+                    IsBothOutdoorIndoor = serviceArea.AdoxioIsbothoutdoorindoor,
+                    IsIndoors = serviceArea.AdoxioIsindoors,
+                    NumberOfMinors = serviceArea.AdoxioNumberofminors,
+                    LicencedAreaNumberOfMinors = serviceArea.AdoxioLicencedareanumberofminors,
+                    Setting = serviceArea.AdoxioSetting,
+                    StatusCode = serviceArea.Statecode,
+                    StateCode = serviceArea.Statecode,
+                    EventName = serviceArea.AdoxioEventname,
+                    IsOutdoors = serviceArea.AdoxioIsoutdoors,
+                    LicencedAreaDescription = serviceArea.AdoxioLicencedareadescription,
+                };
+
+            }
+            return result;
+        }
+
+        public static void CopyValues(this MicrosoftDynamicsCRMadoxioSpecialeventlicencedarea to, ViewModels.SepServiceArea from)
+        {
+            to.AdoxioSpecialeventlicencedareaid = from.Id;
+            to._adoxioSpecialeventlocationidValue = from.SepLocationId;
+            to._adoxioSpecialeventidValue = from.SpecialEventId;
+            to.AdoxioMinorpresent = from.MinorPresent;
+            to.AdoxioLicencedareamaxnumberofguests = from.LicencedAreaMaxNumberOfGuests;
+            to.AdoxioMaximumnumberofguests = from.MaximumNumberOfGuests;
+            to.AdoxioIsbothoutdoorindoor = from.IsBothOutdoorIndoor;
+            to.AdoxioIsindoors = from.IsIndoors;
+            to.AdoxioNumberofminors = from.NumberOfMinors;
+            to.AdoxioLicencedareanumberofminors = from.LicencedAreaNumberOfMinors;
+            to.AdoxioSetting = from.Setting;
+            to.Statecode = from.StatusCode;
+            to.Statecode = from.StateCode;
+            to.AdoxioEventname = from.EventName;
+            to.AdoxioIsoutdoors = from.IsOutdoors;
+            to.AdoxioLicencedareadescription = from.LicencedAreaDescription;
+        }
+
+    }
+}
+
---
+++ cllc-public-app/ViewModels/SepEventLocation.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace Gov.Lclb.Cllb.Public.ViewModels
+{
+    public class SepEventLocation
+    {
+        public string Id { get; set; }
+        public string SpecialEventId { get; set; }
+        public string LocationDescription { get; set; }
+        public string EventLocationCity { get; set; }
+        public string EventLocationPostalCode { get; set; }
+        public string EventLocationStreet1 { get; set; }
+        public string EventLocationStreet2 { get; set; }
+        public string EventLocationProvince { get; set; }
+        public string MaximumNumberOfGuests { get; set; }
+        public string LocationName { get; set; }
+        public string PermitNumber { get; set; }
+        public int? MaxNumberOfGuests { get; set; }
+        public List<SepEventDates> EventDates { get; set; }
+        public List<SepServiceArea> ServiceAreas { get; set; }
+        public int? Statecode { get; set; }
+
+    }
+}
