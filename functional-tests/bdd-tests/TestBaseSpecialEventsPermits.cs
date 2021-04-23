e24aed79d26920a15d8bcf113c60d24c0019cb81
ElizabethWolfe <ejwolfe@gmail.com>
2021-04-23 14:19:04 -0700

    SEP automation.

--- functional-tests/bdd-tests/TestBaseSpecialEventsPermits.cs
+++ functional-tests/bdd-tests/TestBaseSpecialEventsPermits.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using OpenQA.Selenium;
 using Xunit;
 using Xunit.Gherkin.Quick;
@@ -23,6 +24,18 @@ namespace bdd_tests
             // click on the terms and conditions checkbox
             var uiTermsAndConditions = ngDriver.FindElement(By.CssSelector("mat-checkbox[formcontrolname='agreeToTnC']"));
             uiTermsAndConditions.Click();
+        }
+
+        [And(@"the SEP Checklist content is displayed")]
+        public void SpecialEventsPermtsChecklist()
+        {
+            /* 
+            Page Title: Application Checklist
+            */
+
+            Thread.Sleep(5000);
+
+            Assert.True(ngDriver.FindElement(By.XPath("//mat-dialog-content[contains(.,'Application Checklist')]")).Displayed);
         }
     }
 }
