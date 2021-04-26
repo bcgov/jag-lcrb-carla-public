aaa29142546db96921ab57009a29899c10808488
ElizabethWolfe <ejwolfe@gmail.com>
2021-04-26 12:31:28 -0700

    Draft of manage profile test.

---
+++ functional-tests/bdd-tests/LoginManageProfile.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using Xunit.Gherkin.Quick;
+
+/*
+Feature: LoginManageProfile
+    As a business user
+    I want to manage my account after log in
+
+Scenario: Manage Account Profile
+    Given I log in with the new contact and account (cantest4employee)
+    And I click on my user account
+    And I click on the link for Manage Account    
+    And I click on the Save and Continue button
+    And the dashboard is displayed
+    And the account is deleted
+    Then I see the login page
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./LoginManageProfile.feature")]
+    [Collection("Liquor")]
+    public sealed class LoginManageProfile : TestBase
+    {
+        [Given(@"I am logged in to the dashboard as a(.*)")]
+        public void LogInToDashboard(string businessType)
+        {
+            NavigateToFeatures();
+
+            CheckFeatureFlagsLiquorOne();
+
+            CheckFeatureFlagsLGIN();
+
+            CheckFeatureFlagsIN();
+
+            CheckFeatureFlagsLicenseeChanges();
+
+            CheckFeatureFlagsSecurityScreening();
+
+            CheckFeatureLEConnections();
+
+            IgnoreSynchronizationFalse();
+
+            CarlaLogin(businessType);
+        }
+    }
+}
