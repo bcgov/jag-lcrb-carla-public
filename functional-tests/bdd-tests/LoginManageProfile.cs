caf43d008081c4c1413ed14e48f4c1e276247906
ElizabethWolfe <ejwolfe@gmail.com>
2021-04-26 12:40:13 -0700

    Automation.

--- functional-tests/bdd-tests/LoginManageProfile.cs
+++ functional-tests/bdd-tests/LoginManageProfile.cs
@@ -7,9 +7,9 @@ Feature: LoginManageProfile
     I want to manage my account after log in
 
 Scenario: Manage Account Profile
-    Given I log in with the new contact and account (cantest4employee)
-    And I click on my user account
-    And I click on the link for Manage Account    
+    Given I am logged in to the dashboard as a private corporation
+    And I click on the button for my user account
+    And I click on the link for Manage account    
     And I click on the Save and Continue button
     And the dashboard is displayed
     And the account is deleted
