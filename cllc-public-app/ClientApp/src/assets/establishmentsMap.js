f852f837fad7e75a98fdeb87b3cbf7f855884c62
George Walker <george@walkersoftware.ca>
2021-04-29 10:38:42 -0700

    Add documentation

--- cllc-public-app/ClientApp/src/assets/establishmentsMap.js
+++ cllc-public-app/ClientApp/src/assets/establishmentsMap.js
@@ -32,6 +32,13 @@ var searchMapOptions = {
   // WMS layers associated with the map
   wmsLayers: [
     {
+      rootUrl: "https://openmaps.gov.bc.ca/geo/pub/WHSE_IMAGERY_AND_BASE_MAPS.AIMG_ORTHOPHOTO_TILES_POINT/ows?",
+  format: "image/png",
+    layers: "pub:WHSE_IMAGERY_AND_BASE_MAPS.AIMG_ORTHOPHOTO_TILES_POINT",
+    styles: "Airborne_Imagery_Orthophoto_Tile_Points",
+  transparent: false
+},
+    {
       rootUrl: "https://openmaps.gov.bc.ca/geo/pub/WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW/ows?",
       format: "image/png",
       layers: "pub:WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW",
