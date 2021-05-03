4d1a490a74a88b0d5688ece6ef1646a82ff65934
Moffat Sehudi <msehudi@gmail.com>
2021-05-03 14:16:44 -0700

    Dynamics model update

--- cllc-interfaces/Dynamics-Autorest/PolicejurisdictionidExtensions.cs
+++ cllc-interfaces/Dynamics-Autorest/PolicejurisdictionidExtensions.cs
@@ -87,5 +87,74 @@ namespace Gov.Lclb.Cllb.Interfaces
                 return operations.GetWithHttpMessagesAsync(accountid, select, expand, customHeaders, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
             }
 
+            /// <summary>
+            /// Get adoxio_PoliceJurisdictionId from adoxio_specialevents
+            /// </summary>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='adoxioSpecialeventid'>
+            /// key: adoxio_specialeventid of adoxio_specialevent
+            /// </param>
+            /// <param name='select'>
+            /// Select properties to be returned
+            /// </param>
+            /// <param name='expand'>
+            /// Expand related entities
+            /// </param>
+            public static MicrosoftDynamicsCRMadoxioPolicejurisdiction Get1(this IPolicejurisdictionid operations, string adoxioSpecialeventid, IList<string> select = default(IList<string>), IList<string> expand = default(IList<string>))
+            {
+                return operations.Get1Async(adoxioSpecialeventid, select, expand).GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// Get adoxio_PoliceJurisdictionId from adoxio_specialevents
+            /// </summary>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='adoxioSpecialeventid'>
+            /// key: adoxio_specialeventid of adoxio_specialevent
+            /// </param>
+            /// <param name='select'>
+            /// Select properties to be returned
+            /// </param>
+            /// <param name='expand'>
+            /// Expand related entities
+            /// </param>
+            /// <param name='cancellationToken'>
+            /// The cancellation token.
+            /// </param>
+            public static async Task<MicrosoftDynamicsCRMadoxioPolicejurisdiction> Get1Async(this IPolicejurisdictionid operations, string adoxioSpecialeventid, IList<string> select = default(IList<string>), IList<string> expand = default(IList<string>), CancellationToken cancellationToken = default(CancellationToken))
+            {
+                using (var _result = await operations.Get1WithHttpMessagesAsync(adoxioSpecialeventid, select, expand, null, cancellationToken).ConfigureAwait(false))
+                {
+                    return _result.Body;
+                }
+            }
+
+            /// <summary>
+            /// Get adoxio_PoliceJurisdictionId from adoxio_specialevents
+            /// </summary>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='adoxioSpecialeventid'>
+            /// key: adoxio_specialeventid of adoxio_specialevent
+            /// </param>
+            /// <param name='select'>
+            /// Select properties to be returned
+            /// </param>
+            /// <param name='expand'>
+            /// Expand related entities
+            /// </param>
+            /// <param name='customHeaders'>
+            /// Headers that will be added to request.
+            /// </param>
+            public static HttpOperationResponse<MicrosoftDynamicsCRMadoxioPolicejurisdiction> Get1WithHttpMessages(this IPolicejurisdictionid operations, string adoxioSpecialeventid, IList<string> select = default(IList<string>), IList<string> expand = default(IList<string>), Dictionary<string, List<string>> customHeaders = null)
+            {
+                return operations.Get1WithHttpMessagesAsync(adoxioSpecialeventid, select, expand, customHeaders, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+
     }
 }
