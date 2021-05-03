9c94264d21ec25a39e2439ff63bdde56fbc3da04
Moffat Sehudi <msehudi@gmail.com>
2021-05-03 09:20:06 -0700

    SEP partial

--- cllc-public-app/ClientApp/src/app/components/sep/sep-application/liquor/liquor.component.ts
+++ cllc-public-app/ClientApp/src/app/components/sep/sep-application/liquor/liquor.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { SepApplication } from '@models/sep-application.model';
+import { IndexDBService } from '@services/index-db.service';
+import { SpecialEventsDataService } from '@services/special-events-data.service';
 
 @Component({
   selector: 'app-liquor',
@@ -11,7 +14,24 @@ export class LiquorComponent implements OnInit {
   @Output()
   saveComplete = new EventEmitter<boolean>();
 
-  constructor() { }
+  _appID: number;
+  sepApplication: SepApplication;
+
+  @Input()
+  set applicationId(value: number) {
+    this._appID = value;
+    //get the last saved application
+    this.db.getSepApplication(value)
+      .then(app => {
+        this.sepApplication = app;
+      });
+  };
+
+  get applicationId() {
+    return this._appID;
+  }
+
+  constructor(private db: IndexDBService, private sepDataService: SpecialEventsDataService) { }
 
   updateValue(value) {
     this.value = { ...this.value, ...value };
@@ -20,8 +40,44 @@ export class LiquorComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  saveToDB() {
+    const data = {
+      ...this.sepApplication,
+      lastUpdated: new Date(),
+      status: 'unsubmitted',
+      stepsCompleted: (steps => {
+        const step = 'event';
+        if (steps.indexOf(step) === -1) {
+          steps.push(step);
+        }
+        return steps;
+      })(this?.sepApplication?.stepsCompleted || []),
+      ...this.value,
+    } as SepApplication;
+
+    if (data.id) {
+      this.db.applications.update(data.id, data);
+      this.applicationId = data.id;
+    } else {
+      console.error("The id should already exist at this point.")
+    }
+  }
+
+  saveToAPI() {
+    this.db.getSepApplication(this.applicationId)
+      .then((appData) => {
+        debugger;
+        this.sepDataService.createSepApplication(appData)
+          .subscribe(result => {
+            debugger;
+          });
+      });
+  }
+
   save() {
     debugger;
+    this.saveToDB();
+    this.saveToAPI();
   }
 
 }
