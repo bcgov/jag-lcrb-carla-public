e127b12af0bef1d8afd8017c1aa70d847107427e
Alejandro Sanchez <emailforasr@gmail.com>
2021-05-03 16:05:07 -0700

    Fix styles

--- cllc-public-app/ClientApp/src/app/components/navbar/public-nav/public-nav.component.ts
+++ cllc-public-app/ClientApp/src/app/components/navbar/public-nav/public-nav.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-public-nav',
   templateUrl: './public-nav.component.html',
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./public-nav.component.scss']
 })
 export class PublicNavComponent implements OnInit {
   @Input() showMapLink = false;
