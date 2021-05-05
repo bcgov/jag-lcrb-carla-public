a6eaa762bf45e4e9b480fc0179dd3152d52bb0a1
Moffat Sehudi <msehudi@gmail.com>
2021-05-05 12:26:03 -0700

    SEP drink type view models

---
+++ cllc-public-app/Models.Extensions/SepDrinkTypes.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Gov.Lclb.Cllb.Interfaces.Models;
+
+namespace Gov.Lclb.Cllb.Public.Models
+{
+    /// <summary>
+    /// ViewModel transforms.
+    /// </summary>
+    public static class SepEventDatesExtensions
+    {
+        /// <summary>
+        /// Convert a given voteQuestion to a ViewModel
+        /// </summary>
+        public static ViewModels.SepEventDates ToViewModel(this MicrosoftDynamicsCRMadoxioSpecialeventschedule eventDates)
+        {
+            ViewModels.SepEventDates result = null;
+            if (eventDates != null)
+            {
+                result = new ViewModels.SepEventDates
+                {
+                    Id = eventDates.AdoxioSpecialeventscheduleid,
+                    SpecialEventId = eventDates._adoxioSpecialeventidValue,
+                    LocationId = eventDates._adoxioSpecialeventlocationidValue,
+                    EventStart = eventDates.AdoxioEventstart,
+                    EventEnd = eventDates.AdoxioEventend,
+                    ServiceStart = eventDates.AdoxioServicestart,
+                    ServiceEnd = eventDates.AdoxioServiceend,
+                };
+
+            }
+            return result;
+        }
+
+
+        public static void CopyValues(this MicrosoftDynamicsCRMadoxioSpecialeventschedule to, ViewModels.SepEventDates from)
+        {
+            // to.AdoxioSpecialeventscheduleid = from.Id;
+            to._adoxioSpecialeventidValue = from.SpecialEventId;
+            to._adoxioSpecialeventlocationidValue = from.LocationId;
+            to.AdoxioEventstart = from.EventStart;
+            to.AdoxioEventend = from.EventEnd;
+            to.AdoxioServicestart = from.ServiceStart;
+            to.AdoxioServiceend = from.ServiceEnd;
+        }
+
+
+    }
+}
+
---
+++ cllc-public-app/ViewModels/SepDrinkType.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Gov.Lclb.Cllb.Public.ViewModels
+{
+    public enum DrinkTypeGroup
+    {
+        Beer = 845280000,
+        Spirit = 845280002,
+        Wine = 845280001,
+
+    }
+
+    public enum ServingMethod
+    {
+        bottlesCansGlasses = 845280000,
+        glasses = 845280001,
+        shots = 845280002,
+    }
+
+    public enum StorageMethod
+    {
+        kegs = 845280000,
+        bottles = 845280001
+    }
+
+    public class SepDrinkType
+    {
+        public string Id { get; set; }
+        public string UnitSize { get; set; }
+        public string BulkSize { get; set; }
+        public int? BulkMultiplier { get; set; }
+        public decimal? CostPerServing { get; set; }
+        public bool? IsHomeMade { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public DrinkTypeGroup? Group { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public StorageMethod? StorageMethod { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ServingMethod? ServingMethod { get; set; }
+        public decimal? ServingSizeMl { get; set; }
+        public decimal? StoraheSizeMl { get; set; }
+    }
+}
