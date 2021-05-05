26d9ee1fe3897f029659dc5a663453eff576a0ab
Moffat Sehudi <msehudi@gmail.com>
2021-05-05 12:30:30 -0700

    Save to dynamics for SEP (create entitiesl)

--- cllc-interfaces/Dynamics-Autorest/ModelExtensions/MicrosoftDynamicsCRMadoxioSpecialevent.cs
+++ cllc-interfaces/Dynamics-Autorest/ModelExtensions/MicrosoftDynamicsCRMadoxioSpecialevent.cs
@@ -8,16 +8,6 @@ namespace Gov.Lclb.Cllb.Interfaces.Models
     public partial class MicrosoftDynamicsCRMadoxioSpecialevent
     {
 
-        /// <summary>
-        /// </summary>
-        [JsonProperty(PropertyName = "adoxio_specialevent_schedule@odata.bind")]
-        public string SpecialEventScheduleODataBind { get; set; }
-
-        /// <summary>
-        /// </summary>
-        [JsonProperty(PropertyName = "adoxio_specialevent_licencedarea@odata.bind")]
-        public string SpecialEventLicencedAreaODataBind { get; set; }
-        
 
 
     }
