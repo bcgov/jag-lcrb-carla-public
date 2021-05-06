560ff734bf9df9bc20dcf5cbb0e1f505906ac5b2
Alejandro Sanchez <emailforasr@gmail.com>
2021-05-05 17:12:14 -0700

    Initial commit for Police Approval Settings page

---
+++ cllc-public-app/ClientApp/src/app/components/police-representative/approval-settings/approval-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-approval-settings',
+  templateUrl: './approval-settings.component.html',
+  styleUrls: ['./approval-settings.component.scss']
+})
+export class ApprovalSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
