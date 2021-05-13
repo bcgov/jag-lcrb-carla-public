291e88ff4fc240d77ab5c11489319cb286604cc4
Alejandro Sanchez <emailforasr@gmail.com>
2021-05-13 16:55:09 -0700

    LCSD-5609 adding table to page

--- cllc-public-app/ClientApp/src/app/components/police-representative/all-applications/all-applications.component.ts
+++ cllc-public-app/ClientApp/src/app/components/police-representative/all-applications/all-applications.component.ts
@@ -1,32 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AppState } from '@app/app-state/models/app-state';
 import { Store } from '@ngrx/store';
-import { AccountDataService } from '@services/account-data.service';
-import { FormBase } from '@shared/form-base';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { AppState } from '@app/app-state/models/app-state';
+import { SepApplicationSummary } from '@models/sep-application-summary.model';
+import { User } from '@models/user.model';
+import { SpecialEventsDataService } from '@services/special-events-data.service';
+
+// Show text labels instead of numeric enum values in the table; e.g. Status = "In Progress" vs. 100,000,001
+interface TableElement extends SepApplicationSummary {
+  eventStatusLabel?: string;
+  policeDecisionByLabel?: string;
+  typeOfEventLabel?: string;
+}
 
 @Component({
   selector: 'app-sep-all-applications',
   templateUrl: './all-applications.component.html',
   styleUrls: ['./all-applications.component.scss']
 })
-export class AllApplicationsComponent extends FormBase implements OnInit {
+export class AllApplicationsComponent implements OnInit {
+  // icons
 
-  // form
-  form = this.fb.group({
-  });
+  // angular material table columns to display
+  columnsToDisplay = ['dateSubmitted', 'eventName', 'eventStartDate', 'eventStatusLabel', 'policeDecisionByLabel', 'maximumNumberOfGuests', 'typeOfEventLabel', 'actions'];
+
+  // component state
+  currentUser: User;
+  dataSource$: Observable<TableElement[]>;
 
   constructor(
     private store: Store<AppState>,
-    private accountDataService: AccountDataService,
+    private sepDataService: SpecialEventsDataService,
     private fb: FormBuilder,
-    private router: Router,
+    private router: Router
   ) {
-    super();
   }
 
   ngOnInit() {
+    this.subscribeForData();
   }
 
+  private subscribeForData() {
+    // fetch SEP applications waiting for Police Approval
+    this.dataSource$ = this.loadSepApplications();
+
+    this.store.select(state => state.currentUserState.currentUser)
+      .pipe(filter(s => !!s))
+      .subscribe((user: User) => {
+        this.currentUser = user;
+      });
+  }
+
+  private loadSepApplications() {
+    return this.sepDataService.getPoliceApprovalSepApplications()
+      .pipe(map(array => array.map(sepData => {
+        // TODO: Add text labels for numeric status values here
+        return {
+          ...sepData,
+          eventStatusLabel: 'In Progress',
+          typeOfEventLabel: 'Members',
+          policeDecisionByLabel: 'Vancouver PoliceUser',
+        } as TableElement;
+      })));
+  }
 }
