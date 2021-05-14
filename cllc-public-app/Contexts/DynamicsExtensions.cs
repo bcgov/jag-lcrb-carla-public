28e4525482d3a5f418189394ddf725549d3fb6b3
George Walker <george@walkersoftware.ca>
2021-05-14 12:40:29 -0700

    LCSD-5710 - service for account contacts

--- cllc-public-app/Contexts/DynamicsExtensions.cs
+++ cllc-public-app/Contexts/DynamicsExtensions.cs
@@ -1044,6 +1044,33 @@ namespace Gov.Lclb.Cllb.Interfaces
         }
 
         /// <summary>
+        /// Get a list of active contacts by their account ID
+        /// </summary>
+        /// <param name="system"></param>
+        /// <param name="siteminderId"></param>
+        /// <returns></returns>
+        public static List<MicrosoftDynamicsCRMcontact> GetActiveContactsByAccountId(this IDynamicsClient system, string accountId)
+        {
+            string sanitizedSiteminderId = GuidUtility.SanitizeGuidString(accountId);
+            List<MicrosoftDynamicsCRMcontact> result = null;
+            try
+            {
+                var contactsResponse = system.Contacts.Get(filter: "statecode eq 0 and _accountid_value eq " + sanitizedSiteminderId);
+                result = contactsResponse.Value.ToList();
+            }
+            catch (HttpOperationException)
+            {
+                result = null;
+            }
+            catch (Exception)
+            {
+                result = null;
+            }
+
+            return result;
+        }
+
+        /// <summary>
         /// Get a contact by their First Initial, Last Name and Birthdate
         /// </summary>
         /// <param name="system"></param>
