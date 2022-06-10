7f2b59df4441c7c56971bbd9a904d39f9012b682
KyleKayfish <39177955+KyleKayfish@users.noreply.github.com>
2022-06-10 13:01:06 -0700

    LCSD-6392 - Added Production Retail Store button for starting application (#3680)
    Merging as per the request from Kyle.

--- cllc-public-app/ClientApp/src/app/models/application-type.model.ts
+++ cllc-public-app/ClientApp/src/app/models/application-type.model.ts
@@ -81,6 +81,7 @@ export enum ApplicationTypeNames {
   CRSStructuralChange = "CRS Structural Change",
   CRSTransferofOwnership = "CRS Transfer of Ownership",
   CannabisRetailStore = "Cannabis Retail Store",
+  ProductionRetailStore = "Production Retail Store",
   ETHYL = "Ethyl Alcohol Permit",
   FP = "Food Primary",
   FPRelo = "Food Primary Relocation",
