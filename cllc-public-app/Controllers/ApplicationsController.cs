e94ec86dbbdd417a5df470144dd38c6a3e912a46
Zhouhe <Zhouhe@sheridancollege.ca>
2023-03-02 17:21:56 -0500

    Implement: when create an application fetch isFree.endorsement applicationType.

--- cllc-public-app/Controllers/ApplicationsController.cs
+++ cllc-public-app/Controllers/ApplicationsController.cs
@@ -1368,12 +1368,18 @@ namespace Gov.Lclb.Cllb.Public.Controllers
             if (count >= 8 && item.ApplicationType.Name == "Cannabis Retail Store")
                 return BadRequest("8 applications have already been submitted. Can not create more");
             var adoxioApplication = new MicrosoftDynamicsCRMadoxioApplication();
-            //LCSD-6495 set applicationType is free of if this applicationType IsEndorsement and Non licence assigned to this application.
-            if (item.ApplicationType.IsEndorsement.HasValue && item.ApplicationType.IsEndorsement.Value && item.AssignedLicence == null) {
-                item.ApplicationType.IsFree = true;
-            }
+
             // copy received values to Dynamics Application
             adoxioApplication.CopyValues(item);
+
+            //LCSD-6495 set applicationType is free of if this applicationType IsEndorsement and Non licence assigned to this application.    
+            if (item.ApplicationType.IsEndorsement.HasValue && item.ApplicationType.IsEndorsement.Value && item.AssignedLicence == null
+                && !(item.ApplicationType.IsFree.HasValue && item.ApplicationType.IsFree.Value) ) 
+            {
+                string itemApplicationName = item.ApplicationType.Name + " - Free";
+                adoxioApplication.AdoxioApplicationTypeId =_dynamicsClient.GetApplicationTypeByName(itemApplicationName);
+            }   
+
             adoxioApplication.AdoxioApplicanttype = (int?)item.ApplicantType;
             
             // fix for an invalid licence sub category
