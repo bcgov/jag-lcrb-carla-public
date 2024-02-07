af7315d957b87ac5fd5f4c1539dc24cbebcd95b6
KKAYFISH <kyle.kayfish@gmail.com>
2024-02-07 10:36:02 -0800

    LCSD-6488:   Updated to fetch firstname and lastname via bceid webquery instead of siteminder header when logging in or creating account or registering new user.

---
+++ cllc-interfaces/BCeID/BCeIDBasic.cs
@@ -0,0 +1,29 @@
+namespace Gov.Lclb.Cllb.Interfaces
+{
+    public class BCeIDBasic
+    {
+        public string contactEmail { get; set; }
+        public string contactPhone { get; set; }
+
+        public string individualFirstname { get; set; }
+        public string individualMiddlename { get; set; }
+        public string individualOtherMiddlename { get; set; }
+        public string individualSurname { get; set; }
+
+        //Should not applu for Basic, the address referred is business address.
+        /*public string businessTypeName { get; set; }
+        public string businessTypeDescription { get; set; }
+        public string businessTypeCode { get; set; }
+        public string businessTypeOther { get; set; }
+        public string legalName { get; set; }
+        public string businessNumber { get; set; }
+        public string incorporationNumber { get; set; }
+        public string jurisdictionOfIncorporation { get; set; }
+        public string addressLine1 { get; set; }
+        public string addressLine2 { get; set; }
+        public string addressCity { get; set; }
+        public string addressProv { get; set; }
+        public string addressPostal { get; set; }
+        public string addressCountry { get; set; }*/
+    }
+}
