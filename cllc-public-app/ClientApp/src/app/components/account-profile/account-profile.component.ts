2d4dd362f70a0c965f3c609d64875791bbdd40a0
Sam Warren <samwarren4@gmail.com>
2024-06-13 13:44:03 -0700

    Patch: Hide account URL field

--- cllc-public-app/ClientApp/src/app/components/account-profile/account-profile.component.ts
+++ cllc-public-app/ClientApp/src/app/components/account-profile/account-profile.component.ts
@@ -47,7 +47,7 @@ const ValidationFieldNameMap = {
   'businessProfile.businessNumber': "Business Number",
   'businessProfile.businessType': "Business Type",
   'businessProfile.contactPhone': "Corporation Address Business Phone",
-  'businessProfile.accountUrls': "Account URL(s)",
+  // 'businessProfile.accountUrls': "Account URL(s)", TODO: Re-implement this
   // 'businessProfile.contactEmail': 'Corporation Address Business Email',
 
   'businessProfile.physicalAddressStreet': "Physical Address Street",
@@ -166,7 +166,7 @@ export class AccountProfileComponent extends FormBase implements OnInit {
         mailingAddressProvince: ["British Columbia", Validators.required],
         mailingAddressCountry: ["Canada", Validators.required],
         websiteUrl: [""],
-        accountUrls: ["", Validators.pattern("^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[^\s,]*)?(,(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[^\s,]*)?)*$")], 
+        // accountUrls: ["", Validators.pattern("^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[^\s,]*)?(,(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[^\s,]*)?)*$")], TODO: Re-implement this
       }),
       contact: this.fb.group({
         id: [],
