ee9f787532ef62081e9ab14e41e88da145c1c2de
W. Ross White <ross.white@quartech.com>
2024-09-03 19:30:12 -0700

    moved modal button location

--- cllc-public-app/ClientApp/src/app/components/user-menu/user-menu.component.ts
+++ cllc-public-app/ClientApp/src/app/components/user-menu/user-menu.component.ts
@@ -1,9 +1,7 @@
-import { Component, Input, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { User } from '@models/user.model';
 import { faUserCircle, faBuilding } from "@fortawesome/free-regular-svg-icons";
 import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import { MatDialog } from "@angular/material/dialog";
-import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-user-menu',
@@ -12,35 +10,6 @@ import { Clipboard } from '@angular/cdk/clipboard';
   encapsulation: ViewEncapsulation.None
 })
 export class UserMenuComponent implements OnInit {
-
-  @ViewChild('badgeTemplateDialog') badgeTemplateDialog: TemplateRef<any>;
-
-  nonsenseCode = `
-      var baz = bar * 42;
-      if (baz > 100) {
-        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-      } else {
-        for (var i = 0; i < baz; i++) {
-          console.log("Nunc vehicula dapibus nulla, quis tincidunt ligula.");
-        }
-        return "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-      }
-    }
-    
-    class Quux {
-      constructor() {
-        this.quuz = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-      }
-    
-      corge() {
-        return this.quuz.split(" ").reverse().join(" ");
-      }
-    }
-    
-    var grault = new Quux();
-    console.log(grault.corge());
-    `;
-
   // icons
   faUserCircle = faUserCircle;
   faBuilding = faBuilding;
@@ -56,25 +25,9 @@ export class UserMenuComponent implements OnInit {
     return this.faUserCircle;
   }
 
-  constructor(public dialog: MatDialog, private clipboard: Clipboard) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  openBadgeTemplateDialog() {
-    this.dialog.open(this.badgeTemplateDialog, {
-      disableClose: true,
-      autoFocus: true,
-      width: "500px",
-      height: "400px",
-      maxWidth: "80vw",
-      panelClass: 'custom-dialog-container'
-    });
-  }
-
-  onCopy(): void {
-    this.clipboard.copy(this.nonsenseCode);
-    console.log('Code copied to clipboard');
-    this.dialog.closeAll();
-  }
 }
