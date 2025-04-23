31d4ac331f325a39d2dc4537242aca2cd3841754
Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>
2025-04-23 13:49:14 -0700

    Merge to Master: LCSD-7057, 7786 (#4394)
    * LCSD-7734: Update CRS checkbox text (#4374)
    
    update crs checkbox text
    
    * LCSD-7234: Remove Permanent Change to Applicant Functionality (#4373)
    
    * removed permanent changes to an applicant functionality
    
    * Revert "removed permanent changes to an applicant functionality"
    
    This reverts commit ccd595745abf391af6f7e485b3fcebbff8500b5d.
    
    * removed permanent changes to an applicant functionality
    
    * Lcsd 5881 Licensing - Add discretion app info and toggle on permanent hours change applications (#4376)
    
    * LCSD-5881 Licensing - Add discretion app info and toggle on permanent hours change applications
    
    * Typo removed
    
    ---------
    
    Co-authored-by: Fintan Roche <Fintan.Roche@quartech.com>
    
    * LCSD-7757 disable update SEP application from portal once application is submitted (#4377)
    
    LCSD-7757 disable updating application from portal once application is submitted.
    
    * LCSD-7757  (#4378)
    
    LCSD-7757 disable updating application from portal once application is submitted.
    
    * LCSD-7788: Update BC Services Card Setup Link (#4380)
    
    Update BC Services Card Setup Link
    
    * LCSD-6488   Licensing - Primary Contact Error (#4381)
    
    Clear SMSESSION Cache on logout
    
    Co-authored-by: Fintan Roche <Fintan.Roche@quartech.com>
    
    * Lcsd 5881 Licensing - Add discretion app info and toggle on permanent hours change applications (#4382)
    
    Outside hours Toggle button added to Lounge and Sea
    
    Co-authored-by: Fintan Roche <Fintan.Roche@quartech.com>
    
    * fix broken sep login links
    
    * Commenting out the changes for session
    
    * Clear SMSESSION cache remerge (#4385)
    
    Co-authored-by: Fintan Roche <Fintan.Roche@quartech.com>
    
    * Lcsd 7182 temp logger (#4386)
    
    * Clear SMSESSION cache remerge
    
    * Temporary logger to investigate issue
    
    ---------
    
    Co-authored-by: Fintan Roche <Fintan.Roche@quartech.com>
    
    * LCSD-7057: Added Valid Interest to RLRS Licence Type (#4388)
    
    added valid interest to RLRS
    
    * LCSD-7786: Create Policy for BC Service Card Users (#4390)
    
    * added new service card policy
    
    * fix typo
    
    * create combined policy
    
    * Lcsd 6488 primary contact error smsession cookies cleared (#4391)
    
    SMSession cookies cleared
    
    Co-authored-by: Fintan Roche <Fintan.Roche@quartech.com>
    
    * LCSD-7786: Revert New Policy and Remove it From Feedback Controller (#4393)
    
    * added new service card policy
    
    * fix typo
    
    * create combined policy
    
    * revert new policy and remove it from feedback
    
    * Update Startup.cs
    
    Code did not get removed in PR #4393
    
    * Revert "Lcsd 7182 temp logger" (#4395)
    
    Revert "Lcsd 7182 temp logger (#4386)"
    
    This reverts commit adbc004c2f060201e0eb323c18493413c8c30b0b.
    
    * Revert "Lcsd 6488 primary contact error smsession cookies cleared" (#4396)
    
    Revert "Lcsd 6488 primary contact error smsession cookies cleared (#4391)"
    
    This reverts commit 84aa6a6805931ea6dc9ae01a0501395f2a5432c2.
    
    * Update LogoutController.cs
    
    ---------
    
    Co-authored-by: FintanRoche <61281610+FintanRoche@users.noreply.github.com>
    Co-authored-by: Fintan Roche <Fintan.Roche@quartech.com>
    Co-authored-by: Simranjeet Singh <simranjeet.1.singh@gov.bc.ca>

--- cllc-public-app/Controllers/FeedbackController.cs
+++ cllc-public-app/Controllers/FeedbackController.cs
@@ -25,7 +25,7 @@ namespace Gov.Lclb.Cllb.Public.Controllers
 
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Policy = "Business-User")]
+    
     public class FeedbackController : ControllerBase
     {
 
