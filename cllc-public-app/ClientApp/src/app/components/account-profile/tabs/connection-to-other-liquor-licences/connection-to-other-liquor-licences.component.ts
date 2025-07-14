af6166c9cc8cc1b51037d4526f9394dc5bb88250
Nick Phura <nickphura@gmail.com>
2025-07-14 13:46:11 -0700

    Update styling. Update/fix various bits of text. Add new component for account profile checkboxes.

---
+++ cllc-public-app/ClientApp/src/app/components/account-profile/tabs/connection-to-other-liquor-licences/connection-to-other-liquor-licences.component.ts
@@ -0,0 +1,85 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Account } from '@models/account.model';
+import { TiedHouseConnection } from '@models/tied-house-connection.model';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-connection-to-other-liquor-licences',
+  templateUrl: './connection-to-other-liquor-licences.component.html',
+  styleUrls: ['./connection-to-other-liquor-licences.component.scss']
+})
+export class ConnectionToOtherLiquorLicencesComponent implements OnInit, OnDestroy {
+  @Input() account: Account;
+  @Input() isMarketer: boolean;
+  @Input() licensedProducerText = 'federally licensed producer';
+  @Input() federalProducerText = 'federal producer';
+  @Input() applicationTypeName: String;
+
+  @Input('tiedHouse')
+  set tiedHouse(value: TiedHouseConnection) {
+    if (value && this.form) {
+      this.form.patchValue(value);
+    }
+
+    this._tiedHouseData = value;
+  }
+
+  get tiedHouse(): TiedHouseConnection {
+    return { ...this._tiedHouseData };
+  }
+
+  @Output() value = new EventEmitter<TiedHouseConnection>();
+
+  subscriptions: Subscription[] = [];
+
+  form: FormGroup | undefined;
+
+  savedFormData: any = {};
+
+  _tiedHouseData: TiedHouseConnection;
+
+  /**
+   * Indicates whether the tied house declaration section should be shown.
+   *
+   * Show the section if any of the checkboxes are selected.
+   *
+   * @readonly
+   * @type {boolean}
+   */
+  get showTiedHouseDeclarationSection(): boolean {
+    if (!this.form) {
+      return false;
+    }
+
+    return (
+      this.form.get('hasOwnershipOrControl').value === '1' ||
+      this.form.get('hasThirdPartyAssociations').value === '1' ||
+      this.form.get('hasImmediateFamilyMemberInvolvement').value === '1'
+    );
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      hasOwnershipOrControl: [''],
+      hasThirdPartyAssociations: [''],
+      hasImmediateFamilyMemberInvolvement: ['']
+    });
+
+    if (this.tiedHouse) {
+      this.form.patchValue(this.tiedHouse);
+    }
+
+    this.form.valueChanges.subscribe((value) => this.value.emit(Object.assign(this.tiedHouse, value)));
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+}
