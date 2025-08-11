a1e070e00e77aab82ddc33b98ae84a9342197d7f
Fintan Roche <Fintan.Roche@quartech.com>
2025-08-11 15:35:16 -0700

    LCSD-5894 LPC Transition to LP application field for current approved capacity

--- cllc-public-app/ClientApp/src/app/models/application-type.model.ts
+++ cllc-public-app/ClientApp/src/app/models/application-type.model.ts
@@ -125,7 +125,8 @@ export enum ApplicationTypeNames {
   ChangetoApprovedPicnicArea = "Change to Approved Picnic Area",
   ChangeHoursOfService_Outside_Service_Hours = "Change to Hours of Liquor Service (outside Service Hours)",
   ChangeHoursOfService_Lounge_Area = "Change to Hours of Liquor Service (LOUNGE - Outside Service Hours)",
-  ChangeHoursOfService_Special_Event_Area = "Change to Hours of Liquor Service (SEA - Within Service Hours)"
+  ChangeHoursOfService_Special_Event_Area = "Change to Hours of Liquor Service (SEA - Within Service Hours)",
+  ApplicationToTransitionToLiquorPrimaryLicence ="Application to Transition to Liquor Primary Licence"
 
 }
 
