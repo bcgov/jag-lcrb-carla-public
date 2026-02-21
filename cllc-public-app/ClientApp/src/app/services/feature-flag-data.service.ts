ff5e6fa4bd432c1e11d141c7ed7032291b7e9464
Nick Phura <nickphura@gmail.com>
2026-02-20 16:56:18 -0800

    LCSD-8368: Add Feature Flag To Disable Licence Renewal Based on Date (#4576)
    * Feature Flag enhancements/improvements.
    
    * Wrap licence renewal buttons in feature flag, to optionally disable as part of FIP launch.

--- cllc-public-app/ClientApp/src/app/services/feature-flag-data.service.ts
+++ cllc-public-app/ClientApp/src/app/services/feature-flag-data.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from "@angular/core";
-import { DataService } from "./data.service";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FeatureFlag } from '@models/feature-flag.model';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { DataService } from './data.service';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class FeatureFlagDataService extends DataService {
-  apiPath = "api/features";
+  apiPath = 'api/features';
 
   constructor(private http: HttpClient) {
     super();
   }
 
-  getFeatureFlags(): Observable<string[]> {
-    return this.http.get<string[]>(this.apiPath, { headers: this.headers })
-      .pipe(catchError(this.handleError));
+  getFeatureFlags(): Observable<FeatureFlag[]> {
+    return this.http.get<FeatureFlag[]>(this.apiPath, { headers: this.headers }).pipe(catchError(this.handleError));
   }
 }
