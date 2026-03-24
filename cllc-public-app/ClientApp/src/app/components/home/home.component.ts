c1649dc8f7675f171b85e10be7c2829c3c24c66c
Dhaliwal <Vijaivir.Dhaliwal@gov.bc.ca>
2026-03-24 15:56:47 -0700

    Revert "Merge to Master: LCSD-8413, 8401, 8402, 8365 (#4589)"
    This reverts commit 957f919f6b532dc6713983f88d2b152185748c6a.
    
    Remove SPD changes that are not ready

--- cllc-public-app/ClientApp/src/app/components/home/home.component.ts
+++ cllc-public-app/ClientApp/src/app/components/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { Subscription } from "rxjs";
 import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons"
-import { NoticeBoxService, NoticeBox } from "@services/notice-box.service";
 
 @Component({
   selector: "app-home",
@@ -13,16 +12,10 @@ export class HomeComponent implements OnInit {
   window = window;
   busy: Subscription;
   faExclamationCircle = faExclamationCircle;
-  public noticeBox: NoticeBox;
-  public showNoticeBox: boolean = false;
 
-  constructor(private titleService: Title, private noticeBoxService: NoticeBoxService) {}
+  constructor(private titleService: Title) {}
 
   ngOnInit() {
     this.titleService.setTitle("Home - Liquor and Cannabis Regulation Branch");
-    this.noticeBoxService.getNoticeBox().subscribe((noticeBox: NoticeBox) => {
-      this.noticeBox = noticeBox;
-      this.showNoticeBox = noticeBox.noticeBoxEnabled && noticeBox.noticeBoxText?.length > 0;
-    });
   }
 }
